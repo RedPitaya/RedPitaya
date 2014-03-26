f8fdb5db06b7e61e8673ffb140b3a7cd8e0b2491
Ales Bardorfer <ales.bardorfer@redpitaya.com>
2014-03-26 11:31:02 +0100

    Replaced obsolete non-generator parameter prepare_wave with the generator's gen_awg_refresh.

--- Applications/scope/src/main.h
+++ Applications/scope/src/main.h
@@ -45,7 +45,7 @@ typedef struct rp_osc_meas_res_s {
 
 /* Parameters indexes - these defines should be in the same order as 
  * rp_app_params_t structure defined in main.c */
-#define PARAMS_NUM        52
+#define PARAMS_NUM        51
 #define MIN_GUI_PARAM     0
 #define MAX_GUI_PARAM     1
 #define TRIG_MODE_PARAM   2
@@ -78,31 +78,30 @@ typedef struct rp_osc_meas_res_s {
 #define PRB_ATT_CH2       29
 #define GAIN_CH2          30
 #define GUI_RST_Y_RANGE   31
-#define PREPARE_WAVE      32
-#define GEN_DC_OFFS_1     33
-#define GEN_DC_OFFS_2     34
-#define GUI_XMIN          35
-#define GUI_XMAX          36
+#define GEN_DC_OFFS_1     32
+#define GEN_DC_OFFS_2     33
+#define GUI_XMIN          34
+#define GUI_XMAX          35
 /* AWG parameters */
-#define GEN_TRIG_MODE_CH1 37
-#define GEN_SIG_TYPE_CH1  38
-#define GEN_ENABLE_CH1    39
-#define GEN_SINGLE_CH1    40
-#define GEN_SIG_AMP_CH1   41
-#define GEN_SIG_FREQ_CH1  42
-#define GEN_SIG_DCOFF_CH1 43
-#define GEN_TRIG_MODE_CH2 44
-#define GEN_SIG_TYPE_CH2  45
-#define GEN_ENABLE_CH2    46
-#define GEN_SINGLE_CH2    47
-#define GEN_SIG_AMP_CH2   48
-#define GEN_SIG_FREQ_CH2  49
-#define GEN_SIG_DCOFF_CH2 50
-#define GEN_AWG_REFRESH   51
+#define GEN_TRIG_MODE_CH1 36
+#define GEN_SIG_TYPE_CH1  37
+#define GEN_ENABLE_CH1    38
+#define GEN_SINGLE_CH1    39
+#define GEN_SIG_AMP_CH1   40
+#define GEN_SIG_FREQ_CH1  41
+#define GEN_SIG_DCOFF_CH1 42
+#define GEN_TRIG_MODE_CH2 43
+#define GEN_SIG_TYPE_CH2  44
+#define GEN_ENABLE_CH2    45
+#define GEN_SINGLE_CH2    46
+#define GEN_SIG_AMP_CH2   47
+#define GEN_SIG_FREQ_CH2  48
+#define GEN_SIG_DCOFF_CH2 49
+#define GEN_AWG_REFRESH   50
 
 /* Defines from which parameters on are AWG parameters (used in set_param() to
  * trigger update only on needed part - either Oscilloscope or AWG */
-#define PARAMS_AWG_PARAMS 37
+#define PARAMS_AWG_PARAMS 36
 
 /* Output signals */
 #define SIGNAL_LENGTH (1024) /* Must be 2^n! */
