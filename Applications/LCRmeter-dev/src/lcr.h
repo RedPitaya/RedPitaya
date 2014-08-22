0dd46334279abd534a97d8e062c67816e65dfef4
1nfused <enigma4.speed@gmail.com>
2014-08-23 01:55:52 +0200

    Testing the implementation of lcr function.

--- Applications/LCRmeter-dev/src/lcr.h
+++ Applications/LCRmeter-dev/src/lcr.h
@@ -4,7 +4,8 @@
 
 #include <stdint.h>
 #include <complex.h>  
-/*TODO: Write a detailed description of each function. */
+
+
 
 #define M_PI 3.14159265358979323846
 
@@ -12,6 +13,7 @@
 
 /** Signal types */
 typedef enum {
+	/* Always sine */
     eSignalSine_lcr,         ///< Sinusoidal waveform.
     /*eSignalSquare,       ///< Square waveform.
     eSignalTriangle,     ///< Triangular waveform.
@@ -25,7 +27,10 @@ typedef struct {
     uint32_t step;       ///< AWG step interval.
 } awg_param_t_lcr;
 
+
 /* Forward declarations */
+
+/* Functions for signal generation */
 void synthesize_signal_lcr(double ampl, double freq, signal_e_lcr type, double endfreq,
                        int32_t *data,
                        awg_param_t_lcr *params);
@@ -33,9 +38,12 @@ void write_data_fpga_lcr(uint32_t ch,
                      const int32_t *data,
                      const awg_param_t_lcr *awg);
 
+/* Data acquisition function */
 int acquire_data(
                 float **s , 
                 uint32_t size);
+
+
 
 int LCR_data_analasys(float **s ,
                         uint32_t size,
@@ -45,14 +53,16 @@ int LCR_data_analasys(float **s ,
                         double w_out,
                         int f);
 
+/* LCR main function */
+
 int lcr(uint32_t ch, double ampl, uint32_t DC_bias, float R_shunt, uint32_t averaging_num, int calib_function,  uint32_t Z_load_ref_real, uint32_t Z_load_ref_imag, 
         double steps, int sweep_function, double start_frequency, double end_frequency,  int scale_type, int wait_on_user, float *frequency_lcr, float *phase_lcr, float *amplitude_lcr);
 
 
 
-/* New section */
+/* Helper functions used in lcr main method */
 
-int get_gain(int *gain, const char *str);
+
 
 float *create_table();
 
@@ -62,16 +72,27 @@ float **create_2D_table_size(int num_of_rows, int num_of_cols);
 
 float max_array(float *arrayptr, int numofelements);
 
-float trapz(float *arrayptr, float T, int size1);
-
 float mean_array(float *arrayptr, int numofelements);
 
 float mean_array_column(float **arrayptr, int length, int column);
+
+
+/*New section*/
+
+
+float trapz(float *arrayptr, float T, int size1);
+
+int get_gain(int *gain, const char *str);
+
+
+
 
 /*New section*/
 
 void usage();
 
 int inquire_user_wait();
+
+void run_lcr();
 
 #endif
