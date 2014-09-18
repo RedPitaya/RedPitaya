8903f117f37c9c1e89e3f114f9a0ecf0bcc4f17a
1nfused <enigma4.speed@gmail.com>
2014-09-18 06:13:48 -0700

    Default value of start_measure set to -1.

--- Applications/LCRmeter-dev/src/main.c
+++ Applications/LCRmeter-dev/src/main.c
@@ -143,16 +143,15 @@ static rp_app_params_t rp_main_params[PARAMS_NUM+1] = {
     { /* gen_DC_offs_2 - DC offset for channel 2 expressed in [V] requested by 
        * GUI */
         "gen_DC_offs_2", 0, 1, 0, -100, 100 },
-
-    { /* flag_button - General flag used for sending params from the RedPitaya browser. 
+    { /* flag_button - General flag used for sending params from the RedPitaya browser.
+       *   -1 - Application not loaded yet
        *    0 - negative
        *    1 - positive 
        *    2 - Other ( Change the max value for more states )
        *  - Setting max value to 2 for general purposes. Can be changed accordingly. 
        *  - Read only value set to 0, as the flag_button value can be changed from Javascript 
        *    code in index.html as well as from the C controller code. */
-        "flag_button", 0, 1, 0, 0, 2 },
-
+        "start_measure", -1, 1, 0, -1, 3 },
     { /* LCR amplitude. User defined.
        *    Min value - 0
        *    Max value - 2 */
