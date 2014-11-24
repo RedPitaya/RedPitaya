f5deccf29fc0f0fc8093a70f6bf63b6acc9ff07d
1nfused <enigma4.speed@gmail.com>
2014-11-24 10:11:39 -0800

    Added temperature measurment.

--- Test/led_control/led_control.c
+++ Test/led_control/led_control.c
@@ -14,81 +14,120 @@
 #include "led_control.h"
 
 
-/* Init of the led structure */
+/* Init of the control structure */
 led_struct_t *led_struct = NULL;
+temp_struct_t *temp_struct = NULL;
 
 /** The memory file descriptor used to mmap() the FPGA space */
-int led_control_fd = -1;
+int control_fd = -1;
+
+/* Program name */
+const char *t_argv0 = "LCR control application.";
 
 
-int led_control_cleanup(void){
+int control_cleanup(void){
 	//We only need to clean-up, if led_struct_t isn't null
 	if(led_struct){
-		if(munmap(led_struct, LED_BASE_ADDR) < 0){
+		if(munmap(led_struct, LED_BASE_SIZE) < 0){
 			fprintf(stderr, "munmap failed %s\n", strerror(errno));
 			return -1;
 		}
 		led_struct = NULL;
 	}
+	if(temp_struct){
+		if(munmap(temp_struct, TEMP_BASE_SIZE) < 0){
+			fprintf(stderr, "munmap failed %s\n", strerror(errno));
+			return -1;
+		}
+		temp_struct = NULL;
+	}
+		
+	
 	return 0;
 }
 
-int led_control_init(void){
+int control_init(int option){
 
-	/* Page variables used to calculate correct mapping addresses */
+	/* Page variables used to calculate correct mapping addresses for temperature control */
     void *page_ptr;
     long page_addr, page_off, page_size = sysconf(_SC_PAGESIZE);
 
-    if(led_control_cleanup() < 0){
-    	return -1;
-    }
-
     /* Open /dev/mem to access directly system memory */
-    led_control_fd = open("/dev/mem", O_RDWR | O_SYNC);
+    control_fd = open("/dev/mem", O_RDWR | O_SYNC);
     /* Error check */
-    if(led_control_fd < 0) {
+    if(control_fd < 0) {
         fprintf(stderr, " Error opening (/dev/mem): %s\n", strerror(errno));
         return -1;
     }
 
-    /* Offset */
-    page_addr = LED_BASE_ADDR & (~(page_size-1)); // AB
-    
-    page_off  = LED_BASE_ADDR - page_addr;
+	if(control_cleanup() < 0){
+    	return -1;
+    }
 
+	if(option == 1){
+		page_addr = LED_BASE_ADDR & (~(page_size-1)); // AB
+    	page_off  = LED_BASE_ADDR - page_addr;
+
+    	page_ptr = mmap(NULL, LED_BASE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, control_fd, page_addr);
+    	led_struct = page_ptr + page_off;
+	}else{
+		page_addr = TEMP_BASE_ADDR & (~(page_size-1));
+    	page_off = TEMP_BASE_ADDR - page_addr;
+
+    	page_ptr = mmap(NULL, TEMP_BASE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, control_fd, page_addr);
+    	temp_struct = page_ptr + page_off;
+	}
+    
     /* void *mmap(void *addr, size_t length, int prot, int flags,
                   int fd, off_t offset); */
-
-    page_ptr = mmap(NULL, LED_BASE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, led_control_fd, page_addr);
-    
-    /* Set pointers to correct values */
-    led_struct = page_ptr + page_off;
     return 0;
 }
 
-int usage(void){
-	fprintf(NULL, "Led control information.\n");
-	return 0;
+void usage(int ret_val){
+	char *argv_max = "Too many arguments! LCR control takes exactly 1 argument."
+					 "Usage: \n"
+					 "\t-Argument 1: Led control\n"
+					 "\t-Argument 2: Temperature control.\n";
+
+	if(ret_val == 1){
+		fprintf(stderr, argv_max, __TIMESTAMP__, t_argv0);
+	}
 }
 
 
-int main(void){
-	/* Application init */
-	led_control_init();
+int main(int argc, char *argv[]){
+	/* Argument check */
+	if(argc > 2){
+		usage(0);
+	}else if(argc == 1){
+		/* Led option*/
+		control_init(1);
 
-	/* Lighting up led from 1 to 7 */
-	for(int i = 0; i < 9; i++){
-		
-		led_struct->led_control = pow(2, i);
-		usleep(100000);
+		/* Lighting up led from 1 to 7 */
+		for(int i = 0; i < 9; i++){
+			
+			led_struct->led_control = pow(2, i);
+			usleep(100000);
+		}
+		/* Lighting up led from 7 to 1 */
+		for (int j = 9; j > 0; --j)
+		{
+			led_struct->led_control = pow(2, j);
+			usleep(100000);
+		}
+		usleep(10000);
+		led_struct->led_control = 0;
+
+	}else{
+		/* Init the temperature memory space */
+		control_init(2);
+
+		float ret_temp = (float)((temp_struct->temp_control)*503.975) / ADC_FULL_RANGE_CN0T - 273.15;
+
+		printf("Temperature: %.2f\n", ret_temp);
+
 	}
-	/* Lighting up led from 7 to 1 */
-	for (int j = 9; j > 0; --j)
-	{
-		led_struct->led_control = pow(2, j);
-		usleep(100000);
-	}
-	usleep(10000);
-	led_struct->led_control = 0;
+	control_cleanup();
+	
 	return 0;
 }
