cdc7d8f774659ebdd390c642399349347ecafe31
1nfused <enigma4.speed@gmail.com>
2014-12-01 12:49:10 -0800

    hw_monitor attributes changed from public to private.

--- Test/hw_monitor/hw_monitor.h
+++ Test/hw_monitor/hw_monitor.h
@@ -34,51 +34,49 @@ const int TEMP_BASE_SIZE = 0x30;
 
 const int ADC_FULL_RANGE_CNT =  0xfff;
 
+/* Led structure declaration */
+typedef struct led_control_s{
+	unsigned long int id;
+	unsigned long int dna_part1;
+	unsigned long int dna_part2;
+	unsigned long int reserved_1;
+	unsigned long int ex_cd_p;
+	unsigned long int ex_cd_n;
+	unsigned long int ex_co_p;
+	unsigned long int ex_co;
+	unsigned long int ex_ci_p;
+	unsigned long int ex_ci_n;
+	unsigned long int reserved_2;
+	unsigned long int reserved_3;
+	unsigned long int led_control;
+} led_control_t;
 
+/* Temperature structure declaration */
+typedef struct temp_struct_s {
+	unsigned long int xadc_aif0;
+	unsigned long int xadc_aif1;
+	unsigned long int xadc_aif2;
+	unsigned long int xadc_aif3;
+	unsigned long int xadc_aif4;
+	unsigned long int reserved_1;
+	unsigned long int reserved_2;
+	unsigned long int reserved_3;
+	unsigned long int pwm_dac0;
+	unsigned long int pwm_dac1;
+	unsigned long int pwm_dac2;
+	unsigned long int pwm_dac3;
+	unsigned long int temp_control;
+}temp_struct_t;
 
 class hw_monitor {
 
-public:
+private:
 	/* File descriptor */
-	int fd_control = -1;
-
-	/* Led structure declaration */
-	typedef struct led_control_s{
-		unsigned long int id;
-		unsigned long int dna_part1;
-		unsigned long int dna_part2;
-		unsigned long int reserved_1;
-		unsigned long int ex_cd_p;
-		unsigned long int ex_cd_n;
-		unsigned long int ex_co_p;
-		unsigned long int ex_co;
-		unsigned long int ex_ci_p;
-		unsigned long int ex_ci_n;
-		unsigned long int reserved_2;
-		unsigned long int reserved_3;
-		unsigned long int led_control;
-	} led_control_t;
-
-	/* Temperature structure declaration */
-	typedef struct temp_struct_s {
-		unsigned long int xadc_aif0;
-		unsigned long int xadc_aif1;
-		unsigned long int xadc_aif2;
-		unsigned long int xadc_aif3;
-		unsigned long int xadc_aif4;
-		unsigned long int reserved_1;
-		unsigned long int reserved_2;
-		unsigned long int reserved_3;
-		unsigned long int pwm_dac0;
-		unsigned long int pwm_dac1;
-		unsigned long int pwm_dac2;
-		unsigned long int pwm_dac3;
-		unsigned long int temp_control;
-	}temp_struct_t;
+	int fd_control;
 
 	/* Address structure set to null */
-	led_control_t *led_struct = NULL;
-	temp_struct_t *temp_struct = NULL;
+	led_control_t *led_struct;
+	temp_struct_t *temp_struct;
 
 /*Function declarations */
 public:
