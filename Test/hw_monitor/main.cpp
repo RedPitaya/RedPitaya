c7a58430814d87995763ea7d4648c4b351d152a2
1nfused <enigma4.speed@gmail.com>
2014-12-01 05:02:44 -0800

    Class prototype in hw_monitor.h, class definition moved to hw_monitor.cpp.

---
+++ Test/hw_monitor/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 1, 2014
+ *      Author: infused
+ */
+
+#include <iostream>
+using namespace std;
+
+#include "hw_monitor.h"
+
+int main(){
+	hw_monitor hw;
+	hw.power_led(5);
+}
+
