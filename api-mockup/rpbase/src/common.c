cb5e851689e3ad796327d6ed0c89c812ff6eb75a
1nfused <enigma4.speed@gmail.com>
2014-12-08 15:19:22 +0100

    Api mockup.

---
+++ api-mockup/rpbase/src/common.c
@@ -0,0 +1,85 @@
+/**
+ * $Id: $
+ *
+ * @brief Red Pitaya library common module implementation
+ *
+ * @Author Red Pitaya
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "common.h"
+
+static int fd = -1;
+
+int cmn_Init()
+{
+	if (fd == -1) {
+		if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
+			return RP_EOMD;
+		}
+	}
+	return RP_OK;
+}
+
+int cmn_Release()
+{
+	if (fd != -1) {
+		 if(close(fd) < 0) {
+			 return RP_ECMD;
+		 }
+	}
+
+	return RP_OK;
+}
+
+int cmn_Map(size_t size, size_t offset, void** mapped)
+{
+	*mapped = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
+
+	if(mapped == (void *) -1) {
+		return RP_EMMD;
+	}
+
+	return RP_OK;
+}
+
+int cmn_Unmap(size_t size, void** mapped)
+{
+	if(munmap(*mapped, size) < 0){
+		return RP_EUMD;
+	}
+	*mapped = NULL;
+	return RP_OK;
+}
+
+int cmn_SetBits(volatile uint32_t* field, uint32_t bits, uint32_t mask)
+{
+	VALIDATE_BITS(bits, mask);
+	SET_BITS(*field, bits);
+	return RP_OK;
+}
+
+int cmn_UnsetBits(volatile uint32_t* field, uint32_t bits, uint32_t mask)
+{
+	VALIDATE_BITS(bits, mask);
+	UNSET_BITS(*field, bits);
+	return RP_OK;
+}
+
+int cmn_AreBitsSet(uint32_t field, uint32_t bits, uint32_t mask, bool* result)
+{
+	VALIDATE_BITS(bits, mask);
+	*result = ARE_BITS_SET(field, bits);
+	return RP_OK;
+}
+
+
---
+++ api-mockup/rpbase/src/common.h
@@ -0,0 +1,51 @@
+/**
+ * $Id: $
+ *
+ * @brief Red Pitaya library common module interface
+ *
+ * @Author Red Pitaya
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "rp.h"
+
+#define ECHECK(x) { \
+  int retval = (x); \
+  if (retval != RP_OK) { \
+    fprintf(stderr, "Runtime error: %s returned %d at %s:%d", #x, retval, __FILE__, __LINE__); \
+    return retval; \
+  } \
+}
+
+#define SET_BITS(x,b) (x |= b)
+#define UNSET_BITS(x,b) (x &= ~b)
+#define ARE_BITS_SET(x,b) ((x & b) == b)
+
+#define VALIDATE_BITS(b,m) { \
+	if ((b & ~m) != 0) return RP_EOOR; \
+}
+
+
+
+int cmn_Init();
+int cmn_Release();
+
+int cmn_Map(size_t size, size_t offset, void** mapped);
+int cmn_Unmap(size_t size, void** mapped);
+
+int cmn_SetBits(volatile uint32_t* field, uint32_t bits, uint32_t mask);
+int cmn_UnsetBits(volatile uint32_t* field, uint32_t bits, uint32_t mask);
+int cmn_AreBitsSet(uint32_t field, uint32_t bits, uint32_t mask, bool* result);
+
+#endif /* COMMON_H_ */
