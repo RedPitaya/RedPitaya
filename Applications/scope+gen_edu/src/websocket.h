92b667db0b940845411a3da1d346963df9887107
Ales Bardorfer <ales.bardorfer@redpitaya.com>
2014-12-17 16:09:15 +0100

    Added initial scope+gen_edu application, based on the scope+gen.

---
+++ Applications/scope+gen_edu/src/websocket.h
@@ -0,0 +1,22 @@
+/**
+ * $Id: worker.h 881 2013-12-16 05:37:34Z rp_jmenart $
+ *
+ * @brief Red Pitaya Oscilloscope worker.
+ *
+ * @Author Jure Menart <juremenart@gmail.com>
+ *         
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+#ifndef __WEBSOCKET_H
+#define __WEBSOCKET_H
+
+
+int ws_init(void);
+int ws_exit(void);
+
+#endif /* __WEBSOCKET_H */
