64bc06c0afbbc691e6fc6f594f6456eb81d7395e
Ales Bardorfer <ales.bardorfer@redpitaya.com>
2014-12-23 17:28:33 +0100

    Real RP scope signals being sent via websocket instead of synthetics.

--- Applications/scope+gen_edu/src/websocket.c
+++ Applications/scope+gen_edu/src/websocket.c
@@ -7,13 +7,19 @@
 #include <libwebsockets.h>
 
 #include "websocket.h"
+#include "main.h"
 
+#define MIN(a, b)    ( (a) < (b) ? (a) : (b) )
 
 pthread_t *sw_thread_handler = NULL;
 void *ws_worker_thread(void *args);
 int rp_websocket(int *stop);
 int   ws_stop;
 
+static float **rp_signals = NULL;
+
+char *buf = NULL;
+float *fbuf = NULL;
 
 int ws_init(void)
 {
@@ -63,14 +69,6 @@ void *ws_worker_thread(void *arg)
 }
 
 
-static int callback_http(struct libwebsocket_context * this,
-        struct libwebsocket *wsi,
-        enum libwebsocket_callback_reasons reason, void *user,
-        void *in, size_t len)
-{
-    return 0;
-}
-
 
 #define PARAM_LEN 10
 #define WAVE_LEN  50
@@ -89,53 +87,108 @@ int align(size_t in, size_t size) {
     return ret;
 }
 
+struct session_data {
+    int   payload;
+    int   pos;
+};
 
-static int callback_redpitaya_binary(struct libwebsocket_context * this,
+
+void rp_prepare_data_synth(float *fbuf, struct session_data *data)
+{
+    int i, j;
+    int rp_sig_len = 50;
+    float ampl;
+    int k = 0;
+
+    //printf("WS responding (prepare data)...\n");
+
+    for (i = 0; i < PARAM_LEN; i++) {
+        fbuf[k++] = 0.0;
+    }
+
+    ampl = (float)rand()/RAND_MAX * 50.0;
+    for (i = 0; i < rp_sig_len; i++) {
+        fbuf[k++] = ampl * waveform[i];
+    }
+
+    ampl = (float)rand()/RAND_MAX * 50.0;
+    for (i = 0; i < rp_sig_len; i++) {
+        fbuf[k++] = ampl * waveform[i];
+    }
+
+    ampl = (float)rand()/RAND_MAX * 50.0;
+    for (j = 0; j < 3; j++) {
+        for (i = 0; i < rp_sig_len; i++) {
+            fbuf[k++] = ampl * waveform[i];
+        }
+    }
+
+    data->payload = (PARAM_LEN + 5*rp_sig_len) * sizeof(float);
+
+}
+
+
+void rp_prepare_data(float *fbuf, struct session_data *data)
+{
+    int rp_sig_num, rp_sig_len;
+    int ret;
+    int i, j;
+
+    int retries = 200; // ms
+    do {
+        ret = rp_get_signals((float ***)&rp_signals, &rp_sig_num, &rp_sig_len);
+
+        if(ret == -2)
+            break;
+        if(retries-- <= 0) {
+            /* Use old signals */
+            break;
+        } else {
+            usleep(1000);
+        }
+    } while (ret == -1);
+
+    fprintf(stderr, "Got RP data after %d ms.\n", 200 - retries);
+
+    rp_sig_len = 50;
+    int k = 0;
+    for (i = 0; i < PARAM_LEN; i++) {
+        fbuf[k++] = 0.0;
+    }
+    for (i = 0; i < rp_sig_len; i++) {
+        fbuf[k++] = rp_signals[0][i];
+    }
+    for (i = 0; i < rp_sig_len; i++) {
+        fbuf[k++] = rp_signals[1][i]*100;
+    }
+    for (j = 0; j < 3; j++) {
+        for (i = 0; i < rp_sig_len; i++) {
+            fbuf[k++] = rp_signals[2][i]*100;
+        }
+    }
+
+    data->payload = (PARAM_LEN + 5*rp_sig_len) * sizeof(float);
+
+}
+
+static int callback_redpitaya_binary(struct libwebsocket_context *context,
         struct libwebsocket *wsi,
         enum libwebsocket_callback_reasons reason,
         void *user, void *in, size_t len)
 {
+    struct session_data *data = user;
 
     switch (reason) {
 
-    case LWS_CALLBACK_ESTABLISHED: // just log message that someone is connecting
-    printf("connection established\n");
-    break;
+    case LWS_CALLBACK_ESTABLISHED:
+        fprintf(stderr, "LWS_CALLBACK_ESTABLISHED\n");
+        break;
 
     case LWS_CALLBACK_RECEIVE: {
 
         //printf("Received data (%u)\n", len);
-        // the funny part
-        // create a buffer to hold our response
-        // it has to have some pre and post padding. You don't need to care
-        // what comes there, libwebsockets will do everything for you. For more info see
-        // http://git.warmcat.com/cgi-bin/cgit/libwebsockets/tree/lib/libwebsockets.h#n597
 
-        // Make sure the effective start of buffer is basic array type aligned
-        int preamble = align(LWS_SEND_BUFFER_PRE_PADDING, sizeof(float));
-
-        int payload = (PARAM_LEN + WAVE_LEN * SIG_NUM) * sizeof(float);
-        unsigned char *buf = (unsigned char*) malloc(preamble + payload +
-                LWS_SEND_BUFFER_POST_PADDING);
-        float *p = (float *)&buf[preamble];
-        int i, j;
-
-
-        //printf("WS responding (prepare data)...\n");
-
-        // Some synthetic data...
-        int index = 0;
-        for (i = 0; i < PARAM_LEN; i++) {
-            p[index++] = 0.0;
-        }
-        for (i = 0; i < SIG_NUM; i++) {
-            float ampl = (float)rand()/RAND_MAX * 30.0;
-            //printf("ampl(%d) = %4.3f\n", i, ampl);
-            for (j = 0; j < WAVE_LEN; j++) {
-                p[index++] = ampl * waveform[j];
-            }
-        }
-
+        rp_prepare_data(fbuf, data);
 
         /* TODO: Fix libwebsocket writing of more than ~1kB data.
          *
@@ -153,17 +206,19 @@ static int callback_redpitaya_binary(struct libwebsocket_context * this,
          * does have the latest libwebsocket included.
          *
          */
-        //printf("WS responding (write)...\n");
+        unsigned char *fbp = (unsigned char *)fbuf;
+
         //int n = libwebsocket_write(wsi, &buf[preamble], payload, LWS_WRITE_BINARY);
-        libwebsocket_write(wsi, &buf[preamble], payload, LWS_WRITE_BINARY);
+        libwebsocket_write(wsi, fbp, data->payload, LWS_WRITE_BINARY);
 
         //printf("libwebsocket_write() returned: %d\n", n);
 
-
-        // release memory back into the wild
-        free(buf);
         break;
     }
+
+    case LWS_CALLBACK_CLOSED:
+        fprintf(stderr, "LWS_CALLBACK_CLOSED\n");
+        break;
 
     default:
         break;
@@ -173,18 +228,25 @@ static int callback_redpitaya_binary(struct libwebsocket_context * this,
 }
 
 
+static int callback_http(struct libwebsocket_context *context,
+        struct libwebsocket *wsi,
+        enum libwebsocket_callback_reasons reason,
+        void *user, void *in, size_t len)
+{
+    return 0;
+}
+
 
 static struct libwebsocket_protocols protocols[] = {
-        /* first protocol must always be HTTP handler */
         {
-                "http-only",   // name
-                callback_http, // callback
-                0              // per_session_data_size
+                "http-only",
+                callback_http,
+                0
         },
         {
-                "redpitaya-binary",         // protocol name - very important!
-                callback_redpitaya_binary,  // callback
-                0                          // we don't use any per session data
+                "redpitaya-binary",
+                callback_redpitaya_binary,
+                sizeof(struct session_data)
 
         },
         {
@@ -198,25 +260,19 @@ int rp_websocket(int *stop) {
 
     struct libwebsocket_context *context;   
     struct lws_context_creation_info info;
-    const char *iface = NULL;
     int i;
 
     memset(&info, 0, sizeof info);
 
     info.port = 8080;
-    info.iface = iface;
     info.protocols = protocols;
 #ifndef LWS_NO_EXTENSIONS
     info.extensions = libwebsocket_get_internal_extensions();
 #endif
-    info.ssl_cert_filepath = NULL;
-    info.ssl_private_key_filepath = NULL;
     info.gid = -1;
     info.uid = -1;
     info.options = 0;
 
-
-    // create libwebsocket context representing this server
     context = libwebsocket_create_context(&info);
 
     if (context == NULL) {
@@ -224,23 +280,37 @@ int rp_websocket(int *stop) {
         return -1;
     }
 
+    // Make sure the effective start of buffer is basic array type aligned
+    int preamble = align(LWS_SEND_BUFFER_PRE_PADDING, sizeof(float));
+
+    int payload = (PARAM_LEN + WAVE_LEN * SIG_NUM) * sizeof(float);
+    unsigned char *buf = (unsigned char*) malloc(preamble + payload + LWS_SEND_BUFFER_POST_PADDING);
+    fbuf = (float *)&buf[preamble];
+
+    rp_signals = (float **)malloc(3 * sizeof(float *));
+    for(i = 0; i < 3; i++) {
+        rp_signals[i] = (float *)malloc(2048 * sizeof(float));
+    }
 
     for(i = 0; i < WAVE_LEN; i++) {
         waveform[i] = sin((float)i/30.0);
     }
 
-    printf("Websocket starting server... \n");
+    fprintf(stderr, "Websocket starting server... \n");
 
-    // infinite loop, to end this server send SIGTERM. (CTRL+C)
-    while (!(*stop)) {
-        libwebsocket_service(context, 10);
-        // libwebsocket_service will process all waiting events with their
-        // callback functions and then wait 50 ms.
-        // (this is a single threaded webserver and this will keep our server
-        // from generating load while there are not requests to process)
+    int n = 0;
+    while (!n && !(*stop)) {
+        n = libwebsocket_service(context, 10);
     }
 
     libwebsocket_context_destroy(context);
+
+    free(buf);
+
+    for(i = 0; i < 3; i++) {
+        free(rp_signals[i]);
+    }
+    free(rp_signals);
 
     return 0;
 }
