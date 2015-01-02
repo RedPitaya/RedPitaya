8d0c41cadced691466903efbf3ca194a13fc4a10
Tomaž Juretič <tomaz.juretic@gmail.com>
2015-01-02 20:41:42 +0100

    Comments...

--- api-mockup/rpbase/src/rp.h
+++ api-mockup/rpbase/src/rp.h
@@ -190,16 +190,16 @@ typedef enum {
  * Calibration parameters, stored in the EEPROM device
  */
 typedef struct {
-    uint32_t fe_ch1_fs_g_hi; //!< High gain front end full scale voltage, channel 1
-    uint32_t fe_ch2_fs_g_hi; //!< High gain front end full scale voltage, channel 2
-    uint32_t fe_ch1_fs_g_lo; //!< Low gain front end full scale voltage, channel 1
-    uint32_t fe_ch2_fs_g_lo; //!< Low gain front end full scale voltage, channel 2
-    int32_t  fe_ch1_dc_offs; //!< Front end DC offset, channel 1
-    int32_t  fe_ch2_dc_offs; //!< Front end DC offset, channel 2
-    uint32_t be_ch1_fs;      //!< Back end full scale voltage, channel 1
-    uint32_t be_ch2_fs;      //!< Back end full scale voltage, channel 2
-    int32_t  be_ch1_dc_offs; //!< Back end DC offset, channel 1
-    int32_t  be_ch2_dc_offs; //!< Back end DC offset, on channel 2
+    uint32_t fe_ch1_fs_g_hi; //!< High gain front end full scale voltage, channel A
+    uint32_t fe_ch2_fs_g_hi; //!< High gain front end full scale voltage, channel B
+    uint32_t fe_ch1_fs_g_lo; //!< Low gain front end full scale voltage, channel A
+    uint32_t fe_ch2_fs_g_lo; //!< Low gain front end full scale voltage, channel B
+    int32_t  fe_ch1_dc_offs; //!< Front end DC offset, channel A
+    int32_t  fe_ch2_dc_offs; //!< Front end DC offset, channel B
+    uint32_t be_ch1_fs;      //!< Back end full scale voltage, channel A
+    uint32_t be_ch2_fs;      //!< Back end full scale voltage, channel B
+    int32_t  be_ch1_dc_offs; //!< Back end DC offset, channel A
+    int32_t  be_ch2_dc_offs; //!< Back end DC offset, on channel B
 } rp_calib_params_t;
 
 /** @name General
