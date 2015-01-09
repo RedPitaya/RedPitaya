725352db631b9dc138e4a67a25ac442cc3b9e333
Aljoša <aljosa.mrak@gmail.com>
2015-01-09 04:19:43 +0100

    Added API for generate

---
+++ api-mockup/rpbase/src/gen_handler.h
@@ -0,0 +1,44 @@
+/**
+ * $Id: $
+ *
+ * @brief Red Pitaya library Generate handler interface
+ *
+ * @Author Red Pitaya
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+#ifndef GENERATE_HANDLER_H_
+#define GENERATE_HANDLER_H_
+
+
+#include "rp.h"
+
+int gen_Disable(rp_channel_t chanel);
+int gen_Enable(rp_channel_t chanel);
+int gen_setAmplitude(rp_channel_t chanel, double amplitude);
+int gen_Offset(rp_channel_t chanel, double offset) ;
+int gen_Frequency(rp_channel_t chanel, double frequency);
+int gen_Phase(rp_channel_t chanel, double phase);
+int gen_Waveform(rp_channel_t chanel, rp_waveform_t type);
+int gen_ArbWaveform(rp_channel_t chanel, float *waveform, uint32_t length);
+int gen_DutyCycle(rp_channel_t chanel, double ratio);
+int gen_GenMode(rp_channel_t chanel, rp_gen_mode_t mode);
+int gen_BurstCount(rp_channel_t chanel, int num);
+int gen_TriggerSource(rp_channel_t chanel, rp_trig_src_t src);
+int gen_Trigger(int mask);
+
+int synthesize_signal(rp_channel_t chanel);
+int synthesis_sin(double amplitude, float *data_out);
+int synthesis_triangle(double amplitude, float *data_out);
+int synthesis_square(double amplitude, double frequency, float *data_out);
+int synthesis_rampUp(double amplitude, float *data_out);
+int synthesis_rampDown(double amplitude, float *data_out);
+int synthesis_DC(double amplitude, float *data_out);
+int synthesis_PWM(double amplitude, double ratio, float *data_out);
+
+#endif
---
+++ api-mockup/rpbase/src/generate.h
@@ -0,0 +1,61 @@
+/**
+ * $Id: $
+ *
+ * @brief Red Pitaya library Generate module interface
+ *
+ * @Author Red Pitaya
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+#ifndef __GENERATE_H
+#define __GENERATE_H
+
+
+#define AMPLITUDE_MIN  0.0	   // V
+#define AMPLITUDE_MAX  2.0     // V
+#define OFFSET_MIN     0.0     // V
+#define OFFSET_MAX     2.0     // V
+#define FREQUENCY_MIN  0	   // Hz
+#define FREQUENCY_MAX  62.5e6  // Hz
+#define PHASE_MIN	   0       // deg
+#define PHASE_MAX	   180     // deg
+#define DUTY_CYCLE_MIN 0       // %
+#define DUTY_CYCLE_MAX 100     // %
+#define DAC_FREQUENCY 125e6    // Hz
+
+#define BUFFER_LENGTH (16 * 1024)
+#define CHA_DATA_OFFSET 0x10000
+#define CHB_DATA_OFFSET 0x20000
+#define DATA_BIT_LENGTH 14
+
+#define CHECK_OUTPUT(X, Y) { \
+if (chanel == RP_CH_A) { \
+	X; \
+} \
+else if (chanel == RP_CH_B) { \
+	Y; \
+} \
+else { \
+	return RP_EPN; \
+} \
+}
+
+int generate_Init();
+int generate_Release();
+
+int generate_setOutputDisable(rp_channel_t out, bool disable);
+int generate_setDCOffset(rp_channel_t out, uint32_t offset);
+int generate_setFrequency(rp_channel_t out, uint32_t frequency);
+int generate_setPhase(rp_channel_t out, uint32_t phase);
+int generate_setTriggerSource(rp_channel_t out, unsigned short value);
+int generate_GenTrigger(rp_channel_t out);
+int generate_setOneTimeTrigger(rp_channel_t out, uint32_t value);
+
+int generate_writeData(rp_channel_t out, float *data, uint32_t length);
+
+#endif //__GENERATE_H
