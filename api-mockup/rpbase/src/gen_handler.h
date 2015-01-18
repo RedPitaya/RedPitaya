cfef0df750dd6de6b2ae7dd1623e0360998b3ef2
Aljoša <aljosa.mrak@gmail.com>
2015-01-18 02:47:04 +0100

    Fixed: generate amplitude and arbitrary + some refactoring

--- api-mockup/rpbase/src/gen_handler.h
+++ api-mockup/rpbase/src/gen_handler.h
@@ -26,20 +26,21 @@ int gen_Offset(rp_channel_t chanel, double offset) ;
 int gen_Frequency(rp_channel_t chanel, double frequency);
 int gen_Phase(rp_channel_t chanel, double phase);
 int gen_Waveform(rp_channel_t chanel, rp_waveform_t type);
-int gen_ArbWaveform(rp_channel_t chanel, float *waveform, uint32_t length);
+int gen_ArbWaveform(rp_channel_t chanel, float *data, uint32_t length);
 int gen_DutyCycle(rp_channel_t chanel, double ratio);
 int gen_GenMode(rp_channel_t chanel, rp_gen_mode_t mode);
-int gen_BurstCount(rp_channel_t chanel, int num);
+int gen_BurstCount(rp_channel_t channel, int num);
 int gen_TriggerSource(rp_channel_t chanel, rp_trig_src_t src);
 int gen_Trigger(int mask);
 
 int synthesize_signal(rp_channel_t chanel);
-int synthesis_sin(double amplitude, float *data_out);
-int synthesis_triangle(double amplitude, float *data_out);
-int synthesis_square(double amplitude, double frequency, float *data_out);
-int synthesis_rampUp(double amplitude, float *data_out);
-int synthesis_rampDown(double amplitude, float *data_out);
-int synthesis_DC(double amplitude, float *data_out);
-int synthesis_PWM(double amplitude, double ratio, float *data_out);
+int synthesis_sin(float *data_out);
+int synthesis_triangle(float *data_out);
+int synthesis_arbitrary(rp_channel_t channel, float *data_out);
+int synthesis_square(double frequency, float *data_out);
+int synthesis_rampUp(float *data_out);
+int synthesis_rampDown(float *data_out);
+int synthesis_DC(float *data_out);
+int synthesis_PWM(double ratio, float *data_out);
 
 #endif
