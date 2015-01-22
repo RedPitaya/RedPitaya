ff80f3e37c1a20b273dd94c91709a3a61622fece
Tomaž Juretič <tomaz.juretic@gmail.com>
2015-01-22 01:10:59 +0100

    Fixed indentation

--- scpi-server/src/scpi-server.c
+++ scpi-server/src/scpi-server.c
@@ -47,8 +47,8 @@ static char delimiter[] = "\r\n";
 static void handleCloseChildEvents()
 {
     struct sigaction sigchld_action = {
-      .sa_handler = SIG_DFL,
-      .sa_flags = SA_NOCLDWAIT
+            .sa_handler = SIG_DFL,
+            .sa_flags = SA_NOCLDWAIT
     };
     sigaction(SIGCHLD, &sigchld_action, NULL);
 }
@@ -56,18 +56,18 @@ static void handleCloseChildEvents()
 
 static void termSignalHandler(int signum)
 {
-	app_exit = true;
-	syslog (LOG_NOTICE, "Received terminate signal. Exiting...");
+    app_exit = true;
+    syslog (LOG_NOTICE, "Received terminate signal. Exiting...");
 }
 
 
 static void installTermSignalHandler()
 {
-	 struct sigaction action;
-	    memset(&action, 0, sizeof(struct sigaction));
-	    action.sa_handler = termSignalHandler;
-	    sigaction(SIGTERM, &action, NULL);
-	    sigaction(SIGINT, &action, NULL);
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = termSignalHandler;
+    sigaction(SIGTERM, &action, NULL);
+    sigaction(SIGINT, &action, NULL);
 }
 
 /**
@@ -78,30 +78,30 @@ static void installTermSignalHandler()
  */
 static size_t getNextCommand(const char* buffer, size_t bufferLen)
 {
-	size_t delimiterLen = sizeof(delimiter) - 1; // dont count last null char.
-	size_t i = 0;
-	for (i = 0; i < bufferLen; i++) {
+    size_t delimiterLen = sizeof(delimiter) - 1; // dont count last null char.
+    size_t i = 0;
+    for (i = 0; i < bufferLen; i++) {
 
-		// Find match for end of delimiter
-		if (buffer[i] == delimiter[delimiterLen - 1]) {
+        // Find match for end of delimiter
+        if (buffer[i] == delimiter[delimiterLen - 1]) {
 
-			// Now go back checking if all delimiter character matches
-			size_t dist = 0;
-			while (i - dist >= 0 && delimiterLen - dist > 0) {
-				if (buffer[i - dist] != delimiter[delimiterLen - dist - 1]) {
-					break;
-				}
-				if (delimiterLen - dist - 1 == 0) {
-					return i + 1; // Position of next command
-				}
+            // Now go back checking if all delimiter character matches
+            size_t dist = 0;
+            while (i - dist >= 0 && delimiterLen - dist > 0) {
+                if (buffer[i - dist] != delimiter[delimiterLen - dist - 1]) {
+                    break;
+                }
+                if (delimiterLen - dist - 1 == 0) {
+                    return i + 1; // Position of next command
+                }
 
-				dist++;
-			}
-		}
-	}
+                dist++;
+            }
+        }
+    }
 
-	// No match found
-	return -1;
+    // No match found
+    return -1;
 }
 
 void LogMessage(char *m, size_t len) {
@@ -121,16 +121,16 @@ void LogMessage(char *m, size_t len) {
  * @return
  */
 static int handleConnection(int connfd) {
-	int read_size;
+    int read_size;
 
-	size_t message_len = MAX_BUFF_SIZE;
-	char *message_buff = malloc(message_len);
-	char buffer[MAX_BUFF_SIZE];
-	size_t msg_end = 0;
+    size_t message_len = MAX_BUFF_SIZE;
+    char *message_buff = malloc(message_len);
+    char buffer[MAX_BUFF_SIZE];
+    size_t msg_end = 0;
 
-	installTermSignalHandler();
+    installTermSignalHandler();
 
-	prctl( 1, SIGTERM );
+    prctl( 1, SIGTERM );
 
     syslog(LOG_INFO, "Waiting for first client request.");
 
@@ -177,8 +177,8 @@ static int handleConnection(int connfd) {
 
     syslog(LOG_INFO, "Closing client connection...");
 
-	if(read_size == 0)
-	{
+    if(read_size == 0)
+    {
         syslog(LOG_INFO, "Client is disconnected");
         return 0;
     }
@@ -188,7 +188,7 @@ static int handleConnection(int connfd) {
         perror("Receive message failed");
         return 1;
     }
-	return 0;
+    return 0;
 }
 
 
@@ -204,12 +204,12 @@ int main(int argc, char *argv[])
 {
 
     // Open logging into "/var/log/messages" or /var/log/syslog" or other configured...
-	setlogmask (LOG_UPTO (LOG_INFO));
-	openlog ("scpi-server", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    setlogmask (LOG_UPTO (LOG_INFO));
+    openlog ("scpi-server", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
-	syslog (LOG_NOTICE, "scpi-server started");
+    syslog (LOG_NOTICE, "scpi-server started");
 
-	installTermSignalHandler();
+    installTermSignalHandler();
 
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr;
@@ -220,15 +220,15 @@ int main(int argc, char *argv[])
 
     int result = rp_Init();
     if (result != RP_OK) {
-    	syslog(LOG_ERR, "Failed to initialize RP library: %s", rp_GetError(result));
-    	return (EXIT_FAILURE);
+        syslog(LOG_ERR, "Failed to initialize RP library: %s", rp_GetError(result));
+        return (EXIT_FAILURE);
     }
 
-	result = rp_Reset();
-	if (result != RP_OK) {
-		syslog(LOG_ERR, "Failed to reset RP: %s", rp_GetError(result));
-		return (EXIT_FAILURE);
-	}
+    result = rp_Reset();
+    if (result != RP_OK) {
+        syslog(LOG_ERR, "Failed to reset RP: %s", rp_GetError(result));
+        return (EXIT_FAILURE);
+    }
 
     // user_context will be pointer to socket
     scpi_context.user_context = NULL;
@@ -238,9 +238,9 @@ int main(int argc, char *argv[])
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd == -1)
     {
-    	syslog(LOG_ERR, "Failed to create a socket (%s)", strerror(errno));
-    	perror("Failed to create a socket");
-    	return (EXIT_FAILURE);
+        syslog(LOG_ERR, "Failed to create a socket (%s)", strerror(errno));
+        perror("Failed to create a socket");
+        return (EXIT_FAILURE);
     }
 
     memset(&serv_addr, '0', sizeof(serv_addr));
@@ -251,16 +251,16 @@ int main(int argc, char *argv[])
 
     if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
     {
-    	syslog(LOG_ERR, "Failed to bind the socket (%s)", strerror(errno));
-    	perror("Failed to bind the socket");
-    	return (EXIT_FAILURE);
+        syslog(LOG_ERR, "Failed to bind the socket (%s)", strerror(errno));
+        perror("Failed to bind the socket");
+        return (EXIT_FAILURE);
     }
 
     if (listen(listenfd, LISTEN_BACKLOG) == -1)
     {
-    	syslog(LOG_ERR, "Failed to listen on the socket (%s)", strerror(errno));
-    	perror("Failed to listen on the socket");
-    	return (EXIT_FAILURE);
+        syslog(LOG_ERR, "Failed to listen on the socket (%s)", strerror(errno));
+        perror("Failed to listen on the socket");
+        return (EXIT_FAILURE);
     }
 
     syslog(LOG_INFO, "Server is listening on port %d\n", LISTEN_PORT);
@@ -268,20 +268,20 @@ int main(int argc, char *argv[])
     // Socket is opened and listening on port. Now we can accept connections
     while(1)
     {
-    	struct sockaddr_in cliaddr;
-    	socklen_t clilen;
-    	clilen = sizeof(cliaddr);
+        struct sockaddr_in cliaddr;
+        socklen_t clilen;
+        clilen = sizeof(cliaddr);
 
         connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
 
         if (app_exit == true) {
-        	break;
+            break;
         }
 
         if (connfd == -1) {
-        	syslog(LOG_ERR, "Failed to accept connection (%s)", strerror(errno));
-        	perror("Failed to accept connection\n");
-        	return (EXIT_FAILURE);
+            syslog(LOG_ERR, "Failed to accept connection (%s)", strerror(errno));
+            perror("Failed to accept connection\n");
+            return (EXIT_FAILURE);
         }
 
         // Fork a child process, which will talk to the client
@@ -289,23 +289,23 @@ int main(int argc, char *argv[])
 
             syslog(LOG_INFO, "Connection with client ip %s established.", inet_ntoa(cliaddr.sin_addr));
 
-        	// this is the child process
-        	close(listenfd); // child doesn't need the listener
+            // this is the child process
+            close(listenfd); // child doesn't need the listener
 
-        	scpi_context.user_context = &connfd;
+            scpi_context.user_context = &connfd;
 
-        	result = handleConnection(connfd);
+            result = handleConnection(connfd);
 
-        	close(connfd);
+            close(connfd);
 
             syslog(LOG_INFO, "Closing connection with client ip %s.", inet_ntoa(cliaddr.sin_addr));
 
-        	if (result == 0) {
-        		return(EXIT_SUCCESS);
-        	}
-        	else {
-        		return(EXIT_FAILURE);
-        	}
+            if (result == 0) {
+                return(EXIT_SUCCESS);
+            }
+            else {
+                return(EXIT_FAILURE);
+            }
         }
 
         close(connfd);
@@ -316,13 +316,13 @@ int main(int argc, char *argv[])
     result = rp_Release();
 
     if (result != RP_OK) {
-    	syslog(LOG_ERR, "Failed to initialize RP library: %s", rp_GetError(result));
+        syslog(LOG_ERR, "Failed to initialize RP library: %s", rp_GetError(result));
     }
 
 
     syslog(LOG_INFO, "scpi-server stopped.");
 
-	closelog ();
+    closelog ();
 
     return (EXIT_SUCCESS);
 }
