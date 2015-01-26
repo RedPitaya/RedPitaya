9b9613ed7b5c613c9cc6541b004ed3a7829657b4
luka.golinar <luka.golinar@redpitaya.com>
2015-01-26 10:03:57 +0100

    Dir fix

--- Examples/C/acquire_signal_on_fast_analog_input.c
+++ Examples/C/acquire_signal_on_fast_analog_input.c
@@ -12,19 +12,17 @@ int main(int argc, char **argv){
 	if(rp_Init() != RP_OK){
 		fprintf(stderr, "Rp api init failed!\n");
 	}
-
-	rp_channel_t channel = RP_CH_1;
-	rp_acq_trig_src_t trig_source = RP_TRIG_SRC_NOW;
+	
 	uint32_t array_size = 16 * 1024; //Current buffer size. 
 
 	float *buff = (float *)malloc(array_size * sizeof(float));
 
 	/* Starts acquisition */
 	rp_AcqStart();
-	rp_AcqSetTriggerSrc(trig_source);
+	rp_AcqSetTriggerSrc(RP_TRIG_SRC_NOW);
 
 	/* Get the whole buffer into buf */
-	rp_AcqGetOldestDataV(channel, &array_size, buff);
+	rp_AcqGetOldestDataV(RP_CH_1, &array_size, buff);
 
 	int i;
 	for (i = 0; i < array_size; ++i)
