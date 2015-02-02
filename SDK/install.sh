d068f71cd4ab08f147901305f08562466408f991
1nfused <luka.golinar@redpitaya.com>
2015-02-02 21:43:59 +0100

    Added curl program install. Lib install, java sdk install. Fixed path export

--- SDK/install.sh
+++ SDK/install.sh
@@ -3,20 +3,28 @@
 #Redpitaya INSTALL SDK install script
 
 NAME=SDK
-GREET_MSG="\nSTARTING REDPITAYA SDK INSTALLATION.....\nDOWNLOADING ECLIPSE....."
-LIB_MSG="\nINSTALLING REDPITAYA LIBRARIES.....\n"
-GCC_LINARO_MSG="\nINSTALLING GCC LINARO.....\n"
+GREET_MSG="\nSTARTING REDPITAYA SDK INSTALLATION..."
+LIB_MSG="\nINSTALLING REDPITAYA LIBRARIES...\n"
+GCC_LINARO_MSG="\nINSTALLING GCC LINARO...\n"
 
 RP_INCLUDE=./include
-CURR_USER=whoami
-SDK_DIR=$PWD
 ECLIPSE_DL=eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz
 GCC_LINARO_DL=gcc-linaro-arm-linux-gnueabi-2012.03-20120326_linux
 GCC_LINARO_DIR=./gcc_linaro/bin
 
 
 echo -e $GREET_MSG
+echo "DOWNLOADING CURL...\n"
+sudo apt-get install curl
 
+echo -e "\nINSTALLING DEPENDENCIES...\n"
+sudo apt-get install openjdk-7-jre
+sudo chmod 777 /etc/apt/sources.list.d/ia32-libs-raring.list
+sudo echo "deb http://old-releases.ubuntu.com/ubuntu/ raring main restricted universe multiverse" > /etc/apt/sources.list.d/ia32-libs-raring.list
+sudo apt-get update
+sudo apt-get install ia32-libs
+
+echo -e "\nDOWNLOADING ECLIPSE...\n"
 curl --remote-name http://mirrors.nic.cz/eclipse/technology/epp/downloads/\
 release/luna/SR1a/$ECLIPSE_DL
 
@@ -27,10 +35,6 @@ echo -e $LIB_MSG
 sudo cp $RP_INCLUDE/* /usr/lib
 sudo cp $RP_INCLUDE/* /usr/include
 
-sudo chmod 777 /etc/apt/sources.list.d/ia32-libs-raring.list
-sudo echo "deb http://old-releases.ubuntu.com/ubuntu/ raring main restricted universe multiverse" > /etc/apt/sources.list.d/ia32-libs-raring.list
-sudo apt-get update
-sudo apt-get install ia32-libs
 echo -e $GCC_LINARO_MSG
 curl -L --remote-name https://launchpad.net/linaro-toolchain-binaries/trunk/2012.03/+download/$GCC_LINARO_DL.tar.bz2
 
@@ -39,6 +43,6 @@ sudo mv $GCC_LINARO_DL gcc_linaro
 rm -rf $GCC_LINARO_DL.tar.bz2
 
 sudo chmod 777 /etc/bash.bashrc
-export PATH=$PATH:$SDK_DIR/$GCC_LINARO_DIR
+export PATH=$PATH:$PWD/$GCC_LINARO_DIR
 
 
