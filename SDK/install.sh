641479440bf2e70995cba84cbdc66504219168cc
1nfused <luka.golinar@redpitaya.com>
2015-02-03 03:31:26 +0100

    Added architecture check

--- SDK/install.sh
+++ SDK/install.sh
@@ -5,28 +5,39 @@
 NAME=SDK
 GREET_MSG="\nSTARTING REDPITAYA SDK INSTALLATION..."
 LIB_MSG="\nINSTALLING REDPITAYA LIBRARIES...\n"
-GCC_LINARO_MSG="\nINSTALLING GCC LINARO...\n"
+GCC_LINARO_MSG="INSTALLING GCC LINARO...\n"
 
 RP_INCLUDE=./include
-ECLIPSE_DL=eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz
+ECLIPSE_DL=.
 GCC_LINARO_DL=gcc-linaro-arm-linux-gnueabi-2012.03-20120326_linux
 GCC_LINARO_DIR=./gcc_linaro/bin
 
 
 echo -e $GREET_MSG
-echo "DOWNLOADING CURL...\n"
+echo -e "DOWNLOADING CURL...\n"
 sudo apt-get install curl
 
 echo -e "\nINSTALLING DEPENDENCIES...\n"
-sudo apt-get install openjdk-7-jre
+sudo apt-get install default-jre
 sudo chmod 777 /etc/apt/sources.list.d/ia32-libs-raring.list
 sudo echo "deb http://old-releases.ubuntu.com/ubuntu/ raring main restricted universe multiverse" > /etc/apt/sources.list.d/ia32-libs-raring.list
 sudo apt-get update
 sudo apt-get install ia32-libs
 
-echo -e "\nDOWNLOADING ECLIPSE...\n"
-curl --remote-name http://mirrors.nic.cz/eclipse/technology/epp/downloads/\
-release/luna/SR1a/$ECLIPSE_DL
+echo -e "\nDOWNLOADING ECLIPSE..."
+
+#Determine machine type
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	echo -e "DETECTED 64 BIT OS ARCHITECTURE. DOWNLOADING APPROPRIATE ECLIPSE VERSION...\n"
+	ECLIPSE_DL=eclipse-cpp-luna-SR1a-linux-gtk-x86_64.tar.gz
+else
+	echo -e "DETECTED 32 BIT OS ARCHITECTURE. DOWNLOADING APPROPRIATE ECLIPSE VERSION...\n"
+  	ECLIPSE_DL=eclipse-cpp-luna-SR1a-linux-gtk.tar.gz
+fi
+
+#Download eclipse
+curl --remote-name http://mirrors.linux-bg.org/eclipse/technology/epp/downloads/release/luna/SR1a/$ECLIPSE_DL
 
 tar xvf $ECLIPSE_DL eclipse
 rm $ECLIPSE_DL
