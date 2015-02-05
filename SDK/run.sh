c9d07fdc355c765de844475b99617daf1890fdbd
1nfused <luka.golinar@redpitaya.com>
2015-02-05 05:17:52 -0800

    StricHostChecking added in run.sh. Deleted git comments..

--- SDK/run.sh
+++ SDK/run.sh
@@ -10,11 +10,6 @@ fi
 
 make -C $API_BLINK_EXAMPLE_DIR clean
 make -C $API_BLINK_EXAMPLE_DIR
-<<<<<<< HEAD
-scp $PWD/$API_BLINK_EXAMPLE_DIR/api_test root@$1:/tmp/
-sshpass -p root ssh -o StrictHostKeyChecking=no root@$1 'killall api_test &>/dev/null; /tmp/api_test'
-=======
-sshpass -p root ssh root@$1 'killall api_test &>/dev/null'
+sshpass -p root ssh -o StrictHostKeyChecking=no root@$1 'killall api_test &>/dev/null'
 sshpass -p root scp $PWD/$API_BLINK_EXAMPLE_DIR/api_test root@$1:/tmp/
 sshpass -p root ssh root@$1 '/tmp/api_test'
->>>>>>> 63d5808fcc7f7521166edc3ed42584ba5dd7cb4a
