65cffc0165cc7ea7c9753cc25e4a97e5bd8e4b52
Martin Cimerman <cim.martin@gmail.com>
2015-02-05 23:02:04 +0100

    Examples initial commit

---
+++ Examples/C/continuous_signal_generation_external_trigger_pulse.c
@@ -0,0 +1,31 @@
+/* Red Pitaya external trigger pulse generation Example */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "rp.h"
+
+
+int main(int argc, char **argv){
+
+	/* Print error, if rp_Init() function failed */
+	if(rp_Init() != RP_OK){
+		fprintf(stderr, "Rp api init failed!\n");
+	}
+	
+	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
+	rp_GenFreq(RP_CH_1, 200);
+	rp_GenAmp(RP_CH_1, 1);
+
+	rp_GenBurstCount(RP_CH_1, 1);
+	/* Enable output channel */
+	rp_GenOutEnable(RP_CH_1);
+	rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
+	rp_GenTriggerSource(RP_CH_1, RP_GEN_TRIG_SRC_EXT_PE);
+
+	/* Release rp resources */
+	rp_Release();
+
+	return 0;
+}
