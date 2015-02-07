e40e5a3011558e7c59c5dc6bec0611924e33a840
Aljoša <aljosa.mrak@gmail.com>
2015-02-07 16:54:35 +0100

    Added examples in matlab for generate burst

---
+++ Examples/Matlab/generate_burst_gated.m
@@ -0,0 +1,32 @@
+%% Define Red Pitaya as TCP/IP object
+clc
+clear all
+close all
+IP= '192.168.178.56';           % Input IP of your Red Pitaya...
+port = 5000;                    % If you are using WiFi then IP is:              
+tcpipObj=tcpip(IP, port);       % 192.168.128.1
+tcpipObj.InputBufferSize = 16384*64;
+tcpipObj.OutputBufferSize = 16384*64;
+flushinput(tcpipObj)
+flushoutput(tcpipObj)
+
+%% Open connection with your Red Pitaya
+x=instrfind;
+fclose(x);
+fopen(tcpipObj);
+tcpipObj.Terminator = 'CR/LF';
+
+%%
+
+fprintf(tcpipObj,'GEN:RST')                     % Reset to default settings
+
+fprintf(tcpipObj,'SOUR1:FREQ:FIX 5');           % Set a slow frequency of output signal
+
+fprintf(tcpipObj,'SOUR1:TRIG:SOUR GATED');      % Enter gated burst mode. This sets delaut parameters to burst mode
+                                                % This must be called before setting burst parameters
+
+fprintf(tcpipObj,'SOUR1:BURS:NCYC 3');          % Set number of generated signals in one burst
+
+fprintf(tcpipObj,'OUTPUT1:STATE ON'); 
+
+fclose(tcpipObj);
