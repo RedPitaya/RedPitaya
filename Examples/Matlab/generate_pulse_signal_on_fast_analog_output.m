45b6abc0ca6002aa54efc100c7a50410555e329c
Aljoša <aljosa.mrak@gmail.com>
2015-02-10 00:28:03 +0100

    Examples adapted corrected API

--- Examples/Matlab/generate_pulse_signal_on_fast_analog_output.m
+++ Examples/Matlab/generate_pulse_signal_on_fast_analog_output.m
@@ -9,8 +9,8 @@ tcpipObj=tcpip(IP, port);       % 192.168.128.1
 fopen(tcpipObj);
 tcpipObj.Terminator = 'CR/LF';
 
-% Here you can use for loop and pause for generating train of pulses
-% for i=1:1:1000    
+% The example generate sine bursts every 0.5 seconds indefinety
+
 fprintf(tcpipObj,'SOUR1:FUNC SINE');       % Set function of output signal     
                                            % {sine, square, triangle,                       
                                            % sawu,sawd, pwm}
@@ -18,13 +18,14 @@ fprintf(tcpipObj,'SOUR1:FUNC SINE');       % Set function of output signal
 fprintf(tcpipObj,'SOUR1:FREQ:FIX 10');     % Set frequency of output signal                
 fprintf(tcpipObj,'SOUR1:VOLT 1');          % Set amplitude of output signal
 
-fprintf(tcpipObj,'SOUR1:BURS:NCYC 1');     % Set 1 pulses of sine wave 
-fprintf(tcpipObj,'SOUR1:TRIG:IMM');        % Set generator trigger to                               
-                                           % immediately 
-fprintf(tcpipObj,'OUTPUT1:STATE ON');      % Set output to ON 
 fprintf(tcpipObj,'SOUR1:BURS:STAT ON');    % Set burst mode to ON
-% pause(0.2)
-% end
+fprintf(tcpipObj,'SOUR1:BURS:NCYC 1');     % Set 1 pulses of sine wave
+fprintf(tcpipObj,'SOUR1:BURS:NOR INF');    % Infinity number of sine wave pulses
+fprintf(tcpipObj,'SOUR1:BURS:INT:PER 500000');     % Set time of burst period in microseconds = 5 * 1/Frequency * 1000000
+fprintf(tcpipObj,'SOUR1:TRIG:IMM');        % Set generator trigger to immediately
+
+fprintf(tcpipObj,'OUTPUT1:STATE ON');      % Set output to ON
+
 %% Close connection with Red Pitaya
 
 fclose(tcpipObj);
