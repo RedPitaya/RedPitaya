ffb17ee87cb4856338202863e373be6ecbd15bf1
Aljoša <aljosa.mrak@gmail.com>
2015-02-10 23:42:25 +0100

    Merge branch 'master' of https://github.com/RedPitaya/RedPitaya (reverted from commit d158bcc5aa3530ebaee8950532f9e3e116cc460d)

--- api-mockup/rpbase/src/gen_handler.c
+++ api-mockup/rpbase/src/gen_handler.c
@@ -236,14 +236,14 @@ int gen_BurstCount(rp_channel_t channel, int num) {
 }
 
 int gen_BurstRepetitions(rp_channel_t channel, int repetitions) {
-    if ((repetitions < BURST_REPETITIONS_MIN || repetitions > BURST_REPETITIONS_MAX) && repetitions != -1) {
+    if (repetitions < BURST_REPETITIONS_MIN || repetitions > BURST_REPETITIONS_MAX || repetitions == -1) {
         return RP_EOOR;
     }
-    CHECK_OUTPUT(chA_burstRepetition = repetitions,
-            chB_burstRepetition = repetitions)
     if (repetitions == -1) {
         repetitions = 0;
     }
+    CHECK_OUTPUT(chA_burstRepetition = repetitions,
+                 chB_burstRepetition = repetitions)
     return generate_setBurstRepetitions(channel, repetitions-1);
 }
 
