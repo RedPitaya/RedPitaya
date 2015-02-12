2f4d0424d27631f7e70d27186e6aa47116ff4aab
Aljoša <aljosa.mrak@gmail.com>
2015-02-12 12:06:38 +0100

    Merge from public repo (GitHub)

--- clients/matlab/generate.m
+++ clients/matlab/generate.m
@@ -12,8 +12,25 @@ function void=generate(ip_addr,chan,amp,freq,type)
 % freq   : Signal frequency in Hz [0.0 - 6.2e+07].
 % type:  : Signal type [sine, sqr, tri]
 
+% set up commands uppon running O.S.
+% added by David Zuliani 2014/01/23
+%  for MACOSX and LINUX O.S. you need to set up the ssh authorized key
+%  file  inside a .ssh directory of the RedPitaya O.S. Unfortunately at
+%  every RP reboot you must create again the .ssh directory and the
+%  authorized key file
+switch computer
+    case {'PCWIN','PCWIN64'}
+        CMD='plink -l root -pw root ';
+    case {'GLNXA64','MACI64'}
+        CMD='ssh -l root ';
+    otherwise
+        disp('UNKNOW O.S.');
+        return
+end
+
 %Calling generate utility on Red Pitaya
-command=['plink -l root -pw root ',ip_addr,' "/opt/bin/generate ',num2str(chan),' ',num2str(amp),' ',num2str(freq),' ',type,'"'];
+command=[CMD,ip_addr,' "/opt/bin/generate ',num2str(chan),' ',num2str(amp),' ',num2str(freq),' ',type,'"'];
+
 [c,data]=unix(command);
 
 void=0;
