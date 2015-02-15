6ada6ef7d79a49bdd8ee17ed844cfd9b4c9e7e92
Aljoša <aljosa.mrak@gmail.com>
2015-02-15 13:45:58 +0100

    Added generate getters API and SCPI

--- api-mockup/rpbase/src/rp.h
+++ api-mockup/rpbase/src/rp.h
@@ -411,6 +411,7 @@ int rp_ApinSetValueRaw(rp_apin_t pin, uint32_t value);
 int rp_ApinGetRange(rp_apin_t pin, float* min_val,  float* max_val);
 
 
+///@}
 /** @name Acquire
  */
 ///@{
@@ -751,6 +752,7 @@ int rp_AcqGetLatestDataV(rp_channel_t channel, uint32_t* size, float* buffer);
 int rp_AcqGetBufSize(uint32_t* size);
 
 
+///@}
 /** @name Health
  */
 ///@{
@@ -766,10 +768,10 @@ int rp_AcqGetBufSize(uint32_t* size);
 int rp_HealthGetValue(rp_health_t sensor, float* value);
 
 
+///@}
 /** @name Generate
 */
 ///@{
-
 
 
 /**
@@ -794,8 +796,17 @@ int rp_GenOutEnable(rp_channel_t channel);
 int rp_GenOutDisable(rp_channel_t channel);
 
 /**
+* Gets value true if channel is enabled otherwise return false.
+* @param channel Channel A or B.
+* @param value Pointer where value will be returned
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenOutIsEnabled(rp_channel_t channel, bool *value);
+
+/**
 * Sets channel signal peak to peak amplitude.
-* @param channel Channel A or B which we want to enable
+* @param channel Channel A or B for witch we want to set amplitude
 * @param amplitude Amplitude of the generated signal. From 0 to max value. Max amplitude is 1
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
@@ -803,54 +814,109 @@ int rp_GenOutDisable(rp_channel_t channel);
 int rp_GenAmp(rp_channel_t channel, float amplitude);
 
 /**
-* Sets DC offset of the signal. signal = signal + DC_offset
-* @param channel Channel A or B which we want to enable
-* @param offset DC offset of the generated signal. Max offset is 2
+* Gets channel signal peak to peak amplitude.
+* @param channel Channel A or B for witch we want to get amplitude.
+* @param amplitude Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetAmp(rp_channel_t channel, float *amplitude);
+
+/**
+* Sets DC offset of the signal. signal = signal + DC_offset.
+* @param channel Channel A or B for witch we want to set DC offset.
+* @param offset DC offset of the generated signal. Max offset is 2.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenOffset(rp_channel_t channel, float offset);
 
 /**
-* Sets channel signal frequency
-* @param channel Channel A or B which we want to enable
-* @param frequency Frequency of the generated signal in Hz
+* Gets DC offset of the signal.
+* @param channel Channel A or B for witch we want to get amplitude.
+* @param offset Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetOffset(rp_channel_t channel, float *offset);
+
+/**
+* Sets channel signal frequency.
+* @param channel Channel A or B for witch we want to set frequency.
+* @param frequency Frequency of the generated signal in Hz.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenFreq(rp_channel_t channel, float frequency);
 
 /**
-* Sets channel signal phase. This shifts the signal in time
-* @param channel Channel A or B which we want to enable
-* @param phase Phase in degrees of the generated signal. From 0 deg to 180 deg
+* Gets channel signal frequency.
+* @param channel Channel A or B for witch we want to get frequency.
+* @param frequency Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetFreq(rp_channel_t channel, float *frequency);
+
+/**
+* Sets channel signal phase. This shifts the signal in time.
+* @param channel Channel A or B for witch we want to set phase.
+* @param phase Phase in degrees of the generated signal. From 0 deg to 180 deg.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenPhase(rp_channel_t channel, float phase);
 
 /**
-* Sets channel signal form. This determines how the signal looks
-* @param channel Channel A or B which we want to enable
-* @param form Wave form of the generated signal [SINE, SQUARE, TRIANGLE, SAWTOOTH, PWM, DC, ARBITRARY]
+* Gets channel signal phase.
+* @param channel Channel A or B for witch we want to get phase.
+* @param phase Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetPhase(rp_channel_t channel, float *phase);
+
+/**
+* Sets channel signal waveform. This determines how the signal looks.
+* @param channel Channel A or B for witch we want to set waveform type.
+* @param form Wave form of the generated signal [SINE, SQUARE, TRIANGLE, SAWTOOTH, PWM, DC, ARBITRARY].
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenWaveform(rp_channel_t channel, rp_waveform_t type);
 
 /**
-* Sets user defined wave form
-* @param channel Channel A or B which we want to enable
-* @param waveform Use defined wave form, where min is -1V an max is 1V
-* @param length Length of waveform
+* Gets channel signal waveform.
+* @param channel Channel A or B for witch we want to get waveform.
+* @param type Pointer where value will be returned.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
-int rp_GenArbWaveform(rp_channel_t channel, float* waveform, uint32_t length);
+int rp_GenGetWaveform(rp_channel_t channel, rp_waveform_t *type);
 
 /**
-* Sets duty cycle of PWM signal
-* @param channel Channel A or B which we want to enable
+* Sets user defined waveform.
+* @param channel Channel A or B for witch we want to set waveform.
+* @param waveform Use defined wave form, where min is -1V an max is 1V.
+* @param length Length of waveform.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenArbWaveform(rp_channel_t channel, float *waveform, uint32_t length);
+
+/**
+* Gets user defined waveform.
+* @param channel Channel A or B for witch we want to get waveform.
+* @param waveform Pointer where waveform will be returned.
+* @param length Pointer where waveform length will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetArbWaveform(rp_channel_t channel, float *waveform, uint32_t *length);
+
+/**
+* Sets duty cycle of PWM signal.
+* @param channel Channel A or B for witch we want to set duty cycle.
 * @param ratio Ratio betwen the time when signal in HIGH vs the time when signal is LOW.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
@@ -858,63 +924,115 @@ int rp_GenArbWaveform(rp_channel_t channel, float* waveform, uint32_t length);
 int rp_GenDutyCycle(rp_channel_t channel, float ratio);
 
 /**
-* Sets geenration mode
-* @param channel Channel A or B which we want to enable
-* @param mode Type of signal generation (CONTINUOUS, BURST, STREAM)
+* Gets duty cycle of PWM signal.
+* @param channel Channel A or B for witch we want to get duty cycle.
+* @param ratio Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetDutyCycle(rp_channel_t channel, float *ratio);
+
+/**
+* Sets generation mode.
+* @param channel Channel A or B for witch we want to set generation mode.
+* @param mode Type of signal generation (CONTINUOUS, BURST, STREAM).
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenMode(rp_channel_t channel, rp_gen_mode_t mode);
 
 /**
+* Gets generation mode.
+* @param channel Channel A or B for witch we want to get generation mode.
+* @param mode Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetMode(rp_channel_t channel, rp_gen_mode_t *mode);
+
+/**
 * Sets number of generated waveforms in a burst.
-* @param channel Channel A or B which we want to enable
+* @param channel Channel A or B for witch we want to set number of generated waveforms in a burst.
 * @param num Number of generated waveforms. If -1 a continuous signal will be generated.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenBurstCount(rp_channel_t channel, int num);
 
+/**
+* Gets number of generated waveforms in a burst.
+* @param channel Channel A or B for witch we want to get number of generated waveforms in a burst.
+* @param num Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetBurstCount(rp_channel_t channel, int *num);
 
 /**
 * Sets number of burst repetitions. This determines how many bursts will be generated.
-* @param channel Channel A or B which we want to enable
+* @param channel Channel A or B for witch we want to set number of burst repetitions.
 * @param repetitions Number of generated bursts. If -1, infinite bursts will be generated.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenBurstRepetitions(rp_channel_t channel, int repetitions);
 
+/**
+* Gets number of burst repetitions.
+* @param channel Channel A or B for witch we want to get number of burst repetitions.
+* @param repetitions Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetBurstRepetitions(rp_channel_t channel, int *repetitions);
 
 /**
 * Sets the time/period of one burst in micro seconds. Period must be equal or greater then the time of one burst.
 * If it is greater than the difference will be the delay between two consequential bursts.
-* @param channel Channel A or B which we want to enable
-* @param period Time in micro seconds
+* @param channel Channel A or B for witch we want to set burst period.
+* @param period Time in micro seconds.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenBurstPeriod(rp_channel_t channel, uint32_t period);
 
 /**
-* Sets trigger source
-* @param channel Channel A or B which we want to enable
-* @param src Trigger source (INTERNAL, EXTERNAL)
+* Gets the period of one burst in micro seconds.
+* @param channel Channel A or B for witch we want to get burst period.
+* @param period Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetBurstPeriod(rp_channel_t channel, uint32_t *period);
+
+/**
+* Sets trigger source.
+* @param channel Channel A or B for witch we want to set trigger source.
+* @param src Trigger source (INTERNAL, EXTERNAL_PE, EXTERNAL_NE, GATED_BURST).
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenTriggerSource(rp_channel_t channel, rp_trig_src_t src);
 
 /**
-* Sets Trigger for specified channel/channels
-* @param channel Channel A or B which we want to enable
-* @param mask Mask determines channel: 1->ch1, 2->ch2, 3->ch1&ch2
+* Gets trigger source.
+* @param channel Channel A or B for witch we want to get burst period.
+* @param src Pointer where value will be returned.
+* @return If the function is successful, the return value is RP_OK.
+* If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
+*/
+int rp_GenGetTriggerSource(rp_channel_t channel, rp_trig_src_t *src);
+
+/**
+* Sets Trigger for specified channel/channels.
+* @param mask Mask determines channel: 1->ch1, 2->ch2, 3->ch1&ch2.
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
 int rp_GenTrigger(int mask);
 
 
+///@}
 /** @name I2C
 */
 ///@{
@@ -927,7 +1045,7 @@ int rp_GenTrigger(int mask);
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
-int rp_I2cRead(int addr, char* data, int length);
+int rp_I2cRead(int addr, char *data, int length);
 
 
 /**
@@ -938,7 +1056,7 @@ int rp_I2cRead(int addr, char* data, int length);
 * @return If the function is successful, the return value is RP_OK.
 * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
 */
-int rp_I2cWrite(int addr, char* data, int length);
+int rp_I2cWrite(int addr, char *data, int length);
 
 ///@}
 
