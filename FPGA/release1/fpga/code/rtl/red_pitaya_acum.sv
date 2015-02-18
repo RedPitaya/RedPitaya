a774901ecdd771df665549ffcca6fcd8bb1f8360
Iztok Jeras <iztok.jeras@gmail.com>
2015-02-18 01:37:27 +0100

    FPGA-Vivado: avoiding synthesis issues, probably Vivado bug

--- FPGA/release1/fpga/code/rtl/red_pitaya_acum.sv
+++ FPGA/release1/fpga/code/rtl/red_pitaya_acum.sv
@@ -5,43 +5,41 @@
 
 module red_pitaya_acum #(
   // input stream parameters
-  int unsigned IDW = 14,          // input data width
-  int unsigned IDN = 1,           // input data number (paralel processing of multiple samples)
+  parameter IDW = 14,          // input data width
   // accumulation parameters
-  int unsigned ACW = 32,          // accumulation counter width
-  int unsigned AMS = 2**14,       // accumulation memory size
-  int unsigned AAW = $clog2(AMS), // accumulation address width
+  parameter ACW = 32,          // accumulation counter width
+  parameter AMS = 2**14,       // accumulation memory size
+  parameter AAW = $clog2(AMS), // accumulation address width
   // output stream parameters
-  int unsigned ODW = IDW+ACW,     // output data width
-  int unsigned ODN = IDN          // output data number
+  parameter ODW = IDW+ACW      // output data width
 )(
   // system signals
-  input  logic                     clk,  // clock
-  input  logic                     rst,  // reset (synchronous for now)
-  input  logic                     clr,  // clear (synchronous)
+  input  logic           clk,  // clock
+  input  logic           rst,  // reset (synchronous for now)
+  input  logic           clr,  // clear (synchronous)
   // configuration
-  input  logic           [ACW-1:0] cfg_cnt,  // accumulation count
+  input  logic [ACW-1:0] cfg_cnt,  // accumulation count
   // control
-  input  logic                     ctl_run,  // accumulation running status
+  input  logic           ctl_run,  // accumulation running status
   // status
-  input  logic                     sts_end,  // end of sequence
-  output logic           [ACW-1:0] sts_cnt,  // accumulation count
+  input  logic           sts_end,  // end of sequence
+  output logic [ACW-1:0] sts_cnt,  // accumulation count
   // input data stream
-//input  logic                     sti_tkeep ,
-  input  logic                     sti_tlast ,
-  input  logic [IDN-1:0] [IDW-1:0] sti_tdata ,
-  input  logic                     sti_tvalid,
-  output logic                     sti_tready,
+//input  logic           sti_tkeep ,
+  input  logic           sti_tlast ,
+  input  logic [IDW-1:0] sti_tdata ,
+  input  logic           sti_tvalid,
+  output logic           sti_tready,
   // output data stream
-//output logic                     sto_tkeep ,
-  output logic                     sto_tlast ,
-  output logic [ODN-1:0] [ODW-1:0] sto_tdata ,
-  output logic                     sto_tvalid,
-  input  logic                     sto_tready,
+//output logic           sto_tkeep ,
+  output logic           sto_tlast ,
+  output logic [ODW-1:0] sto_tdata ,
+  output logic           sto_tvalid,
+  input  logic           sto_tready,
   // memmory interface (read only)
-  input  logic                     bus_ren,
-  input  logic           [AAW-1:0] bus_adr,
-  output logic [ODN-1:0] [ODW-1:0] bus_rdt
+  input  logic           bus_ren,
+  input  logic [AAW-1:0] bus_adr,
+  output logic [ODW-1:0] bus_rdt
 );
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -58,18 +56,18 @@ logic           acu_end;  // counter reached the end
 logic           acu_beg;  // counter at the beginning
 
 // buffered data/last
-logic                     buf_tlast;
-logic [IDN-1:0] [IDW-1:0] buf_tdata;
+logic           buf_tlast;
+logic [IDW-1:0] buf_tdata;
 
 // memory access
-logic                     mem_vld;  // memory valid
-logic                     mem_end;  // memory size end
-logic           [AAW-1:0] mem_adr;  // address counter
-logic           [AAW-1:0] mem_nxt;  // address next
-logic [ODN-1:0] [ODW-1:0] mem [0:AMS/IDN-1];
-logic                     mem_wen, mem_ren;  // write/read enable
-logic           [AAW-1:0] mem_wad, mem_rad;  // write/read address
-logic [ODN-1:0] [ODW-1:0] mem_wdt, mem_rdt;  // write/read data
+logic           mem_vld;  // memory valid
+logic           mem_end;  // memory size end
+logic [AAW-1:0] mem_adr;  // address counter
+logic [AAW-1:0] mem_nxt;  // address next
+logic [ODW-1:0] mem [0:AMS-1];
+logic           mem_wen, mem_ren;  // write/read enable
+logic [AAW-1:0] mem_wad, mem_rad;  // write/read address
+logic [ODW-1:0] mem_wdt, mem_rdt;  // write/read data
 
 ////////////////////////////////////////////////////////////////////////////////
 // I/O stream
@@ -81,15 +79,15 @@ assign sto_trnsfr = sto_tvalid & sto_tready;
 
 // accumulation counter
 always_ff @ (posedge clk) //, posedge rst)
-if (rst)                            acu_cnt <=           ACW'(0);
+if (rst)                            acu_cnt <=           0;
 else begin
-  if (clr)                          acu_cnt <=           ACW'(0);
-  else if (sti_trnsfr & sti_tlast)  acu_cnt <= acu_end ? ACW'(0) : acu_cnt + ACW'(1);
+  if (clr)                          acu_cnt <=           0;
+  else if (sti_trnsfr & sti_tlast)  acu_cnt <= acu_end ? 0 : acu_cnt + 1;
 end
 
 assign sts_cnt = acu_cnt;
 assign acu_end = (acu_cnt == cfg_cnt);
-assign acu_beg = (acu_cnt == ACW'(0));
+assign acu_beg = (acu_cnt == 0);
 
 // input data/last delayed
 always_ff @ (posedge clk)
@@ -121,7 +119,8 @@ assign sts_end = sti_trnsfr & sti_tlast & acu_end;
 ////////////////////////////////////////////////////////////////////////////////
 
 // write data
-assign mem_wdt = ODW'($signed(buf_tdata)) + $signed(acu_beg ? ODW'(0) : mem_rdt);
+//assign mem_wdt = ODW'($signed(buf_tdata)) + $signed(acu_beg ? ODW'(0) : mem_rdt);
+assign mem_wdt = $signed(buf_tdata) + $signed(acu_beg ? 0 : mem_rdt);
 
 // read enable
 assign mem_vld = sti_trnsfr;
@@ -130,14 +129,14 @@ assign mem_end = sti_tlast;
 
 // address
 always_ff @ (posedge clk) //, posedge rst)
-if (rst)             mem_adr <=           AAW'(0);
+if (rst)             mem_adr <=           0;
 else begin
-  if (clr)           mem_adr <=           AAW'(0);
-  else if (mem_vld)  mem_adr <= mem_end ? AAW'(0) : mem_nxt;
+  if (clr)           mem_adr <=           0;
+  else if (mem_vld)  mem_adr <= mem_end ? 0 : mem_nxt;
 end
 
 // address next
-assign mem_nxt = mem_adr + AAW'(1);
+assign mem_nxt = mem_adr + 1;
 
 // write enable
 always_ff @ (posedge clk) //, posedge rst)
