f40d72e6ac49889631b6ed41807367c02bfd9f01
Iztok Jeras <iztok.jeras@gmail.com>
2015-03-19 12:50:25 +0100

    FPGA-RTL: removing CDC module, since all instances have been removed

--- FPGA/release1/fpga/syn/red_pitaya_fpga.tcl
+++ FPGA/release1/fpga/syn/red_pitaya_fpga.tcl
@@ -35,7 +35,6 @@ file mkdir $path_sdk
 read_verilog $path_rtl/axi_master.v
 read_verilog $path_rtl/axi_slave.v
 read_verilog $path_rtl/axi_wr_fifo.v
-read_verilog $path_rtl/bus_clk_bridge.v
 
 read_verilog $path_rtl/red_pitaya_acum.sv
 read_verilog $path_rtl/red_pitaya_ams.v
