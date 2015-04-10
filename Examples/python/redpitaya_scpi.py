0ffa3ba05a0065e8c552fc83eb6022a4bd1aed14
Iztok Jeras <iztok.jeras@gmail.com>
2015-04-10 16:57:36 +0200

    SCPI module in Python

--- Examples/python/redpitaya_scpi.py
+++ Examples/python/redpitaya_scpi.py
@@ -1,15 +1,20 @@
-
-__author__ = 'infused'
+"""SCPI access to Red Pitaya."""
 
 import socket
 
-class scpi (object):
+__author__ = "Luka Golinar, Iztok Jeras"
+__copyright__ = "Copyright 2015, Red Pitaya"
 
-    # Socket info
+class scpi (object):
+    """SCPI class used to access Red Pitaya over an IP network."""
+
     # SCPI delimiter
     delimiter = '\r\n'
 
     def __init__(self, host, timeout=None, port=5000):
+        """Initialize object and open IP connection.
+        Host IP should be a string in parentheses, like '192.168.1.100'.
+        """
         self.host    = host
         self.port    = port
         self.timeout = timeout
@@ -25,22 +30,37 @@ class scpi (object):
         except socket.error as e:
             print 'SCPI >> connect({:s}:{:d}) failed: {:s}'.format(host, port, e)
 
-    # Receive function
-    def recv(self, chunksize = 4096):
+    def recv_txt(self, chunksize = 4096):
+        """Receive text string and return it after removing the delimiter."""
         msg = ''
         while 1:
-            chunk = (self._socket.recv(chunksize)) # Receive chunk size of 2^n preferably
+            chunk = self._socket.recv(chunksize + len(self.delimiter)) # Receive chunk size of 2^n preferably
             msg += chunk
-            if(len(chunk) and chunk[-1] == '\n'):
+            if (len(chunk) and chunk[-2:] == self.delimiter):
                 break
+        return msg[:-2]
 
+    def recv_raw(self):
+        """Receive RAW binary data, parse the header, and return only the payload."""
+        # receive message header
+        msg = self._socket.recv(6)
+        # parse header to get the length of binary data (number of bytes)
+        num = int(msg)
+        # receive binary data
+        msg = self._socket.recv(num)
+        # return binary data
         return msg
 
-    def send(self, msg):
+    def send_txt(self, msg):
+        """Send text string ending and append delimiter."""
+        return self._socket.send(msg + "; " + self.delimiter)
+
+    def send_raw(self, msg):
+        """Send RAW binary data."""
         return self._socket.send(msg)
 
-
     def close(self):
+        """Close IP connection."""
         self.__del__()
 
     def __del__(self):
