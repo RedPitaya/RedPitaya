9aabc51a09045b49064aa67111c14a3a954e2de1
Iztok Jeras <iztok.jeras@gmail.com>
2015-04-13 16:43:57 +0200

    updated test for SCPI ACQ, specificly filter configuration issue

--- Test/api-scpi/test_scpi_acq.py
+++ Test/api-scpi/test_scpi_acq.py
@@ -4,32 +4,31 @@ __author__ = 'infused'
 from time import sleep
 from redpitaya_scpi import scpi
 
-rp_s = scpi ('192.168.178.36')
-
-wave_form = 'sine'
-freq = 5000
-ampl = 1
+rp_s = scpi ('192.168.178.73')
 
 #Generate signal
-rp_s.send_txt('SOUR1:FUNC '      + str(wave_form).upper())
-rp_s.send_txt('SOUR1:FREQ:FIX '  + str(freq)             )
-rp_s.send_txt('SOUR1:VOLT '      + str(ampl)             )
-rp_s.send_txt('OUTPUT1:STATE ON'                         )
+rp_s.gen (src=1, waveform='sine', freq=50000, ampl=1)
 
-buff = []
-buff_string = ''
-rp_s.send_txt('ACQ:DEC 8'       )
+rp_s.send_txt('ACQ:SOUR1:GAIN LV')
+rp_s.send_txt('ACQ:DEC 1'       )
 rp_s.send_txt('ACQ:TRIG:LEV 100')
 rp_s.send_txt('ACQ:START'       )
 rp_s.send_txt('ACQ:TRIG CH1_PE' )
 while 1:
     rp_s.send_txt('ACQ:TRIG:STAT?')
-    if rp_s.recv_txt(2) == 'TD':
+    if rp_s.recv_txt(4) == 'TD':
         break
 rp_s.send_txt('ACQ:SOUR1:DATA?')
 buff_string = rp_s.recv_txt()
 buff_string = buff_string.strip('{} ').split(',')
 buff = map(float, buff_string)
+
+# check trigger settings
+rp_s.send_txt('ACQ:TRIG:DLY?')
+print('ACQ:TRIG:DLY? = ' + rp_s.recv_txt())
+
+rp_s.send_txt('ACQ:SOUR1:GAIN?')
+print('ACQ:SOUR1:GAIN = ' + rp_s.recv_txt())
 
 import matplotlib.pyplot as plt
 plt.plot(buff)
