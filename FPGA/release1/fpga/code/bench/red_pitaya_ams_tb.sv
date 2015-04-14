7eebe42a19dd3a8d316eb7e32ab79f3438a9395b
Iztok Jeras <iztok.jeras@gmail.com>
2015-04-14 15:05:58 +0200

    FPGA-bench: renamed bench files for SystemVerilog

--- FPGA/release1/fpga/code/bench/red_pitaya_ams_tb.sv
+++ FPGA/release1/fpga/code/bench/red_pitaya_ams_tb.sv
@@ -12,6 +12,8 @@
  * for more details on the language used herein.
  */
 
+
+
 /**
  * GENERAL DESCRIPTION:
  *
@@ -22,111 +24,123 @@
  * 
  */
 
+
+
 `timescale 1ns / 1ps
 
-module red_pitaya_ams_tb #(
-  // time periods
-  realtime  TP = 8.0ns  // 125MHz
+module red_pitaya_ams_tb(
 );
 
-glbl glbl();
+reg              clk             ;
+reg              rstn            ;
 
-////////////////////////////////////////////////////////////////////////////////
+reg              sys_clk         ;
+reg              sys_rstn        ;
+wire  [ 32-1: 0] sys_addr        ;
+wire  [ 32-1: 0] sys_wdata       ;
+wire  [  4-1: 0] sys_sel         ;
+wire             sys_wen         ;
+wire             sys_ren         ;
+wire  [ 32-1: 0] sys_rdata       ;
+wire             sys_err         ;
+wire             sys_ack         ;
+
+
+sys_bus_model i_bus
+(
+  .sys_clk_i      (  sys_clk      ),
+  .sys_rstn_i     (  sys_rstn     ),
+  .sys_addr_o     (  sys_addr     ),
+  .sys_wdata_o    (  sys_wdata    ),
+  .sys_sel_o      (  sys_sel      ),
+  .sys_wen_o      (  sys_wen      ),
+  .sys_ren_o      (  sys_ren      ),
+  .sys_rdata_i    (  sys_rdata    ),
+  .sys_err_i      (  sys_err      ),
+  .sys_ack_i      (  sys_ack      ) 
+);
+
+
+
+red_pitaya_ams i_ams
+(
+  .clk_i           (  clk           ),  // clock
+  .rstn_i          (  rstn          ),  // reset - active low
+
+  .vinp_i          (     ),  // voltages p
+  .vinn_i          (     ),  // voltages n
+
+  .dac_a_o         (     ),
+  .dac_b_o         (     ),
+  .dac_c_o         (     ),
+  .dac_d_o         (     ),
+
+   // System bus
+  .sys_clk_i       (  sys_clk       ),  // clock
+  .sys_rstn_i      (  sys_rstn      ),  // reset - active low
+  .sys_addr_i      (  sys_addr      ),  // address
+  .sys_wdata_i     (  sys_wdata     ),  // write data
+  .sys_sel_i       (  sys_sel       ),  // write byte select
+  .sys_wen_i       (  sys_wen       ),  // write enable
+  .sys_ren_i       (  sys_ren       ),  // read enable
+  .sys_rdata_o     (  sys_rdata     ),  // read data
+  .sys_err_o       (  sys_err       ),  // error indicator
+  .sys_ack_o       (  sys_ack       )   // acknowledge signal
+);
+
+
+
+
+
+//---------------------------------------------------------------------------------
+//
 // signal generation
-////////////////////////////////////////////////////////////////////////////////
 
-logic            clk ;
-logic            rstn;
-
-// ADC clock
-initial        clk = 1'b0;
-always #(TP/2) clk = ~clk;
-
-// ADC reset
 initial begin
-  rstn = 1'b0;
-  repeat(4) @(posedge clk);
-  rstn = 1'b1;
+   sys_clk  <= 1'b0 ;
+   sys_rstn <= 1'b0 ;
+   repeat(10) @(posedge sys_clk);
+      sys_rstn <= 1'b1  ;
 end
 
-////////////////////////////////////////////////////////////////////////////////
-// test sequence
-////////////////////////////////////////////////////////////////////////////////
+always begin
+   #5  sys_clk <= !sys_clk ;
+end
 
-logic [ 32-1: 0] sys_addr ;
-logic [ 32-1: 0] sys_wdata;
-logic [  4-1: 0] sys_sel  ;
-logic            sys_wen  ;
-logic            sys_ren  ;
-logic [ 32-1: 0] sys_rdata;
-logic            sys_err  ;
-logic            sys_ack  ;
 
-logic [ 32-1: 0] rdata;
 
 initial begin
-   wait (rstn)
+   clk  <= 1'b0  ;
+   rstn <= 1'b0  ;
    repeat(10) @(posedge clk);
+      rstn <= 1'b1  ;
+end
 
-   bus.write(32'h20,32'h10000);  // test write
+always begin
+   #4  clk <= !clk ;
+end
+
+
+
+
+initial begin
+   wait (sys_rstn && rstn)
+   repeat(10) @(posedge sys_clk);
+
+   i_bus.bus_write(32'h20,32'h10000);  // test write
 
    repeat(20000) @(posedge clk);
 
-   bus.read(32'h30, rdata);
-   bus.read(32'h34, rdata);
-   bus.read(32'h38, rdata);
-   bus.read(32'h20, rdata);
-   bus.read(32'h24, rdata);
+   i_bus.bus_read(32'h30);
+   i_bus.bus_read(32'h34);
+   i_bus.bus_read(32'h38);
+   i_bus.bus_read(32'h20);
+   i_bus.bus_read(32'h24);
+
 end
 
-////////////////////////////////////////////////////////////////////////////////
-// module instances
-////////////////////////////////////////////////////////////////////////////////
 
-sys_bus_model bus (
-  // system signals
-  .clk          (clk      ),
-  .rstn         (rstn     ),
-  // bus protocol signals
-  .sys_addr     (sys_addr ),
-  .sys_wdata    (sys_wdata),
-  .sys_sel      (sys_sel  ),
-  .sys_wen      (sys_wen  ),
-  .sys_ren      (sys_ren  ),
-  .sys_rdata    (sys_rdata),
-  .sys_err      (sys_err  ),
-  .sys_ack      (sys_ack  ) 
-);
 
-red_pitaya_ams ams (
-  .clk_i        (clk ),
-  .rstn_i       (rstn),
 
-  .vinp_i       (   ),  // voltages p
-  .vinn_i       (   ),  // voltages n
 
-  .dac_a_o      (   ),
-  .dac_b_o      (   ),
-  .dac_c_o      (   ),
-  .dac_d_o      (   ),
-   // System bus
-  .sys_addr     (sys_addr ),
-  .sys_wdata    (sys_wdata),
-  .sys_sel      (sys_sel  ),
-  .sys_wen      (sys_wen  ),
-  .sys_ren      (sys_ren  ),
-  .sys_rdata    (sys_rdata),
-  .sys_err      (sys_err  ),
-  .sys_ack      (sys_ack  )
-);
-
-////////////////////////////////////////////////////////////////////////////////
-// waveforms
-////////////////////////////////////////////////////////////////////////////////
-
-initial begin
-  $dumpfile("red_pitaya_ams_tb.vcd");
-  $dumpvars(0, red_pitaya_ams_tb);
-end
-
-endmodule: red_pitaya_ams_tb
+endmodule
--- FPGA/release1/fpga/code/bench/red_pitaya_asg_tb.sv
+++ FPGA/release1/fpga/code/bench/red_pitaya_asg_tb.sv
@@ -12,6 +12,8 @@
  * for more details on the language used herein.
  */
 
+
+
 /**
  * GENERAL DESCRIPTION:
  *
@@ -21,185 +23,202 @@
  * 
  */
 
+
+
 `timescale 1ns / 1ps
 
-module red_pitaya_asg_tb #(
-  // time periods
-  realtime  TP = 8.0ns,  // 125MHz
-  // DUT configuration
-  int unsigned DAC_DW = 14, // ADC data width
-  int unsigned RSZ = 14  // RAM size is 2**RSZ
+module red_pitaya_asg_tb(
 );
 
-////////////////////////////////////////////////////////////////////////////////
-// ADC signal generation
-////////////////////////////////////////////////////////////////////////////////
 
 
-logic               clk ;
-logic               rstn;
+wire  [ 14-1: 0] dac_a           ;
+wire  [ 14-1: 0] dac_b           ;
+reg              dac_clk         ;
+reg              dac_rstn        ;
 
-logic [DAC_DW-1: 0] dac_a;
-logic [DAC_DW-1: 0] dac_b;
+reg              sys_clk         ;
+reg              sys_rstn        ;
+wire  [ 32-1: 0] sys_addr        ;
+wire  [ 32-1: 0] sys_wdata       ;
+wire  [  4-1: 0] sys_sel         ;
+wire             sys_wen         ;
+wire             sys_ren         ;
+wire  [ 32-1: 0] sys_rdata       ;
+wire             sys_err         ;
+wire             sys_ack         ;
 
-logic               trig;
+reg              trig            ;
 
-// DAC clock
-initial        clk = 1'b0;
-always #(TP/2) clk = ~clk;
 
-// DAC reset
+sys_bus_model i_bus
+(
+  .sys_clk_i      (  sys_clk      ),
+  .sys_rstn_i     (  sys_rstn     ),
+  .sys_addr_o     (  sys_addr     ),
+  .sys_wdata_o    (  sys_wdata    ),
+  .sys_sel_o      (  sys_sel      ),
+  .sys_wen_o      (  sys_wen      ),
+  .sys_ren_o      (  sys_ren      ),
+  .sys_rdata_i    (  sys_rdata    ),
+  .sys_err_i      (  sys_err      ),
+  .sys_ack_i      (  sys_ack      ) 
+);
+
+
+
+red_pitaya_asg i_asg
+(
+  // ADC
+  .dac_a_o         (  dac_a         ),  // CH 1
+  .dac_b_o         (  dac_b         ),  // CH 2
+  .dac_clk_i       (  dac_clk       ),  // clock
+  .dac_rstn_i      (  dac_rstn      ),  // reset - active low
+  .trig_a_i        (  trig          ),
+  .trig_b_i        (  trig          ),
+
+   // System bus
+  .sys_clk_i       (  sys_clk       ),  // clock
+  .sys_rstn_i      (  sys_rstn      ),  // reset - active low
+  .sys_addr_i      (  sys_addr      ),  // address
+  .sys_wdata_i     (  sys_wdata     ),  // write data
+  .sys_sel_i       (  sys_sel       ),  // write byte select
+  .sys_wen_i       (  sys_wen       ),  // write enable
+  .sys_ren_i       (  sys_ren       ),  // read enable
+  .sys_rdata_o     (  sys_rdata     ),  // read data
+  .sys_err_o       (  sys_err       ),  // error indicator
+  .sys_ack_o       (  sys_ack       )   // acknowledge signal
+);
+
+
+
+
+
+//---------------------------------------------------------------------------------
+//
+// signal generation
+
 initial begin
-  rstn = 1'b0;
-  repeat(4) @(posedge clk);
-  rstn = 1'b1;
+   sys_clk  <= 1'b0 ;
+   sys_rstn <= 1'b0 ;
+   repeat(10) @(posedge sys_clk);
+      sys_rstn <= 1'b1  ;
 end
-
-// ADC cycle counter
-int unsigned dac_cyc=0;
-always_ff @ (posedge clk)
-dac_cyc <= dac_cyc+1;
 
 always begin
-  trig <= 1'b0 ;
-  repeat(100000) @(posedge clk);
-  trig <= 1'b1 ;
-  repeat(1200) @(posedge clk);
-  trig <= 1'b0 ;
+   #5  sys_clk <= !sys_clk ;
 end
+
+
+
+initial begin
+   dac_clk  <= 1'b0  ;
+   dac_rstn <= 1'b0  ;
+   repeat(10) @(posedge dac_clk);
+      dac_rstn <= 1'b1  ;
+end
+
+always begin
+   #4  dac_clk <= !dac_clk ;
+end
+
+always begin
+          trig <= 1'b0 ;
+   repeat(100000) @(posedge dac_clk);
+      trig <= 1'b1 ;
+   repeat(1200) @(posedge dac_clk);
+      trig <= 1'b0 ;
+end
+
+
+
 
 reg [9-1: 0] ch0_set;
 reg [9-1: 0] ch1_set;
-
-////////////////////////////////////////////////////////////////////////////////
-// test sequence
-////////////////////////////////////////////////////////////////////////////////
-
-logic [ 32-1: 0] sys_addr ;
-logic [ 32-1: 0] sys_wdata;
-logic [  4-1: 0] sys_sel  ;
-logic            sys_wen  ;
-logic            sys_ren  ;
-logic [ 32-1: 0] sys_rdata;
-logic            sys_err  ;
-logic            sys_ack  ;
-
-logic        [ 32-1: 0] rdata;
-logic signed [ 32-1: 0] rdata_blk [];
+integer k;
 
 initial begin
-  wait (rstn)
-  repeat(4) @(posedge clk);
+   wait (sys_rstn && dac_rstn)
+   repeat(10) @(posedge sys_clk);
+      //CH0 DAC data
+      i_bus.bus_write(32'h10000, 32'd3     );  // write table
+      i_bus.bus_write(32'h10004, 32'd30    );  // write table
+      i_bus.bus_write(32'h10008, 32'd8000  );  // write table
+      i_bus.bus_write(32'h1000C,-32'd4     );  // write table
+      i_bus.bus_write(32'h10010,-32'd40    );  // write table
+      i_bus.bus_write(32'h10014,-32'd8000  );  // write table
+      i_bus.bus_write(32'h10018,-32'd2000  );  // write table
+      i_bus.bus_write(32'h1001c, 32'd250   );  // write table
 
-  //CH0 DAC data
-  bus.write(32'h10000, 32'd3     );  // write table
-  bus.write(32'h10004, 32'd30    );  // write table
-  bus.write(32'h10008, 32'd8000  );  // write table
-  bus.write(32'h1000C,-32'd4     );  // write table
-  bus.write(32'h10010,-32'd40    );  // write table
-  bus.write(32'h10014,-32'd8000  );  // write table
-  bus.write(32'h10018,-32'd2000  );  // write table
-  bus.write(32'h1001c, 32'd250   );  // write table
+      //CH0 DAC settings
+      i_bus.bus_write(32'h00004,{2'h0,-14'd500, 2'h0, 14'h2F00}  );  // DC offset, amplitude
+      i_bus.bus_write(32'h00008,{2'h0, 14'd7, 16'd0}             );  // table size
+      i_bus.bus_write(32'h0000C,{2'h0, 14'h1, 16'h0}             );  // reset offset
+      i_bus.bus_write(32'h00010,{2'h0, 14'h2, 16'h0}             );  // table step
+      i_bus.bus_write(32'h00018,{16'h0, 16'd0}                   );  // number of cycles
+      i_bus.bus_write(32'h0001C,{16'h0, 16'd0}                   );  // number of repetitions
+      i_bus.bus_write(32'h00020,{32'd3}                          );  // number of 1us delay between repetitions
 
-  //CH0 DAC settings
-  bus.write(32'h00004,{2'h0,-14'd500, 2'h0, 14'h2F00}  );  // DC offset, amplitude
-  bus.write(32'h00008,{2'h0, 14'd7, 16'd0}             );  // table size
-  bus.write(32'h0000C,{2'h0, 14'h1, 16'h0}             );  // reset offset
-  bus.write(32'h00010,{2'h0, 14'h2, 16'h0}             );  // table step
-  bus.write(32'h00018,{16'h0, 16'd0}                   );  // number of cycles
-  bus.write(32'h0001C,{16'h0, 16'd0}                   );  // number of repetitions
-  bus.write(32'h00020,{32'd3}                          );  // number of 1us delay between repetitions
+      ch0_set = {1'b0 ,1'b0, 1'b0, 1'b0, 1'b1,    1'b0, 3'h2} ;  // set_rgate, set_zero, set_rst, set_once(NA), set_wrap, 1'b0, trig_src
 
-  ch0_set = {1'b0 ,1'b0, 1'b0, 1'b0, 1'b1,    1'b0, 3'h2} ;  // set_rgate, set_zero, set_rst, set_once(NA), set_wrap, 1'b0, trig_src
 
-  //CH1 DAC data
-  for (int k=0; k<8000; k++) begin
-    bus.write(32'h20000 + (k*4), k   );  // write table
-  end
 
-  //CH1 DAC settings
-  bus.write(32'h00024,{2'h0, 14'd0, 2'h0, 14'h2000}    );  // DC offset, amplitude
-  bus.write(32'h00028,{2'h0, 14'd7999, 16'd0}          );  // table size
-  bus.write(32'h0002C,{2'h0, 14'h5, 16'h0}             );  // reset offset
-  bus.write(32'h00030,{2'h0, 14'h9, 16'h0}             );  // table step
-  bus.write(32'h00038,{16'h0, 16'd0}                   );  // number of cycles
-  bus.write(32'h0003C,{16'h0, 16'd5}                   );  // number of repetitions
-  bus.write(32'h00040,{32'd10}                         );  // number of 1us delay between repetitions
+      //CH1 DAC data
+    k = 0;
+    while (k<8000) begin
+      i_bus.bus_write(32'h20000 + (k*4), k   );  // write table
+      k= k + 1 ;
+    end
 
-  ch1_set = {1'b0, 1'b0, 1'b0, 1'b1, 1'b1,    1'b0, 3'h1} ;  // set_rgate, set_zero, set_rst, set_once(NA), set_wrap, 1'b0, trig_src
+      //CH1 DAC settings
+      i_bus.bus_write(32'h00024,{2'h0, 14'd0, 2'h0, 14'h2000}    );  // DC offset, amplitude
+      i_bus.bus_write(32'h00028,{2'h0, 14'd7999, 16'd0}          );  // table size
+      i_bus.bus_write(32'h0002C,{2'h0, 14'h5, 16'h0}             );  // reset offset
+      i_bus.bus_write(32'h00030,{2'h0, 14'h9, 16'h0}             );  // table step
+      i_bus.bus_write(32'h00038,{16'h0, 16'd0}                   );  // number of cycles
+      i_bus.bus_write(32'h0003C,{16'h0, 16'd5}                   );  // number of repetitions
+      i_bus.bus_write(32'h00040,{32'd10}                         );  // number of 1us delay between repetitions
 
-  bus.write(32'h00000,{8'h0, ch1_set,  8'h0, ch0_set}  ); // write configuration
+      ch1_set = {1'b0, 1'b0, 1'b0, 1'b1, 1'b1,    1'b0, 3'h1} ;  // set_rgate, set_zero, set_rst, set_once(NA), set_wrap, 1'b0, trig_src
 
-  repeat(2000) @(posedge clk);
+      i_bus.bus_write(32'h00000,{8'h0, ch1_set,  8'h0, ch0_set}  ); // write configuration
 
-  ch1_set = {1'b0, 1'b0, 1'b1, 1'b1,    1'b0, 3'h1} ;  // set_a_zero, set_a_rst, set_a_once, set_a_wrap, 1'b0, trig_src
 
-  bus.write(32'h00000,{7'h0, ch1_set,  7'h0, ch0_set}  ); // write configuration
+   repeat(2000) @(posedge dac_clk);
 
-  repeat(200) @(posedge clk);
 
-  // CH1 table data readback
-  for (int k=0; k<80; k++) begin
-    bus.read(32'h20000 + (k*4), rdata);  // read table
-  end
+      ch1_set = {1'b0, 1'b0, 1'b1, 1'b1,    1'b0, 3'h1} ;  // set_a_zero, set_a_rst, set_a_once, set_a_wrap, 1'b0, trig_src
 
-  // CH1 table data readback
-  for (int k=0; k<20; k++) begin
-    bus.read(32'h00014, rdata);  // read read pointer
-    bus.read(32'h00034, rdata);  // read read pointer
-    repeat(1737) @(posedge clk);
-  end
+      i_bus.bus_write(32'h00000,{7'h0, ch1_set,  7'h0, ch0_set}  ); // write configuration
 
-  repeat(20000) @(posedge clk);
+
+
+   repeat(200) @(posedge dac_clk);
+
+      //CH1 table data readback
+    k = 0;
+    while (k<80) begin
+      i_bus.bus_read(32'h20000 + (k*4));  // read table
+      k= k + 1 ;
+    end
+
+
+      //CH1 table data readback
+    k = 0;
+    while (k<20) begin
+      i_bus.bus_read(32'h00014);  // read read pointer
+      i_bus.bus_read(32'h00034);  // read read pointer
+      k= k + 1 ;
+      repeat(1737) @(posedge dac_clk);
+    end
+
+
+
+   repeat(20000) @(posedge dac_clk);
+
 end
 
-////////////////////////////////////////////////////////////////////////////////
-// module instances
-////////////////////////////////////////////////////////////////////////////////
 
-sys_bus_model bus (
-  // system signals
-  .clk          (clk      ),
-  .rstn         (rstn     ),
-  // bus protocol signals
-  .sys_addr     (sys_addr ),
-  .sys_wdata    (sys_wdata),
-  .sys_sel      (sys_sel  ),
-  .sys_wen      (sys_wen  ),
-  .sys_ren      (sys_ren  ),
-  .sys_rdata    (sys_rdata),
-  .sys_err      (sys_err  ),
-  .sys_ack      (sys_ack  ) 
-);
 
-red_pitaya_asg asg (
-  // DAC
-  .dac_clk_i      (clk      ),
-  .dac_rstn_i     (rstn     ),
-  .dac_a_o        (dac_a    ),  // CH 1
-  .dac_b_o        (dac_b    ),  // CH 2
-  // trigger
-  .trig_a_i       (trig     ),
-  .trig_b_i       (trig     ),
-  .trig_out_o     (         ),
-   // System bus
-  .sys_addr       (sys_addr ),
-  .sys_wdata      (sys_wdata),
-  .sys_sel        (sys_sel  ),
-  .sys_wen        (sys_wen  ),
-  .sys_ren        (sys_ren  ),
-  .sys_rdata      (sys_rdata),
-  .sys_err        (sys_err  ),
-  .sys_ack        (sys_ack  )
-);
 
-////////////////////////////////////////////////////////////////////////////////
-// waveforms
-////////////////////////////////////////////////////////////////////////////////
-
-initial begin
-  $dumpfile("red_pitaya_asg_tb.vcd");
-  $dumpvars(0, red_pitaya_asg_tb);
-end
-
-endmodule: red_pitaya_asg_tb
+endmodule
