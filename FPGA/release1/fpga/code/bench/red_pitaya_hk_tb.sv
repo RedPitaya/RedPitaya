8edf31e05ea5f3ff09ca72e14d323f99ad71d57a
Iztok Jeras <iztok.jeras@gmail.com>
2015-04-15 17:18:49 +0200

    FPGA-bench: updated HK bench

--- FPGA/release1/fpga/code/bench/red_pitaya_hk_tb.sv
+++ FPGA/release1/fpga/code/bench/red_pitaya_hk_tb.sv
@@ -12,8 +12,6 @@
  * for more details on the language used herein.
  */
 
-
-
 /**
  * GENERAL DESCRIPTION:
  *
@@ -24,137 +22,131 @@
  * 
  */
 
-
-
-
 `timescale 1ns / 1ps
 
-module red_pitaya_hk_tb(
+module red_pitaya_hk_tb #(
+  // time periods
+  realtime  TP = 8.0ns  // 125MHz
 );
 
+glbl glbl ();
 
-
-wire  [  8-1: 0] led             ;
-wire  [  8-1: 0] exp_dat         ;
-PULLDOWN i_p0 (.O(exp_dat[0])) ;
-PULLDOWN i_p1 (.O(exp_dat[1])) ;
-PULLDOWN i_p2 (.O(exp_dat[2])) ;
-PULLDOWN i_p3 (.O(exp_dat[3])) ;
-PULLDOWN i_p4 (.O(exp_dat[4])) ;
-PULLDOWN i_p5 (.O(exp_dat[5])) ;
-PULLDOWN i_p6 (.O(exp_dat[6])) ;
-PULLDOWN i_p7 (.O(exp_dat[7])) ;
-reg              clk             ;
-reg              rstn            ;
-
-reg              sys_clk         ;
-reg              sys_rstn        ;
-wire  [ 32-1: 0] sys_addr        ;
-wire  [ 32-1: 0] sys_wdata       ;
-wire  [  4-1: 0] sys_sel         ;
-wire             sys_wen         ;
-wire             sys_ren         ;
-wire  [ 32-1: 0] sys_rdata       ;
-wire             sys_err         ;
-wire             sys_ack         ;
-
-
-
-sys_bus_model i_bus
-(
-  .sys_clk_i      (  sys_clk      ),
-  .sys_rstn_i     (  sys_rstn     ),
-  .sys_addr_o     (  sys_addr     ),
-  .sys_wdata_o    (  sys_wdata    ),
-  .sys_sel_o      (  sys_sel      ),
-  .sys_wen_o      (  sys_wen      ),
-  .sys_ren_o      (  sys_ren      ),
-  .sys_rdata_i    (  sys_rdata    ),
-  .sys_err_i      (  sys_err      ),
-  .sys_ack_i      (  sys_ack      ) 
-);
-
-
-
-red_pitaya_hk i_hk
-(
-
-  .clk_i           (  clk           ),  // clock
-  .rstn_i          (  rstn          ),  // reset - active low
-
-   // LED
-  .led_o           (  led           ),  // LED output
-   // Expansion connector
-  .exp_p_io        (  exp_dat       ),
-  .exp_n_io        (  exp_dat       ),
-
-
-   // System bus
-  .sys_clk_i       (  sys_clk       ),  // clock
-  .sys_rstn_i      (  sys_rstn      ),  // reset - active low
-  .sys_addr_i      (  sys_addr      ),  // address
-  .sys_wdata_i     (  sys_wdata     ),  // write data
-  .sys_sel_i       (  sys_sel       ),  // write byte select
-  .sys_wen_i       (  sys_wen       ),  // write enable
-  .sys_ren_i       (  sys_ren       ),  // read enable
-  .sys_rdata_o     (  sys_rdata     ),  // read data
-  .sys_err_o       (  sys_err       ),  // error indicator
-  .sys_ack_o       (  sys_ack       )   // acknowledge signal
-);
-
-
-
-
-
-//---------------------------------------------------------------------------------
-//
+////////////////////////////////////////////////////////////////////////////////
 // signal generation
+////////////////////////////////////////////////////////////////////////////////
+
+logic              clk ;
+logic              rstn;
+
+// ADC clock
+initial        clk = 1'b0;
+always #(TP/2) clk = ~clk;
+
+// ADC reset
+initial begin
+  rstn = 1'b0;
+  repeat(4) @(posedge clk);
+  rstn = 1'b1;
+end
+
+// tristate signals
+wire  [  8-1: 0] led    ;
+wire  [  8-1: 0] exp_p_dat;
+wire  [  8-1: 0] exp_n_dat;
+//               output       input        enable
+logic [  8-1: 0] exp_p_dat_o, exp_p_dat_i, exp_p_dat_e;
+logic [  8-1: 0] exp_n_dat_o, exp_n_dat_i, exp_n_dat_e;
+
+pullup   pullup_exp_dat   [8-1:0] (exp_p_dat);
+pulldown pulldown_exp_dat [8-1:0] (exp_n_dat);
+
+////////////////////////////////////////////////////////////////////////////////
+// test sequence
+////////////////////////////////////////////////////////////////////////////////
+
+logic [ 32-1: 0] sys_addr ;
+logic [ 32-1: 0] sys_wdata;
+logic [  4-1: 0] sys_sel  ;
+logic            sys_wen  ;
+logic            sys_ren  ;
+logic [ 32-1: 0] sys_rdata;
+logic            sys_err  ;
+logic            sys_ack  ;
+
+logic [ 32-1: 0] rdata;
 
 initial begin
-   sys_clk  <= 1'b0 ;
-   sys_rstn <= 1'b0 ;
-   repeat(10) @(posedge sys_clk);
-      sys_rstn <= 1'b1  ;
+  wait (rstn)
+  repeat(600) @(posedge clk);
+    // LED & Expansion
+    bus.write(32'h30, 32'h00000003); // LED
+    bus.write(32'h10, 32'h00000033); // direction
+    bus.write(32'h18, 32'h000000FF); // value
+
+    bus.read(32'h4 , rdata);
+    bus.read(32'h8 , rdata);
+    bus.read(32'h24, rdata);
+    bus.read(32'h30, rdata);
+  repeat(20000) @(posedge clk);
+  $finish();
 end
 
-always begin
-   #5  sys_clk <= !sys_clk ;
-end
+////////////////////////////////////////////////////////////////////////////////
+// module instances
+////////////////////////////////////////////////////////////////////////////////
 
+sys_bus_model bus (
+  // system signals
+  .clk          (clk      ),
+  .rstn         (rstn     ),
+  // bus protocol signals
+  .sys_addr     (sys_addr ),
+  .sys_wdata    (sys_wdata),
+  .sys_sel      (sys_sel  ),
+  .sys_wen      (sys_wen  ),
+  .sys_ren      (sys_ren  ),
+  .sys_rdata    (sys_rdata),
+  .sys_err      (sys_err  ),
+  .sys_ack      (sys_ack  ) 
+);
 
+red_pitaya_hk hk (
+  .clk_i          (clk      ),
+  .rstn_i         (rstn     ),
+   // LED
+  .led_o          (led      ),
+  // Expansion connector
+  .exp_p_dat_i    (exp_p_dat_i),
+  .exp_p_dat_o    (exp_p_dat_o),
+  .exp_p_dir_o    (exp_p_dat_e),
+  .exp_n_dat_i    (exp_n_dat_i),
+  .exp_n_dat_o    (exp_n_dat_o),
+  .exp_n_dir_o    (exp_n_dat_e),
+   // System bus
+  .sys_addr       (sys_addr ),
+  .sys_wdata      (sys_wdata),
+  .sys_sel        (sys_sel  ),
+  .sys_wen        (sys_wen  ),
+  .sys_ren        (sys_ren  ),
+  .sys_rdata      (sys_rdata),
+  .sys_err        (sys_err  ),
+  .sys_ack        (sys_ack  )
+);
+
+//                               tristate   output       enable
+bufif0 bufif0_exp_p_dat [8-1:0] (exp_p_dat, exp_p_dat_o, exp_p_dat_e);
+bufif0 bufif0_exp_n_dat [8-1:0] (exp_n_dat, exp_n_dat_o, exp_n_dat_e);
+//     input         tristate
+assign exp_p_dat_i = exp_p_dat;
+assign exp_n_dat_i = exp_n_dat;
+
+////////////////////////////////////////////////////////////////////////////////
+// waveforms
+////////////////////////////////////////////////////////////////////////////////
 
 initial begin
-   clk  <= 1'b0  ;
-   rstn <= 1'b0  ;
-   repeat(10) @(posedge clk);
-      rstn <= 1'b1  ;
+  $dumpfile("red_pitaya_hk_tb.vcd");
+  $dumpvars(0, red_pitaya_hk_tb);
 end
 
-always begin
-   #4  clk <= !clk ;
-end
-
-
-
-
-
-initial begin
-   wait (sys_rstn && rstn)
-   repeat(600) @(posedge sys_clk);
-      // LED & Expansion
-      i_bus.bus_write(32'h30, 32'h3     ); // LED
-      i_bus.bus_write(32'h10, 32'h33    ); // direction
-      i_bus.bus_write(32'h18, 32'hFF    ); // value
-
-      i_bus.bus_read(32'h4    );
-      i_bus.bus_read(32'h8    );
-      i_bus.bus_read(32'h24   );
-      i_bus.bus_read(32'h30   );
-   repeat(20000) @(posedge clk);
-
-end
-
-
-
-
-endmodule
+endmodule: red_pitaya_hk_tb
