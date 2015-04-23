3d10ec573166baf1dfc4fe444011ea7d9ac65763
Iztok Jeras <iztok.jeras@gmail.com>
2015-04-23 14:54:54 +0200

    FPGA-bench: updates

--- FPGA/release1/fpga/code/bench/red_pitaya_scope_tb.sv
+++ FPGA/release1/fpga/code/bench/red_pitaya_scope_tb.sv
@@ -1,5 +1,4 @@
 /**
- * $Id: red_pitaya_scope_tb.v 961 2014-01-21 11:40:39Z matej.oblak $
  *
  * @brief Red Pitaya oscilloscope testbench.
  *
@@ -33,14 +32,12 @@
 // 1.6. external from ASG
 // 2. filter/decimator configurations
 // 2. ...
-// 3. accumulation
 
 `timescale 1ns / 1ps
 
 module red_pitaya_scope_tb #(
   // time periods
-  realtime  TP_ADC = 8.0ns,  // 125MHz
-  realtime  TP_SYS = 9.8ns,  // 102MHz
+  realtime  TP = 8.0ns,  // 125MHz
   // DUT configuration
   int unsigned ADC_DW = 14, // ADC data width
   int unsigned RSZ = 14  // RAM size is 2**RSZ
@@ -59,49 +56,30 @@ function [ADC_DW-1:0] saw_b (input int unsigned cyc);
   saw_b = -2**(ADC_DW-1) + ADC_DW'(cyc*5);
 endfunction: saw_b
 
-logic              adc_clk ;
-logic              adc_rstn;
+logic              clk ;
+logic              rstn;
 
-logic [ADC_DW-1:0] adc_a    ;
-logic [ADC_DW-1:0] adc_b    ;
-logic              adc_b_dir;
+logic [ADC_DW-1:0] adc_a;
+logic [ADC_DW-1:0] adc_b;
 
-logic [ADC_DW-1:0] saw_adc_a    ;
-logic [ADC_DW-1:0] saw_adc_b    ;
-
-assign saw_adc_a = saw_a(adc_cyc);
-assign saw_adc_b = saw_b(adc_cyc);
+assign adc_a = saw_a(adc_cyc);
+assign adc_b = saw_b(adc_cyc);
 
 // ADC clock
-initial            adc_clk = 1'b0;
-always #(TP_ADC/2) adc_clk = ~adc_clk;
+initial        clk = 1'b0;
+always #(TP/2) clk = ~clk;
 
 // ADC reset
 initial begin
-  adc_rstn = 1'b0;
-  repeat(4) @(posedge adc_clk);
-  adc_rstn = 1'b1;
+  rstn = 1'b0;
+  repeat(4) @(posedge clk);
+  rstn = 1'b1;
 end
 
 // ADC cycle counter
 int unsigned adc_cyc=0;
-always_ff @ (posedge adc_clk)
+always_ff @ (posedge clk)
 adc_cyc <= adc_cyc+1;
-
-initial begin
-  adc_a = 0;
-  adc_b = -2**(ADC_DW-1);
-  adc_b_dir = 1'b1;
-end
-
-always @(posedge adc_clk)
-adc_a <= adc_a + 14'h17;
-
-always @(posedge adc_clk) begin
-  if      ($signed(adc_b) > $signed( 14'd8000)) adc_b_dir <= 1'b0;
-  else if ($signed(adc_b) < $signed(-14'd8000)) adc_b_dir <= 1'b1;
-  adc_b <= adc_b_dir ? adc_b + 14'h5 : adc_b - 14'h5;
-end
 
 ////////////////////////////////////////////////////////////////////////////////
 // test sequence
@@ -109,8 +87,6 @@ end
 
 logic            trig_ext ;
 
-logic            sys_clk  ;
-logic            sys_rstn ;
 logic [ 32-1: 0] sys_addr ;
 logic [ 32-1: 0] sys_wdata;
 logic [  4-1: 0] sys_sel  ;
@@ -120,103 +96,141 @@ logic [ 32-1: 0] sys_rdata;
 logic            sys_err  ;
 logic            sys_ack  ;
 
-logic [ 32-1: 0] rdata;
+logic        [ 32-1: 0] rdata;
+logic signed [ 32-1: 0] rdata_blk [];
+bit   signed [ 32-1: 0] rdata_ref [];
+int unsigned            rdata_trg [$];
+int unsigned            blk_size;
 
-// system clock & reset
-initial begin
-  sys_rstn = 1'b0;
-  repeat(4) @(posedge sys_clk);
-  sys_rstn = 1'b1;
-end
-
-initial          sys_clk = 1'b0;
-always #(TP_SYS) sys_clk = ~sys_clk;
+task read_blk (
+  input int          adr,
+  input int unsigned len
+);
+  rdata_blk = new [len];
+  for (int unsigned i=0; i<len; i++) begin
+    bus.read(adr+4*i, rdata_blk[i]);
+  end 
+endtask: read_blk
 
 // State machine programming
 initial begin
-   // external trigger
-   trig_ext = 1'b0;
+  // external trigger
+  trig_ext = 1'b0;
 
-   wait (sys_rstn && adc_rstn)
-   repeat(10) @(posedge sys_clk);
+  blk_size = 20;
 
-   bus.bus_write(32'h08,-32'd0000 );  // A trigger treshold  (trigger at treshold     0 where signal range is -8192:+8191)
-   bus.bus_write(32'h0C,-32'd7000 );  // B trigger treshold  (trigger at treshold -7000 where signal range is -8192:+8191)
-   bus.bus_write(32'h10, 32'd10000);  // after trigger delay (the buffer contains 2**14=16384 locations, 6384 before and 10000 after trigger)
-   bus.bus_write(32'h14, 32'd8    );  // data decimation     (data is decimated by a factor of 8)
-   bus.bus_write(32'h20, 32'd20   );  // A hysteresis
-   bus.bus_write(32'h24, 32'd200  );  // B hysteresis
+  wait (rstn)
+  repeat(10) @(posedge clk);
 
-   // software trigger
-   bus.bus_write(32'h00, 32'h1    );  // start aquisition (ARM, start writing data into memory
-   repeat(800) @(posedge sys_clk);
-   bus.bus_write(32'h04, 32'h1    );  // do SW trigger
-   repeat(100) @(posedge sys_clk);
-   bus.bus_write(32'h00, 32'h2    );  // reset before aquisition ends
-   repeat(100) @(posedge sys_clk);
+  // configure filter and decimator
+  bus.write(32'h14, 32'd1    );  // data decimation     (data is decimated by a factor of 8)
 
-   // A ch rising edge trigger
-   bus.bus_write(32'h04, 32'h2    );  // configure trigger mode
-   repeat(800) @(posedge sys_clk);
-   bus.bus_write(32'h00, 32'h2    );  // reset before aquisition ends
-   repeat(100) @(posedge sys_clk);
+  // configure internal trigger level
+  bus.write(32'h08,-32'd0000 );  // A trigger treshold  (trigger at treshold     0 where signal range is -8192:+8191)
+  bus.write(32'h0C,-32'd7000 );  // B trigger treshold  (trigger at treshold -7000 where signal range is -8192:+8191)
+  bus.write(32'h20, 32'd20   );  // A hysteresis
+  bus.write(32'h24, 32'd200  );  // B hysteresis
 
-//   repeat(800) @(posedge sys_clk);
-//   bus.bus_write(32'h00,32'h1     );  // start aquisition
-//   repeat(100000) @(posedge sys_clk);
-//   bus.bus_write(32'h04,32'h5     );  // do trigger
+  // configure AXI memory limits
+  bus.write(32'h50, 32'd0100);  // A start address
+  bus.write(32'h54, 32'd0200);  // A stop  address
+  bus.write(32'h70, 32'd0100);  // B start address
+  bus.write(32'h74, 32'd0200);  // B stop  address
+
+  bus.write(32'h10, blk_size );  // after trigger delay (the buffer contains 2**14=16384 locations, 16384-10 before and 32 after trigger)
+
+//  // software trigger
+//  bus.write(32'h00, 32'h1    );  // start aquisition (ARM, start writing data into memory
+//  repeat(200) @(posedge clk);
+//  bus.write(32'h04, 32'h1    );  // do SW trigger
+//  repeat(200) @(posedge clk);
+//  bus.write(32'h00, 32'h2    );  // reset before aquisition ends
+//  repeat(200) @(posedge clk);
 //
-//   repeat(20000) @(posedge adc_clk);
-//   repeat(1) @(posedge sys_clk);
-//   bus.bus_read(32'h10000, rdata);  // read value from memory
-//   bus.bus_read(32'h10004, rdata);  // read value from memory
-//   bus.bus_read(32'h20000, rdata);  // read value from memory
-//   bus.bus_read(32'h20004, rdata);  // read value from memory
+//  // A ch rising edge trigger
+//  bus.write(32'h04, 32'h2    );  // configure trigger mode
+//  repeat(200) @(posedge clk);
+//  bus.write(32'h00, 32'h2    );  // reset before aquisition ends
+//  repeat(200) @(posedge clk);
 //
-   $finish ();
+//  // external rising edge trigger
+//  bus.write(32'h90, 32'h0    );  // set debouncer length to zero
+//  bus.write(32'h04, 32'h6    );  // configure trigger mode
+//  bus.write(32'h00, 32'h1    );  // start aquisition (ARM, start writing data into memory
+//  repeat(200) @(posedge clk);
+//  trig_ext = 1'b1;
+//  repeat(200) @(posedge clk);
+//  trig_ext = 1'b0;
+
+  fork
+    // provide external trigger
+    begin: scope_trg
+      // short trigger pulse
+      repeat(20) @(posedge clk);       trig_ext = 1'b1;
+      repeat( 1) @(posedge clk);       trig_ext = 1'b0;
+    end
+    // pool accumulation run status
+    begin: scope_run
+      // pooling loop
+      do begin
+        bus.read(32'h94, rdata);  // read value from memory
+        repeat(20) @(posedge clk);
+      end while (rdata & 2);
+      repeat(20) @(posedge clk);
+      // readout
+      read_blk (32'h30000, blk_size);
+      // check
+      $display ("trigger positions: %p", rdata_trg);
+      $display ("data reference: %p", rdata_ref);
+      $display ("data read     : %p", rdata_blk);
+      if (rdata_ref == rdata_blk) $display ("SUCCESS");
+      else                        $display ("FAILURE");
+    end
+  join
+
+  repeat(100) @(posedge clk);
+  $finish ();
 end
 
 ////////////////////////////////////////////////////////////////////////////////
-// clock & reset
+// module instances
 ////////////////////////////////////////////////////////////////////////////////
 
 sys_bus_model bus (
   // system signals
-  .sys_clk_i      (sys_clk  ),
-  .sys_rstn_i     (sys_rstn ),
+  .clk          (clk      ),
+  .rstn         (rstn     ),
   // bus protocol signals
-  .sys_addr_o     (sys_addr ),
-  .sys_wdata_o    (sys_wdata),
-  .sys_sel_o      (sys_sel  ),
-  .sys_wen_o      (sys_wen  ),
-  .sys_ren_o      (sys_ren  ),
-  .sys_rdata_i    (sys_rdata),
-  .sys_err_i      (sys_err  ),
-  .sys_ack_i      (sys_ack  ) 
+  .sys_addr     (sys_addr ),
+  .sys_wdata    (sys_wdata),
+  .sys_sel      (sys_sel  ),
+  .sys_wen      (sys_wen  ),
+  .sys_ren      (sys_ren  ),
+  .sys_rdata    (sys_rdata),
+  .sys_err      (sys_err  ),
+  .sys_ack      (sys_ack  ) 
 );
 
 red_pitaya_scope #(
   .RSZ (RSZ)
 ) scope (
   // ADC
-  .adc_clk_i      (adc_clk  ),  // clock
-  .adc_rstn_i     (adc_rstn ),  // reset - active low
+  .adc_clk_i      (clk      ),
+  .adc_rstn_i     (rstn     ),
   .adc_a_i        (adc_a    ),  // CH 1
   .adc_b_i        (adc_b    ),  // CH 2
   // trigger sources
   .trig_ext_i     (trig_ext ),  // external trigger
   .trig_asg_i     (trig_ext ),  // ASG trigger
    // System bus
-  .sys_clk_i      (sys_clk  ),  // clock
-  .sys_rstn_i     (sys_rstn ),  // reset - active low
-  .sys_addr_i     (sys_addr ),  // address
-  .sys_wdata_i    (sys_wdata),  // write data
-  .sys_sel_i      (sys_sel  ),  // write byte select
-  .sys_wen_i      (sys_wen  ),  // write enable
-  .sys_ren_i      (sys_ren  ),  // read enable
-  .sys_rdata_o    (sys_rdata),  // read data
-  .sys_err_o      (sys_err  ),  // error indicator
-  .sys_ack_o      (sys_ack  )   // acknowledge signal
+  .sys_addr       (sys_addr ),
+  .sys_wdata      (sys_wdata),
+  .sys_sel        (sys_sel  ),
+  .sys_wen        (sys_wen  ),
+  .sys_ren        (sys_ren  ),
+  .sys_rdata      (sys_rdata),
+  .sys_err        (sys_err  ),
+  .sys_ack        (sys_ack  )
 );
 
 ////////////////////////////////////////////////////////////////////////////////
