697c7367d9589367ecc112cff592296ef6b54583
Aljoša <aljosa.mrak@gmail.com>
2015-05-11 00:18:50 +0200

    Added resize and math support

--- Applications/scope/src/new_main.h
+++ Applications/scope/src/new_main.h
@@ -1,5 +1,11 @@
 #pragma once
 
-#define CH_SIGNAL_SIZE		1024
+#define CH_SIGNAL_SIZE_DEFAULT		1024
+
+#define IF_VALUE_CHANGED(X, ACTION) \
+if (X.Value() != X.NewValue()) { \
+    if (ACTION) { \
+        X.Update(); } }
+
 
 float getMeasureValue(int measure);
