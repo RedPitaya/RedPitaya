655a17aca2ba4dbd545bf948a6cce2f5b67e782c
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-05-11 12:18:56 +0200

    moving environment settings to

--- patches/zynq_red_pitaya.h
+++ patches/zynq_red_pitaya.h
@@ -38,4 +38,26 @@
 #define CONFIG_ENV_SIZE		1024 /* Total Size of Environment Sector */
 #define CONFIG_ENV_OFFSET	(2048*3) /* WP area starts at last 1/4 of 8k eeprom */
 
+#define CONFIG_EXTRA_ENV_SETTINGS      \
+	"ethaddr=00:26:33:14:50:00\0"   \
+	"hw_rev=B\0"    \
+	"prod_date=12/22/13\0"  \
+	"serial=00000000\0"     \
+	"kernel_image=uImage\0"    \
+	"ramdisk_image=uramdisk.image.gz\0"     \
+	"devicetree_image=devicetree.dtb\0"     \
+	"bitstream_image=system.bit.bin\0"      \
+	"loadbit_addr=0x100000\0"       \
+	"kernel_size=0x500000\0"        \
+	"devicetree_size=0x20000\0"     \
+	"ramdisk_size=0x5E0000\0"       \
+	"fdt_high=0x20000000\0" \
+	"initrd_high=0x20000000\0"      \
+	"sdboot=echo Copying Linux from SD to RAM... && " \
+		"mmcinfo && " \
+		"fatload mmc 0 0x3000000 ${kernel_image} && " \
+		"fatload mmc 0 0x2A00000 ${devicetree_image} && " \
+		"fatload mmc 0 0x2000000 ${ramdisk_image} && " \
+		"bootm 0x3000000 0x2000000 0x2A00000\0"
+
 #endif /* __CONFIG_ZYNQ_RED_PITAYA_H */
