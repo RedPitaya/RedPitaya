01972a1fc84d74bde5e3e58aae4b954628d11711
Aljoša <aljosa.mrak@gmail.com>
2015-05-14 22:23:48 +0200

    fix generate problem

--- api-mockup/rpbase/src/common.h
+++ api-mockup/rpbase/src/common.h
@@ -80,6 +80,6 @@ uint32_t cmn_CalibFullScaleFromVoltage(float voltageScale);
 int32_t cmn_CalibCnts(uint32_t field_len, uint32_t cnts, int calib_dc_off);
 float cmn_CnvCalibCntToV(uint32_t field_len, int32_t calib_cnts, float adc_max_v, float calibScale, float user_dc_off);
 float cmn_CnvCntToV(uint32_t field_len, uint32_t cnts, float adc_max_v, uint32_t calibScale, int calib_dc_off, float user_dc_off);
-uint32_t cmn_CnvVToCnt(uint32_t field_len, float voltage, float adc_max_v, uint32_t calib_scale, int calib_dc_off, float user_dc_off);
+uint32_t cmn_CnvVToCnt(uint32_t field_len, float voltage, float adc_max_v, bool calibFS_LO, uint32_t calib_scale, int calib_dc_off, float user_dc_off);
 
 #endif /* COMMON_H_ */
