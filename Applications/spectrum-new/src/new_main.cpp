c382422af486d320aceb8e02118c7a0dcb0857e6
a.solovev@integraspurces.com <a.solovev@integraspurces.com>
2015-05-29 18:24:26 +0600

    spectrum peak power/freq parameter

--- Applications/spectrum-new/src/new_main.cpp
+++ Applications/spectrum-new/src/new_main.cpp
@@ -8,7 +8,7 @@ CFloatSignal ch1("ch1", CH_SIGNAL_SIZE, 0.0f);
 CFloatSignal ch2("ch2", CH_SIGNAL_SIZE, 0.0f);
 
 CIntParameter freq_range("freq_range", CBaseParameter::RW, 0, 1, 0, 5); // IN
-CIntParameter freq_unit("freq_unit", CBaseParameter::RW, 0, 0, 0, 2);
+CIntParameter freq_unit("freq_unit", CBaseParameter::RWSA, 0, 0, 0, 2);
 
 //power 
 CFloatParameter in1Scale("SPEC_CH1_SCALE", CBaseParameter::RW, 10, 0, 0, 1000);
@@ -19,13 +19,13 @@ CFloatParameter xmin("xmin", CBaseParameter::RW, 0, 0, -1e6, +1e6);
 CFloatParameter xmax("xmax", CBaseParameter::RW, 1000, 0, -1e6, +1e6);
 
 
-CFloatParameter peak1_freq("peak1_freq", CBaseParameter::RO, 0, 0, 0, +1e6);
-CFloatParameter peak1_power("peak1_power", CBaseParameter::RO, 0, 0, -10000000, +10000000);
-CFloatParameter peak1_unit("peak1_unit", CBaseParameter::RO, 0, 1, 0, 2);
+CFloatParameter peak1_freq("peak1_freq", CBaseParameter::ROSA, 0, 0, 0, +1e6);
+CFloatParameter peak1_power("peak1_power", CBaseParameter::ROSA, 0, 0, -10000000, +10000000);
+CFloatParameter peak1_unit("peak1_unit", CBaseParameter::ROSA, 0, 1, 0, 2);
 
-CFloatParameter peak2_freq("peak2_freq", CBaseParameter::RO, 0, 0, 0, 1e6);
-CFloatParameter peak2_power("peak2_power", CBaseParameter::RO, 0, 0, -1e7, 1e7);
-CFloatParameter peak2_unit("peak2_unit", CBaseParameter::RO, 0, 0, 0, 2);
+CFloatParameter peak2_freq("peak2_freq", CBaseParameter::ROSA, 0, 0, 0, 1e6);
+CFloatParameter peak2_power("peak2_power", CBaseParameter::ROSA, 0, 0, -1e7, 1e7);
+CFloatParameter peak2_unit("peak2_unit", CBaseParameter::ROSA, 0, 0, 0, 2);
 
 CIntParameter w_idx("w_idx", CBaseParameter::RO, 0, 0, 0, 1000);
 CBooleanParameter en_avg_at_dec("en_avg_at_dec", CBaseParameter::RO, true, 0);
@@ -40,6 +40,7 @@ CBooleanParameter in2Show("CH2_SHOW", CBaseParameter::RW, false, 0);
 
 void UpdateParams(void)
 {
+/*
 	static bool set_period = false;
 	if (!set_period)
 	{
@@ -47,6 +48,7 @@ void UpdateParams(void)
 		CDataManager::GetInstance()->SetSignalInterval(INTERVAL);
 		set_period = true;
 	}
+*/
 
 	int ret = rpApp_SpecGetJpgIdx(&w_idx.Value());
 	ret = rpApp_SpecGetPeakPower(RP_CH_1, &peak1_power.Value());
@@ -71,7 +73,7 @@ void UpdateSignals(void)
 				ch1[i] = data[i];
 		}
 	}
-/*
+
 	if (in2Show.Value())
 	{
 		ret = rpApp_SpecGetViewData(RP_CH_2, data, CH_SIGNAL_SIZE);
@@ -81,7 +83,6 @@ void UpdateSignals(void)
 				ch2[i] = data[i];
 		}
 	}
-*/
 }
 
 void OnNewParams(void)
@@ -93,6 +94,17 @@ void OnNewParams(void)
 		run = true;
 	}
 
-	//freq_range.Update();
-	//rpApp_SpecSetUnit(freq_range.Value()); // set unit by range
+	if (xmax.IsNewValue())
+	{
+		xmax.Update();
+		//fprintf(stderr, "xmax = %f\n", xmax.Value());
+	}
+}
+
+extern "C" void SpecIntervalInit()
+{
+	CDataManager::GetInstance()->SetParamInterval(INTERVAL);
+	CDataManager::GetInstance()->SetSignalInterval(INTERVAL);
+
+	fprintf(stderr, "Interval Init\n");
 }
