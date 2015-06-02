93744c9b864c262cda876a59bcd30935b6099e59
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-06-03 00:48:24 +0200

    Debian: updated scripts after first run

--- OS/debian/image.sh
+++ OS/debian/image.sh
@@ -1,19 +1,13 @@
-script=$1
-image=$2
+#image=$1
+image=debian.img
 
-size=512
-
-if [ $# -eq 3 ]
-then
-  size=$3
-fi
+#size=$2
+size=1024
 
 dd if=/dev/zero of=$image bs=1M count=$size
 
 device=`losetup -f`
 
-losetup $device $image
-
-sh $script $device
-
-losetup -d $device
+losetup                $device $image
+sh OS/debian/debian.sh $device
+losetup -d             $device
