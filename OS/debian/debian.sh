7216dcbfdb7feebf15cf620870ede8348495f3cf
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-06-03 21:26:27 +0200

    Debian: image script updated after first test

--- OS/debian/debian.sh
+++ OS/debian/debian.sh
@@ -37,30 +37,32 @@ debootstrap --foreign --arch $arch $distro $root_dir $mirror
 cp /etc/resolv.conf         $root_dir/etc/
 cp /usr/bin/qemu-arm-static $root_dir/usr/bin/
 
+chroot $root_dir <<- EOF_CHROOT
+export LANG=C
+/debootstrap/debootstrap --second-stage
+EOF_CHROOT
+
 # copy overlay
 mkdir -p                    $root_dir/var/log/nginx
 cp patches/fw_env.config    $root_dir/etc/
 cp -r OS/debian/overlay/*   $root_dir/
 
 chroot $root_dir <<- EOF_CHROOT
-export LANG=C
-
-/debootstrap/debootstrap --second-stage
-
 # TODO sees sytemd is not running without /proc/cmdline or something
 #hostnamectl set-hostname redpitaya
+#timedatectl set-timezone Europe/Ljubljana
+#localectl set-locale LANG="en_US.UTF-8"
+
 echo redpitaya > /etc/hostname
-timedatectl set-timezone Europe/Ljubljana
+echo Europe/Ljubljana > /etc/timezone
 
 apt-get update
 apt-get -y upgrade
 apt-get -y install locales
 
-sed -i "/^# en_US.UTF-8 UTF-8$/s/^# //" etc/locale.gen
+sed -i "/^# en_US.UTF-8 UTF-8$/s/^# //" /etc/locale.gen
 locale-gen
-#update-locale LANG=en_US.UTF-8
-
-localectl set-locale LANG="en_US.UTF-8"
+update-locale LANG=en_US.UTF-8
 
 dpkg-reconfigure --frontend=noninteractive tzdata
 
@@ -69,11 +71,14 @@ apt-get -y install openssh-server ca-certificates ntp ntpdate fake-hwclock \
   iw firmware-realtek firmware-ralink build-essential libluajit-5.1 lua-cjson \
   unzip ifplugd
 
-sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' etc/ssh/sshd_config
+# TODO: /etc/default/hostapd should be copied only here
+# TODO: /etc/dhcp/dhcpd.conf should be copied only here
+
+sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 systemctl enable nginx
 
-sed -i '/^#net.ipv4.ip_forward=1$/s/^#//' etc/sysctl.conf
+sed -i '/^#net.ipv4.ip_forward=1$/s/^#//' /etc/sysctl.conf
 
 apt-get clean
 
