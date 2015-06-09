e6ccc56b4dead8869ba4582b9e1453e3af490d39
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-06-09 20:34:40 +0200

    added missing file

---
+++ OS/debian/redpitaya.sh
@@ -0,0 +1,28 @@
+################################################################################
+# TODO: copyright notice and authors should be listed here
+################################################################################
+
+# enable chroot access with native execution
+cp /etc/resolv.conf         $root_dir/etc/
+cp /usr/bin/qemu-arm-static $root_dir/usr/bin/
+
+# Copy files to the boot file system
+unzip ecosystem*.zip -d $boot_dir
+
+# Nginx service
+install -v -m 664 -o root -d                                                     $root_dir/var/log/redpitaya_nginx
+install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_nginx.service $root_dir/etc/systemd/system/redpitaya_nginx.service
+install -v -m 664 -o root -D $OVERLAY/etc/sysconfig/redpitaya                    $root_dir/etc/sysconfig/redpitaya
+
+chroot $root_dir <<- EOF_CHROOT
+systemctl enable redpitaya_nginx
+
+apt-get -y install libluajit-5.1 lua-cjson unzip
+EOF_CHROOT
+
+# final operations and cleanup
+install -v -m 664 -o root -D $OVERLAY/etc/profile.d/redpitaya.sh $root_dir/etc/profile.d/redpitaya.sh
+
+# disable chroot access with native execution
+rm $root_dir/etc/resolv.conf
+rm $root_dir/usr/bin/qemu-arm-static
