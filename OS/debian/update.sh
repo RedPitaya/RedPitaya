b25a488b0adc6cd677a7d8721b27e35dc5ec06d8
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-06-15 20:29:22 +0200

    Debian: script update

--- OS/debian/update.sh
+++ OS/debian/update.sh
@@ -4,22 +4,10 @@
 ################################################################################
 
 ################################################################################
-# prepating and mounting image
+# mounting image
 ################################################################################
 
-# current time and date are used to create the image name
-DATE=`date +"%H_%M_%S__%d_%b_%Y"`
-
-# the ABI (armel/armhf) provided by the compiler is used to create the image name
-ARCH=armel
-
-# default image size if 3GB, which is appropriate for all 4BG SD cards
-SIZE=3500
-
-#IMAGE=$1
-IMAGE=debian_${ARCH}__${DATE}.img
-
-dd if=/dev/zero of=$IMAGE bs=1M count=$SIZE
+IMAGE=$1
 
 DEVICE=`losetup -f`
 
@@ -28,17 +16,8 @@ losetup $DEVICE $IMAGE
 BOOT_DIR=boot
 ROOT_DIR=root
 
-# Create partitions
-parted -s $DEVICE mklabel msdos
-parted -s $DEVICE mkpart primary fat16   4MB 128MB
-parted -s $DEVICE mkpart primary ext4  128MB 100%
-
 BOOT_DEV=/dev/`lsblk -lno NAME $DEVICE | sed '2!d'`
 ROOT_DEV=/dev/`lsblk -lno NAME $DEVICE | sed '3!d'`
-
-# Create file systems
-mkfs.vfat -v    $BOOT_DEV
-mkfs.ext4 -F -j $ROOT_DEV
 
 # Mount file systems
 mkdir -p $BOOT_DIR $ROOT_DIR
@@ -51,9 +30,14 @@ mount $ROOT_DEV $ROOT_DIR
 
 OVERLAY=OS/debian/overlay
 
-source OS/debian/debian.sh 
-source OS/debian/redpitaya.sh
-#source OS/debian/wyliodrin.sh
+#source OS/debian/debian.sh 
+
+# enable chroot access with native execution
+cp /etc/resolv.conf         $ROOT_DIR/etc/
+cp /usr/bin/qemu-arm-static $ROOT_DIR/usr/bin/
+
+#source OS/debian/redpitaya.sh
+source OS/debian/wyliodrin.sh
 
 # disable chroot access with native execution
 rm $ROOT_DIR/etc/resolv.conf
@@ -67,4 +51,4 @@ rm $ROOT_DIR/usr/bin/qemu-arm-static
 umount $BOOT_DIR $ROOT_DIR
 rmdir $BOOT_DIR $ROOT_DIR
 
-losetup -d             $DEVICE
+losetup -d $DEVICE
