1a61798605ea91b60f36743a63526f71d94424cd
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-06-18 14:18:06 +0200

    Listed authors and license

--- OS/debian/debian.sh
+++ OS/debian/debian.sh
@@ -1,5 +1,9 @@
 ################################################################################
-# TODO: copyright notice and authors should be listed here
+# Authors:
+# - Pavel Demin <pavel.demin@uclouvain.be>
+# - Iztok Jeras <iztok.jeras@redpitaya.com>
+# License:
+# https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/COPYING
 ################################################################################
 
 # Install Debian base system to the root file system
