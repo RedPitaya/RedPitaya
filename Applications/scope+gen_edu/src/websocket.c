38629da40badaf00b8f7ac60d7ff393ebeac2dde
luka.golinar <luka.golinar@redpitaya.com>
2015-07-13 15:58:01 +0200

    Changed from controller.so to controllerhf.so. Added fpga.conf in scope-new and spectrum-new

--- Applications/scope+gen_edu/src/websocket.c
+++ Applications/scope+gen_edu/src/websocket.c
@@ -1,5 +1,5 @@
 /* The Websockets have moved to nginx RP module in order to remove the
- * dependency of the end-user application (controller.so) on the
+ * dependency of the end-user application (controllerhf.so) on the
  * libwebsocket library.
  *
  * This file is to be removed when the all relevant developers are
