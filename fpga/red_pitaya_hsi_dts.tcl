e186b390fe1847f06116a8982c4eddc2a16241e1
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-07-23 14:19:22 +0200

    FPGA: corrected kernel version

--- fpga/red_pitaya_hsi_dts.tcl
+++ fpga/red_pitaya_hsi_dts.tcl
@@ -9,15 +9,15 @@ set path_sdk sdk
 
 #set boot_args {console=ttyPS0,115200n8 root=/dev/ram rw initrd=0x00800000,16M earlyprintk mtdparts=physmap-flash.0:512K(nor-fsbl),512K(nor-u-boot),5M(nor-linux),9M(nor-user),1M(nor-scratch),-(nor-rootfs)}
 
+open_hw_design $path_sdk/red_pitaya.sysdef
+
 set_repo_path ../tmp/device-tree-xlnx-xilinx-v2015.2.01/
 
-open_hw_design $path_sdk/red_pitaya.sysdef
 create_sw_design device-tree -os device_tree -proc ps7_cortexa9_0
 
-set_property CONFIG.kernel_version {2015.1} [get_os]
+set_property CONFIG.kernel_version {2015.2} [get_os]
 #set_property CONFIG.bootargs $boot_args [get_os]
 
-#generate_bsp -dir $path_sdk/dts # TODO, check if this is more appropriate
 generate_target -dir $path_sdk/dts
 
 exit
