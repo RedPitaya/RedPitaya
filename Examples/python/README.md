fe15fe3354ce5163b8bfbe97395f6c9e3e2a0b0f
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-08-04 13:56:44 +0200

    SCPI: moved some duplicated code into redpitaya_scpi.py library

--- Examples/python/README.md
+++ Examples/python/README.md
@@ -1,5 +1,7 @@
-#Defined functions are each a mirror example from
-# MathLab - std functions such as __init__, rp_rcv,...
+Python applications should be executed with the device IP address in the command line.
+```bash
+$ ./
+```
 
-#This example is written and completely supported by Python 2.7
-#TODO:Export to python 3.4
+This example is written and completely supported by Python 2.7
+TODO:Export to python 3.4
--- Examples/python/acq_buff.py
+++ Examples/python/acq_buff.py
@@ -1,76 +1,25 @@
+#!/usr/bin/python
 
-__author__ = 'infused'
-
-import socket
+import redpitaya_scpi as scpi
 import time
+import sys
 
-class SCPI(object):
+__author__ = "Luka Golinar, Iztok Jeras"
+__copyright__ = "Copyright 2015, Red Pitaya"
 
-    #Socket info
-    HOST = None
-    PORT = 5000
-    TIME_OUT = None
-    BUFF_SIZE = 16384 * 32
-    #SCPI delimiter
-    delimiter = '\r\n'
+rp_s = scpi.scpi(sys.argv[1])
 
-    def __init__(self, host, time_out, port=PORT):
-        self.HOST = host
-        self.TIME_OUT = time_out
-
-        try:
-            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            if self.TIME_OUT is not None:
-                self._socket.settimeout(self.TIME_OUT)
-
-            self._socket.connect((self.HOST, self.PORT))
-
-        except socket.error as e:
-            print 'SCPI >> connect({:s}:{:d}) failed: {:s}'.format(host, port, e)
-
-    #Receive function
-    def rp_rcv(self, buff_size):
-        chunks = []
-        bytes_read = 0
-        while 1:
-            chunk = (self._socket.recv(buff_size)) #Receive chunk size of 2^n preferably
-            chunks.append(chunk)
-            bytes_read += len(chunk)
-            if(len(chunk) and chunk[-1] == '\n'):
-                break
-        return b''.join(chunks)
-
-    def rp_write(self, message):
-        return self._socket.send(message)
-
-    #RP help functions
-
-    def choose_state(self, led, state):
-        return 'DIG:PIN LED' + str(led) + ', ' + str(state) + self.delimiter
-
-    def close(self):
-        self.__del__()
-
-    def __del__(self):
-        if self._socket is not None:
-            self._socket.close()
-        self._socket = None
-
-
-rp_s = SCPI('IP', None)
 wave_form = 'sine'
 freq = 1000
 ampl = 1
 
 buff = []
 buff_string = ''
-rp_s.rp_write('ACQ:START' + rp_s.delimiter)
-rp_s.rp_write('ACQ:TRIG NOW' + rp_s.delimiter)
-rp_s.rp_write('ACQ:TRIG:STAT?' + rp_s.delimiter)
-rp_s.rp_rcv(2)
-rp_s.rp_write('ACQ:SOUR1:DATA?' + rp_s.delimiter)
-buff_string = rp_s.rp_rcv(rp_s.BUFF_SIZE)
+rp_s.tx_txt('ACQ:START')
+rp_s.tx_txt('ACQ:TRIG NOW')
+rp_s.tx_txt('ACQ:TRIG:STAT?')
+rp_s.rx_txt()
+rp_s.tx_txt('ACQ:SOUR1:DATA?')
+buff_string = rp_s.rx_txt()
 buff_string = buff_string.strip('{}\n\r').replace("  ", "").split(',')
 buff = map(float, buff_string)
-
