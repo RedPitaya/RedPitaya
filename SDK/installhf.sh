f35893de40aae2b053c6840ab4017d3ce2d3148f
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-08-14 00:19:55 +0200

    Install script now fully working. Sub

--- SDK/installhf.sh
+++ SDK/installhf.sh
@@ -10,7 +10,7 @@ GREET_MSG="\nEXECUTING RED PITAYA COMPILE SCRIPT..."
 LINARO_HF_DL="https://releases.linaro.org/14.11/components/toolchain/binaries/arm-linux-gnueabihf/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf.tar.xz"
 
 LINARO_HF_TAR=gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf.tar.xz
-EXEC_PATH="/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin"
+EXEC_PATH="/opt/linaro/bin"
 
 #Installing dependency tools
 echo -e $GREET_MSG
@@ -20,25 +20,25 @@ echo -e "\nINSTALLING CURL..."
 sudo apt-get install curl
 echo -e "\nINSTALLING PLINK..."
 sudo apt-get install putty-tools
-echo -e
 
 #Installing compilement tools
 echo -e "\nINSTALLING COMPILE GCC LINARO HF..."
 curl -L --remote-name $LINARO_HF_DL #Permission feedback required
 sudo tar xvf $LINARO_HF_TAR
-LINARO_HF_SUB=$LINARO_HF_TAR | sed -e 's/.tar.xz//'
+LINARO_HF_SUB=$(echo $LINARO_HF_TAR | sed -e 's/.tar.xz//')
 
 echo -e "\nCREATING LINARO ROOT DIRECTORIES..."
-cd /opt; mkdir -p linaro; 
+cd /opt; sudo mkdir -p linaro; 
 cd -
 echo -e "\nCOPYING DATA..."
 pwd
-echo $LINARO_HF_SUB
+echo "Linaro sub tar: " $LINARO_HF_SUB
+echo "Linaro hf: "  $LINARO_HF_TAR
 sudo cp -r $LINARO_HF_SUB/* /opt/linaro
 sudo rm -rf $LINARO_HF_SUB
 sudo rm -rf $LINARO_HF_DL 
 
 #Seting seassion environment
 echo -e "\nSETTING ENVIRONMENTAL VARIABLES..."
-export PATH:$EXEC_PATH
+export PATH=$PATH:$EXEC_PATH
 export CROSS_COMPILE=arm-linux-gnueabihf-
