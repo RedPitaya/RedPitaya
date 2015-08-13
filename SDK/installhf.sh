b43b18c68f85df10ca13dee762794eafcbfc3b36
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-08-13 23:03:04 +0200

    Changed installhf script.

--- SDK/installhf.sh
+++ SDK/installhf.sh
@@ -9,31 +9,36 @@
 GREET_MSG="\nEXECUTING RED PITAYA COMPILE SCRIPT..."
 LINARO_HF_DL="https://releases.linaro.org/14.11/components/toolchain/binaries/arm-linux-gnueabihf/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf.tar.xz"
 
-LINARO_HF_TAR = "gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf.tar.xz"
-RP_INCLUDE=./include
+LINARO_HF_TAR=gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf.tar.xz
+EXEC_PATH="/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin"
 
 #Installing dependency tools
 echo -e $GREET_MSG
-echo -e "INSTALLING NANO...\n"
+echo -e "\nINSTALLING NANO..."
 sudo apt-get install nano
-echo -e "INSTALLING CURL...\n"
+echo -e "\nINSTALLING CURL..."
 sudo apt-get install curl
-echo -e "INSTALLING PLINK...\n"
+echo -e "\nINSTALLING PLINK..."
 sudo apt-get install putty-tools
+echo -e
 
 #Installing compilement tools
-echo -e "INSTALLING COMPILE GCC LINARO HF...\n"
+echo -e "\nINSTALLING COMPILE GCC LINARO HF..."
 curl -L --remote-name $LINARO_HF_DL #Permission feedback required
 sudo tar xvf $LINARO_HF_TAR
-LINARO_HF_SUB=$LINARO_HF_DL | sed -e 's/.tar.xz//'
-sudo cp -ar $LINARO_HF_SUB/* /opt/
+LINARO_HF_SUB=$LINARO_HF_TAR | sed -e 's/.tar.xz//'
 
-echo -e "CREATING LINARO DIRECTORY..."
+echo -e "\nCREATING LINARO ROOT DIRECTORIES..."
 cd /opt; mkdir -p linaro; 
-sudo cp -ar $LINARO_HF_SUB/* linaro
-rm -rf ../$LINARO_HF_SUB
-cd -;
+cd -
+echo -e "\nCOPYING DATA..."
+pwd
+echo $LINARO_HF_SUB
+sudo cp -r $LINARO_HF_SUB/* /opt/linaro
 sudo rm -rf $LINARO_HF_SUB
 sudo rm -rf $LINARO_HF_DL 
 
-
+#Seting seassion environment
+echo -e "\nSETTING ENVIRONMENTAL VARIABLES..."
+export PATH:$EXEC_PATH
+export CROSS_COMPILE=arm-linux-gnueabihf-
