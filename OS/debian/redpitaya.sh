1e599bc1fb33e8d0c7abd4a0359d9002d0ab2a3c
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-08-21 14:39:55 +0200

    Debian: profile updates

--- OS/debian/redpitaya.sh
+++ OS/debian/redpitaya.sh
@@ -10,7 +10,7 @@
 unzip ecosystem*.zip -d $BOOT_DIR
 cp -f $BOOT_DIR/u-boot.scr.debian $BOOT_DIR/u-boot.scr
 
-# Nginx service
+# Systemd services
 install -v -m 664 -o root -d                                                         $ROOT_DIR/var/log/redpitaya_nginx
 install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_discovery.service $ROOT_DIR/etc/systemd/system/redpitaya_discovery.service
 install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_nginx.service     $ROOT_DIR/etc/systemd/system/redpitaya_nginx.service
@@ -28,5 +28,9 @@ apt-get -y install libluajit-5.1 lua-cjson unzip
 apt-get -y install libboost-system1.55.0 libboost-regex1.55.0 libboost-thread1.55.0
 EOF_CHROOT
 
-# final operations and cleanup
+# profile for PATH variables, ...
+install -v -m 664 -o root -D $OVERLAY/etc/profile.d/profile.sh   $ROOT_DIR/etc/profile.d/profile.sh
+install -v -m 664 -o root -D $OVERLAY/etc/profile.d/alias.sh     $ROOT_DIR/etc/profile.d/alias.sh
 install -v -m 664 -o root -D $OVERLAY/etc/profile.d/redpitaya.sh $ROOT_DIR/etc/profile.d/redpitaya.sh
+
+ln -s /opt/redpitaya/version.txt $ROOT_DIR/etc/motd 
