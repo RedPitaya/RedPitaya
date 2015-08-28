ec7fdc525d49254a4d67fc2b02f39bbf7952c418
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-08-28 14:59:11 +0200

    Debian: WiFi access point config file was previously moved to the FAT partition, this is causing issues while attempting to reconfigure the used driver line in the config file

--- OS/debian/debian.sh
+++ OS/debian/debian.sh
@@ -58,7 +58,7 @@ EOF_CHROOT
 # network configuration
 install -v -m 664 -o root -D $OVERLAY/etc/udev/rules.d/75-persistent-net-generator.rules $ROOT_DIR/etc/udev/rules.d/75-persistent-net-generator.rules
 install -v -m 664 -o root -D $OVERLAY/etc/default/ifplugd                                $ROOT_DIR/etc/default/ifplugd
-# TODO: the next line is a bit improvized
+# TODO: the next line is a bit improvized, there are issues with write access to /opt/redpitaya, so switching the wireless driver will not work
 install -v -m 664 -o root -D $OVERLAY/etc/hostapd/hostapd.conf                           $BOOT_DIR/hostapd.conf
 install -v -m 664 -o root -D $OVERLAY/etc/default/hostapd                                $ROOT_DIR/etc/default/hostapd
 install -v -m 664 -o root -D $OVERLAY/etc/dhcp/dhcpd.conf                                $ROOT_DIR/etc/dhcp/dhcpd.conf
