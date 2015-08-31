daff4a9722ffc8116f1cf5c532b62b1bbdddae63
Iztok Jeras <iztok.jeras@gmail.com>
2015-08-31 20:54:16 +0200

    Discovery: updated by Martin

--- OS/tools/discovery.sh
+++ OS/tools/discovery.sh
@@ -1,43 +1,31 @@
 #!/bin/sh
 
-################
-# Old Discovery
-################
-
-MAC=`cat /sys/class/net/eth0/address`
-IP=`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}'`
-
-PAYLOAD=payload={\"mac\":\"$MAC\",\"ips\":{\"eth0\":\"$IP\"}}
-
-URL=http://discovery.redpitaya.com/update
-
-curl -X POST -d "$PAYLOAD" $URL
-
-################
-# New Discovery
-################
-
-URL_PREFIX="http://account.staging.redpitaya.com/discovery.php"
+URL_PREFIX="http://account.staging1.redpitaya.com/discovery.php"
 LOGFILE="/var/log/discovery.log"
 
 MAC_DEFAULT=$(cat /sys/class/net/eth0/address)
+MAC_UPPER_CASE=$(echo "$MAC_DEFAULT" | awk '{print toupper($0)}')
 IP_LAN=$(ip -o -f inet addr show eth0  | awk '{print $4}' | cut -d"/" -f1)
 IP_WAN=$(ip -o -f inet addr show wlan0 | awk '{print $4}' | cut -d"/" -f1)
 
 if [ -n "$IP_LAN" ];then
     PAYLOAD="?mac=$MAC_DEFAULT&ip=$IP_LAN"
+    PAYLOAD_UPPER="?mac=$MAC_DEFAULT&ip=$IP_LAN"
     if [ -n "$IP_WAN" ]; then
         PAYLOAD=$PAYLOAD"&ipwifi=$IP_WAN"
+        PAYLOAD_UPPER=$PAYLOAD_UPPER"&ipwifi=$IP_WAN"
     fi
 else
     PAYLOAD="?mac=$MAC_DEFAULT&ip=$IP_WAN"
+    PAYLOAD_UPPER="?mac=$MAC_DEFAULT&ip=$IP_WAN"
 fi
 
 curl $URL_PREFIX$PAYLOAD >> $LOGFILE 2>&1
+curl $URL_PREFIX$PAYLOAD_UPPER >> $LOGFILE 2>&1
+
 if [ $? -ne 0 ]
 then
-    echo "Discovery update failed!"
+    echo "Discovery update failed!" >> $LOGFILE
 else
-    echo "Discovery update was successful."
+    echo "Discovery update was successful." >> $LOGFILE
 fi
-
