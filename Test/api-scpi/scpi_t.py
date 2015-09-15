3818e3e02f0c039ce11e5725db589378930b9573
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-09-15 13:42:08 +0200

    scpi test adding unittests for phase and offset.

--- Test/api-scpi/scpi_t.py
+++ Test/api-scpi/scpi_t.py
@@ -11,16 +11,18 @@ import unittest
 rp_scpi = scpi.scpi('192.168.178.111')
 
 #Global variables
-rp_dpin_p = {1: 'DIO1_P', 2: 'DIO2_P', 3: 'DIO3_P', 4: 'DIO4_P', 5: 'DIO5_P', 6: 'DIO6_P', 7: 'DIO7_P', 8: 'DIO8_P'}
-rp_dpin_n = {0: 'DIO0_N', 1: 'DIO1_N', 2: 'DIO2_N', 3: 'DIO3_N', 4: 'DIO4_N', 5: 'DIO5_N', 6: 'DIO6_N', 7: 'DIO7_N', 8:'DIO8_N'}
+rp_dpin_p  = {1: 'DIO1_P', 2: 'DIO2_P', 3: 'DIO3_P', 4: 'DIO4_P', 5: 'DIO5_P', 6: 'DIO6_P', 7: 'DIO7_P', 8: 'DIO8_P'}
+rp_dpin_n  = {0: 'DIO0_N', 1: 'DIO1_N', 2: 'DIO2_N', 3: 'DIO3_N', 4: 'DIO4_N', 5: 'DIO5_N', 6: 'DIO6_N', 7: 'DIO7_N', 8:'DIO8_N'}
 rp_a_pin_o = {0: 'AOUT0', 1: 'AOUT1', 2: 'AOUT2', 3: 'AOUT3'}
 rp_a_pin_i = {0: 'AIN0', 1: 'AIN1', 2: 'AIN2', 3: 'AIN3'}
-rp_leds = {1: 'LED1', 2: 'LED2', 3: 'LED3', 4: 'LED4', 5: 'LED5', 6: 'LED6', 7: 'LED7', 8: 'LED8'}
+rp_leds    = {1: 'LED1', 2: 'LED2', 3: 'LED3', 4: 'LED4', 5: 'LED5', 6: 'LED6', 7: 'LED7', 8: 'LED8'}
 
-rp_freq_range = ['100', '1000', '10000', '100000', '1e+06', '1e+07', '3e+07']
-rp_volt_range = ['1.0']
+rp_freq_range  = [100, 1000, 10000, 100000, 1e+06, 1e+07, 3e+07]
+rp_volt_range  = [0.25, 0.5, 0.75, 1.0]
+rp_phase_range = [-30, -90, -180,-360, 30, 90, 180 , 360]
+rp_offs_range  = [-0.75, -0.5, -0.25, 0.25, 0.5 , 0.75]
 
-rp_wave_forms = ['SINE', 'SQUARE', 'TRIANGLE', 'PWM', 'SAW_N', 'SAW_P']
+rp_wave_forms  = ['SINE', 'SQUARE', 'TRIANGLE', 'PWM', 'SAW_N', 'SAW_P']
 
 # Base functions
 class Base(object):
@@ -44,7 +46,7 @@ class Base(object):
         return rp_scpi.rx_txt()
 
     def rp_freq(self, channel, freq):
-        rp_scpi.tx_txt('SOUR' + str(channel) + ':FREQ:FIX ' + freq)
+        rp_scpi.tx_txt('SOUR' + str(channel) + ':FREQ:FIX ' + str(freq))
         rp_scpi.tx_txt('SOUR' + str(channel) + ':FREQ:FIX?')
         return rp_scpi.rx_txt()
 
@@ -53,13 +55,25 @@ class Base(object):
         rp_scpi.tx_txt('SOUR' + str(channel) + ':VOLT?')
         return rp_scpi.rx_txt()
 
+    def rp_offs(self, channel, offs):
+        #AMPL + OFFS <= |1V|
+        rp_scpi.tx_txt('SOUR' + str(channel) + ':VOLT 0.01')
+        rp_scpi.tx_txt('SOUR' + str(channel) + ':VOLT:OFFS ' + str(offs))
+        rp_scpi.tx_txt('SOUR' + str(channel) + ':VOLT:OFFS?')
+        return rp_scpi.rx_txt()
+
+    def rp_phase(self, channel, phase):
+        rp_scpi.tx_txt('SOUR' + str(channel) + ':PHAS ' + str(phase))
+        rp_scpi.tx_txt('SOUR' + str(channel) + ':PHAS?')
+        return rp_scpi.rx_txt()
+
+
     def generate_wform(self):
         lines = [line.rstrip('\n') for line in open('./control_data')]
         return True
 
 # Main test class
 class MainTest(unittest.TestCase):
-
 
     ############### LEDS and GPIOs ###############
     def test_led(self):
@@ -88,21 +102,40 @@ class MainTest(unittest.TestCase):
     def test_freq(self):
         for i in range(0, len(rp_freq_range)):
             freq = rp_freq_range[i]
-            self.assertEquals(Base().rp_freq(1, freq), freq)
-            self.assertEquals(Base().rp_freq(2, freq), freq)
+            self.assertEquals(float(Base().rp_freq(1, freq)), freq)
+            self.assertEquals(float(Base().rp_freq(2, freq)), freq)
 
     def test_volt(self):
         for i in range(0, len(rp_volt_range)):
             volt = rp_volt_range[i]
-            self.assertAlmostEquals(float(Base().rp_ampl(1, volt)), float(volt))
-            self.assertAlmostEquals(float(Base().rp_ampl(2, volt)), float(volt))
+            self.assertAlmostEquals(float(Base().rp_ampl(1, volt)), volt)
+            self.assertAlmostEquals(float(Base().rp_ampl(2, volt)), volt)
 
+    def test_offs(self):
+        for i in range(0, len(rp_offs_range)):
+            offs = rp_offs_range[i]
+            self.assertAlmostEquals(float(Base().rp_offs(1, offs)), offs)
+            self.assertAlmostEquals(float(Base().rp_offs(2, offs)), offs)
 
+    def test_phase(self):
+        for i in range(0, len(rp_phase_range)):
+            phase = rp_phase_range[i]
+            if(phase < 0):
+                phase_new = phase + 360
+                self.assertAlmostEquals(float(Base().rp_phase(1, phase)), phase_new)
+                self.assertAlmostEquals(float(Base().rp_phase(2, phase)), phase_new)
+            else:
+                self.assertAlmostEquals(float(Base().rp_phase(1, phase)), phase)
+                self.assertAlmostEquals(float(Base().rp_phase(2, phase)), phase)
+
+'''
     def test_generate(self):
         for form in rp_wave_forms:
             assert Base().generate_wform(form) is True
+'''
 
 if __name__ == '__main__':
+    #TODO: Implement specific tests
     unittest.main(verbosity=2)
 
 
