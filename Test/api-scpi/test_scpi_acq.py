a401a19aa5404ec95527020b366db84cc7515395
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-09-22 11:37:14 +0200

    Added proper generate unittest. Acquire function changed. Start acquisition and digital loop before generate. Set generator options and then set trigger to CH1_PE and OUTPUT of generator to ON.

--- Test/api-scpi/test_scpi_acq.py
+++ Test/api-scpi/test_scpi_acq.py
@@ -8,31 +8,38 @@ from redpitaya_scpi import scpi
 
 rp_s = scpi (sys.argv[1])
 
-#Generate signal
-rp_s.gen (src=1, waveform='sine', freq=10000, ampl=0.9)
+wave_form = 'sine'
+freq = 10000
+ampl = 0.9
 
-rp_s.send_txt('ACQ:RST')
+#rp_s.tx_txt('OSC:RUN:DIGLOOP')
 
-rp_s.send_txt('ACQ:SOUR1:GAIN LV')
-rp_s.send_txt('ACQ:DEC 1'       )
-rp_s.send_txt('ACQ:TRIG:LEV 0 mV')
-rp_s.send_txt('ACQ:START'       )
-rp_s.send_txt('ACQ:TRIG CH1_PE' )
+rp_s.tx_txt('SOUR1:FUNC ' + str(wave_form).upper())
+rp_s.tx_txt('SOUR1:FREQ:FIX ' + str(freq))
+rp_s.tx_txt('SOUR1:VOLT ' + str(ampl))
+
+rp_s.tx_txt('ACQ:RST')
+
+rp_s.tx_txt('ACQ:SOUR1:GAIN LV')
+rp_s.tx_txt('ACQ:DEC 1'       )
+rp_s.tx_txt('ACQ:TRIG:LEV 0 mV')
+rp_s.tx_txt('ACQ:START'       )
+rp_s.tx_txt('ACQ:TRIG CH1_PE' )
 while 1:
-    rp_s.send_txt('ACQ:TRIG:STAT?')
-    if rp_s.recv_txt(4) == 'TD':
+    rp_s.tx_txt('ACQ:TRIG:STAT?')
+    if rp_s.rx_txt() == 'TD':
         break
-rp_s.send_txt('ACQ:SOUR1:DATA?')
-buff_string = rp_s.recv_txt()
+rp_s.tx_txt('ACQ:SOUR1:DATA?')
+buff_string = rp_s.rx_txt()
 buff_string = buff_string.strip('{} ').split(',')
 buff = map(float, buff_string)
 
 # check trigger settings
-rp_s.send_txt('ACQ:TRIG:DLY?')
-print('ACQ:TRIG:DLY? = ' + rp_s.recv_txt())
+rp_s.tx_txt('ACQ:TRIG:DLY?')
+print('ACQ:TRIG:DLY? = ' + rp_s.rx_txt())
 
-rp_s.send_txt('ACQ:SOUR1:GAIN?')
-print('ACQ:SOUR1:GAIN = ' + rp_s.recv_txt())
+rp_s.tx_txt('ACQ:SOUR1:GAIN?')
+print('ACQ:SOUR1:GAIN = ' + rp_s.rx_txt())
 
 import matplotlib.pyplot as plt
 plt.plot(buff)
