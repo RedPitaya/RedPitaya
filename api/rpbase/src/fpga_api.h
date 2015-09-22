0d417801697e2a5b060584067d2b493ebcf98d38
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-09-22 18:15:57 +0200

    Added api fpga files to access fpga modules. This is a test functionalitie and its inclusion in the api structure is yet to be determined.

---
+++ api/rpbase/src/fpga_api.h
@@ -0,0 +1,21 @@
+/**
+ * $Id: $
+ *
+ * @brief Red Pitaya library Generate module interface
+ *
+ * @Author Red Pitaya
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+ #ifndef __FPGA_H
+ #define __FPGA_H
+
+
+int fpga_load(char *fpga_bit);
+
+ #endif /* __FPGA_H */
