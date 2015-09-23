fa0c00b4f981099d699ebf9ed17aadcf67c1723a
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-09-23 16:36:30 +0200

    FPGA: added design documents for scope filter

--- fpga2/rtl/scope_filter.sv
+++ fpga2/rtl/scope_filter.sv
@@ -8,6 +8,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 // GENERAL DESCRIPTION:
 // Filter to equalize input analog chain. 
+//
+//  s14  -----  s23  -------  s15  -------  s15  -------  s40  -----  s14
+// ---->| FIR |---->| IIR 1 |---->| IIR 2 |---->| SCALE |---->| SAT |---->
+//       -----       -------       -------       -------       -----
+//         ^            ^             ^             ^
+//         | s25        | s18         | s25         | s25
+//         |            |             |             |
+//         BB           AA            PP            KK
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 module scope_filter #(
@@ -15,43 +24,60 @@ module scope_filter #(
   int unsigned DWO = 14
 )(
   // system signals
-  input  logic            clk ,  // clock
-  input  logic            rstn,  // reset - active low
+  input  logic                   clk ,     // clock
+  input  logic                   rstn,     // reset - active low
   // input stream
-  input  logic [DWI-1: 0] adc_dat_i       ,  //!< ADC data
+  input  logic signed [DWI-1: 0] sti_dat,  // data
+  input  logic                   sti_vld,  // valid
+  output logic                   sti_rdy,  // ready
   // output stream
-  output logic [DWO-1: 0] adc_dat_o       ,  //!< ADC data
+  output logic signed [DWO-1: 0] sto_vld,  // ADC data
+  output logic                   sto_vld,  // valid
+  input  logic                   sto_rdy,  // ready
   // configuration
-  input  logic [ 18-1: 0] cfg_aa,  // config AA coefficient
-  input  logic [ 25-1: 0] cfg_bb,  // config BB coefficient
-  input  logic [ 25-1: 0] cfg_kk,  // config KK coefficient
-  input  logic [ 25-1: 0] cfg_pp   // config PP coefficient
+  input  logic signed [ 18-1: 0] cfg_aa,   // config AA coefficient
+  input  logic signed [ 25-1: 0] cfg_bb,   // config BB coefficient
+  input  logic signed [ 25-1: 0] cfg_kk,   // config KK coefficient
+  input  logic signed [ 25-1: 0] cfg_pp    // config PP coefficient
 );
 
 ////////////////////////////////////////////////////////////////////////////////
 // FIR
+//
+//
+//
+// Time domain:
+//
+// y[n] = ( (     2**18) * x[n-0] +
+//          (bb - 2**18) * x[n-1] ) / 2**10
+//
+// Frequency domain:
+//
+// y[n] = ( (     2**18) *        ) +
+//          (bb - 2**18) * z^(-1) ) / 2**10
+//
 ////////////////////////////////////////////////////////////////////////////////
 
-logic [ 39-1: 0] bb_mult   ;
-logic [ 33-1: 0] r2_sum    ;
-logic [ 33-1: 0] r1_reg    ;
-logic [ 23-1: 0] r2_reg    ;
-logic [ 32-1: 0] r01_reg   ;
-logic [ 28-1: 0] r02_reg   ;
+logic signed [ 39-1: 0] bb_mult   ;
+logic signed [ 33-1: 0] r2_sum    ;
+logic signed [ 33-1: 0] r1_reg    ;
+logic signed [ 23-1: 0] r2_reg    ;
+logic signed [ 32-1: 0] r01_reg   ;
+logic signed [ 28-1: 0] r02_reg   ;
 
-assign bb_mult = $signed(adc_dat_i) * $signed(cfg_bb);
-assign r2_sum  = $signed(r01_reg) + $signed(r1_reg);
+assign bb_mult = sti_vld * cfg_bb;
+assign r2_sum  = r01_reg + r1_reg;
 
 always @(posedge clk)
-if (rstn == 1'b0) begin
-  r1_reg  <= 33'h0 ;
-  r2_reg  <= 23'h0 ;
-  r01_reg <= 32'h0 ;
-  r02_reg <= 28'h0 ;
+if (~rstn) begin
+  r1_reg  <= '0;
+  r2_reg  <= '0;
+  r01_reg <= '0;
+  r02_reg <= '0;
 end else begin
-  r1_reg  <= $signed(r02_reg) - $signed(r01_reg) ;
+  r1_reg  <= r02_reg - r01_reg;
   r2_reg  <= r2_sum[33-1:10];
-  r01_reg <= {adc_dat_i,18'h0};
+  r01_reg <= {sti_vld,18'h0};
   r02_reg <= bb_mult[39-2:10];
 end
 
@@ -67,7 +93,7 @@ assign aa_mult = $signed(r3_reg) * $signed(cfg_aa);
 assign r3_sum  = $signed({r2_reg,25'h0}) + $signed({r3_reg,25'h0}) - $signed(aa_mult[41-1:0]);
 
 always @(posedge clk)
-if (rstn == 1'b0) begin
+if (~rstn) begin
   r3_reg  <= 23'h0 ;
 end else begin
   r3_reg  <= r3_sum[49-2:25] ;
@@ -86,7 +112,7 @@ assign pp_mult = $signed(r4_reg) * $signed(cfg_pp);
 assign r4_sum  = $signed(r3_shr) + $signed(pp_mult[40-2:16]);
 
 always @(posedge clk)
-if (rstn == 1'b0) begin
+if (~rstn) begin
   r3_shr <= 15'h0 ;
   r4_reg <= 15'h0 ;
 end else begin
@@ -106,7 +132,7 @@ logic [ 14-1: 0] r5_reg    ;
 assign kk_mult = $signed(r4_reg_rr) * $signed(cfg_kk);
 
 always @(posedge clk)
-if (rstn == 1'b0) begin
+if (~rstn) begin
   r4_reg_r  <= 15'h0 ;
   r4_reg_rr <= 15'h0 ;
   r5_reg    <= 14'h0 ;
@@ -122,6 +148,6 @@ end else begin
      r5_reg <= kk_mult[24+14-1:24];
 end
 
-assign adc_dat_o = r5_reg ;
+assign sto_vld = r5_reg ;
 
 endmodule: scope_filter
