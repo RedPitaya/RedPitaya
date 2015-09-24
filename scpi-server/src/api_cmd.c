bb0a69c6e4df99501ce4c58e49a22d4143a0694f
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-09-24 17:31:28 +0200

    Format changes.

--- scpi-server/src/api_cmd.c
+++ scpi-server/src/api_cmd.c
@@ -135,12 +135,12 @@ scpi_result_t RP_EnableDigLoop(scpi_t *context){
 
     if(result != RP_OK){
         syslog(LOG_ERR, "*RP:DIG:LOop Failed to initialize Red Pitaya"
-            "digital loop.\n");
+            " digital loop.\n");
         return SCPI_RES_ERR;
     }
 
     syslog(LOG_INFO, "*RP:DIG:LOop Successfully initialize Red Pitaya"
-        "digital loop.\n");
+        " digital loop.\n");
 
     return SCPI_RES_OK;
 }
