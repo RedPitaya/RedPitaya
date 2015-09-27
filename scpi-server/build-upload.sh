3c7feee6658dea9daf061bbaa0624877fab36ed1
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-09-27 12:56:25 +0200

    SCPI: removing deprecated build files

--- scpi-server/build-upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./build.sh
-
-sshpass -p 'root' scp scpi-server root@192.168.128.1:/root
-sshpass -p 'root' scp 3rdparty/libs/scpi-parser/libscpi/dist/libscpi.so root@192.168.128.1:/usr/lib/
-sshpass -p 'root' scp ../api/rpbase/src/librp.so root@192.168.128.1:/usr/lib/
