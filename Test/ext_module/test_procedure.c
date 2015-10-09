0f6364d1218b162ee5c3d7a2ce5df1d3afa36d85
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-10-09 11:31:32 +0200

    Added successfull pass message.

--- Test/ext_module/test_procedure.c
+++ Test/ext_module/test_procedure.c
@@ -36,31 +36,33 @@ int main(int argc, char *argv[]){
 		rp_DpinSetDirection(pin, RP_IN);
 	}
 	rp_DpinSetDirection(RP_DIO0_P, RP_OUT);
+
 	rp_DpinSetState(RP_DIO0_P, 1);
 	usleep(1000);
 	rp_DpinSetState(RP_DIO0_P, 0);
 
 	rp_ApinSetValue(RP_AOUT0, 0.5);
-	usleep(1000);
 	rp_ApinSetValue(RP_AOUT1, 1);
 
 	/* Get DIGITAL pin value */
-	rp_dpin_t d_pin = RP_DIO2_P;
-	while(d_pin <= RP_DIO1_N){
+	rp_dpin_t d_pin = RP_DIO1_P;
+	while(d_pin++ <= RP_DIO1_N){
 		rp_pinState_t state;
 		rp_DpinGetState(d_pin, &state);
 		if((d_pin % 2 == 0) && state == 0){
 			check[global_c++] = 1;
 			sum++;
+			continue;
 		}else if((d_pin % 2 != 0) && state == 1){
 			check[global_c++] = 1;
 			sum++;
+			continue;
 		}
-		d_pin++;
+		global_c++;
 	}
 
-	rp_apin_t a_pin = RP_AIN0;
-	while(a_pin <= RP_AIN3){
+	rp_apin_t a_pin = RP_AOUT3;
+	while(a_pin++ <= RP_AIN3){
 		float a_val;
 		rp_ApinGetValue(a_pin, &a_val);
 		if(a_pin % 2 == 0){
@@ -78,16 +80,16 @@ int main(int argc, char *argv[]){
 			}
 			global_c++;
 		}
-
-		a_pin++;
 	}
 
 	if(sum != 12.0){
+		printf("Error on zero state pins...\n\n");
 		printf("D2\tD3\tD4\tD5\tD6\tD7\tD8\tD9\tD10\tA0\tA1\tA2\tA3\n");
 		printf("%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\t%.1d\n", 
 			check[0], check[1], check[2], check[3], check[4], check[5], check[6], 
 			check[7], check[8], check[9], check[10], check[11], check[12]);
 		return -1;
 	}
+	printf("Pass!\n");
 	return 0;
 }
