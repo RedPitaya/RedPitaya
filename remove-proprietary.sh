aff5de333bbf60d12f8751a399a31467ce282c09
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-10-12 18:03:33 +0200

    Makefile: removing application library from public build

--- remove-proprietary.sh
+++ remove-proprietary.sh
@@ -1,1 +1,1 @@
-git filter-branch -f --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch Applications/scopegenpro Applications/spectrumpro Bazaar/nginx/ngx_ext_modules/ws_server/rp_sdk/licverify remove-proprietary.sh' HEAD
+git filter-branch -f --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch api/rpApplications Applications/scopegenpro Applications/spectrumpro Bazaar/nginx/ngx_ext_modules/ws_server/rp_sdk/licverify remove-proprietary.sh' HEAD
