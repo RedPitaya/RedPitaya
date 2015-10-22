4de360d65bffc285185ba3c2dcc80b22e45d8f3c
Zumy Topcagic <zumret.topcagic@redpitaya.com>
2015-10-22 12:31:29 +0200

    Matlab examples cleanup

--- Examples/Matlab/acquire_trigger_posedge.m
+++ Examples/Matlab/acquire_trigger_posedge.m
@@ -2,7 +2,7 @@
 clear all
 close all
 clc
-IP= '192.168.178.56';                % Input IP of your Red Pitaya...
+IP= '192.168.178.102';                % Input IP of your Red Pitaya...
 port = 5000;
 tcpipObj = tcpip(IP, port);
 tcpipObj.InputBufferSize = 16384*32;
@@ -15,10 +15,24 @@ tcpipObj.Terminator = 'CR/LF';
 flushinput(tcpipObj);
 flushoutput(tcpipObj);
 
-% Set decimation vale (sampling rate) in respect to you
+fprintf(tcpipObj,'ACQ:RST');
+%fprintf(tcpipObj,'GEN:RST');
+
+%%SIGNAL GENERATION
+
+%fprintf(tcpipObj,'SOUR2:FUNC SINE');       % Set function of output signal
+                                           % {sine, square, triangle, sawu,sawd, pwm}
+%fprintf(tcpipObj,'SOUR2:FREQ:FIX 20000');  % Set frequency of output signal
+%fprintf(tcpipObj,'SOUR2:VOLT 0.5');        % Set amplitude of output signal
+
+%fprintf(tcpipObj,'OUTPUT2:STATE ON');      % Set output to ON
+
+%%
+
+% Set decimation vale (sampling rate) in respect to you 
 % acquired signal frequency
 
-fprintf(tcpipObj,'ACQ:DEC 8');
+fprintf(tcpipObj,'ACQ:DEC 1');
 
 % Set trigger level to 100 mV
 
@@ -32,16 +46,15 @@ fprintf(tcpipObj,'ACQ:TRIG:LEV 100');
 
 fprintf(tcpipObj,'ACQ:TRIG:DLY 0');
 
-pause(0.1) % Wait for data writing
+pause(2) % Wait for data writing
 
 %% Start & Trigg
 % Trigger source setting must be after ACQ:START
 % Set trigger to source 1 positive edge
 
-
 fprintf(tcpipObj,'ACQ:START');
 fprintf(tcpipObj,'ACQ:TRIG CH1_PE');  
-
+ 
 % Wait for trigger
 % Until trigger is true wait with acquiring
 % Be aware of while loop if trigger is not achieved
@@ -59,31 +72,33 @@ while 1
  end
  
  
-% Read data from buffer
-
+% Read data from buffer 
 signal_str=query(tcpipObj,'ACQ:SOUR1:DATA?');
 
-% Convert values to numbers.% First character in string is “{“
-% and 2 latest are empty spaces and last is “}”.
+% Convert values to numbers.% First character in string is “{“   
+% and 2 latest are empty spaces and last is “}”.  
 
 signal_num=str2num(signal_str(1,2:length(signal_str)-3));
+signal_str_2=query(tcpipObj,'ACQ:SOUR2:DATA?');
 
-plot(signal_num)
-grid on
+% Convert values to numbers.% First character in string is “{“   
+% and 2 latest are empty spaces and last is “}”.  
 
-%For plotting signal in respect to time you can use code below
+signal_num_2=str2num(signal_str_2(1,2:length(signal_str_2)-3));
 
-Fs=str2num(query(tcpipObj,'ACQ:SRA:HZ?'));
-dec=str2num(query(tcpipObj,'ACQ:DEC?'));
+trigger_dey_samp=str2num(query(tcpipObj,'ACQ:TRIG:DLY?'));
 buffer_ln=16384;
-%Create time vector in respect to
-%decimation value
-t=0:1/(Fs/dec):1/(Fs/dec)*(buffer_ln-1);
-%plot(t,signal_num);
-%grid on
+trigger_level=str2num(query(tcpipObj,'ACQ:TRIG:LEV?'))/1000;
+trigger_level=ones(1,buffer_ln).*trigger_level;
+time_delay=(trigger_dey_samp+8192);
+ 
+plot(signal_num)
+hold on
+plot(trigger_level,'r')
+hold on
+plot((time_delay.*ones(1,buffer_ln)), signal_num, 'k' )
+grid on
+ylabel('Voltage / V')
+xlabel('samples')
 
-%Reset to deafault values
-
-fprintf(tcpipObj,'ACQ:RST');
-
-fclose(tcpipObj);
+fclose(tcpipObj)
