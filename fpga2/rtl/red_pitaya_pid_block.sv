d42c5352be1ff23659895cac5393f1a82c6d09a8
Iztok Jeras <iztok.jeras@gmail.com>
2015-10-28 22:14:18 +0100

    FPGA: define local variables as signed

--- fpga2/rtl/red_pitaya_pid_block.sv
+++ fpga2/rtl/red_pitaya_pid_block.sv
@@ -34,126 +34,124 @@
  */
 
 module red_pitaya_pid_block #(
+  // stream parameters
+  int unsigned DWI = 14,
+  int unsigned DWO = 14,
+  // configuration parameters
   int unsigned PSR = 12,
   int unsigned ISR = 18,
   int unsigned DSR = 10
 )(
   // system signals
-  input  logic            clk ,  // clock
-  input  logic            rstn,  // reset - active low
+  input  logic           clk ,  // clock
+  input  logic           rstn,  // reset - active low
   // data
-  input  logic [ 14-1: 0] dat_i,  // input data
-  output logic [ 14-1: 0] dat_o,  // output data
-
-  // settings
-  input  logic [ 14-1: 0] set_sp,  // set point
-  input  logic [ 14-1: 0] set_kp,  // Kp
-  input  logic [ 14-1: 0] set_ki,  // Ki
-  input  logic [ 14-1: 0] set_kd,  // Kd
-  input  logic            int_rst  // integrator reset
+  input  logic signed [DWI-1:0] dat_i,  // input data
+  output logic signed [DWO-1:0] dat_o,  // output data
+  // configuration
+  input  logic signed [DWI-1: 0] set_sp,  // set point
+  input  logic signed [ 14-1: 0] set_kp,  // Kp
+  input  logic signed [ 14-1: 0] set_ki,  // Ki
+  input  logic signed [ 14-1: 0] set_kd,  // Kd
+  // control
+  input  logic                   int_rst  // integrator reset
 );
 
 ////////////////////////////////////////////////////////////////////////////////
 //  Set point error calculation
 ////////////////////////////////////////////////////////////////////////////////
 
-logic [15-1:0] error;
+logic signed [DWI+1-1:0] error;
 
-always @(posedge clk)
-if (rstn == 1'b0) begin
-  error <= 15'h0 ;
+always_ff @(posedge clk)
+if (!rstn) begin
+  error <= '0;
 end else begin
-  error <= $signed(set_sp) - $signed(dat_i) ;
+  error <= set_sp - dat_i;
 end
 
 ////////////////////////////////////////////////////////////////////////////////
 //  Proportional part
 ////////////////////////////////////////////////////////////////////////////////
 
-logic [29-PSR-1:0] kp_reg ;
-logic [    29-1:0] kp_mult;
+logic signed [29-PSR-1:0] kp_reg;
+logic signed [29    -1:0] kp_mult;
 
-always @(posedge clk)
-if (rstn == 1'b0) begin
-  kp_reg  <= {29-PSR{1'b0}};
+always_ff @(posedge clk)
+if (!rstn) begin
+  kp_reg  <= '0;
 end else begin
-  kp_reg <= kp_mult[29-1:PSR] ;
+  kp_reg <= kp_mult[29-1:PSR];
 end
 
-assign kp_mult = $signed(error) * $signed(set_kp);
+assign kp_mult = error * set_kp;
 
 ////////////////////////////////////////////////////////////////////////////////
 //  Integrator
 ////////////////////////////////////////////////////////////////////////////////
 
-logic [    29-1: 0] ki_mult;
-logic [    33-1: 0] int_sum;
-logic [    32-1: 0] int_reg;
-logic [32-ISR-1: 0] int_shr;
+logic signed [    29-1: 0] ki_mult;
+logic signed [    33-1: 0] int_sum;
+logic signed [    32-1: 0] int_reg;
+logic signed [32-ISR-1: 0] int_shr;
 
-always @(posedge clk)
-if (rstn == 1'b0) begin
-  ki_mult  <= {29{1'b0}};
-  int_reg  <= {32{1'b0}};
+always_ff @(posedge clk)
+if (!rstn) begin
+  ki_mult  <= '0;
+  int_reg  <= '0;
 end else begin
-  ki_mult <= $signed(error) * $signed(set_ki) ;
+  ki_mult <= error * set_ki;
   if (int_rst)
-    int_reg <= 32'h0; // reset
-  else if (int_sum[33-1:33-2] == 2'b01) // positive saturation
-    int_reg <= 32'h7FFFFFFF; // max positive
-  else if (int_sum[33-1:33-2] == 2'b10) // negative saturation
-    int_reg <= 32'h80000000; // max negative
+    int_reg <= '0;
   else
-    int_reg <= int_sum[32-1:0]; // use sum as it is
+    // saturation
+    int_reg <= ^int_sum[33-1:33-2] ? {int_sum[33-1], {32-1{~int_sum[33-1]}}}
+                                   :  int_sum[32-1:0];
 end
 
-assign int_sum = $signed(ki_mult) + $signed(int_reg) ;
+assign int_sum = ki_mult + int_reg;
 assign int_shr = int_reg[32-1:ISR] ;
 
 ////////////////////////////////////////////////////////////////////////////////
 //  Derivative
 ////////////////////////////////////////////////////////////////////////////////
 
-logic [    29-1: 0] kd_mult ;
-logic [29-DSR-1: 0] kd_reg  ;
-logic [29-DSR-1: 0] kd_reg_r;
-logic [29-DSR  : 0] kd_reg_s;
+logic signed [    29-1: 0] kd_mult ;
+logic signed [29-DSR-1: 0] kd_reg  ;
+logic signed [29-DSR-1: 0] kd_reg_r;
+logic signed [29-DSR  : 0] kd_reg_s;
 
-always @(posedge clk)
-if (rstn == 1'b0) begin
-  kd_reg   <= {29-DSR{1'b0}};
-  kd_reg_r <= {29-DSR{1'b0}};
-  kd_reg_s <= {29-DSR+1{1'b0}};
+always_ff @(posedge clk)
+if (!rstn) begin
+  kd_reg   <= '0;
+  kd_reg_r <= '0;
+  kd_reg_s <= '0;
 end else begin
-  kd_reg   <= kd_mult[29-1:DSR] ;
+  kd_reg   <= kd_mult[29-1:DSR];
   kd_reg_r <= kd_reg;
-  kd_reg_s <= $signed(kd_reg) - $signed(kd_reg_r);
+  kd_reg_s <= kd_reg - kd_reg_r;
 end
 
-assign kd_mult = $signed(error) * $signed(set_kd) ;
+assign kd_mult = error * set_kd;
 
 ////////////////////////////////////////////////////////////////////////////////
 //  Sum together - saturate output
 ////////////////////////////////////////////////////////////////////////////////
 
-wire  [   33-1: 0] pid_sum     ; // biggest posible bit-width
-reg   [   14-1: 0] pid_out     ;
+logic signed [33-1:0] pid_sum; // biggest posible bit-width
 
-always @(posedge clk)
-if (rstn == 1'b0) begin
-  pid_out    <= 14'b0 ;
+assign pid_sum = kp_reg + int_shr + kd_reg_s;
+
+always_ff @(posedge clk)
+if (!rstn) begin
+  dat_o <= '0;
 end else begin
   if ({pid_sum[33-1],|pid_sum[32-2:13]} == 2'b01) //positive overflow
-    pid_out <= 14'h1FFF ;
+    dat_o <= 14'h1FFF ;
   else if ({pid_sum[33-1],&pid_sum[33-2:13]} == 2'b10) //negative overflow
-    pid_out <= 14'h2000 ;
+    dat_o <= 14'h2000 ;
   else
-    pid_out <= pid_sum[14-1:0] ;
+    dat_o <= pid_sum[14-1:0] ;
 end
-
-assign pid_sum = $signed(kp_reg) + $signed(int_shr) + $signed(kd_reg_s) ;
-
-
-assign dat_o = pid_out ;
 
 endmodule: red_pitaya_pid_block
