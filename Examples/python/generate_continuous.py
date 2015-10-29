0dbcf74b410384697270347542de34ba93f45bdb
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-10-29 16:48:49 +0100

    Examples/python: examples should figrst reset generator

--- Examples/python/generate_continuous.py
+++ Examples/python/generate_continuous.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 
-import redpitaya_scpi as scpi
-import time
 import sys
-
-__author__ = "Luka Golinar, Iztok Jeras"
-__copyright__ = "Copyright 2015, Red Pitaya"
+import redpitaya_scpi as scpi
 
 rp_s = scpi.scpi(sys.argv[1])
 
 wave_form = 'sine'
-freq = 1000
+freq = 10000
 ampl = 1
 
+rp_s.tx_txt('GEN:RST')
 rp_s.tx_txt('SOUR1:FUNC ' + str(wave_form).upper())
 rp_s.tx_txt('SOUR1:FREQ:FIX ' + str(freq))
 rp_s.tx_txt('SOUR1:VOLT ' + str(ampl))
