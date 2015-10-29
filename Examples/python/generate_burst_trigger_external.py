d311c79e70a5cc9ba7bcd1efeb1bd1582879710c
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-10-29 16:48:49 +0100

    Examples/python: examples should figrst reset generator

--- Examples/python/generate_burst_trigger_external.py
+++ Examples/python/generate_burst_trigger_external.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-import redpitaya_scpi as scpi
-import time
 import sys
+import redpitaya_scpi as scpi
 
 rp_s = scpi.scpi(sys.argv[1])
 
@@ -11,10 +10,10 @@ freq = 10000
 ampl = 1
 
 rp_s.tx_txt('GEN:RST')
-rp_s.tx_txt('SOUR2:FUNC ' + str(wave_form).upper())
-rp_s.tx_txt('SOUR2:FREQ:FIX ' + str(freq))
-rp_s.tx_txt('SOUR2:VOLT ' + str(ampl))
-rp_s.tx_txt('SOUR2:BURS:NCYC 2')
-rp_s.tx_txt('OUTPUT2:STATE ON')
-rp_s.tx_txt('SOUR2:BURS:STAT ON')
-rp_s.tx_txt('SOUR2:TRIG:SOUR EXT_PE')
+rp_s.tx_txt('SOUR1:FUNC ' + str(wave_form).upper())
+rp_s.tx_txt('SOUR1:FREQ:FIX ' + str(freq))
+rp_s.tx_txt('SOUR1:VOLT ' + str(ampl))
+rp_s.tx_txt('SOUR1:BURS:NCYC 2')
+rp_s.tx_txt('OUTPUT1:STATE ON')
+rp_s.tx_txt('SOUR1:BURS:STAT ON')
+rp_s.tx_txt('SOUR1:TRIG:SOUR EXT_PE')
