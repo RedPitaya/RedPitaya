c87b2803e3f0a24936da4da7260711a3506ec540
Zumret Topcagic <zumret.topcagic@redpitaya.com>
2015-10-30 14:32:24 +0100

    update

--- Examples/Matlab/acquire_trigger_from_generator.m
+++ Examples/Matlab/acquire_trigger_from_generator.m
@@ -1,9 +1,8 @@
-%% Define Red Pitaya as TCP/IP object
 clc
 clear all
 close all
 
-IP= '192.168.178.56';           % Input IP of your Red Pitaya...
+IP= '192.168.178.111';           % Input IP of your Red Pitaya...
 port = 5000;
 tcpipObj=tcpip(IP, port);
 tcpipObj.InputBufferSize = 16384*32;
@@ -15,28 +14,34 @@ tcpipObj.Terminator = 'CR/LF';
 flushinput(tcpipObj)
 flushoutput(tcpipObj)
 
-%% Generate
+%% Loop back for testing Generate 
 
-fprintf(tcpipObj,'SOUR1:FUNC SINE');          % Set function of output signal {sine, square, triangle,sawu,sawd, pwm}
-fprintf(tcpipObj,'SOUR1:FREQ:FIX 50000');     % Set frequency of output signal
-fprintf(tcpipObj,'SOUR1:VOLT 1');             % Set amplitude of output signal
-fprintf(tcpipObj,'SOUR1:BURS:NCYC 1');        % Set 1 pulses of sine wave
+%% The example generate sine bursts every 0.5 seconds indefinety
+fprintf(tcpipObj,'GEN:RST');
+fprintf(tcpipObj,'ACQ:RST');
+
+fprintf(tcpipObj,'SOUR1:FUNC SINE');                                                 
+fprintf(tcpipObj,'SOUR1:FREQ:FIX 1000');     % Set frequency of output signal
+fprintf(tcpipObj,'SOUR1:VOLT 1');          % Set amplitude of output signal
+
+fprintf(tcpipObj,'SOUR1:BURS:STAT ON');    % Set burst mode to ON
+fprintf(tcpipObj,'SOUR1:BURS:NCYC 1');       % Set 1 pulses of sine wave
+fprintf(tcpipObj,'SOUR1:TRIG:IMM');          % Set generator trigger to immediately
 fprintf(tcpipObj,'OUTPUT1:STATE ON');         % Set output to ON
-fprintf(tcpipObj,'SOUR1:BURS:STAT ON');       % Set burst mode to ON
 
 %% Set Acquire
 
-fprintf(tcpipObj,'ACQ:DEC 1');
-fprintf(tcpipObj,'ACQ:TRIG:LEV 50');
+fprintf(tcpipObj,'ACQ:DEC 64');
+fprintf(tcpipObj,'ACQ:TRIG:LEV 0');
 fprintf(tcpipObj,'ACQ:TRIG:DLY 0');
 
-pause(0.01)
 
 %% Start gen % acq
 
 fprintf(tcpipObj,'ACQ:START');
+pause(1);
 fprintf(tcpipObj,'ACQ:TRIG AWG_PE');
-fprintf(tcpipObj,'SOUR1:TRIG:IMM');           % Set generator trigger to immediately
+fprintf(tcpipObj,'SOUR2:TRIG:IMM');           % Set generator trigger to immediately
 
 %% Wait for trigger
  while 1
@@ -55,4 +60,4 @@ hold on
 grid on
 
 %% Close connection with Red Pitaya
-fprintf(tcpipObj,'ACQ:RST');
+fclose(tcpipObj);
