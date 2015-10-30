48b7a337438ad353225f01e448def91eff6e3bfa
Zumret Topcagic <zumret.topcagic@redpitaya.com>
2015-10-30 13:24:59 +0100

    Update. Info - you need to have some pause before enabeling output.

--- Examples/C/generate_burst_trigger_software.c
+++ Examples/C/generate_burst_trigger_software.c
@@ -9,35 +9,27 @@
 
 int main(int argc, char **argv){
 
-	/* Burst count */
-	int i;
+        /* Burst count */
 
-	/* Print error, if rp_Init() function failed */
-	if(rp_Init() != RP_OK){
-		fprintf(stderr, "Rp api init failed!\n");
-	}
-	
-	rp_GenFreq(RP_CH_1, 100);
-	rp_GenAmp(RP_CH_1, 1.0);
-	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
 
-	/* Enable output channel */
-	rp_GenOutEnable(RP_CH_1);
+        /* Print error, if rp_Init() function failed */
+        if(rp_Init() != RP_OK){
+                fprintf(stderr, "Rp api init failed!\n");
+        }
 
-	for(i = 0; i < 100; i++){
-		usleep(200000);
-		rp_GenBurstCount(RP_CH_1, 1);
-		usleep(200000);
-		rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
-	} 
+        rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
+        rp_GenFreq(RP_CH_1, 1000);
+        rp_GenAmp(RP_CH_1, 1.0);
 
-	/*
-	usleep(50000000);
-	rp_GenBurstCount(channel, 1);
-	rp_GenMode(channel, gen_mode);
-	usleep(5000000);
-	*/
-
-	/* Releasing resources */
-	rp_Release();
+        rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
+        rp_GenBurstCount(RP_CH_1, 1);
+        rp_GenBurstRepetitions(RP_CH_1, 10000);
+        rp_GenBurstPeriod(RP_CH_1, 5000);
+        rp_GenTrigger(1);
+        sleep(1);
+        rp_GenOutEnable(RP_CH_1);
+        rp_Release();
 }
+
+
+
