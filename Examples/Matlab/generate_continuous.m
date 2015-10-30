ea27ec53a43568459f0eceb6756d9f17cbc7d01b
Zumret Topcagic <zumret.topcagic@redpitaya.com>
2015-10-30 11:53:54 +0100

    Update

--- Examples/Matlab/generate_continuous.m
+++ Examples/Matlab/generate_continuous.m
@@ -1,6 +1,6 @@
 %% Define Red Pitaya as TCP/IP object
 
-IP= '192.168.178.56';           % Input IP of your Red Pitaya...
+IP= '192.168.178.111';           % Input IP of your Red Pitaya...
 port = 5000;
 tcpipObj=tcpip(IP, port);
 
@@ -9,6 +9,7 @@ tcpipObj=tcpip(IP, port);
 fopen(tcpipObj);
 tcpipObj.Terminator = 'CR/LF';
 
+fprintf(tcpipObj,'GEN:RST');
 fprintf(tcpipObj,'SOUR1:FUNC SINE');       % Set function of output signal
                                            % {sine, square, triangle, sawu,sawd, pwm}
 fprintf(tcpipObj,'SOUR1:FREQ:FIX 2000');   % Set frequency of output signal
