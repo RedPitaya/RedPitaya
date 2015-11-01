20110430015b519b599a00bb8151f3b7887da386
Iztok Jeras <iztok.jeras@gmail.com>
2015-11-01 10:32:46 +0100

    FPGA: added improvized top level bench, it does not do anything

---
+++ fpga2/tbn/system_wrapper.sv
@@ -0,0 +1,296 @@
+//Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2015.2 (lin64) Build 1266856 Fri Jun 26 16:35:25 MDT 2015
+//Date        : Sun Nov  1 08:48:37 2015
+//Host        : Dent running 64-bit Ubuntu 15.04
+//Command     : generate_target system_wrapper.bd
+//Design      : system_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module system_wrapper (
+  inout  logic [14:0] DDR_addr   ,
+  inout  logic  [2:0] DDR_ba     ,
+  inout  logic        DDR_cas_n  ,
+  inout  logic        DDR_ck_n   ,
+  inout  logic        DDR_ck_p   ,
+  inout  logic        DDR_cke    ,
+  inout  logic        DDR_cs_n   ,
+  inout  logic  [3:0] DDR_dm     ,
+  inout  logic [31:0] DDR_dq     ,
+  inout  logic  [3:0] DDR_dqs_n  ,
+  inout  logic  [3:0] DDR_dqs_p  ,
+  inout  logic        DDR_odt    ,
+  inout  logic        DDR_ras_n  ,
+  inout  logic        DDR_reset_n,
+  inout  logic        DDR_we_n   ,
+
+  output logic        FCLK_CLK0,
+  output logic        FCLK_CLK1,
+  output logic        FCLK_CLK2,
+  output logic        FCLK_CLK3,
+  output logic        FCLK_RESET0_N,
+  output logic        FCLK_RESET1_N,
+  output logic        FCLK_RESET2_N,
+  output logic        FCLK_RESET3_N,
+
+  inout  logic        FIXED_IO_ddr_vrn ,
+  inout  logic        FIXED_IO_ddr_vrp ,
+  inout  logic [53:0] FIXED_IO_mio     ,
+  inout  logic        FIXED_IO_ps_clk  ,
+  inout  logic        FIXED_IO_ps_porb ,
+  inout  logic        FIXED_IO_ps_srstb,
+
+  input  logic        M_AXI_GP0_ACLK   ,
+  output logic [31:0] M_AXI_GP0_araddr ,
+  output logic  [1:0] M_AXI_GP0_arburst,
+  output logic  [3:0] M_AXI_GP0_arcache,
+  output logic [11:0] M_AXI_GP0_arid   ,
+  output logic  [3:0] M_AXI_GP0_arlen  ,
+  output logic  [1:0] M_AXI_GP0_arlock ,
+  output logic  [2:0] M_AXI_GP0_arprot ,
+  output logic  [3:0] M_AXI_GP0_arqos  ,
+  input  logic        M_AXI_GP0_arready,
+  output logic  [2:0] M_AXI_GP0_arsize ,
+  output logic        M_AXI_GP0_arvalid,
+  output logic [31:0] M_AXI_GP0_awaddr ,
+  output logic  [1:0] M_AXI_GP0_awburst,
+  output logic  [3:0] M_AXI_GP0_awcache,
+  output logic [11:0] M_AXI_GP0_awid   ,
+  output logic  [3:0] M_AXI_GP0_awlen  ,
+  output logic  [1:0] M_AXI_GP0_awlock ,
+  output logic  [2:0] M_AXI_GP0_awprot ,
+  output logic  [3:0] M_AXI_GP0_awqos  ,
+  input  logic        M_AXI_GP0_awready,
+  output logic  [2:0] M_AXI_GP0_awsize ,
+  output logic        M_AXI_GP0_awvalid,
+  input  logic [11:0] M_AXI_GP0_bid    ,
+  output logic        M_AXI_GP0_bready ,
+  input  logic  [1:0] M_AXI_GP0_bresp  ,
+  input  logic        M_AXI_GP0_bvalid ,
+  input  logic [31:0] M_AXI_GP0_rdata  ,
+  input  logic [11:0] M_AXI_GP0_rid    ,
+  input  logic        M_AXI_GP0_rlast  ,
+  output logic        M_AXI_GP0_rready ,
+  input  logic  [1:0] M_AXI_GP0_rresp  ,
+  input  logic        M_AXI_GP0_rvalid ,
+  output logic [31:0] M_AXI_GP0_wdata  ,
+  output logic [11:0] M_AXI_GP0_wid    ,
+  output logic        M_AXI_GP0_wlast  ,
+  input  logic        M_AXI_GP0_wready ,
+  output logic  [3:0] M_AXI_GP0_wstrb  ,
+  output logic        M_AXI_GP0_wvalid ,
+
+  input  logic        S_AXI_HP1_aclk   , S_AXI_HP0_aclk   ,
+  input  logic [31:0] S_AXI_HP1_araddr , S_AXI_HP0_araddr ,
+  input  logic  [1:0] S_AXI_HP1_arburst, S_AXI_HP0_arburst,
+  input  logic  [3:0] S_AXI_HP1_arcache, S_AXI_HP0_arcache,
+  input  logic  [5:0] S_AXI_HP1_arid   , S_AXI_HP0_arid   ,
+  input  logic  [3:0] S_AXI_HP1_arlen  , S_AXI_HP0_arlen  ,
+  input  logic  [1:0] S_AXI_HP1_arlock , S_AXI_HP0_arlock ,
+  input  logic  [2:0] S_AXI_HP1_arprot , S_AXI_HP0_arprot ,
+  input  logic  [3:0] S_AXI_HP1_arqos  , S_AXI_HP0_arqos  ,
+  output logic        S_AXI_HP1_arready, S_AXI_HP0_arready,
+  input  logic  [2:0] S_AXI_HP1_arsize , S_AXI_HP0_arsize ,
+  input  logic        S_AXI_HP1_arvalid, S_AXI_HP0_arvalid,
+  input  logic [31:0] S_AXI_HP1_awaddr , S_AXI_HP0_awaddr ,
+  input  logic  [1:0] S_AXI_HP1_awburst, S_AXI_HP0_awburst,
+  input  logic  [3:0] S_AXI_HP1_awcache, S_AXI_HP0_awcache,
+  input  logic  [5:0] S_AXI_HP1_awid   , S_AXI_HP0_awid   ,
+  input  logic  [3:0] S_AXI_HP1_awlen  , S_AXI_HP0_awlen  ,
+  input  logic  [1:0] S_AXI_HP1_awlock , S_AXI_HP0_awlock ,
+  input  logic  [2:0] S_AXI_HP1_awprot , S_AXI_HP0_awprot ,
+  input  logic  [3:0] S_AXI_HP1_awqos  , S_AXI_HP0_awqos  ,
+  output logic        S_AXI_HP1_awready, S_AXI_HP0_awready,
+  input  logic  [2:0] S_AXI_HP1_awsize , S_AXI_HP0_awsize ,
+  input  logic        S_AXI_HP1_awvalid, S_AXI_HP0_awvalid,
+  output logic  [5:0] S_AXI_HP1_bid    , S_AXI_HP0_bid    ,
+  input  logic        S_AXI_HP1_bready , S_AXI_HP0_bready ,
+  output logic  [1:0] S_AXI_HP1_bresp  , S_AXI_HP0_bresp  ,
+  output logic        S_AXI_HP1_bvalid , S_AXI_HP0_bvalid ,
+  output logic [63:0] S_AXI_HP1_rdata  , S_AXI_HP0_rdata  ,
+  output logic  [5:0] S_AXI_HP1_rid    , S_AXI_HP0_rid    ,
+  output logic        S_AXI_HP1_rlast  , S_AXI_HP0_rlast  ,
+  input  logic        S_AXI_HP1_rready , S_AXI_HP0_rready ,
+  output logic  [1:0] S_AXI_HP1_rresp  , S_AXI_HP0_rresp  ,
+  output logic        S_AXI_HP1_rvalid , S_AXI_HP0_rvalid ,
+  input  logic [63:0] S_AXI_HP1_wdata  , S_AXI_HP0_wdata  ,
+  input  logic  [5:0] S_AXI_HP1_wid    , S_AXI_HP0_wid    ,
+  input  logic        S_AXI_HP1_wlast  , S_AXI_HP0_wlast  ,
+  output logic        S_AXI_HP1_wready , S_AXI_HP0_wready ,
+  input  logic  [7:0] S_AXI_HP1_wstrb  , S_AXI_HP0_wstrb  ,
+  input  logic        S_AXI_HP1_wvalid , S_AXI_HP0_wvalid ,
+
+  input  logic        Vaux0_v_n,
+  input  logic        Vaux0_v_p,
+  input  logic        Vaux1_v_n,
+  input  logic        Vaux1_v_p,
+  input  logic        Vaux8_v_n,
+  input  logic        Vaux8_v_p,
+  input  logic        Vaux9_v_n,
+  input  logic        Vaux9_v_p,
+  input  logic        Vp_Vn_v_n,
+  input  logic        Vp_Vn_v_p
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// DDR memory
+////////////////////////////////////////////////////////////////////////////////
+
+// assign DDR_addr    = 'z;
+// assign DDR_ba      = 'z;
+// assign DDR_cas_n   = 'z;
+// assign DDR_ck_n    = 'z;
+// assign DDR_ck_p    = 'z;
+// assign DDR_cke     = 'z;
+// assign DDR_cs_n    = 'z;
+// assign DDR_dm      = 'z;
+// assign DDR_dq      = 'z;
+// assign DDR_dqs_n   = 'z;
+// assign DDR_dqs_p   = 'z;
+// assign DDR_odt     = 'z;
+// assign DDR_ras_n   = 'z;
+// assign DDR_reset_n = 'z;
+// assign DDR_we_n    = 'z;
+
+////////////////////////////////////////////////////////////////////////////////
+// clock and reset
+////////////////////////////////////////////////////////////////////////////////
+
+logic clk;
+logic rstn;
+
+initial       clk = 1'b1;
+always #(8ns) clk = ~clk;
+
+initial begin
+  rstn = 1'b0;
+  repeat (8) @ (posedge clk);
+  rstn = 1'b1;
+end
+
+assign FCLK_CLK0 = clk;
+assign FCLK_CLK1 = clk;
+assign FCLK_CLK2 = clk;
+assign FCLK_CLK3 = clk;
+assign FCLK_RESET0_N = rstn;
+assign FCLK_RESET1_N = rstn;
+assign FCLK_RESET2_N = rstn;
+assign FCLK_RESET3_N = rstn;
+
+////////////////////////////////////////////////////////////////////////////////
+// Fixed IO
+////////////////////////////////////////////////////////////////////////////////
+
+// assign FIXED_IO_ddr_vrn  = 'z;
+// assign FIXED_IO_ddr_vrp  = 'z;
+// assign FIXED_IO_mio      = 'z;
+// assign FIXED_IO_ps_clk   = 'z;
+// assign FIXED_IO_ps_porb  = 'z;
+// assign FIXED_IO_ps_srstb = 'z;
+
+////////////////////////////////////////////////////////////////////////////////
+// AMBA AXI-4 GP bus
+////////////////////////////////////////////////////////////////////////////////
+
+//     M_AXI_GP0_ACLK   ,
+assign M_AXI_GP0_araddr  = '0;
+assign M_AXI_GP0_arburst = '0;
+assign M_AXI_GP0_arcache = '0;
+assign M_AXI_GP0_arid    = '0;
+assign M_AXI_GP0_arlen   = '0;
+assign M_AXI_GP0_arlock  = '0;
+assign M_AXI_GP0_arprot  = '0;
+assign M_AXI_GP0_arqos   = '0;
+//     M_AXI_GP0_arready,
+assign M_AXI_GP0_arsize  = '0;
+assign M_AXI_GP0_arvalid = '0;
+assign M_AXI_GP0_awaddr  = '0;
+assign M_AXI_GP0_awburst = '0;
+assign M_AXI_GP0_awcache = '0;
+assign M_AXI_GP0_awid    = '0;
+assign M_AXI_GP0_awlen   = '0;
+assign M_AXI_GP0_awlock  = '0;
+assign M_AXI_GP0_awprot  = '0;
+assign M_AXI_GP0_awqos   = '0;
+//     M_AXI_GP0_awready,
+assign M_AXI_GP0_awsize  = '0;
+assign M_AXI_GP0_awvalid = '0;
+//     M_AXI_GP0_bid    ,
+assign M_AXI_GP0_bready  = '0;
+//     M_AXI_GP0_bresp  ,
+//     M_AXI_GP0_bvalid ,
+//     M_AXI_GP0_rdata  ,
+//     M_AXI_GP0_rid    ,
+//     M_AXI_GP0_rlast  ,
+assign M_AXI_GP0_rready  = '0;
+//     M_AXI_GP0_rresp  ,
+//     M_AXI_GP0_rvalid ,
+assign M_AXI_GP0_wdata   = '0;
+assign M_AXI_GP0_wid     = '0;
+assign M_AXI_GP0_wlast   = '0;
+//     M_AXI_GP0_wready ,
+assign M_AXI_GP0_wstrb   = '0;
+assign M_AXI_GP0_wvalid  = '0;
+
+////////////////////////////////////////////////////////////////////////////////
+// AMBA AXI-4 HP bus
+////////////////////////////////////////////////////////////////////////////////
+
+//     {S_AXI_HP1_aclk   , S_AXI_HP0_aclk   } = '0;
+//     {S_AXI_HP1_araddr , S_AXI_HP0_araddr } = '0;
+//     {S_AXI_HP1_arburst, S_AXI_HP0_arburst} = '0;
+//     {S_AXI_HP1_arcache, S_AXI_HP0_arcache} = '0;
+//     {S_AXI_HP1_arid   , S_AXI_HP0_arid   } = '0;
+//     {S_AXI_HP1_arlen  , S_AXI_HP0_arlen  } = '0;
+//     {S_AXI_HP1_arlock , S_AXI_HP0_arlock } = '0;
+//     {S_AXI_HP1_arprot , S_AXI_HP0_arprot } = '0;
+//     {S_AXI_HP1_arqos  , S_AXI_HP0_arqos  } = '0;
+assign {S_AXI_HP1_arready, S_AXI_HP0_arready} = '0;
+//     {S_AXI_HP1_arsize , S_AXI_HP0_arsize } = '0;
+//     {S_AXI_HP1_arvalid, S_AXI_HP0_arvalid} = '0;
+//     {S_AXI_HP1_awaddr , S_AXI_HP0_awaddr } = '0;
+//     {S_AXI_HP1_awburst, S_AXI_HP0_awburst} = '0;
+//     {S_AXI_HP1_awcache, S_AXI_HP0_awcache} = '0;
+//     {S_AXI_HP1_awid   , S_AXI_HP0_awid   } = '0;
+//     {S_AXI_HP1_awlen  , S_AXI_HP0_awlen  } = '0;
+//     {S_AXI_HP1_awlock , S_AXI_HP0_awlock } = '0;
+//     {S_AXI_HP1_awprot , S_AXI_HP0_awprot } = '0;
+//     {S_AXI_HP1_awqos  , S_AXI_HP0_awqos  } = '0;
+assign {S_AXI_HP1_awready, S_AXI_HP0_awready} = '0;
+//     {S_AXI_HP1_awsize , S_AXI_HP0_awsize } = '0;
+//     {S_AXI_HP1_awvalid, S_AXI_HP0_awvalid} = '0;
+assign {S_AXI_HP1_bid    , S_AXI_HP0_bid    } = '0;
+//     {S_AXI_HP1_bready , S_AXI_HP0_bready } = '0;
+assign {S_AXI_HP1_bresp  , S_AXI_HP0_bresp  } = '0;
+assign {S_AXI_HP1_bvalid , S_AXI_HP0_bvalid } = '0;
+assign {S_AXI_HP1_rdata  , S_AXI_HP0_rdata  } = '0;
+assign {S_AXI_HP1_rid    , S_AXI_HP0_rid    } = '0;
+assign {S_AXI_HP1_rlast  , S_AXI_HP0_rlast  } = '0;
+//     {S_AXI_HP1_rready , S_AXI_HP0_rready } = '0;
+assign {S_AXI_HP1_rresp  , S_AXI_HP0_rresp  } = '0;
+assign {S_AXI_HP1_rvalid , S_AXI_HP0_rvalid } = '0;
+//     {S_AXI_HP1_wdata  , S_AXI_HP0_wdata  } = '0;
+//     {S_AXI_HP1_wid    , S_AXI_HP0_wid    } = '0;
+//     {S_AXI_HP1_wlast  , S_AXI_HP0_wlast  } = '0;
+assign {S_AXI_HP1_wready , S_AXI_HP0_wready } = '0;
+//     {S_AXI_HP1_wstrb  , S_AXI_HP0_wstrb  } = '0;
+//     {S_AXI_HP1_wvalid , S_AXI_HP0_wvalid } = '0;
+
+////////////////////////////////////////////////////////////////////////////////
+// analog inputs
+////////////////////////////////////////////////////////////////////////////////
+
+//  input  logic        Vaux0_v_n,
+//  input  logic        Vaux0_v_p,
+//  input  logic        Vaux1_v_n,
+//  input  logic        Vaux1_v_p,
+//  input  logic        Vaux8_v_n,
+//  input  logic        Vaux8_v_p,
+//  input  logic        Vaux9_v_n,
+//  input  logic        Vaux9_v_p,
+//  input  logic        Vp_Vn_v_n,
+//  input  logic        Vp_Vn_v_p
+
+endmodule
---
+++ fpga2/tbn/top_tb.sv
@@ -0,0 +1,203 @@
+////////////////////////////////////////////////////////////////////////////////
+// Module: Red Pitaya top FPGA module
+// Author: Iztok Jeras
+// (c) Red Pitaya  (redpitaya.com)
+////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+
+module top_tb #(
+  // time period
+  realtime  TP = 8.0ns,  // 125MHz
+  // DUT configuration
+  int unsigned DAC_DW = 14, // ADC data width
+  int unsigned RSZ = 14  // RAM size is 2**RSZ
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// DAC signal generation
+////////////////////////////////////////////////////////////////////////////////
+
+
+logic               clk ;
+logic               rstn;
+
+logic [DAC_DW-1: 0] dac_a;
+logic [DAC_DW-1: 0] dac_b;
+
+logic               trig;
+
+// DAC clock
+initial        clk = 1'b0;
+always #(TP/2) clk = ~clk;
+
+// DAC reset
+initial begin
+  rstn = 1'b0;
+  repeat(4) @(posedge clk);
+  rstn = 1'b1;
+end
+
+// ADC cycle counter
+int unsigned dac_cyc=0;
+always_ff @ (posedge clk)
+dac_cyc <= dac_cyc+1;
+
+always begin
+  trig <= 1'b0 ;
+  repeat(100000) @(posedge clk);
+  trig <= 1'b1 ;
+  repeat(1200) @(posedge clk);
+  trig <= 1'b0 ;
+end
+
+////////////////////////////////////////////////////////////////////////////////
+// test sequence
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// signal generation
+////////////////////////////////////////////////////////////////////////////////
+
+initial begin
+  repeat(10) @(posedge clk);
+  $finish();
+end
+
+////////////////////////////////////////////////////////////////////////////////
+// module instances
+////////////////////////////////////////////////////////////////////////////////
+
+// PS connections
+wire  [54-1:0] FIXED_IO_mio     ;
+wire           FIXED_IO_ps_clk  ;
+wire           FIXED_IO_ps_porb ;
+wire           FIXED_IO_ps_srstb;
+wire           FIXED_IO_ddr_vrn ;
+wire           FIXED_IO_ddr_vrp ;
+// DDR
+wire  [15-1:0] DDR_addr   ;
+wire  [ 3-1:0] DDR_ba     ;
+wire           DDR_cas_n  ;
+wire           DDR_ck_n   ;
+wire           DDR_ck_p   ;
+wire           DDR_cke    ;
+wire           DDR_cs_n   ;
+wire  [ 4-1:0] DDR_dm     ;
+wire  [32-1:0] DDR_dq     ;
+wire  [ 4-1:0] DDR_dqs_n  ;
+wire  [ 4-1:0] DDR_dqs_p  ;
+wire           DDR_odt    ;
+wire           DDR_ras_n  ;
+wire           DDR_reset_n;
+wire           DDR_we_n   ;
+
+// ADC
+logic [2-1:0] [16-1:2] adc_dat;
+logic                  adc_clk_p;
+logic                  adc_clk_n;
+logic         [ 2-1:0] adc_clk_o;   // optional ADC clock source
+logic                  adc_cdcs;    // ADC clock duty cycle stabilizer
+// DAC
+logic         [14-1:0] dac_dat;     // DAC combined data
+logic                  dac_wrt;     // DAC write
+logic                  dac_sel;     // DAC channel select
+logic                  dac_clk;     // DAC clock
+logic                  dac_rst;     // DAC reset
+// PDM DAC
+logic         [ 4-1:0] dac_pwm;     // 1-bit PDM DAC
+// XADC
+logic         [ 5-1:0] vinp;        // voltages p
+logic         [ 5-1:0] vinn;        // voltages n
+// Expansion connector
+wire          [ 8-1:0] exp_p_io;
+wire          [ 8-1:0] exp_n_io;
+// LED
+logic         [ 8-1:0] led;
+
+glbl glbl();
+
+red_pitaya_top top (
+  // PS connections
+  .FIXED_IO_mio      (FIXED_IO_mio     ),
+  .FIXED_IO_ps_clk   (FIXED_IO_ps_clk  ),
+  .FIXED_IO_ps_porb  (FIXED_IO_ps_porb ),
+  .FIXED_IO_ps_srstb (FIXED_IO_ps_srstb),
+  .FIXED_IO_ddr_vrn  (FIXED_IO_ddr_vrn ),
+  .FIXED_IO_ddr_vrp  (FIXED_IO_ddr_vrp ),
+  // DDR
+  .DDR_addr       (DDR_addr   ),
+  .DDR_ba         (DDR_ba     ),
+  .DDR_cas_n      (DDR_cas_n  ),
+  .DDR_ck_n       (DDR_ck_n   ),
+  .DDR_ck_p       (DDR_ck_p   ),
+  .DDR_cke        (DDR_cke    ),
+  .DDR_cs_n       (DDR_cs_n   ),
+  .DDR_dm         (DDR_dm     ),
+  .DDR_dq         (DDR_dq     ),
+  .DDR_dqs_n      (DDR_dqs_n  ),
+  .DDR_dqs_p      (DDR_dqs_p  ),
+  .DDR_odt        (DDR_odt    ),
+  .DDR_ras_n      (DDR_ras_n  ),
+  .DDR_reset_n    (DDR_reset_n),
+  .DDR_we_n       (DDR_we_n   ),
+
+  // Red Pitaya periphery
+  
+  // ADC
+  .adc_dat_a_i    (adc_dat[0]),
+  .adc_dat_b_i    (adc_dat[1]),
+  .adc_clk_p_i    (adc_clk_p),
+  .adc_clk_n_i    (adc_clk_n),
+  .adc_clk_o      (adc_clk_o),
+  .adc_cdcs_o     (adc_cdcs_o),
+  // DAC
+  .dac_dat_o      (dac_dat),
+  .dac_wrt_o      (dac_wrt),
+  .dac_sel_o      (dac_sel),
+  .dac_clk_o      (dac_clk),
+  .dac_rst_o      (dac_rst),
+  // PDM DAC
+  .dac_pwm_o      (dac_pwm),
+  // XADC
+  .vinp_i         (vinp),
+  .vinn_i         (vinn),
+  // Expansion connector
+  .exp_p_io       (exp_p_io),
+  .exp_n_io       (exp_n_io),
+  // SATA connector
+  .daisy_p_o      (),
+  .daisy_n_o      (),
+  .daisy_p_i      ('0),
+  .daisy_n_i      ('0),
+  // LED
+  .led_o          (led)
+);
+
+// ADC
+assign adc_dat = '0;
+assign adc_clk_p =  clk;
+assign adc_clk_n = ~clk;
+// adc_clk_o
+// adc_cdcs
+
+// XADC
+assign vinp = '0;
+assign vinn = '0;
+
+// Expansion connector
+assign exp_p_io = 8'hz;
+assign exp_n_io = 8'hz;
+
+// LED
+
+////////////////////////////////////////////////////////////////////////////////
+// waveforms
+////////////////////////////////////////////////////////////////////////////////
+
+initial begin
+  $dumpfile("top_tb.vcd");
+  $dumpvars(0, top_tb);
+end
+
+endmodule: top_tb
