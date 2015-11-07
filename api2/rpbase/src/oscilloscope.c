0df6f1e300c76beb96f8cdbf2327bb6ed2340d26
Iztok Jeras <iztok.jeras@gmail.com>
2015-11-07 23:41:22 +0100

    API: further cleanup of osciloscope API

--- api2/rpbase/src/oscilloscope.c
+++ api2/rpbase/src/oscilloscope.c
@@ -15,237 +15,185 @@
 #include "common.h"
 #include "oscilloscope.h"
 
+// structure containing available ranges
+static const float ranges [2] = {1.0, 20.0};
+
 // The FPGA register structure for oscilloscope
-static volatile osc_control_t *osc_reg = NULL;
-
-// The FPGA input signal buffer pointer for channel A
-static volatile uint32_t *osc_cha = NULL;
-
-// The FPGA input signal buffer pointer for channel B
-static volatile uint32_t *osc_chb = NULL;
-
+static volatile regsetset_t *regset = NULL;
 
 /**
  * general
  */
 
-int osc_Init()
-{
-//    ECHECK(cmn_Init());
-    ECHECK(cmn_Map(OSC_BASE_SIZE, OSC_BASE_ADDR, (void**)&osc_reg));
-    osc_cha = (uint32_t*)((char*)osc_reg + OSC_CHA_OFFSET);
-    osc_chb = (uint32_t*)((char*)osc_reg + OSC_CHB_OFFSET);
+int osc_Init() {
+    cmn_Map(OSC_BASE_SIZE, OSC_BASE_ADDR, (void**)&regset);
     return RP_OK;
 }
 
-int osc_Release()
-{
-    ECHECK(cmn_Unmap(OSC_BASE_SIZE, (void**)&osc_reg));
-    osc_cha = NULL;
-    osc_chb = NULL;
-//    ECHECK(cmn_Release());
+int osc_Release() {
+    cmn_Unmap(OSC_BASE_SIZE, (void**)&regset);
     return RP_OK;
-}
-
-
-/**
- * decimation
- */
-
-int osc_SetDecimation(uint32_t decimation)
-{
-    return cmn_SetValue(&osc_reg->data_dec, decimation, DATA_DEC_MASK);
-}
-
-int osc_GetDecimation(uint32_t* decimation)
-{
-    return cmn_GetValue(&osc_reg->data_dec, decimation, DATA_DEC_MASK);
-}
-
-int osc_SetAveraging(bool enable)
-{
-    if (enable) {
-        return cmn_SetBits(&osc_reg->other, 0x1, DATA_AVG_MASK);
-    }
-    else {
-        return cmn_UnsetBits(&osc_reg->other, 0x1, DATA_AVG_MASK);
-    }
-}
-
-int osc_GetAveraging(bool* enable)
-{
-    return cmn_AreBitsSet(osc_reg->other, 0x1, DATA_AVG_MASK, enable);
-}
-
-/**
- * trigger source
- */
-
-int osc_SetTriggerSource(uint32_t source)
-{
-    return cmn_SetValue(&osc_reg->trig_source, source, TRIG_SRC_MASK);
-}
-
-int osc_GetTriggerSource(uint32_t* source)
-{
-    return cmn_GetValue(&osc_reg->trig_source, source, TRIG_SRC_MASK);
-}
-
-int osc_WriteDataIntoMemory(bool enable)
-{
-    if (enable) {
-        return cmn_SetBits(&osc_reg->conf, 0x1, START_DATA_WRITE_MASK);
-    }
-    else {
-        return cmn_UnsetBits(&osc_reg->conf, 0x1, START_DATA_WRITE_MASK);
-    }
-}
-
-int osc_ResetWriteStateMachine()
-{
-    return cmn_SetBits(&osc_reg->conf, (0x1 << 1), RST_WR_ST_MCH_MASK);
-}
-
-int osc_SetArmKeep(bool enable)
-{
-    if (enable)
-        return cmn_SetBits(&osc_reg->conf, 0x8, ARM_KEEP_MASK);
-    else
-        return cmn_UnsetBits(&osc_reg->conf, 0x8, ARM_KEEP_MASK);
-}
-
-int osc_GetTriggerState(bool *received)
-{
-    return cmn_AreBitsSet(osc_reg->conf, (0x1 << 2), TRIG_ST_MCH_MASK, received);
-}
-
-int osc_GetPreTriggerCounter(uint32_t *value)
-{
-    return cmn_GetValue(&osc_reg->pre_trigger_counter, value, PRE_TRIGGER_COUNTER);
-}
-
-/**
- * trigger delay
- */
-
-int osc_SetTriggerDelay(uint32_t decimated_data_num)
-{
-    return cmn_SetValue(&osc_reg->trigger_delay, decimated_data_num, TRIG_DELAY_MASK);
-}
-
-int osc_GetTriggerDelay(uint32_t* decimated_data_num)
-{
-    return cmn_GetValue(&osc_reg->trigger_delay, decimated_data_num, TRIG_DELAY_MASK);
-}
-
-/**
- * Threshold
- */
-
-int osc_SetThresholdChA(uint32_t threshold)
-{
-    return cmn_SetValue(&osc_reg->cha_thr, threshold, THRESHOLD_MASK);
-}
-
-int osc_GetThresholdChA(uint32_t* threshold)
-{
-    return cmn_GetValue(&osc_reg->cha_thr, threshold, THRESHOLD_MASK);
-}
-
-int osc_SetThresholdChB(uint32_t threshold)
-{
-    return cmn_SetValue(&osc_reg->chb_thr, threshold, THRESHOLD_MASK);
-}
-
-int osc_GetThresholdChB(uint32_t* threshold)
-{
-    return cmn_GetValue(&osc_reg->chb_thr, threshold, THRESHOLD_MASK);
-}
-
-/**
- * Hysteresis
- */
-int osc_SetHysteresisChA(uint32_t hysteresis)
-{
-    return cmn_SetValue(&osc_reg->cha_hystersis, hysteresis, HYSTERESIS_MASK);
-}
-
-int osc_GetHysteresisChA(uint32_t* hysteresis)
-{
-    return cmn_GetValue(&osc_reg->cha_hystersis, hysteresis, HYSTERESIS_MASK);
-}
-
-int osc_SetHysteresisChB(uint32_t hysteresis)
-{
-    return cmn_SetValue(&osc_reg->chb_hystersis, hysteresis, HYSTERESIS_MASK);
-}
-
-int osc_GetHysteresisChB(uint32_t* hysteresis)
-{
-    return cmn_GetValue(&osc_reg->chb_hystersis, hysteresis, HYSTERESIS_MASK);
 }
 
 /**
  * Equalization filters
  */
-int osc_SetEqFiltersChA(uint32_t coef_aa, uint32_t coef_bb, uint32_t coef_kk, uint32_t coef_pp)
-{
-    ECHECK(cmn_SetValue(&osc_reg->cha_filt_aa, coef_aa, EQ_FILTER_AA));
-    ECHECK(cmn_SetValue(&osc_reg->cha_filt_bb, coef_bb, EQ_FILTER));
-    ECHECK(cmn_SetValue(&osc_reg->cha_filt_kk, coef_kk, EQ_FILTER));
-    ECHECK(cmn_SetValue(&osc_reg->cha_filt_pp, coef_pp, EQ_FILTER));
+
+int rp_AcqSetEqFilters(int32_t aa, bb, kk, pp) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    iowrite32(&regset[channel]->cfg_faa, aa);
+    iowrite32(&regset[channel]->cfg_fbb, bb);
+    iowrite32(&regset[channel]->cfg_fkk, kk);
+    iowrite32(&regset[channel]->cfg_fpp, pp);
     return RP_OK;
 }
 
-int osc_GetEqFiltersChA(uint32_t* coef_aa, uint32_t* coef_bb, uint32_t* coef_kk, uint32_t* coef_pp)
-{
-    ECHECK(cmn_GetValue(&osc_reg->cha_filt_aa, coef_aa, EQ_FILTER_AA));
-    ECHECK(cmn_GetValue(&osc_reg->cha_filt_bb, coef_bb, EQ_FILTER));
-    ECHECK(cmn_GetValue(&osc_reg->cha_filt_kk, coef_kk, EQ_FILTER));
-    ECHECK(cmn_GetValue(&osc_reg->cha_filt_pp, coef_pp, EQ_FILTER));
+int rp_AcqGetEqFilters(int32_t *aa, *bb, *kk, *pp) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    ioread32(&regset[channel]->cfg_faa, aa);
+    ioread32(&regset[channel]->cfg_fbb, bb);
+    ioread32(&regset[channel]->cfg_fkk, kk);
+    ioread32(&regset[channel]->cfg_fpp, pp);
     return RP_OK;
 }
 
-int osc_SetEqFiltersChB(uint32_t coef_aa, uint32_t coef_bb, uint32_t coef_kk, uint32_t coef_pp)
-{
-    ECHECK(cmn_SetValue(&osc_reg->chb_filt_aa, coef_aa, EQ_FILTER_AA));
-    ECHECK(cmn_SetValue(&osc_reg->chb_filt_bb, coef_bb, EQ_FILTER));
-    ECHECK(cmn_SetValue(&osc_reg->chb_filt_kk, coef_kk, EQ_FILTER));
-    ECHECK(cmn_SetValue(&osc_reg->chb_filt_pp, coef_pp, EQ_FILTER));
+int rp_AcqSetAveraging(int unsigned channel, bool averaging) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    iowrite32(averageing, &regset[channel]->cfg_avg);
     return RP_OK;
 }
 
-int osc_GetEqFiltersChB(uint32_t* coef_aa, uint32_t* coef_bb, uint32_t* coef_kk, uint32_t* coef_pp)
-{
-    ECHECK(cmn_GetValue(&osc_reg->chb_filt_aa, coef_aa, EQ_FILTER_AA));
-    ECHECK(cmn_GetValue(&osc_reg->chb_filt_bb, coef_bb, EQ_FILTER));
-    ECHECK(cmn_GetValue(&osc_reg->chb_filt_kk, coef_kk, EQ_FILTER));
-    ECHECK(cmn_GetValue(&osc_reg->chb_filt_pp, coef_pp, EQ_FILTER));
+int rp_AcqGetAveraging(int unsigned channel, bool *averaging) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    *averaging = ioread32(&regset[channel]->cfg_avg);
     return RP_OK;
 }
+
+int rp_AcqSetDecimation(int unsigned channel, uint32_t decimation) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    iowrite32(decimation, &regset[channel]->cfg_dec);
+    return RP_OK;
+}
+
+int rp_AcqGetDecimation(int unsigned channel, uint32_t *decimation) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    *decimation = ioread32(&regset[channel]->cfg_dec);
+    return RP_OK;
+}
+
+int rp_AcqSetShiftRight(int unsigned channel, uint32_t shift) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    iowrite32(shift, &regset[channel]->cfg_shr);
+    return RP_OK;
+}
+
+int rp_AcqGetShiftRight(int unsigned channel, uint32_t *shift) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    *shift = ioread32(&regset[channel]->cfg_shr);
+    return RP_OK;
+}
+
+int rp_AcqSetTriggerSrc(int unsigned channel, rp_acq_trig_src_t source) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    iowrite32(source, &regset[channel]->cfg_lvl);
+    return RP_OK;
+}
+
+int rp_AcqGetTriggerSrc(int unsigned channel, rp_acq_trig_src_t *source) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    *source = (rp_acq_trig_src_t) ioread32(&regset[channel]->cfg_sel);
+    return RP_OK;
+}
+
+int rp_AcqSetTriggerDelay(int unsigned channel, uint32_t value) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    iowrite32(value, &regset[channel]->cfg_dly);
+    return RP_OK;
+}
+
+int rp_AcqGetTriggerDelay(int unsigned channel, uint32_t *value) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    *value = ioread32(&regset[channel]->cfg_dly);
+    return RP_OK;
+}
+
+int rp_AcqSetTriggerLevel(int unsigned channel, float voltage) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    float range = ranges[ioread32(&regset[channel]->cfg_rng)];
+    iowrite32((uint32_t) (voltage / range * (1 << RP_ACQ_TODO)), &regset[channel]->cfg_lvl);
+    return RP_OK;
+}
+
+int rp_AcqGetTriggerLevel(int unsigned channel, float *voltage) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    float range = ranges[ioread32(&regset[channel]->cfg_rng)];
+    *voltage = (float) (ioread32(&regset[channel]->cfg_lvl) >> RP_ACQ_TODO) * range);
+    return RP_OK;
+}
+
+int rp_AcqSetTriggerHyst(int unsigned channel, float voltage) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    float range = ranges[ioread32(&regset[channel]->cfg_rng)];
+    iowrite32((uint32_t) (voltage / range * (1 << RP_ACQ_TODO)), &regset[channel]->cfg_hst);
+    return RP_OK;
+}
+
+int rp_AcqGetTriggerHyst(int unsigned channel, float *voltage) {
+    if (channel >= RP_MNG)  return RP_EPN;
+    float range = ranges[ioread32(&regset[channel]->cfg_rng)];
+    *voltage = (float) (ioread32(&regset[channel]->cfg_hst) >> RP_ACQ_TODO) * range);
+    return RP_OK;
+}
+
+
+
 
 /**
  * Write pointer
  */
 int osc_GetWritePointer(uint32_t* pos)
 {
-    return cmn_GetValue(&osc_reg->wr_ptr_cur, pos, WRITE_POINTER_MASK);
+    return cmn_GetValue(&regset->wr_ptr_cur, pos, WRITE_POINTER_MASK);
 }
 
 int osc_GetWritePointerAtTrig(uint32_t* pos)
 {
-    return cmn_GetValue(&osc_reg->wr_ptr_trigger, pos, WRITE_POINTER_MASK);
+    return cmn_GetValue(&regset->wr_ptr_trigger, pos, WRITE_POINTER_MASK);
 }
 
-/**
- * Raw buffers
- */
-const volatile uint32_t* osc_GetDataBufferChA()
-{
-    return osc_cha;
+int rp_AcqGetPreTriggerCounter(uint32_t* value) {
+    return acq_GetPreTriggerCounter(value);
 }
 
-const volatile uint32_t* osc_GetDataBufferChB()
-{
-    return osc_chb;
+int rp_AcqGetTriggerState(rp_acq_trig_state_t* state) {
+    return acq_GetTriggerState(state);
 }
+
+int rp_AcqStart() {
+    return acq_Start();
+}
+
+int rp_AcqStop() {
+    return acq_Stop();
+}
+
+int rp_AcqReset() {
+    return acq_Reset();
+}
+
+uint32_t rp_AcqGetNormalizedDataPos(uint32_t pos) {
+    return acq_GetNormalizedDataPos(pos);
+}
+
+int rp_AcqGetDataRaw(rp_channel_t channel,  uint32_t pos, uint32_t* size, int16_t* buffer) {
+    return acq_GetDataRaw(channel, pos, size, buffer);
+}
+
+int rp_AcqGetBufSize(uint32_t *size) {
+    return acq_GetBufferSize(size);
+}
+
+float rp_CmnCnvCntToV(uint32_t field_len, uint32_t cnts, float adc_max_v) {
+	return cmn_CnvCntToV(field_len, cnts, adc_max_v);
+}
+
