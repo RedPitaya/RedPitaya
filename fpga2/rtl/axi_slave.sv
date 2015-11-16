dc3e38c6d60a1bea9c35d4dc71da923bb58274a1
Iztok Jeras <iztok.jeras@gmail.com>
2015-11-16 13:04:41 +0100

    FPGA: using SystemVerilog

--- fpga2/rtl/axi_slave.sv
+++ fpga2/rtl/axi_slave.sv
@@ -47,92 +47,92 @@
  */
 
 module axi_slave #(
-  parameter           AXI_DW         =  64           , // data width (8,16,...,1024)
-  parameter           AXI_AW         =  32           , // address width
-  parameter           AXI_IW         =   8           , // ID width
-  parameter           AXI_SW         = AXI_DW >> 3     // strobe width - 1 bit for every data byte
+  int unsigned AXI_DW =  64        , // data width (8,16,...,1024)
+  int unsigned AXI_AW =  32        , // address width
+  int unsigned AXI_IW =   8        , // ID width
+  int unsigned AXI_SW = AXI_DW >> 3  // sobe width - 1 bit for every data byte
 )(
   // global signals
-  input                     axi_clk_i      ,  //!< AXI global clock
-  input                     axi_rstn_i     ,  //!< AXI global reset
+  input  logic              axi_clk_i    ,  // AXI global clock
+  input  logic              axi_rstn_i   ,  // AXI global reset
   // axi write address channel
-  input      [ AXI_IW-1: 0] axi_awid_i     ,  //!< AXI write address ID
-  input      [ AXI_AW-1: 0] axi_awaddr_i   ,  //!< AXI write address
-  input      [      4-1: 0] axi_awlen_i    ,  //!< AXI write burst length
-  input      [      3-1: 0] axi_awsize_i   ,  //!< AXI write burst size
-  input      [      2-1: 0] axi_awburst_i  ,  //!< AXI write burst type
-  input      [      2-1: 0] axi_awlock_i   ,  //!< AXI write lock type
-  input      [      4-1: 0] axi_awcache_i  ,  //!< AXI write cache type
-  input      [      3-1: 0] axi_awprot_i   ,  //!< AXI write protection type
-  input                     axi_awvalid_i  ,  //!< AXI write address valid
-  output                    axi_awready_o  ,  //!< AXI write ready
-  // axi write data channel
-  input      [ AXI_IW-1: 0] axi_wid_i      ,  //!< AXI write data ID
-  input      [ AXI_DW-1: 0] axi_wdata_i    ,  //!< AXI write data
-  input      [ AXI_SW-1: 0] axi_wstrb_i    ,  //!< AXI write strobes
-  input                     axi_wlast_i    ,  //!< AXI write last
-  input                     axi_wvalid_i   ,  //!< AXI write valid
-  output                    axi_wready_o   ,  //!< AXI write ready
+  input  logic [AXI_IW-1:0] axi_awid_i   ,  // AXI write address ID
+  input  logic [AXI_AW-1:0] axi_awaddr_i ,  // AXI write address
+  input  logic [     4-1:0] axi_awlen_i  ,  // AXI write burst length
+  input  logic [     3-1:0] axi_awsize_i ,  // AXI write burst size
+  input  logic [     2-1:0] axi_awburst_i,  // AXI write burst type
+  input  logic [     2-1:0] axi_awlock_i ,  // AXI write lock type
+  input  logic [     4-1:0] axi_awcache_i,  // AXI write cache type
+  input  logic [     3-1:0] axi_awprot_i ,  // AXI write protection type
+  input  logic              axi_awvalid_i,  // AXI write address valid
+  output logic              axi_awready_o,  // AXI write ready
+  // axi write dta channel
+  input  logic [AXI_IW-1:0] axi_wid_i    ,  // AXI write data ID
+  input  logic [AXI_DW-1:0] axi_wdata_i  ,  // AXI write data
+  input  logic [AXI_SW-1:0] axi_wstrb_i  ,  // AXI write strobes
+  input  logic              axi_wlast_i  ,  // AXI write last
+  input  logic              axi_wvalid_i ,  // AXI write valid
+  output logic              axi_wready_o ,  // AXI write ready
   // axi write response channel
-  output     [ AXI_IW-1: 0] axi_bid_o      ,  //!< AXI write response ID
-  output reg [      2-1: 0] axi_bresp_o    ,  //!< AXI write response
-  output reg                axi_bvalid_o   ,  //!< AXI write response valid
-  input                     axi_bready_i   ,  //!< AXI write response ready
+  output logic [AXI_IW-1:0] axi_bid_o    ,  // AXI write response ID
+  output logic [     2-1:0] axi_bresp_o  ,  // AXI write response
+  output logic              axi_bvalid_o ,  // AXI write response valid
+  input  logic              axi_bready_i ,  // AXI write response ready
   // axi read address channel
-  input      [ AXI_IW-1: 0] axi_arid_i     ,  //!< AXI read address ID
-  input      [ AXI_AW-1: 0] axi_araddr_i   ,  //!< AXI read address
-  input      [      4-1: 0] axi_arlen_i    ,  //!< AXI read burst length
-  input      [      3-1: 0] axi_arsize_i   ,  //!< AXI read burst size
-  input      [      2-1: 0] axi_arburst_i  ,  //!< AXI read burst type
-  input      [      2-1: 0] axi_arlock_i   ,  //!< AXI read lock type
-  input      [      4-1: 0] axi_arcache_i  ,  //!< AXI read cache type
-  input      [      3-1: 0] axi_arprot_i   ,  //!< AXI read protection type
-  input                     axi_arvalid_i  ,  //!< AXI read address valid
-  output                    axi_arready_o  ,  //!< AXI read address ready
+  input  logic [AXI_IW-1:0] axi_arid_i   ,  // AXI read address ID
+  input  logic [AXI_AW-1:0] axi_araddr_i ,  // AXI read address
+  input  logic [     4-1:0] axi_arlen_i  ,  // AXI read burst length
+  input  logic [     3-1:0] axi_arsize_i ,  // AXI read burst size
+  input  logic [     2-1:0] axi_arburst_i,  // AXI read burst type
+  input  logic [     2-1:0] axi_arlock_i ,  // AXI read lock type
+  input  logic [     4-1:0] axi_arcache_i,  // AXI read cache type
+  input  logic [     3-1:0] axi_arprot_i ,  // AXI read protection type
+  input  logic              axi_arvalid_i,  // AXI read address valid
+  output logic              axi_arready_o,  // AXI read address ready
   // axi read data channel
-  output     [ AXI_IW-1: 0] axi_rid_o      ,  //!< AXI read response ID
-  output reg [ AXI_DW-1: 0] axi_rdata_o    ,  //!< AXI read data
-  output reg [      2-1: 0] axi_rresp_o    ,  //!< AXI read response
-  output reg                axi_rlast_o    ,  //!< AXI read last
-  output reg                axi_rvalid_o   ,  //!< AXI read response valid
-  input                     axi_rready_i   ,  //!< AXI read response ready
+  output logic [AXI_IW-1:0] axi_rid_o    ,  // AXI read response ID
+  output logic [AXI_DW-1:0] axi_rdata_o  ,  // AXI read data
+  output logic [     2-1:0] axi_rresp_o  ,  // AXI read response
+  output logic              axi_rlast_o  ,  // AXI read last
+  output logic              axi_rvalid_o ,  // AXI read response valid
+  input  logic              axi_rready_i ,  // AXI read response ready
   // RP system read/write channel
-  output     [ AXI_AW-1: 0] sys_addr_o     ,  //!< system bus read/write address.
-  output     [ AXI_DW-1: 0] sys_wdata_o    ,  //!< system bus write data.
-  output reg [ AXI_SW-1: 0] sys_sel_o      ,  //!< system bus write byte select.
-  output reg                sys_wen_o      ,  //!< system bus write enable.
-  output reg                sys_ren_o      ,  //!< system bus read enable.
-  input      [ AXI_DW-1: 0] sys_rdata_i    ,  //!< system bus read data.
-  input                     sys_err_i      ,  //!< system bus error indicator.
-  input                     sys_ack_i         //!< system bus acknowledge signal.
+  output logic [AXI_AW-1:0] sys_addr_o   ,  // system bus read/write address.
+  output logic [AXI_DW-1:0] sys_wdata_o  ,  // system bus write data.
+  output logic [AXI_SW-1:0] sys_sel_o    ,  // system bus write byte select.
+  output logic              sys_wen_o    ,  // system bus write enable.
+  output logic              sys_ren_o    ,  // system bus read enable.
+  input  logic [AXI_DW-1:0] sys_rdata_i  ,  // system bus read data.
+  input  logic              sys_err_i    ,  // system bus error indicator.
+  input  logic              sys_ack_i       // system bus acknowledge signal.
 );
 
 //---------------------------------------------------------------------------------
 //  AXI slave Module
 //---------------------------------------------------------------------------------
 
-wire                 ack         ;
-reg   [      6-1: 0] ack_cnt     ;
+logic                ack      ;
+logic [      6-1: 0] ack_cnt  ;
 
-reg                  rd_do       ;
-reg   [ AXI_IW-1: 0] rd_arid     ;
-reg   [ AXI_AW-1: 0] rd_araddr   ;
-reg                  rd_error    ;
-wire                 rd_errorw   ;
+logic                rd_do    ;
+logic [ AXI_IW-1: 0] rd_arid  ;
+logic [ AXI_AW-1: 0] rd_araddr;
+logic                rd_error ;
+logic                rd_errorw;
 
-reg                  wr_do       ;
-reg   [ AXI_IW-1: 0] wr_awid     ;
-reg   [ AXI_AW-1: 0] wr_awaddr   ;
-reg   [ AXI_IW-1: 0] wr_wid      ;
-reg   [ AXI_DW-1: 0] wr_wdata    ;
-reg                  wr_error    ;
-wire                 wr_errorw   ;
+logic                wr_do    ;
+logic [ AXI_IW-1: 0] wr_awid  ;
+logic [ AXI_AW-1: 0] wr_awaddr;
+logic [ AXI_IW-1: 0] wr_wid   ;
+logic [ AXI_DW-1: 0] wr_wdata ;
+logic                wr_error ;
+logic                wr_errorw;
 
 assign wr_errorw = (axi_awlen_i != 4'h0) || (axi_awsize_i != 3'b010); // error if write burst and more/less than 4B transfer
 assign rd_errorw = (axi_arlen_i != 4'h0) || (axi_arsize_i != 3'b010); // error if read burst and more/less than 4B transfer
 
-always @(posedge axi_clk_i)
-if (axi_rstn_i == 1'b0) begin
+always_ff @(posedge axi_clk_i)
+if (~axi_rstn_i) begin
    rd_do    <= 1'b0 ;
    rd_error <= 1'b0 ;
 end else begin
@@ -148,8 +148,8 @@ end else begin
    end
 end
 
-always @(posedge axi_clk_i)
-if (axi_rstn_i == 1'b0) begin
+always_ff @(posedge axi_clk_i)
+if (~axi_rstn_i) begin
    wr_do    <= 1'b0 ;
    wr_error <= 1'b0 ;
 end else begin
@@ -175,8 +175,8 @@ assign axi_bid_o     = wr_awid                               ;
 assign axi_arready_o = !rd_do && !wr_do && !axi_awvalid_i     ;
 assign axi_rid_o     = rd_arid                                ;
 
-always @(posedge axi_clk_i)
-if (axi_rstn_i == 1'b0) begin
+always_ff @(posedge axi_clk_i)
+if (~axi_rstn_i) begin
    axi_bvalid_o  <= 1'b0 ;
    axi_bresp_o   <= 2'h0 ;
    axi_rlast_o   <= 1'b0 ;
@@ -192,8 +192,8 @@ end else begin
 end
 
 // acknowledge protection
-always @(posedge axi_clk_i)
-if (axi_rstn_i == 1'b0) begin
+always_ff @(posedge axi_clk_i)
+if (~axi_rstn_i) begin
    ack_cnt   <= 6'h0 ;
 end else begin
    if ((axi_arvalid_i && axi_arready_o) || (axi_awvalid_i && axi_awready_o))  // rd || wr request
@@ -209,8 +209,8 @@ assign ack = sys_ack_i || ack_cnt[5] || (rd_do && rd_errorw) || (wr_do && wr_err
 //------------------------------------------
 //  Simple slave interface
 
-always @(posedge axi_clk_i)
-if (axi_rstn_i == 1'b0) begin
+always_ff @(posedge axi_clk_i)
+if (~axi_rstn_i) begin
    sys_wen_o  <= 1'b0 ;
    sys_ren_o  <= 1'b0 ;
    sys_sel_o  <= {AXI_SW{1'b0}} ;
@@ -223,4 +223,4 @@ end
 assign sys_addr_o  = rd_do ? rd_araddr : wr_awaddr  ;
 assign sys_wdata_o = wr_wdata                       ;
 
-endmodule
+endmodule: axi_slave_ps
