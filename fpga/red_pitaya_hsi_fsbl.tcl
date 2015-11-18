68dc1f55afc98fa8104cdc22dda3eac6a5c8825a
Ulrich Habel <espero7757@gmx.net>
2015-11-18 22:34:58 +0100

    fpga - red_pitaya_hsi_fsbltcl.tcl, red_pitaya_hsi_dram_test.tcl - -hw system changed to system_0

--- fpga/red_pitaya_hsi_fsbl.tcl
+++ fpga/red_pitaya_hsi_fsbl.tcl
@@ -8,6 +8,6 @@
 set path_sdk sdk
 
 open_hw_design $path_sdk/red_pitaya.sysdef
-generate_app -hw system -os standalone -proc ps7_cortexa9_0 -app zynq_fsbl -compile -sw fsbl -dir $path_sdk/fsbl
+generate_app -hw system_0 -os standalone -proc ps7_cortexa9_0 -app zynq_fsbl -compile -sw fsbl -dir $path_sdk/fsbl
 
 exit
