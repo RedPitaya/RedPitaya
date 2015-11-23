b4f37dab1a4ac97f5f071d4681687e01f011b3de
TheMuffinMaster <luka.golinar@redpitaya.com>
2015-11-23 02:40:57 +0100

    A lot of changed in main.cpp. Everything is still work in progress.

--- Applications/lcr_meter/src/main.cpp
+++ Applications/lcr_meter/src/main.cpp
@@ -1,30 +1,76 @@
 
-#include "new_main.h"
+#include "main.h"
 
-#include <DataManager.h>
-#include <CustomParameters.h>
-//#include "../../rp_sdk/include/lcr_app.h"
-#include "../../rp_sdk/include/CustomParameters.h"
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include "version.h"
 
 /***************************************************************************************
 *                                     LCR METER                                        *
 ****************************************************************************************/
 
 //TODO make a more detailed parameters specification. More parameters soon to be added.
+CIntParameter parameterPeriiod("DEBUG_PARAM_PERIOD", CBaseParameter::RW, 200, 0, 0, 100);
 
-CIntParameter startMeasure("START_MEASURE", CBaseParameter::RW, -1, 0, -1, 3);
-CFloatParameter genAmplitude("GEN_AMPL", CBaseParameter::RW, 0, 0, 0, 1.0);
-CFloatParameter genAveraging("GEN_AVG", CBaseParameter::RW, 0, 0, 0, 10);
-CFloatParameter genDcBias("DC_BIAS", CBaseParameter::RW, 0, 0, -2, 2);
-
-CIntParameter measSteps("MEAS_STEPS", CBaseParameter::RW, 100, 0, 1, 1000);
-CFloatParameter genStartFreq("GEN_START_FREQ", CBaseParameter::RW, 1000, 0, 0, 1000000);
-CFloatParameter genEndFreq("GEN_END_FREQ", CBaseParameter::RW, 10000, 0, 0, 1000000);
-CIntParameter plotData("PLOT_DATA", CBaseParameter::RW, 0, 0, 0, 15);
-CIntParameter scaleType("SCALE_TYPE", CBaseParameter::RW, 0, 0, 0, 1);
-CFloatParameter genLoadRe("LOAD_RE", CBaseParameter::RW, 0, 0, 0, 1);
-CFloatParameter genLoadImg("LOAD_IMG", CBaseParameter::RW, 0, 0, 0, 1);
-CIntParameter genCalibration("GEN_CALIB", CBaseParameter::RW, 0, 0, 0, 3);
-CIntParameter genSaveData("SAVE_DATA", CBaseParameter::RW, 0, 0, 0, 1);
+CIntParameter dummyData("DUMMY_PARAM", CBaseParameter::RW, -1, 0, -1, 10);
+CBooleanParameter startMeasure("LCR_RUN", CBaseParameter::RW, false, 0);
 
 
+
+const char *rp_app_desc(void){
+	return (const char *)"Red Pitaya Lcr meter application.\n";
+}
+
+int rp_app_init(void){	
+	fprintf(stderr, "Loading lcr meter version %s-%s.\n", VERSION_STR, REVISION_STR);
+	CDataManager::GetInstance()->SetParamInterval(parameterPeriiod.Value());
+
+	//impApp_ImpInit();
+	return 0;
+}
+
+int rp_app_exit(void){
+	fprintf(stderr, "Unloading lcr meter version %s-%s.\n", VERSION_STR, REVISION_STR);
+	//impApp_ImpRelease();
+	return 0;
+}
+
+int rp_set_params(rp_app_params_t *p, int len) {
+    return 0;
+}
+
+int rp_get_params(rp_app_params_t **p) {
+    return 0;
+}
+
+int rp_get_signals(float ***s, int *sig_num, int *sig_len) {
+    return 0;
+}
+
+void UpdateSignals(void){}
+
+void UpdateParams(void){
+	CDataManager::GetInstance()->SetParamInterval(parameterPeriiod.Value());
+	CDataManager::GetInstance()->SendAllParams();
+
+	float amplitudez;
+
+	if(startMeasure.NewValue() == false){
+		dummyData.Value() = 2;
+	}else if(startMeasure.NewValue() == true){
+		lcrApp_LcrRun(&amplitudez);
+		dummyData.Value() = amplitudez;
+	}
+}
+
+void OnNewParams(void){
+
+	parameterPeriiod.Update();
+
+	UpdateParams();
+	//Returned changed data to the client.
+	CDataManager::GetInstance()->SendAllParams();
+}
+
+void OnNewSignals(void){}
