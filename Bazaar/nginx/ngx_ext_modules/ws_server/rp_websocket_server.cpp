d52609b4381bbca3c116e008c104c36ce9aeeb8e
a.solovev <a.solovev@integrasources.com>
2015-12-04 15:00:08 +0600

    delete fprintfs

--- Bazaar/nginx/ngx_ext_modules/ws_server/rp_websocket_server.cpp
+++ Bazaar/nginx/ngx_ext_modules/ws_server/rp_websocket_server.cpp
@@ -129,11 +129,9 @@ void rp_websocket_server::on_signal_timer(websocketpp::lib::error_code const & e
 	}
 
 	std::string js(signals);
-	//fprintf(stderr, "JS size %lu\n", js.size());
 	static char buf[1000000];
 	size_t size;
 	m_params->gzip_func(js.c_str(), buf, &size);
-	//fprintf(stderr, "AFTER GZIP size = %lu\n", size);
 
 	if (size) {
 		for (it = m_connections.begin(); it != m_connections.end(); ++it) {
@@ -163,11 +161,9 @@ void rp_websocket_server::on_param_timer(websocketpp::lib::error_code const & ec
 	}
 
 	std::string js(params);
-	fprintf(stderr, "JS param size %lu\n", js.size());
 	static char buf[1000000];
 	size_t size;
 	m_params->gzip_func(js.c_str(), buf, &size);
-	fprintf(stderr, "AFTER GZIP param size = %lu\n", size);
 
 	if (size) {
 		for (it = m_connections.begin(); it != m_connections.end(); ++it) {
