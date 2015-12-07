53cb35637621013320d363aaea3bdd220980e005
User <e.gutorov@integrasources.com>
2015-12-07 17:59:46 +0600

    Redmine #655; Fixed spectrum crash; Fixed scope crash; Added some debug info

--- Applications/scopegenpro/js/scope.js
+++ Applications/scopegenpro/js/scope.js
@@ -59,6 +59,12 @@
     // Volts
     1, 2, 5
   ];
+  
+  OSC.bad_connection = [ false, false, false, false ]; // time in s.
+  
+  OSC.compressed_data = 0;
+  OSC.decompressed_data = 0;
+  OSC.refresh_times = [];
 
   // Sampling rates
   OSC.sample_rates = ['125M', '15.625M', '1.953M', '122.070k', '15.258k', '1.907k'];
@@ -111,7 +117,24 @@
       console.log('Could not start the application (ERR3)');
     });
   };
+  
+	Date.prototype.format = function (mask, utc) {
+		return dateFormat(this, mask, utc);
+	};
+	
+  setInterval(function(){
+	var now = new Date();
+	var now_str = now.getHours() +":"+ now.getMinutes() +":"+ now.getSeconds()+":"+now.getMilliseconds();
+	var times = "";
+	for(var i=0; i<OSC.refresh_times.length; i++)
+		times += OSC.refresh_times[i] + " ";
 
+	console.log(now_str + " | compressed="+OSC.compressed_data + "; decompressed: "+OSC.decompressed_data + " | Updates: " + OSC.refresh_times.length + "; Delays: "+times);
+	OSC.compressed_data = 0;
+	OSC.decompressed_data = 0;
+	OSC.refresh_times = [];
+  }, 1000);
+  
   // Creates a WebSocket connection with the web server
   OSC.connectWebSocket = function() {
 
@@ -153,6 +176,8 @@
 	  var g_time = 0;
 	  var g_iter = 10;
 	  var g_delay = 200;
+	  var g_counter = 0;
+	  
       OSC.ws.onmessage = function(ev) {
 		++g_count;
 		var start_time = +new Date();
@@ -164,8 +189,12 @@
 
 		try {
 			var data = new Uint8Array(ev.data);
+			OSC.compressed_data += data.length;
 			var inflate = new Zlib.Gunzip(data);
 			var text = String.fromCharCode.apply(null, new Uint16Array(inflate.decompress()));
+			
+			OSC.decompressed_data += text.length;
+			
 			var receive = JSON.parse(text);
 
 			if(receive.parameters) {
@@ -183,13 +212,27 @@
 			}
 			OSC.state.processing = false;
 
-			g_time += (+new Date() - start_time);
+			var diff = new Date() - start_time;
+			g_time += diff;
+			OSC.refresh_times.push(diff);
+			
 			if (g_count == g_iter && OSC.params.orig['DEBUG_SIGNAL_PERIOD']) {
+				
 				g_delay = (g_time/g_count)*10; // TODO
-				if (g_delay > 300)
+				if (g_delay > 350)
+					OSC.bad_connection[g_counter] = true;
+				else
+					OSC.bad_connection[g_counter] = false;
+				
+				g_counter++; 
+				if(g_counter == 4) g_counter = 0;
+				
+				if(OSC.bad_connection[0] && OSC.bad_connection[1] && OSC.bad_connection[2] && OSC.bad_connection[3])
 					$('#weak_conn_msg').show();
 				else
 					$('#weak_conn_msg').hide();
+					
+				
 				var period = {};
 				period['DEBUG_SIGNAL_PERIOD'] = { value: g_delay };
 				OSC.ws.send(JSON.stringify({ parameters: period }));
@@ -198,9 +241,12 @@
 			}
 		}
 		catch (e) {
+			OSC.state.processing = false;
 			console.log(e);
 		}
-		finally {}
+		finally {
+			OSC.state.processing = false;
+		}
       };
     }
   };
