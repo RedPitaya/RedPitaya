74314ba6d642e32f4b38ff8ad306eab580bd5fbd
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-01-14 17:59:44 +0100

    FPGA: unfinished integration of new acq into LA

--- fpga2/rtl/la_top.sv
+++ fpga2/rtl/la_top.sv
@@ -12,8 +12,7 @@ module la_top #(
   // decimation parameters
   int unsigned DWC = 17,  // data width for counter
   // trigger parameters
-  int unsigned TWA =  4,          // external trigger array  width
-  int unsigned TWS = $clog2(TWA)  // external trigger select width
+  int unsigned TWA =  4   // external trigger array  width
 )(
   // streams
   str_bus_if.d                  sti,      // input
@@ -34,15 +33,38 @@ module la_top #(
 str_bus_if #(.DAT_T (DAT_T)) stf (.clk (sti.clk), .rstn (sti.rstn));  // from filter
 str_bus_if #(.DAT_T (DAT_T)) std (.clk (sti.clk), .rstn (sti.rstn));  // from decimator
 
+////////////////////////////////////////////////////////////////////////////////
+// acquire regset
+////////////////////////////////////////////////////////////////////////////////
+
+// current time stamp
+logic [TW-1:0] cts;
 // control
-logic                  ctl_rst;  // synchronous clear
-logic                  ctl_acq;  // start acquire run
-// status
-logic                  sts_acq;  // acquire status
+logic          ctl_rst;
+// configuration (mode)
+logic          cfg_con;  // continuous
+logic          cfg_aut;  // automatic
+// configuration/status pre trigger
+logic [CW-1:0] cfg_pre;
+logic [CW-1:0] sts_pre;
+// configuration/status post trigger
+logic [CW-1:0] cfg_pst;
+logic [CW-1:0] sts_pst;
+// control/status/timestamp acquire
+logic          ctl_acq;  // acquire start
+logic          sts_acq;
+logic [TW-1:0] cts_acq;
+// control/status/timestamp trigger
+logic          ctl_trg;
+logic          sts_trg;
+logic [TW-1:0] cts_trg;
+// control/status/timestamp stop
+logic          ctl_stp;  // acquire stop
+logic [TW-1:0] cts_stp;
+
 // trigger
-logic        [TWS-1:0] cfg_sel;  // trigger select
-logic        [ 32-1:0] cfg_dly;  // delay value
-logic        [ 32-1:0] sts_dly;  // delay counter
+logic        [TWA-1:0] cfg_trg;  // trigger select
+
 // trigger source configuration
 DAT_T                  cfg_old_val;  // old     value
 DAT_T                  cfg_old_msk;  // old     mask
@@ -76,11 +98,12 @@ end
 // write access
 always @(posedge bus.clk)
 if (~bus.rstn) begin
-  // control
-  ctl_acq <= 1'b0;
-  // trigger
-  cfg_sel <= '0;
-  cfg_dly <= '0;
+  // acquire regset
+  cfg_con <= 1'b0;
+  cfg_aut <= 1'b0;
+  cfg_trg <= '0;
+  cfg_pre <= '0;
+  cfg_pst <= '0;
   // trigger detection
   cfg_old_val <= '0;
   cfg_old_msk <= '0;
@@ -90,9 +113,14 @@ if (~bus.rstn) begin
   cfg_dec <= '0;
 end else begin
   if (bus.wen) begin
+    // acquire regset
+    if (bus.addr[6-1:0]==6'h04)   cfg_con <= bus.wdata[0];
+    if (bus.addr[6-1:0]==6'h04)   cfg_aut <= bus.wdata[1];
+    if (bus.addr[6-1:0]==6'h08)   cfg_trg <= bus.wdata[TWA-1:0];
+    if (bus.addr[6-1:0]==6'h10)   cfg_pre <= bus.wdata[CW-1:0];
+    if (bus.addr[6-1:0]==6'h14)   cfg_pst <= bus.wdata[CW-1:0];
+
     // trigger
-    if (bus.addr[6-1:0]==6'h08)   cfg_sel <= bus.wdata[TWS-1:0];
-    if (bus.addr[6-1:0]==6'h0c)   cfg_dly <= bus.wdata[ 32-1:0];
     // trigger detection
     if (bus.addr[6-1:0]==6'h10)   cfg_old_val <= DAT_T'(bus.wdata);
     if (bus.addr[6-1:0]==6'h14)   cfg_old_msk <= DAT_T'(bus.wdata);
@@ -104,20 +132,27 @@ end else begin
 end
 
 // control signals
+assign trg_swo = bus.wen & (bus.addr[6-1:0]==6'h00) & bus.wdata[3];  // trigger
+assign ctl_acq = bus.wen & (bus.addr[6-1:0]==6'h00) & bus.wdata[1];  // acquire start
+assign ctl_stp = bus.wen & (bus.addr[6-1:0]==6'h00) & bus.wdata[2];  // acquire stop
 assign ctl_rst = bus.wen & (bus.addr[6-1:0]==6'h00) & bus.wdata[0];  // reset
-assign trg_swo = bus.wen & (bus.addr[6-1:0]==6'h00) & bus.wdata[1];  // trigger
-assign sts_run = bus.wen & (bus.addr[6-1:0]==6'h00) & bus.wdata[2];  // run acquire
 
 // read access
 always_ff @(posedge bus.clk)
 begin
   casez (bus.addr[19:0])
-    // control/status
-    6'h00 : bus.rdata <= {{32-  3{1'b0}}, sts_acq,
-                                          sts_trg, 1'b0};
-    // trigger
-    6'h08 : bus.rdata <= {{32-TWS{1'b0}}, cfg_sel}; 
-    6'h0c : bus.rdata <=                  cfg_dly ;
+    // acquire regset
+    6'h00 : bus.rdata <= {{32-  4{1'b0}}, sts_trg, ~sts_acq, sts_acq, 1'b0};
+    6'h04 : bus.rdata <= {{32-  2{1'b0}}, cfg_aut, cfg_con};
+    6'h08 : bus.rdata <= {{32-TWA{1'b0}}, cfg_trg};
+    6'h10 : bus.rdata <= {{32- CW{1'b0}}, cfg_pre};
+    6'h14 : bus.rdata <= {{32- CW{1'b0}}, cfg_pst};
+    6'h18 : bus.rdata <= {{32- CW{1'b0}}, sts_pre};
+    6'h1c : bus.rdata <= {{32- CW{1'b0}}, sts_pst};
+    6'h20 : bus.rdata <= {{32- TW{1'b0}}, cts_acq};
+    6'h24 : bus.rdata <= {{32- TW{1'b0}}, cts_trg};
+    6'h28 : bus.rdata <= {{32- TW{1'b0}}, cts_stp};
+
     // trigger detection
     6'h10 : bus.rdata <=                  cfg_old_val;
     6'h14 : bus.rdata <=                  cfg_old_msk;
@@ -170,23 +205,39 @@ la_trigger #(
 // aquire and trigger status handler
 ////////////////////////////////////////////////////////////////////////////////
 
-assign trg_mux = trg_ext [cfg_sel];
+assign trg_mux = trg_ext [cfg_trg];
 
-acq #(.DW (DWO), .CW (32)) acq (
-  // streams
-  .sti      (std),
+acq #(
+  .TW (TW),
+  .CW (CW)
+) acq (
+  // stream input/output
+  .sti      (sti),
   .sto      (sto),
+  // current time stamp
+  .cts      (cts),
   // control
   .ctl_rst  (ctl_rst),
-  // delay configuration/status
-  .cfg_dly  (cfg_dly),
-  .sts_dly  (sts_dly),
-  // acquire control/status
-  .ctl_acq  (ctl_acq),
+  // configuration (mode)
+  .cfg_con  (cfg_con),
+  .cfg_aut  (cfg_aut),
+  // configuration/status pre trigger
+  .cfg_pre  (cfg_pre),
+  .sts_pre  (sts_pre),
+  // configuration/status post trigger
+  .cfg_pst  (cfg_pst),
+  .sts_pst  (sts_pst),
+  // control/status/timestamp acquire
+  .ctl_acq  (ctl_acq),  // acquire start
   .sts_acq  (sts_acq),
-  // trigger control/status
+  .cts_acq  (cts_acq),
+  // control/status/timestamp trigger
   .ctl_trg  (ctl_trg),
-  .sts_trg  (sts_trg)
+  .sts_trg  (sts_trg),
+  .cts_trg  (cts_trg),
+  // control/status/timestamp stop
+  .ctl_stp  (ctl_stp),  // acquire stop
+  .cts_stp  (cts_stp)
 );
 
 endmodule: la_top
