8c9b5d582dbe30c5b7dc2857dd726f78e4d06f6e
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-01-15 15:53:37 +0100

    FPGA: changed handling of some parameters

--- fpga2/rtl/asg_top.sv
+++ fpga2/rtl/asg_top.sv
@@ -26,9 +26,8 @@
 module asg_top #(
   type DAT_T = logic [8-1:0],
   // data parameters
-  int unsigned DWO = 14,  // RAM data width
   int unsigned DWM = 16,  // data width for multiplier (gain)
-  int unsigned DWS = DWO, // data width for summation (offset)
+  int unsigned DWS = 14,  // data width for summation (offset)
   // buffer parameters
   int unsigned CWM = 14,  // counter width magnitude (fixed point integer)
   int unsigned CWF = 16,  // counter width fraction  (fixed point fraction)
@@ -45,10 +44,14 @@ module asg_top #(
   sys_bus_if.s           bus
 );
 
+// RAM data width
+parameter int unsigned DWO = $bits(DAT_T);
+
 ////////////////////////////////////////////////////////////////////////////////
 // read/write access to buffer
 ////////////////////////////////////////////////////////////////////////////////
 
+// TODO: the generic bus decoder should be used instead
 sys_bus_if #(.DW (DWO), .AW (CWM)) bus_buf (.clk (clk), .rstn (rstn));
 
 assign bus_buf.ren   = bus.ren & bus.addr[CWM+2];
@@ -205,8 +208,8 @@ asg #(
 // TODO: this will be a continuous stream, data stream control needs rethinking
 
 linear #(
-  .DWI (DWO),
-  .DWO (DWO),
+  .DTI (DAT_T),
+  .DTO (DAT_T),
   .DWM (DWM)
 ) linear (
   // stream input/output
