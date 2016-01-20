6755438bf651da048d39185b996be6455266bc46
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-01-20 17:45:30 +0100

    API: started testing on the RP board

--- api2/test/digital_led_blink.c
+++ api2/test/digital_led_blink.c
@@ -3,10 +3,12 @@
 #include <unistd.h>
 
 #include "redpitaya/rp2.h"
+#include "housekeeping.h"
 
 int main (int argc, char **argv) {
     int unsigned period = 1000000; // uS
     int unsigned led;
+    rp_handle_uio_t handle;
 
     // index of blinking LED can be provided as an argument
     if (argc > 1) {
@@ -19,21 +21,21 @@ int main (int argc, char **argv) {
     led += RP_LED0;
 
     // Initialization of API
-    if (rp_HousekeepingInit() != RP_OK) {
+    if (rp_HousekeepingInit("/dev/uio0", &handle) != RP_OK) {
         fprintf(stderr, "Red Pitaya API init failed!\n");
         return EXIT_FAILURE;
     }
 
     int unsigned retries = 1000;
     while (retries--){
-        rp_DpinSetState(led, RP_HIGH);
+        rp_DpinSetState(&handle, led, RP_HIGH);
         usleep(period/2);
-        rp_DpinSetState(led, RP_LOW);
+        rp_DpinSetState(&handle, led, RP_LOW);
         usleep(period/2);
     }
 
     // Releasing resources
-    rp_Release();
+    rp_HousekeepingRelease(&handle);
 
     return EXIT_SUCCESS;
 }
