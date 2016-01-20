d7fb7941f0ed7576129626a284c56df4376c7f6e
Črt Valentinčič <crt@MacBook-Pro.local>
2016-01-20 21:52:29 +0100

    CUnit features tested; an example suite was made

--- api2/utests/ut_example.c
+++ api2/utests/ut_example.c
@@ -1,35 +1,14 @@
 /*
  *  Simple example of a CUnit unit test.
- *
- *  This program (crudely) demonstrates a very simple "black box"
- *  test of the standard library functions fprintf() and fread().
- *  It uses suite initialization and cleanup functions to open
- *  and close a common temporary file used by the test functions.
- *  The test functions then write to and read from the temporary
- *  file in the course of testing the library functions.
- *
- *  The 2 test functions are added to a single CUnit suite, and
- *  then run using the CUnit Basic interface.  The output of the
- *  program (on CUnit version 2.0-2) is:
- *
- *           CUnit : A Unit testing framework for C.
- *           http://cunit.sourceforge.net/
- *
- *       Suite: Suite_1
- *         Test: test of fprintf() ... passed
- *         Test: test of fread() ... passed
- *
- *       --Run Summary: Type      Total     Ran  Passed  Failed
- *                      suites        1       1     n/a       0
- *                      tests         2       2       2       0
- *                      asserts       5       5       5       0
  */
 
 #include <stdio.h>
 #include <string.h>
-#include "CUnit/Basic.h"
 
-#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+#include "CUnit/Console.h"
+#include "CUnit/Automated.h"
+#include "CUnit/CUCurses.h"
 
 /* Pointer to the file used by the tests. */
 static FILE* temp_file = NULL;
@@ -94,6 +73,107 @@ void testFREAD(void)
    }
 }
 
+/** Example Suite */
+
+int init_example_suite(void){
+    return 0;
+    // return -1; in case of error
+}
+
+int clean_example_suite(void){
+    return 0;
+    // return -1; in case of error
+}
+
+void pass_assertions_test(void)
+{
+	// Notice: FATAL versions of assertions - failure will abort the original
+	// test function and its entire call chain.
+
+	/* Assert that expression is TRUE (non-zero) */
+	CU_ASSERT(0==0);
+	CU_ASSERT_FATAL(1==1);
+	CU_TEST(2==2);
+	CU_TEST_FATAL(3==3);
+	/* Assert that value is TRUE (non-zero); */
+	CU_ASSERT_TRUE(1);
+	CU_ASSERT_TRUE_FATAL(2.0);
+	/* Assert that value is FALSE (zero) */
+	CU_ASSERT_FALSE(0);
+	CU_ASSERT_FALSE_FATAL(0.0);
+	/* Assert that actual = = expected */
+	CU_ASSERT_EQUAL(0, 0)
+	CU_ASSERT_EQUAL_FATAL(1, 1)
+	/* Assert that actual != expected */
+	CU_ASSERT_NOT_EQUAL(1, 0);
+	CU_ASSERT_NOT_EQUAL_FATAL(4, 3);
+	/* Assert that pointers actual = = expected */
+	int a=10, b=6;
+	int *ptr=&a;
+	CU_ASSERT_PTR_EQUAL(&a, ptr);
+	CU_ASSERT_PTR_EQUAL_FATAL(&a, ptr);
+	/* Assert that pointers actual != expected */
+	CU_ASSERT_PTR_NOT_EQUAL(&a, &b);
+	CU_ASSERT_PTR_NOT_EQUAL_FATAL(&a, &b);
+	/* Assert that pointer value == NULL */
+	ptr=NULL;
+	CU_ASSERT_PTR_NULL(ptr);
+	CU_ASSERT_PTR_NULL_FATAL(ptr);
+	/* Assert that pointer value != NULL */
+	ptr=&a;
+	CU_ASSERT_PTR_NOT_NULL(ptr);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(ptr);
+	/* Assert that strings actual and expected are equivalent */
+	CU_ASSERT_STRING_EQUAL("test", "test");
+	CU_ASSERT_STRING_EQUAL_FATAL("test", "test");
+	/* Assert that strings actual and expected are equivalent */
+	CU_ASSERT_STRING_NOT_EQUAL("test", "rest");
+	CU_ASSERT_STRING_NOT_EQUAL_FATAL("test", "rest");
+    /* Assert that 1st count chars of actual and expected are the same */
+	CU_ASSERT_NSTRING_EQUAL("testxxx", "testyyy", 4);
+	CU_ASSERT_NSTRING_EQUAL_FATAL("testxxx", "testyyy", 4);
+	/* Assert that 1st count chars of actual and expected differ */
+	CU_ASSERT_NSTRING_NOT_EQUAL("xxxtest", "yyytest", 3);
+	CU_ASSERT_NSTRING_NOT_EQUAL_FATAL("xxxtest", "yyytest", 3);
+    /* Assert that |actual - expected| <= |granularity|
+	   Math library must be linked in for this assertion. */
+	CU_ASSERT_DOUBLE_EQUAL(1.0, 1.01, 0.02);
+	CU_ASSERT_DOUBLE_EQUAL_FATAL(-1.0, -1.1, 0.1000001);
+	/* Assert that |actual - expected| > |granularity|
+	Math library must be linked in for this assertion. */
+	CU_ASSERT_DOUBLE_NOT_EQUAL(1, 1.1, 0.05);
+	CU_ASSERT_DOUBLE_NOT_EQUAL_FATAL(1, 1.1, 0.1);
+	/* Register a passing assertion with the specified message.
+	   No logical test is performed. */
+	CU_PASS("Test has passed..");
+}
+
+void fail_assertions_test(void)
+{
+	/* other */
+	CU_FAIL("Problem here!")
+	CU_FAIL_FATAL("This one will abort this test..");
+}
+
+void cu_add_test_macro_test(void)
+{
+
+}
+
+/** simplified solution with tables */
+
+CU_TestInfo example_test_array[] = {
+  { "pass assertions test", pass_assertions_test},
+  { "fail assertions test", fail_assertions_test },
+  CU_TEST_INFO_NULL,
+};
+
+CU_SuiteInfo suites[] = {
+  { "SuiteExampleTest", init_example_suite, clean_example_suite, example_test_array},
+  //{ "suitename2", suite2_init-func, suite2_cleanup_func, test_array2 },
+  CU_SUITE_INFO_NULL,
+};
+
 /* The main() function for setting up and running the tests.
  * Returns a CUE_SUCCESS on successful running, another
  * CUnit error code on failure.
@@ -106,6 +186,47 @@ int main()
    if (CUE_SUCCESS != CU_initialize_registry())
       return CU_get_error();
 
+   /* ADD SUITS BELOW THIS COMMENT */
+
+   /* EXAMPLE SUIT */
+   /* add a suite to the registry - give it unique name
+    * init and clean functions allows the suite to set up and tear down temporary
+    * fixtures to support running the tests.
+    * These functions take no arguments and should return zero if they are
+    * completed successfully (non-zero otherwise).
+    * If a suite does not require one or both of these functions, pass NULL to CU_add_suite.
+    * */
+
+   pSuite = CU_add_suite("SuiteExampleTest", init_example_suite, clean_example_suite);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   if ((NULL == CU_add_test(pSuite, "pass assertions test", pass_assertions_test)) ||
+       (NULL == CU_add_test(pSuite, "fail assertions test", fail_assertions_test)) ||
+	   (NULL == CU_ADD_TEST(pSuite, cu_add_test_macro_test))) // simplified - name will be generated automatically
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* during development, you can simply run a single suit or test */
+   CU_basic_run_suite(pSuite);
+   CU_pTest pTest=CU_add_test(pSuite, "fail assertions test", fail_assertions_test);
+   CU_basic_run_test(pSuite, pTest);
+
+   /* simplified solution - with tables - replaces all upper code */
+   /*
+   if(CUE_SUCCESS!=CU_register_suites(suites)){
+	   CU_cleanup_registry();
+	   return CU_get_error();
+   }
+   */
+
+
+   /* SUITE 1 */
    /* add a suite to the registry */
    pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
    if (NULL == pSuite) {
@@ -122,9 +243,31 @@ int main()
       return CU_get_error();
    }
 
+
+
+   /* RUN TEST / SELECT INTERFACE  */
    /* Run all tests using the CUnit Basic interface */
+   /*
+   CU_BRM_NORMAL	Failures and run summary are printed.
+   CU_BRM_SILENT	No output is printed except error messages.
+   CU_BRM_VERBOSE	Maximum output of run details.
+   */
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
+   CU_basic_show_failures(CU_get_failure_list());
+   printf("\n");
+
+   /* Run all tests using the console interface */
+   // CU_console_run_tests();
+
+   /* Run all tests using the curses interface */
+   // CU_curses_run_tests();
+
+   /* Run automated interface (non-interactive) that will generate XML file. */
+   //CU_automated_run_tests();
+   //CU_list_tests_to_file();
+
+   /* CLEAN UP */
    CU_cleanup_registry();
    return CU_get_error();
 }
