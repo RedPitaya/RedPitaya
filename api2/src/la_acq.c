295637de5d742840159bd6bc2103d507fd2c1c52
crt.valentincic <crt.valentincic@redpitaya.com>
2016-01-25 23:00:16 +0100

    API: la acq. api functions added

--- api2/src/la_acq.c
+++ api2/src/la_acq.c
@@ -1,2 +1,170 @@
+/**
+ * $Id: $
+ *
+ * @brief Red Pitaya library Logic analyzer acquisition module interface
+ *
+ * @Author Red Pitaya
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
 
 #include "la_acq.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rp2.h"
+
+int rp_LaGenOpen(char *dev, rp_handle_uio_t *handle) {
+    // make a copy of the device path
+    handle->dev = (char*) malloc((strlen(dev)+1) * sizeof(char));
+    strncpy(handle->dev, dev, strlen(dev)+1);
+
+    // try opening the device
+    handle->fd = open(handle->dev, O_RDWR);
+    if (handle->fd==-1) {
+        return -1;
+    }
+
+    // get regset pointer
+    handle->regset = mmap(NULL, sizeof(la_acq_regset_t), PROT_READ|PROT_WRITE, MAP_SHARED, handle->fd, 0x0);
+    if (handle->regset == MAP_FAILED) {
+    	return -1;
+    }
+
+    return RP_OK;
+}
+
+int rp_LaGenClose(rp_handle_uio_t *handle) {
+	int r=RP_OK;
+
+	// release regset
+    if(munmap((void *) handle->regset, sizeof(la_acq_regset_t))!=0){
+    	r=-1;
+    }
+
+    // close device
+    if(close (handle->fd)!=0){
+    	r=-1;
+    }
+
+    // free device path
+    free(handle->dev);
+
+    // free name
+    free(handle->name);
+
+    return r;
+}
+
+/** Control registers setter & getter */
+static int rp_LaGenSetControl(rp_handle_uio_t *handle, rp_la_ctl_regset_t a_reg) {
+	rp_la_ctl_regset_t *regset = (rp_la_ctl_regset_t *) handle->regset.ctl;
+	iowrite32(a_reg.ctl, &regset->ctl);
+    return RP_OK;
+}
+
+
+static int rp_LaGenGetControl(rp_handle_uio_t *handle, rp_la_ctl_regset_t * a_reg) {
+
+	rp_la_ctl_regset_t *regset = (rp_la_ctl_regset_t *) handle->regset.ctl;
+	iowrite32(a_reg.ctl, &regset->ctl);
+    return RP_OK;
+}
+
+
+/** Acq. control */
+int rp_LaGenReset(rp_handle_uio_t *handle) {
+	rp_la_ctl_regset_t reg;
+	reg.ctl=RP_LA_ACQ_CTL_RST_MASK;
+	rp_LaGenSetControl(reg);
+}
+
+int rp_LaGenRunAcq(rp_handle_uio_t *handle) {
+	rp_la_ctl_regset_t reg;
+	reg.ctl=RP_LA_ACQ_CTL_RST_MASK;
+	rp_LaGenSetControl(reg);
+}
+
+int rp_LaGenStopAcq(rp_handle_uio_t *handle) {
+	rp_la_ctl_regset_t reg;
+	reg.ctl=RP_LA_ACQ_CTL_STO_MASK;
+	rp_LaGenSetControl(reg);
+}
+
+int rp_LaGenTriggerAcq(rp_handle_uio_t *handle) {
+	rp_la_ctl_regset_t reg;
+	reg.ctl=RP_LA_ACQ_CTL_SWT_MASK;
+	rp_LaGenSetControl(reg);
+}
+
+/** Configuration registers setter & getter */
+int rp_LaGenSetConfig(rp_handle_uio_t *handle, rp_la_cfg_regset_t a_reg) {
+	rp_la_cfg_regset_t *regset = (rp_la_cfg_regset_t *) handle->regset.cfg;
+	iowrite32(a_reg.acq, &regset->acq);
+	iowrite32(a_reg.trg, &regset->trg);
+	iowrite32(a_reg.pre, &regset->pre);
+	iowrite32(a_reg.pst, &regset->pst);
+    return RP_OK;
+}
+
+int rp_LaGenGetConfig(rp_handle_uio_t *handle, rp_la_cfg_regset_t * a_reg) {
+	rp_la_cfg_regset_t *regset = (rp_la_cfg_regset_t *) handle->regset.cfg;
+	*a_reg->acq = ioread32(&regset->acq);
+	*a_reg->trg = ioread32(&regset->trg);
+	*a_reg->pre = ioread32(&regset->pre);
+	*a_reg->pst = ioread32(&regset->pst);
+    return RP_OK;
+}
+
+/** Trigger settings setter & getter */
+int rp_LaGenSetTrigSettings(rp_handle_uio_t *handle, rp_la_trg_regset_t a_reg) {
+	rp_la_trg_regset_t *regset = (rp_la_trg_regset_t *) handle->regset.trg;
+	iowrite32(a_reg.cmp_msk, &regset->cmp_msk);
+	iowrite32(a_reg.cmp_val, &regset->cmp_val);
+	iowrite32(a_reg.edg_pos, &regset->edg_pos);
+	iowrite32(a_reg.edg_neg, &regset->edg_neg);
+    return RP_OK;
+}
+
+int rp_LaGenGetTrigSettings(rp_handle_uio_t *handle, rp_la_trg_regset_t * a_reg) {
+	rp_la_trg_regset_t *regset = (rp_la_trg_regset_t *) handle->regset.trg;
+	*a_reg->cmp_msk = ioread32(&regset->cmp_msk);
+	*a_reg->cmp_val = ioread32(&regset->cmp_val);
+	*a_reg->edg_pos = ioread32(&regset->edg_pos);
+	*a_reg->edg_neg = ioread32(&regset->edg_neg);
+    return RP_OK;
+}
+
+/** Decimation settings setter & getter */
+int rp_LaGenSetDecimation(rp_handle_uio_t *handle, rp_la_decimation_regset_t a_reg) {
+	rp_la_decimation_regset_t *regset = (rp_la_decimation_regset_t *) handle->regset.dec;
+	//iowrite32(a_reg.avg, &regset->avg);
+	iowrite32(a_reg.dec, &regset->dec);
+	//iowrite32(a_reg.shr, &regset->shr);
+	return RP_OK;
+}
+
+int rp_LaGenGetDecimation(rp_handle_uio_t *handle, rp_la_decimation_regset_t * a_reg) {
+	rp_la_decimation_regset_t *regset = (rp_la_decimation_regset_t *) handle->regset.dec;
+	//*a_reg->avg = ioread32(&regset->avg);
+	*a_reg->dec = ioread32(&regset->dec);
+	//*a_reg->shr = ioread32(&regset->shr);
+    return RP_OK;
+}
+
+/** Data buffer pointers */
+int rp_LaGenGetDataPointers(rp_handle_uio_t *handle, rp_data_ptrs_regset_t * a_reg) {
+	rp_data_ptrs_regset_t *regset = (rp_data_ptrs_regset_t *) handle->regset.dpt;
+	*a_reg->start = ioread32(&regset->start);
+	*a_reg->trig = ioread32(&regset->trig);
+	*a_reg->stopped = ioread32(&regset->stopped);
+    return RP_OK;
+}
--- api2/src/la_acq.h
+++ api2/src/la_acq.h
@@ -11,20 +11,60 @@
  * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
  * for more details on the language used herein.
  */
+
+// For more information see: /doc/logic/README.md & la_top.sv
+
 #ifndef __LA_ACQ_H
 #define __LA_ACQ_H
 
-// For more information see: /doc/logic/README.md
+#include <stdint.h>
 
-/** trigger settings  */
-struct {
+#include "rp2.h"
+
+/** control register masks  */
+#define RP_LA_ACQ_CTL_RST_MASK (1<<0) ///< 1 - reset acq. state machine (TODO: check what exactly it does)
+#define RP_LA_ACQ_CTL_STA_MASK (1<<1) ///< 1 - starts acq.; 0 - when acq. is stopped
+#define RP_LA_ACQ_CTL_STO_MASK (1<<2) ///< 1 - stops acq. (also used to about the acq.)
+#define RP_LA_ACQ_CTL_SWT_MASK (1<<3) ///< 1 - triggers acq. (sw trigger must be enabled)
+
+
+/** configuration register  */
+#define RP_LA_ACQ_CFG_CONT_MASK (1<<0) ///< if set acq. will work in continuous mode and will not stop after post trig.
+#define RP_LA_ACQ_CFG_AUTO_MASK (1<<1) ///< if set acq. will be triggered when started (only post trig. acq.)
+
+/** pre & post trigger mask  */
+#define RP_LA_ACQ_CFG_TRIG_MAX ((1<<26)-1) ///< max. number of samples for pre & post trigger
+
+/** TODO: trigger enable mask  */
+#define RP_LA_TRI_EN_SWT_MASK (1<<0) ///< software trigger mask
+#define RP_LA_TRI_EN_EXT_MASK (1<<1) ///< external trigger mask
+#define RP_LA_TRI_EN_LGA_MASK (1<<2) ///< logic analyzer trigger mask
+#define RP_LA_TRI_EN_OSC_MASK (1<<3) ///< oscilloscope trigger mask
+
+
+/** Control registers */
+typedef struct {
+    uint32_t ctl;
+} rp_la_ctl_regset_t;
+
+
+/** Configuration registers */
+typedef struct {
+    uint32_t acq; 	///< acq. configuration register
+    uint32_t trg; 	///< trig. enable mask, triggers are ORed
+    uint32_t pre; 	///< pre-trigger [number of samples] ; 0 - means that no sample will be taken before trigger
+	uint32_t pst; 	///< post-trigger [number of samples] ; 0 - means that no sample will be taken after trigger
+} rp_la_cfg_regset_t;
+
+/** Trigger settings  */
+typedef struct {
     uint32_t cmp_msk; ///<  digital comparator mask
     uint32_t cmp_val; ///<  digital comparator value
     uint32_t edg_pos; ///<  digital edge positive mask
     uint32_t edg_neg; ///<  digital edge negative mask
-} regset_la_trg_t;
+} rp_la_trg_regset_t;
 
-/** decimation  */
+/** Decimation  */
 typedef struct {
     uint32_t avg;  ///< (not used by logic analyzer!)
     uint32_t dec;  ///<  sample rate = 125Msps/(dec+1)
@@ -40,42 +80,35 @@ typedef struct {
     int32_t  pp;   ///<
 } rp_adc_eqfilter_regset_t;
 
-/** control register masks  */
-#define RP_LA_ACQ_CTL_RST_MASK (1<<0) ///< 1 - reset acq. state machine (TODO: check what exactly it does)
-#define RP_LA_ACQ_CTL_STA_MASK (1<<1) ///< 1 - starts acq.; 0 - when acq. is stopped
-#define RP_LA_ACQ_CTL_STO_MASK (1<<2) ///< 1 - stops acq. (also used to about the acq.)
-#define RP_LA_ACQ_CTL_SWT_MASK (1<<3) ///< 1 - triggers acq. (sw trigger must be enabled)
-
-/** configuration register  */
-#define RP_LA_ACQ_CFG_CONT_MASK (1<<0) ///< if set acq. will work in continuous mode and will not stop after post trig.
-#define RP_LA_ACQ_CFG_AUTO_MASK (1<<1) ///< if set acq. will be triggered when started (only post trig. acq.)
-
-/** pre & post trigger mask  */
-#define RP_LA_ACQ_TRIG_MAX ((1<<26)-1) ///< max. number of samples for pre & post trigger
-
-/** TODO: trigger enable mask  */
-#define RP_LA_TRI_EN_SWT_MASK (1<<0) ///< software trigger mask
-#define RP_LA_TRI_EN_EXT_MASK (1<<1) ///< external trigger mask
-#define RP_LA_TRI_EN_LGA_MASK (1<<2) ///< logic analyzer trigger mask
-#define RP_LA_TRI_EN_LGA_MASK (1<<3) ///< oscilloscope trigger mask
+/** Data buf. pointers  */
+typedef struct {
+    uint32_t start;  	///< position in the buffer where acq. was started
+    uint32_t trig;   	///< position in the buffer where trigger appeared
+    uint32_t stopped;	///< position in the buffer where acq. was stopped
+} rp_data_ptrs_regset_t;
 
 /** logic analyzer acquire structure declaration */
 typedef struct {
-    uint32_t ctl; 		///< control register
-    uint32_t cfg; 		///< configuration register
-    uint32_t cfg_trg; 	///< trig. enable mask, triggers are ORed
-    uint32_t cfg_pre; 	///< pre-trigger [number of samples] ; 0 - means that no sample will be taken before trigger
-	uint32_t cfg_pst; 	///< post-trigger [number of samples] ; 0 - means that no sample will be taken after trigger
-    regset_la_trg_t    trg;
-    rp_scope_decimation_regset_t dec;
-    rp_adc_eqfilter_regset_t     fil; // (TODO: this filter will be removed for LA)
-    // TODO: data pointer are missing (start, trigger, stopped)
-} acq_regset_t;
+	rp_la_ctl_regset_t ctl; 		///< control register
+    rp_la_cfg_regset_t cfg; 		///< configuration registers
+    rp_la_trg_regset_t trg;			///< trigger settings register
+    rp_la_decimation_regset_t dec;  ///< decimation
+    rp_adc_eqfilter_regset_t fil;   ///< (TODO: this filter will be removed for LA)
+    rp_data_ptrs_regset_t dpt;		///< data buf. pointers
+} rp_la_acq_regset_t;
 
-// Expected procedure:
-// set trig.
-// start acg.
-// irq - > when it stops
-// stop (abort acq.)
+
+int rp_LaGenOpen(char *dev, rp_handle_uio_t *handle);
+int rp_LaGenClose(rp_handle_uio_t *handle);
+int rp_LaGenReset(rp_handle_uio_t *handle);
+int rp_LaGenRunAcq(rp_handle_uio_t *handle);
+int rp_LaGenTriggerAcq(rp_handle_uio_t *handle);
+int rp_LaGenSetConfig(rp_handle_uio_t *handle, rp_la_cfg_regset_t a_reg);
+int rp_LaGenGetConfig(rp_handle_uio_t *handle, rp_la_cfg_regset_t * a_reg);
+int rp_LaGenSetTrigSettings(rp_handle_uio_t *handle, rp_la_trg_regset_t a_reg);
+int rp_LaGenGetTrigSettings(rp_handle_uio_t *handle, rp_la_trg_regset_t * a_reg);
+int rp_LaGenSetDecimation(rp_handle_uio_t *handle, rp_la_decimation_regset_t a_reg);
+int rp_LaGenGetDecimation(rp_handle_uio_t *handle, rp_la_decimation_regset_t * a_reg);
+int rp_LaGenGetDataPointers(rp_handle_uio_t *handle, rp_data_ptrs_regset_t * a_reg);
 
 #endif // __LA_ACQ_H
