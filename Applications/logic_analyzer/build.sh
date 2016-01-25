646c9237ad682f7d420127a5a92634265e2b7b35
A. Kaygorodov <a.kaygorodov@integrasources.com>
2016-01-25 15:20:45 +0600

    Renamed application

--- Applications/logic_analyzer/build.sh
+++
@@ -1,8 +0,0 @@
-source /opt/Xilinx/Vivado/2015.2/settings64.sh
-export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf
-export PATH=$TOOLCHAIN_PATH/bin:$PATH
-export CROSS_COMPILE=arm-linux-gnueabihf-
-export BR2_DL_DIR=`pwd`/workspace/buildroot/dl
-export SYSROOT=`pwd`/../../OS/buildroot/buildroot-2014.02/output/host/usr/arm-buildroot-linux-gnueabihf/sysroot
-mkdir -p $BR2_DL_DIR
-make
