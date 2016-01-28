df9a492ceb7ea8ba82be5505acf7b24c578e4eb2
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-01-28 10:39:20 +0100

    FPGA: avoiding issues in simulation

--- fpga2/tbn/system_wrapper.sv
+++ fpga2/tbn/system_wrapper.sv
@@ -45,7 +45,7 @@ module system_wrapper #(
   inout  logic        FIXED_IO_ps_srstb,
 
   input  logic        M_AXI_GP0_ACLK   ,
-  input  logic        M_AXI_GP0_ARESETn,
+//input  logic        M_AXI_GP0_ARESETn,
   output logic [31:0] M_AXI_GP0_araddr ,
   output logic  [1:0] M_AXI_GP0_arburst,
   output logic  [3:0] M_AXI_GP0_arcache,
@@ -175,7 +175,8 @@ assign FCLK_RESET3_N = rstn;
 
 axi_bus_if #(.DW (32), .AW (32), .IW (12), .LW (4)) axi_gp (
   .ACLK    (M_AXI_GP0_ACLK   ),
-  .ARESETn (M_AXI_GP0_ARESETn)
+//.ARESETn (M_AXI_GP0_ARESETn)
+  .ARESETn (S_AXI_STR_RX0_arstn)
 );
 
 assign                  M_AXI_GP0_araddr  = axi_gp.ARADDR ;
