7940e786eadb168479d7e0cc4351a8f7fa0cd114
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-02-10 12:36:39 +0100

    DMA: updated RX test

--- Test/rpdma/rx/rpdma_test.c
+++ Test/rpdma/rx/rpdma_test.c
@@ -5,53 +5,51 @@
 #include <pthread.h>
 
 #include <unistd.h>
+#include <time.h>
 #define STOP_TX 0
 #define STOP_RX 1
 #define SET_TX 2
 #define SET_RX 3
 #define START_RX 4
 #define START_TX 5
-int fd;
-char buf[256];
-unsigned char* map=NULL;
-int main(int argc, char *argv[])
-{
-	int i;
-	int dummy;
 
-	printf("rpdma read test\n");
+int main(int argc, char *argv[]) {
+    int i;
+    int dummy;
+    int fd;
+    char buf[256];
+    unsigned char* map=NULL;
 
-	fd = open("/dev/rpdma", O_RDWR);
+    printf("rpdma read test\n");
 
-	if (fd < 1) {
-		printf("Unable to open device file");
-		return -1;
-	}
-	//prepair dma
-	ioctl(fd,10,0);
-	//whait a bit
-	usleep(1000);
+    fd = open("/dev/rpdma", O_RDWR);
+    if (fd < 1) {
+        printf("Unable to open device file");
+        return -1;
+    }
 
-        if(read(fd, buf, 1)<0){
-            printf("read error\n");
-	}
+    map = (unsigned char *)mmap(NULL,4*1024,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (map==NULL) {
+        printf("Failed to mmap\n");
+        if (fd) close(fd);
+        return -1;
+    }
+    for(int l=0; l<4*1024; l++)
+        map[l] = 0;
 
-	printf(".");
-	map = (unsigned char *)mmap(NULL,4*1024,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    //prepair dma
+    ioctl(fd,10,0);
+    //whait a bit
 
-    	if (map==NULL) {
-        	printf("Failed to mmap\n");
-        	if(fd)close(fd);
-        	return -1;
-    	}
-int l;
-	for(l=0;l<4*1024;l++)
-		printf("%c ",(char)map[l]);
+    if(read(fd, buf, 1)<0){
+        printf("read error\n");
+    }
+
+    for(int l=0;l<4*1024;l++)
+        printf("%02x",(char)map[l]);
 	
-        ioctl(fd,STOP_RX,0);
+    ioctl(fd,STOP_RX,0);
 
-//	ioctl(fd,STOP_TX,0);        
-
-if(fd)close(fd);
-	return 0;
+    if (fd) close(fd);
+    return 0;
 }
