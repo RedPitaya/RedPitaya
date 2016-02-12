8c327ffc90772490a03142e5f94b206cb10f9111
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-02-12 11:46:08 +0100

    DMA: updated DMA test case

--- Test/rpdma/rx/rpdma_test.c
+++ Test/rpdma/rx/rpdma_test.c
@@ -14,41 +14,53 @@
 #define START_TX 5
 
 int main(int argc, char *argv[]) {
-    int i;
-    int dummy;
     int fd;
+    int status;
     char buf[256];
     unsigned char* map=NULL;
 
     printf("rpdma read test\n");
 
+    // open DMA driver device
     fd = open("/dev/rpdma", O_RDWR);
     if (fd < 1) {
         printf("Unable to open device file");
         return -1;
     }
 
-    map = (unsigned char *)mmap(NULL,4*1024,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    // allocate data buffer memory
+    map = (unsigned char *) mmap(NULL, 4*1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (map==NULL) {
         printf("Failed to mmap\n");
         if (fd) close(fd);
         return -1;
     }
+    // clear buffer
     for(int l=0; l<4*1024; l++)
         map[l] = 0;
 
-    //prepair dma
-    ioctl(fd,10,0);
-    //whait a bit
+    // DMA prepare
+//    ioctl(fd, 10, 0);  // single RX
+    ioctl(fd, SET_RX, 0);  // cyclic RX
 
-    if(read(fd, buf, 1)<0){
-        printf("read error\n");
+    for (int b=0; b<2; b++) {
+      // blocking read waiting for DMA data
+      status = read(fd, buf, 1);
+      if (status<0) {
+          printf("read error\n");
+      }
+      printf("Block %i received.\n", b);
     }
 
+    // printout data
     for(int l=0;l<4*1024;l++)
         printf("%02x",(char)map[l]);
 	
+    // DMA stop
     ioctl(fd,STOP_RX,0);
+
+    // release memory
+    status = munmap (map, 4*1024);
 
     if (fd) close(fd);
     return 0;
