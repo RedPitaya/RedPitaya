9351107218bc9bbd8a29aa5aac163cb99a25826f
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-02-15 15:04:57 +0100

    DMA: DMA test should use headers defined in the kernel driver code

--- Test/rpdma/rx/rpdma_test.c
+++ Test/rpdma/rx/rpdma_test.c
@@ -6,14 +6,10 @@
 
 #include <unistd.h>
 #include <time.h>
-#define STOP_TX 0
-#define STOP_RX 1
-#define SET_TX 2
-#define SET_RX 3
-#define START_RX 4
-#define START_TX 5
 
-#define SIZE 4*16*1024
+#include "rpdma.h"
+
+#define SIZE SEGMENT_CNT*SGMNT_SIZE
 
 int main(int argc, char *argv[]) {
     int fd;
@@ -37,13 +33,17 @@ int main(int argc, char *argv[]) {
         if (fd) close(fd);
         return -1;
     }
+
     // clear buffer
     for (int l=0; l<SIZE; l++)
         map[l] = 0;
 
+    // release memory
+    status = munmap (map, 4*1024);
+
     // DMA prepare
-    ioctl(fd, 10, 0);  // single RX
-//    ioctl(fd, SET_RX, 0);  // cyclic RX
+    ioctl(fd, SINGLE_RX, 0);  // single RX
+//    ioctl(fd, CYCLIC_RX, 0);  // cyclic RX
 
     for (int b=0; b<1; b++) {
       // blocking read waiting for DMA data
@@ -54,6 +54,14 @@ int main(int argc, char *argv[]) {
       printf("Block %i received.\n", b);
     }
 
+    // allocate data buffer memory
+    map = (unsigned char *) mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (map==NULL) {
+        printf("Failed to mmap\n");
+        if (fd) close(fd);
+        return -1;
+    }
+
     // printout data
     for (int i=0; i<SIZE; i++) {
         if ((i%64)==0 ) printf("@%08x: ", i);
@@ -61,11 +69,11 @@ int main(int argc, char *argv[]) {
         if ((i%64)==63) printf("\n");
     }
 
-    // DMA stop
-    ioctl(fd,STOP_RX,0);
-
     // release memory
     status = munmap (map, 4*1024);
+
+    // DMA stop
+    ioctl(fd,STOP_RX,0);
 
     if (fd) close(fd);
     return 0;
