4c1805c11f967dcb721489f8b861f1fd7d33c3bc
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-02-15 15:03:12 +0100

    FPGA: added tests for LG burst mode, and LA automatic acquisition

---
+++ api2/test/test_la.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "redpitaya/rp2.h"
+#include "la_acq.h"
+
+int main (int argc, char **argv) {
+    rp_handle_uio_t handle;
+//    int status;
+    int unsigned length;
+
+    if (argc > 1) {
+        length = atoi(argv[1]);
+    } else {
+        length = 256;
+    }
+    printf("Length = %u\n", length);
+
+    // Initialization of API
+    if (rp_LaAcqOpen("/dev/uio12", &handle) != RP_OK) {
+        fprintf(stderr, "Red Pitaya API init failed!\n");
+        return EXIT_FAILURE;
+    }
+
+    rp_la_cfg_regset_t cfg;
+    cfg.pre=0;
+    cfg.pst=length;
+    rp_LaAcqSetCntConfig (&handle,cfg);
+
+    rp_LaAcqSetConfig    (&handle, 0x2);
+    rp_LaAcqGlobalTrigSet(&handle, 0x0);
+    rp_LaAcqRunAcq       (&handle);
+
+    bool tmp;
+    do {
+        rp_LaAcqAcqIsStopped (&handle, &tmp);
+    } while (!tmp);
+
+    // Releasing resources
+    rp_LaAcqClose(&handle);
+
+    return EXIT_SUCCESS;
+}
--- api2/test/test_lg_burst.c
+++ api2/test/test_lg_burst.c
@@ -16,27 +16,27 @@ int main (int argc, char **argv) {
     }
 
     int unsigned length = 256;
-    double frequency = 1000;
-    double phase = 180;
+    uint32_t stp = 0x00010000;
+    uint32_t off = 0x00000000;
 
     rp_GenSetWaveformUpCountSeq(&handle, length);
 
-    status = rp_GenSetFreqPhase(&handle,  frequency,  phase);
+    status = rp_GenSetStepOffset(&handle, stp, off);
     if (status != RP_OK) {
-        fprintf(stderr, "Red Pitaya API access failed!\n");
-    }
-    status = rp_GenGetFreqPhase(&handle, &frequency, &phase);
-    if (status != RP_OK) {
-        fprintf(stderr, "Red Pitaya API access failed!\n");
+        fprintf(stderr, "Error: failed to write step and offset!\n");
+        return EXIT_FAILURE;
     }
 
     rp_GenOutputEnable(&handle, 0xffff);
 
+    rp_GenSetMode               (&handle, RP_GEN_MODE_BURST);
+    rp_GenSetBurstModeDataLen   (&handle, length);
+    rp_GenSetBurstModePeriodLen (&handle, length);
+
+    rp_GenSetBurstModeRepetitions (&handle, RP_GEN_REP_INF);
+
     rp_GenGlobalTrigSet(&handle, RP_TRG_DGEN_SWE_MASK);
     rp_GenTrigger(&handle);
-
-    usleep(5*1000*1000);
-    fprintf(stderr, "frequency = %f, phase = %f\n", frequency, phase);
 
     // Releasing resources
     rp_GenClose(&handle);
