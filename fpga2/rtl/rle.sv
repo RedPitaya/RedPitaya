b2d3001c97b65f2331c6f5548090e7e268cd3157
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-02-22 13:13:20 +0100

    FPGA: removing copied code

--- fpga2/rtl/rle.sv
+++ fpga2/rtl/rle.sv
@@ -12,12 +12,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 module rle #(
+  // counter properties
+  int unsigned CW = 8,
   // stream properties
   int unsigned DN = 1,
-  type DTI = logic signed [8-1:0], // data type for input
-  type DTO = logic signed [8-1:0], // data type for output
-  // counter properties
-  int unsigned CW = 8
+  type DTI = logic [8-1:0], // data type for input
+  type DTO = logic [8-1:0]  // data type for output
 )(
   // input stream input/output
   axi4_stream_if.d sti,  // input
@@ -31,11 +31,15 @@ module rle #(
 // store previous value
 ////////////////////////////////////////////////////////////////////////////////
 
-DTI old_tdata ;
-DTI old_tvalid;
+DTI   old_tdata ;
+logic old_tvalid;
+logic old_tlast ;
 
 always_ff @(posedge sti.ACLK)
-if (sti.transf)  old_tdata <= sti.TDATA;
+if (sti.transf) begin
+  old_tdata <= sti.TDATA;
+  old_tlast <= sti.TLAST;
+end
 
 always_ff @(posedge sti.ACLK)
 if (~sti.ARESETn)     old_tvalid <= 1'b0;
@@ -49,81 +53,26 @@ logic cmp;  // compare
 
 assign cmp = (old_tdata == sti.TDATA);
 
-
-always_ff @(posedge sti.ACLK)
-if (sti.transf)  str_mul.TLAST <= sti.TLAST;
-
-always_ff @(posedge sti.ACLK)
-if (~sti.ARESETn)     str_mul.TVALID <= 1'b0;
-else if (sti.TREADY)  str_mul.TVALID <= sti.TVALID;
-
-assign sti.TREADY = str_mul.TREADY | ~str_mul.TVALID;
-
 ////////////////////////////////////////////////////////////////////////////////
-// shift
+// counter
 ////////////////////////////////////////////////////////////////////////////////
 
-generate
-for (genvar i=0; i<DN; i++) begin: for_shf
-  assign str_shf.TDATA[i] = str_mul.TDATA[i] >>> (DWM-2);
-  assign str_shf.TKEEP[i] = str_mul.TKEEP[i] >>> (DWM-2);
-end: for_shf
-endgenerate
-
-assign str_shf.TLAST  = str_mul.TLAST;
-assign str_shf.TVALID = str_mul.TVALID;
-
-assign str_mul.TREADY = str_shf.TREADY;
-
-////////////////////////////////////////////////////////////////////////////////
-// summation
-////////////////////////////////////////////////////////////////////////////////
-
-generate
-for (genvar i=0; i<DN; i++) begin: for_sum
-  always_ff @(posedge sti.ACLK)
-  if (str_shf.transf) begin
-    str_sum.TDATA[i] <= str_shf.TDATA[i] + cfg_sum;
-    str_sum.TKEEP[i] <= str_shf.TKEEP[i];
-  end
-end: for_sum
-endgenerate
+logic [CW-1:0] cnt;
+logic [CW-1:0] nxt;
 
 always_ff @(posedge sti.ACLK)
-if (str_shf.transf) begin
-  str_sum.TLAST <= str_shf.TLAST;
-end
+if (~sti.ARESETn)     cnt <= '0;
+else if (sti.transf)  cnt <= nxt;
 
-always_ff @(posedge sti.ACLK)
-if (~sti.ARESETn)         str_sum.TVALID <= 1'b0;
-else if (str_shf.TREADY)  str_sum.TVALID <= str_shf.TVALID;
-
-assign str_shf.TREADY = sto.TREADY | ~str_sum.TVALID;
+assign nxt = cnt + 1;
 
 ////////////////////////////////////////////////////////////////////////////////
-// saturation
+// output
 ////////////////////////////////////////////////////////////////////////////////
 
-generate
-for (genvar i=0; i<DN; i++) begin: for_sat
-  always_ff @(posedge sti.ACLK)
-  if (str_sum.transf) begin
-    sto.TDATA[i] <= ^str_sum.TDATA[i][DWO:DWO-1] ? {str_sum.TDATA[i][DWO], {DWO-1{~str_sum.TDATA[i][DWO-1]}}}
-                                                 :  str_sum.TDATA[i][DWO-1:0];
-    sto.TKEEP[i] <= str_sum.TKEEP[i];
-  end
-end: for_sat
-endgenerate
-
 always_ff @(posedge sti.ACLK)
-if (str_sum.transf) begin
-  sto.TLAST <= str_sum.TLAST;
-end
+if (sti.transf)  sto.TLAST <= old_tlast;
 
-always_ff @(posedge sti.ACLK)
-if (~sti.ARESETn)         sto.TVALID <= 1'b0;
-else if (str_sum.TREADY)  sto.TVALID <= str_sum.TVALID;
-
-assign str_sum.TREADY = sto.TREADY | ~str_sum.TVALID;
+assign sto.TDATA = {cnt, old_tdata};
 
 endmodule: rle
