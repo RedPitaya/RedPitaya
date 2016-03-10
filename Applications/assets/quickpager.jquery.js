0ffe53f6c2766ec1973742902e89c6e4b17e8a03
A. Kaygorodov <a.kaygorodov@integrasources.com>
2016-03-10 14:21:13 +0600

    #834 new desctop layot

--- Applications/assets/quickpager.jquery.js
+++ Applications/assets/quickpager.jquery.js
@@ -10,105 +10,99 @@
 
 (function($) {
 
-	$.fn.quickPager = function(options) {
+    $.fn.quickPager = function(options) {
 
-		var defaults = {
-			pageSize: 10,
-			currentPage: 1,
-			holder: null,
-			pagerLocation: "after"
-		};
+        var defaults = {
+            pageSize: 10,
+            currentPage: 1,
+            holder: null,
+            pagerLocation: "after"
+        };
 
-		var options = $.extend(defaults, options);
-
-
-		return this.each(function() {
+        var options = $.extend(defaults, options);
 
 
-			var selector = $(this);
-			var pageCounter = 1;
+        return this.each(function() {
 
-			selector.wrap("<div class='simplePagerContainer'></div>");
 
-			selector.parents(".simplePagerContainer").find("ul.simplePagerNav").remove();
+            var selector = $(this);
+            var pageCounter = 1;
 
-			selector.children().each(function(i){
+            selector.wrap("<div class='simplePagerContainer'></div>");
 
-				if(i < pageCounter*options.pageSize && i >= (pageCounter-1)*options.pageSize) {
-				$(this).addClass("simplePagerPage"+pageCounter);
-				}
-				else {
-					$(this).addClass("simplePagerPage"+(pageCounter+1));
-					pageCounter ++;
-				}
+            selector.parents(".simplePagerContainer").find("ul.simplePagerNav").remove();
 
-			});
+            selector.children().each(function(i) {
 
-			// show/hide the appropriate regions
-			selector.children().hide();
-			selector.children(".simplePagerPage"+options.currentPage).show();
+                if (i < pageCounter * options.pageSize && i >= (pageCounter - 1) * options.pageSize) {
+                    $(this).addClass("simplePagerPage" + pageCounter);
+                } else {
+                    $(this).addClass("simplePagerPage" + (pageCounter + 1));
+                    pageCounter++;
+                }
 
-			if(pageCounter <= 1) {
-				return;
-			}
+            });
 
-			//Build pager navigation
-			var pageNav = "<ul class='simplePagerNav'>";
-			for (i=1;i<=pageCounter;i++){
-				if (i==options.currentPage) {
-					pageNav += "<li class='currentPage simplePageNav"+i+"'><a rel='"+i+"' href='#'>"+i+"</a></li>";
-				}
-				else {
-					pageNav += "<li class='simplePageNav"+i+"'><a rel='"+i+"' href='#'>"+i+"</a></li>";
-				}
-			}
-			pageNav += "</ul>";
+            // show/hide the appropriate regions
+            selector.children().hide();
+            selector.children(".simplePagerPage" + options.currentPage).show();
 
-			if(!options.holder) {
-				switch(options.pagerLocation)
-				{
-				case "before":
-					selector.before(pageNav);
-				break;
-				case "both":
-					selector.before(pageNav);
-					selector.after(pageNav);
-				break;
-				default:
-					selector.after(pageNav);
-				}
-			}
-			else {
-				$(options.holder).append(pageNav);
-			}
+            if (pageCounter <= 1) {
+                return;
+            }
 
-			//pager navigation behaviour
-			selector.parent().find(".simplePagerNav a").click(function() {
+            //Build pager navigation
+            var pageNav = "<ul class='simplePagerNav'>";
+            for (i = 1; i <= pageCounter; i++) {
+                if (i == options.currentPage) {
+                	pageNav += "<li class='currentPage simplePageNav" + i + "'><a rel='" + i + "' href='#'><div class='active-dot'></div></a></li>";
+                } else {
+                    pageNav += "<li class='simplePageNav" + i + "'><a rel='" + i + "' href='#'><div class='dot'></div></a></li>";
+                }
+            }
+            pageNav += "</ul>";
 
-				//grab the REL attribute
-				var clickedLink = $(this).attr("rel");
-				options.currentPage = clickedLink;
+            if (!options.holder) {
+                switch (options.pagerLocation) {
+                    case "before":
+                        selector.before(pageNav);
+                        break;
+                    case "both":
+                        selector.before(pageNav);
+                        selector.after(pageNav);
+                        break;
+                    default:
+                        selector.after(pageNav);
+                }
+            } else {
+                $(options.holder).append(pageNav);
+            }
 
-				if(options.holder) {
-					$(this).parent("li").parent("ul").parent(options.holder).find("li.currentPage").removeClass("currentPage");
-					$(this).parent("li").parent("ul").parent(options.holder).find("a[rel='"+clickedLink+"']").parent("li").addClass("currentPage");
-				}
-				else {
-					//remove current current (!) page
-					$(this).parent("li").parent("ul").parent(".simplePagerContainer").find("li.currentPage").removeClass("currentPage");
-					//Add current page highlighting
-					$(this).parent("li").parent("ul").parent(".simplePagerContainer").find("a[rel='"+clickedLink+"']").parent("li").addClass("currentPage");
-				}
+            //pager navigation behaviour
+            selector.parent().find(".simplePagerNav a").click(function() {
 
-				//hide and show relevant links
-				selector.children().hide();
-				selector.find(".simplePagerPage"+clickedLink).show();
+                //grab the REL attribute
+                var clickedLink = $(this).attr("rel");
+                options.currentPage = clickedLink;
 
-				return false;
-			});
-		});
-	}
+                if (options.holder) {
+                    $(this).parent("li").parent("ul").parent(options.holder).find("li.currentPage").removeClass("currentPage");
+                    $(this).parent("li").parent("ul").parent(options.holder).find("a[rel='" + clickedLink + "']").parent("li").addClass("currentPage");
+                } else {
+                    //remove current current (!) page
+                    $(this).parent("li").parent("ul").parent(".simplePagerContainer").find("li.currentPage").removeClass("currentPage");
+                    //Add current page highlighting
+                    $(this).parent("li").parent("ul").parent(".simplePagerContainer").find("a[rel='" + clickedLink + "']").parent("li").addClass("currentPage");
+                }
+
+                //hide and show relevant links
+                selector.children().hide();
+                selector.find(".simplePagerPage" + clickedLink).show();
+
+                return false;
+            });
+        });
+    }
 
 
 })(jQuery);
-
