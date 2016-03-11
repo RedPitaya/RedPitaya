7dd5cc77033b315ae1361bab5fd8ac909b4980fe
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-03-11 15:23:24 +0100

    FPGA2: untested paralel LA trigger

--- fpga2/rtl/la_trigger.sv
+++ fpga2/rtl/la_trigger.sv
@@ -9,29 +9,40 @@ module la_trigger #(
   type DT = logic [8-1:0]  // str.dat type
 )(
   // control
-  input  logic ctl_rst,  // synchronous reset
+  input  logic          ctl_rst,  // synchronous reset
   // configuration
-  input  DT    cfg_cmp_msk,  // comparator mask
-  input  DT    cfg_cmp_val,  // comparator value
-  input  DT    cfg_edg_pos,  // edge positive
-  input  DT    cfg_edg_neg,  // edge negative
+  input  DT             cfg_cmp_msk,  // comparator mask
+  input  DT             cfg_cmp_val,  // comparator value
+  input  DT             cfg_edg_pos,  // edge positive
+  input  DT             cfg_edg_neg,  // edge negative
   // output triggers
-  output logic sts_trg,  // TODO: should have DN width
+  output logic [DN-1:0] sts_trg,
   // stream monitor
-  axi4_stream_if.m str
+  axi4_stream_if.m      str
 );
 
-DT    str_old;
+DT    [DN-0:0] dat;
 
-logic sts_cmp;
-logic sts_edg;
+logic [DN-1:0] sts_cmp;
+logic [DN-1:0] sts_edg;
 
-assign sts_cmp = (str.TDATA & cfg_cmp_msk) == (cfg_cmp_val & cfg_cmp_msk);
-assign sts_edg = |(cfg_edg_pos & (~str_old &  str.TDATA))
-               | |(cfg_edg_neg & ( str_old & ~str.TDATA));
+generate
+for (genvar i=0; i<DN; i++) begin: for_dn
 
+// comparator
+assign sts_cmp [i] = (str.TDATA[i] & cfg_cmp_msk) == (cfg_cmp_val & cfg_cmp_msk);
+// edge detection
+assign sts_edg = |(cfg_edg_pos & (~dat[i] &  dat[i+1]))
+               | |(cfg_edg_neg & ( dat[i] & ~dat[i+1]));
+
+end: for_dn
+endgenerate
+
+// data chain for checking edges
 always @(posedge str.ACLK)
-if (str.transf)  str_old <= str.TDATA;
+if (str.transf)  dat [0] <= str.TDATA [DN-1];
+
+assign dat [DN:1] = str.TDATA;
 
 always @(posedge str.ACLK)
 if (~str.ARESETn) begin
@@ -40,7 +51,7 @@ end else begin
   if (ctl_rst) begin
     sts_trg <= '0;
   end if (str.transf) begin
-    sts_trg <= sts_cmp & sts_edg;
+    sts_trg <= str.TKEEP & sts_cmp & sts_edg;
   end
 end
 
