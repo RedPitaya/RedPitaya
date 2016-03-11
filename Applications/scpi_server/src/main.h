6fde0be5a5efb6018d6d1564dcb6901406194cd9
Artem Kokos <a.kokos@integrasources.com>
2016-03-11 11:29:39 +0600

    Some progress for SCPI server application: run/stop by click, stop if exit, checking status

--- Applications/scpi_server/src/main.h
+++ Applications/scpi_server/src/main.h
@@ -59,6 +59,7 @@ int rp_app_exit(void);
 int rp_set_params(rp_app_params_t *p, int len);
 int rp_get_params(rp_app_params_t **p);
 int rp_get_signals(float ***s, int *sig_num, int *sig_len);
+int IsRunning(void);
 
 #ifdef __cplusplus
 }
