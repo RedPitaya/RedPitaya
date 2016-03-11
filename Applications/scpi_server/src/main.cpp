9d4fb8c5df2a5c1f892fe1c7c3523b6c999782ed
Artem Kokos <a.kokos@integrasources.com>
2016-03-11 11:29:39 +0600

    Some progress for SCPI server application: run/stop by click, stop if exit, checking status

--- Applications/scpi_server/src/main.cpp
+++ Applications/scpi_server/src/main.cpp
@@ -9,6 +9,7 @@
 
 /* --------------------------------  OUT PARAMETERS  ------------------------------ */
 CBooleanParameter inRun("OSC_RUN", CBaseParameter::RW, false, 0);
+CBooleanParameter isRunning("OSC_RUNNING", CBaseParameter::RWSA, false, 0);
 
 static const float DEF_MIN_SCALE = 1.f/1000.f;
 static const float DEF_MAX_SCALE = 5.f;
@@ -18,7 +19,7 @@ const char *rp_app_desc(void) {
 }
 
 int rp_app_init(void) {
-    fprintf(stderr, "Loading scope version %s-%s.\n", VERSION_STR, REVISION_STR);
+    fprintf(stderr, "Loading SCPI server version %s-%s.\n", VERSION_STR, REVISION_STR);
     CDataManager::GetInstance()->SetParamInterval(1000);
 
     rpApp_Init();
@@ -27,7 +28,7 @@ int rp_app_init(void) {
 }
 
 int rp_app_exit(void) {
-    fprintf(stderr, "Unloading scope version %s-%s.\n", VERSION_STR, REVISION_STR);
+    fprintf(stderr, "Unloading SCPI server version %s-%s.\n", VERSION_STR, REVISION_STR);
     rpApp_Release();
     return 0;
 }
@@ -51,12 +52,12 @@ double roundUpTo1(double data) {
     return (dataNorm * pow(10, power));         // unnormalize data
 }
 
-void UpdateParams(void) {
-	CDataManager::GetInstance()->SetParamInterval(1000);
+void UpdateSignals(void) {
 
-    bool running;
-    rpApp_OscIsRunning(&running);
-    inRun.Value() = running;
+}
+
+void UpdateParams(void) {
+    isRunning.Value() = IsRunning();
 }
 
 bool check_params(const rp_calib_params_t& current_params, int step) {
@@ -64,7 +65,28 @@ bool check_params(const rp_calib_params_t& current_params, int step) {
 	return false;
 }
 
+int IsRunning(void) {
+    FILE* pop = popen("/usr/bin/pgrep scpi-server", "r");
+    char buf[1024];
+    fgets(buf, 1024, pop);
+    pclose(pop);
+
+    return atoi(buf);
+}
+
 void OnNewParams(void) {
 /* ------ SEND OSCILLOSCOPE PARAMETERS TO API ------*/
-    IF_VALUE_CHANGED_BOOL(inRun, rpApp_OscRun(), rpApp_OscStop())
+	fprintf(stderr, "-----LOG SCPI SERVER OnNewParams()------\n");
+    //IF_VALUE_CHANGED_BOOL(inRun, rpApp_OscRun(), rpApp_OscStop())
+
+    if(IsRunning() && inRun.NewValue() == false)
+    {
+    	system("killall scpi-server");
+    	inRun.Value() = false;
+    }
+    else if(!IsRunning() && inRun.NewValue() == true)
+    {
+    	system("export LD_LIBRARY_PATH=/opt/redpitaya/lib/ && /opt/redpitaya/bin/scpi-server &");
+    	inRun.Value() = true;
+    }
 }
