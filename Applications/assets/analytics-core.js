1735f06f0a7bf3bd6beded9e4170bf18a75d0ad1
A. Kaygorodov <a.kaygorodov@integrasources.com>
2016-03-16 18:00:41 +0600

    Analytics fix

--- Applications/assets/analytics-core.js
+++ Applications/assets/analytics-core.js
@@ -129,8 +129,15 @@
             sendData = true;
             successCallback();
         }
-
-
     };
+
+    AnalyticsCore.sleep = function(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
+            }
+        }
+    }
 
 })(window.AnalyticsCore = window.AnalyticsCore || {}, jQuery);
