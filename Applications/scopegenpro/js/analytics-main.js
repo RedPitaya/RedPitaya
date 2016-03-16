eba211b08bc754e022e6a04656285fe6afcaa131
A. Kaygorodov <a.kaygorodov@integrasources.com>
2016-03-16 18:00:41 +0600

    Analytics fix

--- Applications/scopegenpro/js/analytics-main.js
+++ Applications/scopegenpro/js/analytics-main.js
@@ -4,17 +4,18 @@
 //-------------------------------------------------
 
 (function($) {
-	var startUsing = 0;
+    var startUsing = 0;
     $(document).ready(function($) {
-        AnalyticsCore.init(function(){
+        AnalyticsCore.init(function() {
             AnalyticsCore.sendScreenView('/scopegenpro', 'Redpitaya', 'Oscilloscope Pro');
             AnalyticsCore.sendSysInfo('/scopegenpro');
             startUsing = performance.now();
         });
     });
 
-	$(window).on('beforeunload', function(){
-	      AnalyticsCore.sendTiming('/scopegenpro', 'App executed', 'Oscilloscope Pro', performance.now() - startUsing);
-	});
+    $(window).on('beforeunload', function(e) {
+        AnalyticsCore.sendTiming('/scopegenpro', 'App executed', 'Oscilloscope Pro', performance.now() - startUsing);
+        AnalyticsCore.sleep(2500);
+    });
 
 })(jQuery);
