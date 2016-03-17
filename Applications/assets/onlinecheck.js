a3caa65c62f8ec3f0bb7626ecdbe222831bcac5e
Artem Kokos <a.kokos@integrasources.com>
2016-03-17 15:22:42 +0600

    Added header with online/offline status for apps

---
+++ Applications/assets/onlinecheck.js
@@ -0,0 +1,35 @@
+//-------------------------------------------------
+//      Redpitaya analytics core system
+//      Created by Artem Kokos
+//-------------------------------------------------
+
+(function(OnlineChecker, $) {
+	OnlineChecker.online = true;
+
+	OnlineChecker.isOnline = function() {
+		return OnlineChecker.online;
+	}
+
+	$(document).ready(function($) {
+        Offline.options = {
+            checks: {
+                xhr: {
+                    url: '/check_inet'
+                }
+            },
+            checkOnLoad: false,
+        };
+        Offline.on('up', function() {
+            OnlineChecker.online = true;
+        });
+        Offline.on('down', function() {
+            OnlineChecker.online = false;
+        });
+        Offline.check();
+        var run = function() {
+            Offline.check();
+        }
+        setInterval(run, 3000);
+	});
+
+})(window.OnlineChecker = window.OnlineChecker || {}, jQuery);
