f94e7da0ea96d7287eecb7e2c95b22519dfa8b33
Iztok Jeras <iztok.jeras@gmail.com>
2016-03-17 13:56:39 +0100

    Revert "Makefile: using older Linaro compiler"
    This reverts commit 769c104ad73d3a6cc620bb9a4287fa34fcd58eda.

--- settings.sh
+++ settings.sh
@@ -9,8 +9,7 @@
 ################################################################################
 
 #export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2015.02-3-x86_64_arm-linux-gnueabihf
-#export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/
-export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux
+export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/
 export PATH=$TOOLCHAIN_PATH/bin:$PATH
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
