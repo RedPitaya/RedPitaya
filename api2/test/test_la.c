c143a6d6566eba4a25b034703bc5283576a0d692
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-03-18 17:46:52 +0100

    API2: added new tests

--- api2/test/test_la.c
+++ api2/test/test_la.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include "rpdma.h"
+
 #include "redpitaya/rp2.h"
 #include "la_acq.h"
+#include "rp_dma.h"
+
+#define SIZE 0x100
 
 int main (int argc, char **argv) {
     rp_handle_uio_t handle;
@@ -13,7 +22,7 @@ int main (int argc, char **argv) {
     if (argc > 1) {
         length = atoi(argv[1]);
     } else {
-        length = 256;
+        length = 64;
     }
     printf("Length = %u\n", length);
 
@@ -24,19 +33,48 @@ int main (int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+    rp_SetSgmntC (&handle, 4);
+    rp_SetSgmntS (&handle, SIZE*2);
+    handle.dma_size = 4*SIZE*2;
+
+    int16_t * map=NULL;
+    map = (int16_t *) mmap(NULL, handle.dma_size, PROT_READ | PROT_WRITE, MAP_SHARED, handle.dma_fd, 0);
+    if (map==NULL) {
+        printf("Failed to mmap\n");
+        return -1;
+    }
+
+    // configure acquire
     rp_la_cfg_regset_t cfg;
     cfg.pre=0;
     cfg.pst=length;
     rp_LaAcqSetCntConfig (&handle, cfg);
-
     rp_LaAcqSetConfig    (&handle, 0x2);
     rp_LaAcqGlobalTrigSet(&handle, 0x0);
-    rp_LaAcqRunAcq       (&handle);
 
-    bool tmp;
-    do {
-        rp_LaAcqAcqIsStopped (&handle, &tmp);
-    } while (!tmp);
+    // test loop
+    for (int n=0; n<6; n++) {
+        printf("Repeat: %d\n", n);
+
+        // clear data
+        for (int i=0; i<handle.dma_size/sizeof(*map); i++) {
+            map[i]=0;
+        }
+
+        // run acquire and wait for it to stop
+        rp_LaAcqRunAcq       (&handle);
+        rp_LaAcqBlockingRead (&handle);
+
+        // printout data
+        for (int i=0; i<handle.dma_size/sizeof(*map); i++) {
+            if ((i%32)==0 ) printf("@%08x: ", i * sizeof(*map));
+            printf("%04x", map[i]);
+            if ((i%32)==31) printf("\n");
+        }
+    }
+
+    // release memory
+    status = munmap (map, sizeof(*map));
 
     // Releasing resources
     rp_LaAcqClose(&handle);
