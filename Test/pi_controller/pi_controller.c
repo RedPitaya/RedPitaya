367516619784156921d65b518c300a900caac61a
rag <ragnar@dm9.se>
2016-03-21 18:50:18 +0100

    18:50

--- Test/pi_controller/pi_controller.c
+++ Test/pi_controller/pi_controller.c
@@ -236,7 +236,7 @@ int main(int argc, char *argv[]) {
 
 				if (s[1][2]==s[2][1]||size==3){};
 	for(int i = 0; i < n; i++) {
-            data[i]=round(s[1][i]*8191);
+            data[i]=round(s[1][i]);
           //  data[i] = round(ampl * cos(2*M_PI*(double)i/(double)n));
 			/* Truncate to max value if needed */
 			if (data[i] > 8191) {
