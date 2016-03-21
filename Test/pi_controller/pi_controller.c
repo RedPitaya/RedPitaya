18e667fb01ef42d6ee8abf5a15d000d00881dda9
rag <ragnar@dm9.se>
2016-03-21 18:25:54 +0100

    1825

--- Test/pi_controller/pi_controller.c
+++ Test/pi_controller/pi_controller.c
@@ -174,7 +174,7 @@ int main(int argc, char *argv[]) {
    // uint32_t  min_periodes = 1; // max 20
     uint32_t  size; // number of samples varies with number of periodes
   //  signal_e type = eSignalSine;
-    int       f = 1; // used in for lop, setting the decimation
+    int       f = 0; // used in for lop, setting the decimation
     int       equal = 0; // parameter initialized for generator functionality
     int       shaping = 0; // parameter initialized for generator functionality
 	double    freq_act = 0;
