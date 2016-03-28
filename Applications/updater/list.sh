f4e0a31d6bd7ae2e640a5ea3f858438b30084c67
a.solovev <a.solovev@integrasources.com>
2016-03-28 19:08:24 +0600

    add scripts

---
+++ Applications/updater/list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -O download.html http://downloads.redpitaya.com/downloads/ &> /dev/null
+
+IFS=$'\n'
+for l in `cat download.html`; do
+	echo $l | grep -Po 'ecosystem-.+\.zip"'
+	if [[ $l == *"ecosystem"* ]]
+		then echo $l | awk '{ print $5}'
+	fi
+done
