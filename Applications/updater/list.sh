8a555c791cc682e3dc70e0704c85f7299e03cc40
a.solovev <a.solovev@integrasources.com>
2016-03-28 19:34:42 +0600

    updater nginx conf

--- Applications/updater/list.sh
+++ Applications/updater/list.sh
@@ -3,7 +3,7 @@ wget -O download.html http://downloads.redpitaya.com/downloads/ &> /dev/null
 
 IFS=$'\n'
 for l in `cat download.html`; do
-	echo $l | grep -Po 'ecosystem-.+\.zip"'
+	echo $l | grep -Po 'ecosystem-.+\.zip"' | tr -d '"'
 	if [[ $l == *"ecosystem"* ]]
 		then echo $l | awk '{ print $5}'
 	fi
