a7dde2fec0b538e02bc74a7111b155492b84dddd
a.solovev <a.solovev@integrasources.com>
2016-03-29 13:03:14 +0600

    updater scripts

--- Applications/updater/update.sh
+++ Applications/updater/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /tmp/build/ecosystem-0 && killall nginx && rw && rm -rf /opt/redpitaya/ && cd /tmp/build/ecosystem-0 && cp -fr * /opt/ && reboot
+cd /tmp/build/ecosystem* && killall nginx && rw && rm -rf /opt/redpitaya/* && cp -fr * /opt/redpitaya && reboot
