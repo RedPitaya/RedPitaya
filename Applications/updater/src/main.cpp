8143b70e258a0b2976019491cb04cdfbff14d455
A. Kaygorodov <a.kaygorodov@integrasources.com>
2016-04-06 13:12:27 +0700

    Fixed update tool

--- Applications/updater/src/main.cpp
+++ Applications/updater/src/main.cpp
@@ -11,6 +11,12 @@
 #include <unistd.h>
 #include <signal.h>
 
+// FILE* logFile;
+void signalHandler( int signum )
+{
+    //fprintf(logFile, "Interrupt signal %d received.\n", signum); fflush(logFile);
+}
+
 void StartDaemon()
 {
     pid_t pid;
@@ -30,30 +36,60 @@ void StartDaemon()
     if (setsid() < 0)
         exit(EXIT_FAILURE);
 
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-
     /* Close all open file descriptors */
     int x;
     for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
     {
         close (x);
     }
-
     /* Open the log file */
 }
 
 
 int main(int argc, char *argv[])
 {
-	StartDaemon();
+
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+
+    StartDaemon();
+    signal(SIGCHLD, signalHandler);
+    signal(SIGHUP, signalHandler);
+    signal(SIGABRT, signalHandler);
+    signal(SIGFPE, signalHandler);
+    signal(SIGILL, signalHandler);
+    signal(SIGINT, signalHandler);
+    signal(SIGSEGV, signalHandler);
+    signal(SIGTERM, signalHandler);
+    signal(SIGUSR1, signalHandler);
+    signal(SIGUSR2, signalHandler);
+
+    // logFile = fopen ("/root/daemon.log","a");
+    //fprintf (logFile, "----------------------------------------------------\n"); fflush(logFile);
+    //fprintf (logFile, "[StartDaemon] Daemon have started\n"); fflush(logFile);
 	sleep(1);
-	system("killall nginx");
-    system("killall hostapd");
-	system("mount -o rw,remount /opt/redpitaya");
-	system("/bin/cp -fr /tmp/build/* /opt/redpitaya/");
-	system("reboot");
+    /*int res = 0;*/
+	/*res = */system("nohup killall nginx & disown");
+    //fprintf (logFile, "> nohup killall nginx : %d\n", res); fflush(logFile);
+    sleep(1);
+
+    /*res = */system("nohup killall hostapd & disown");
+    //fprintf (logFile, "> nohup killall hostapd : %d\n", res); fflush(logFile);
+    sleep(1);
+
+	/*res = */system("nohup mount -o rw,remount /opt/redpitaya & disown");
+    //fprintf (logFile, "> nohup mount -o rw,remount /opt/redpitaya : %d\n", res); fflush(logFile);
+    sleep(1);
+
+	/*res = */system("nohup /bin/cp -fvr /tmp/build/* /opt/redpitaya/ & disown");
+    //fprintf (logFile, "> nohup /bin/cp -fvr /tmp/build/* /opt/redpitaya/ : %d\n", res); fflush(logFile);
+    sleep(17);
+
+	/*res = */system("nohup reboot & disown");
+    //fprintf (logFile, "> nohup reboot : %d\n", res); fflush(logFile);
+    sleep(1);
+
+    // fclose(logFile);
     return 0;
 }
