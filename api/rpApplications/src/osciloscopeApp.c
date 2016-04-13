8968600a1b46f6c47374f03e84418b301a25b1d1
a.solovev <a.solovev@integrasources.com>
2016-04-13 16:53:30 +0600

    fix #981 Fake Signal spikes after acq. is stopped and t/div is changed

--- api/rpApplications/src/osciloscopeApp.c
+++ api/rpApplications/src/osciloscopeApp.c
@@ -1498,23 +1498,20 @@ static inline void threadUpdateView(uint16_t data[2][ADC_BUFFER_SIZE], uint32_t 
 
         if(curDeltaSample < 1.0f) {
             int i;
-            for (i = 0; i < maxViewIdx /*&& (int) (((float)i * curDeltaSample) + buffFullOffset) < ADC_BUFFER_SIZE*/; ++i) {
-                int x0 = ((int)((float)i * curDeltaSample) + buffFullOffset) % ADC_BUFFER_SIZE;
-                int x1 = (x0 + 1) % ADC_BUFFER_SIZE;
-                float y = linear(x0, convertRawData(data[channel][x0], gainV1, calibScale1, dc_offs1), x0 + 1,
+            for (i = 0; i < maxViewIdx; ++i) {
+                int x0 = (((float)i * curDeltaSample) + buffFullOffset);
+                int x00 = ((size_t)x0) % ADC_BUFFER_SIZE;
+                int x1 = ((size_t)(x0 + 1)) % ADC_BUFFER_SIZE;
+                float y = linear(x0, convertRawData(data[channel][x00], gainV1, calibScale1, dc_offs1), x0 + 1,
 					convertRawData(data[channel][x1], gainV2, calibScale2, dc_offs2), ((float)i * curDeltaSample) + buffFullOffset);
                 ECHECK_APP_THREAD(scaleAmplitudeChannel((rpApp_osc_source) channel, y, view + viewFullOffset + i));
             }
             maxViewIdx = i;
         } else {
             int i;
-            for (i = 0; i < maxViewIdx /*&& (int) (((float)i * curDeltaSample) + buffFullOffset) < ADC_BUFFER_SIZE*/; ++i) {
+            for (i = 0; i < maxViewIdx; ++i) {
 				const size_t idx = ((size_t)((float)i * curDeltaSample) + buffFullOffset) % ADC_BUFFER_SIZE;
 				ECHECK_APP_THREAD(scaleAmplitudeChannel((rpApp_osc_source) channel, convertRawData(data[channel][idx], gainV1, calibScale1, dc_offs1), view + viewFullOffset + i));
-/*
-                ECHECK_APP_THREAD(scaleAmplitudeChannel((rpApp_osc_source) channel,
-					convertRawData(data[channel][((size_t) ((size_t)((float)i * curDeltaSample) + buffFullOffset)) % ADC_BUFFER_SIZE], gainV1, calibScale1, dc_offs1), view + viewFullOffset + i));
-*/
             }
             maxViewIdx = i;
         }
