09556eefa4652d19050bcd0bc474c35518e63bf4
Nils Roos <security@techno.ms>
2016-05-05 15:03:37 +0200

    DTS: MDIO fixed, heartbeat fixed (#63)
    - changed the ethernet mdio entry to avoid warnings and busprobing during boot
    - fixed typo in gpio-led entry to install heartbeat trigger correctly
    - removed now obsolete workaround for heartbeat via systemd
    - cleaned up the dts a little

--- OS/debian/redpitaya.sh
+++ OS/debian/redpitaya.sh
@@ -14,7 +14,6 @@ install -v -m 664 -o root -d                                                    
 install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_discovery.service $ROOT_DIR/etc/systemd/system/redpitaya_discovery.service
 install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_nginx.service     $ROOT_DIR/etc/systemd/system/redpitaya_nginx.service
 install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_scpi.service      $ROOT_DIR/etc/systemd/system/redpitaya_scpi.service
-install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/redpitaya_heartbeat.service $ROOT_DIR/etc/systemd/system/redpitaya_heartbeat.service
 install -v -m 664 -o root -D $OVERLAY/etc/sysconfig/redpitaya                        $ROOT_DIR/etc/sysconfig/redpitaya
 # TODO: this Wyliodrin service is only here since wyliodrin.sh can not be run in a virtualized environment
 # Wyliodrin service
@@ -24,7 +23,6 @@ chroot $ROOT_DIR <<- EOF_CHROOT
 systemctl enable redpitaya_discovery
 systemctl enable redpitaya_nginx
 #systemctl enable redpitaya_scpi
-systemctl enable redpitaya_heartbeat
 
 apt-get -y install libluajit-5.1 lua-cjson unzip
 apt-get -y install libboost-system1.55.0 libboost-regex1.55.0 libboost-thread1.55.0
