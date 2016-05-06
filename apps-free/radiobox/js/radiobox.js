8969ac3f1e333f753662d8ec206153d55ee3b3ce
Ulrich Habel <espero7757@gmx.net>
2016-05-06 20:49:54 +0200

    RedPitaya ALSA - pre-set frequency-controller to 7210 kHz of receiver qrg field

--- apps-free/radiobox/js/radiobox.js
+++ apps-free/radiobox/js/radiobox.js
@@ -64,7 +64,7 @@
         },
         mousewheelsum: 0,
         digit: {
-            e: [ 0, 0, 0, 0, 0, 1, 0, 0 ]  // reversed digits
+            e: [ 0, 0, 0, 0, 1, 2, 7, 0 ]  // reversed digits
         },
         editing: false,
         enter: false,
@@ -89,7 +89,7 @@
     rx_muxin_src_s:         2,  // receiver RF input set to RF Input 2
 
     tx_car_osc_qrg_f:       0,  //    0 kHz
-    rx_car_osc_qrg_f: 7210000,  // 7210 kHz
+    rx_car_osc_qrg_f: 7210000,  // 7210 kHz, @see also: RB.state.qrgController.digit.e[]
 
     tx_mod_osc_qrg_f:    1000,  //   1 kHz
     tx_muxin_gain_s:       80,  // slider position in % of 100% (80% = FS input with booster 1:1)
