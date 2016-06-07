b809a2e9edbf403c5d362b18e4554c7840ce8ec1
Iztok Jeras <iztok.jeras@gmail.com>
2016-06-07 14:30:34 +0200

    OS: fixing issues with building OS

--- OS/debian/network.sh
+++ OS/debian/network.sh
@@ -16,18 +16,9 @@
 #ln -s                                                          wlan0.ap                  $ROOT_DIR/etc/network/interfaces.d/wlan0
 
 # systemd-networkd wired/wireless network configuration (DHCP and WPA suplicant for WiFi)
-install -v -m 664 -o root -D $OVERLAY/etc/systemd/network/wired.network                   $ROOT_DIR/etc/systemd/network/wired.network
-install -v -m 664 -o root -D $OVERLAY/etc/systemd/network/wireless.network                $ROOT_DIR/etc/systemd/network/wireless.network
-
-# this enables placing the WiFi WPA configuration into the FAT partition
-ln -s /opt/redpitaya/wpa_suplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-
-# this is a fix for persistent naming rules for USB network adapters
-# otherwise WiFi adapters are named "wlx[MACAddress]"
-ln -s /dev/null /etc/udev/rules.d/73-special-net-names.rules
-
-# use systemd-reloslver
-ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+install -v -m 664 -o root -D $OVERLAY/etc/systemd/network/wired.network                  $ROOT_DIR/etc/systemd/network/wired.network
+install -v -m 664 -o root -D $OVERLAY/etc/systemd/network/wireless.network               $ROOT_DIR/etc/systemd/network/wireless.network
+install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/wpa_supplicant@.service         $ROOT_DIR/etc/systemd/system/wpa_supplicant@.service
 
 chroot $ROOT_DIR <<- EOF_CHROOT
 # network tools
@@ -44,6 +35,16 @@ sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 # TODO: install was asking about /etc/{protocols,services}
 apt-get -y install linux-firmware
 apt-get -y install wpasupplicant iw
+
+# this enables placing the WiFi WPA configuration into the FAT partition
+ln -s /opt/redpitaya/wpa_suplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+
+# this is a fix for persistent naming rules for USB network adapters
+# otherwise WiFi adapters are named "wlx[MACAddress]"
+ln -s /dev/null /etc/udev/rules.d/73-special-net-names.rules
+
+# use systemd-reloslver
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # there is a Systemd approach to this, might be used later
 #sed -i '/^#net.ipv4.ip_forward=1$/s/^#//' /etc/sysctl.conf
