6ef9f5691e62793ef8d8ad9f18b6e2efb5a36133
Artem Kokos <a.kokos@integrasources.com>
2016-07-01 19:31:21 +0600

    Network manager: fixed creating access point

--- apps-tools/network_manager/lua/create_wifi_point.lua
+++ apps-tools/network_manager/lua/create_wifi_point.lua
@@ -3,22 +3,26 @@ local shell = require("shell")
 
 local args = ngx.req.get_uri_args()
 
-local status, out, err = shell.execute("rm -f /opt/redpitaya/wpa_supplicant.conf")
-local status, out, err = shell.execute("systemctl stop wpa_supplicant_wext@wlan0wext.service")
-local status, out, err = shell.execute("rm -f /opt/redpitaya/hostapd.conf")
+local status, out, err = os.execute("rm -f /opt/redpitaya/wpa_supplicant.conf")
+local status, out, err = os.execute("systemctl stop wpa_supplicant_wext@wlan0wext.service")
+local status, out, err = os.execute("rm -f /opt/redpitaya/hostapd.conf")
 
-local status, out, err = shell.execute("echo interface=wlan0wext >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo ssid="..args.essid.." >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo driver=rtl871xdrv >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo hw_mode=g >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo channel=6 >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo macaddr_acl=0 >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo auth_algs=1 >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo ignore_broadcast_ssid=0 >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo wpa=2 >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo wpa_passphrase="..args.password.." >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo wpa_key_mgmt=WPA-PSK >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo wpa_pairwise=TKIP >> /opt/redpitaya/hostapd.conf")
-local status, out, err = shell.execute("echo rsn_pairwise=CCMP >> /opt/redpitaya/hostapd.conf")
+local hostapd = "interface=wlan0wext\n"
+hostapd.."ssid="..args.essid.."\n"
+hostapd.."driver=rtl871xdrv\n"
+hostapd.."hw_mode=g\n"
+hostapd.."channel=6\n"
+hostapd.."macaddr_acl=0\n"
+hostapd.."auth_algs=1\n"
+hostapd.."ignore_broadcast_ssid=0\n"
+hostapd.."wpa=2\n"
+hostapd.."wpa_passphrase="..args.password.."\n"
+hostapd.."wpa_key_mgmt=WPA-PSK\n"
+hostapd.."wpa_pairwise=TKIP\n"
+hostapd.."rsn_pairwise=CCMP\n"
 
-local status, out, err = shell.execute("systemctl restart hostapd@wlan0wext.service")
+local hostapd_handler = io.open("/opt/redpitaya/hostapd.conf", "w")
+hostapd_handler:write(hostapd)
+hostapd_handler:close()
+
+local status, out, err = os.execute("systemctl restart hostapd@wlan0wext.service")
