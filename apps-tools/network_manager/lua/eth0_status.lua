e5eca487d6b00d60cecd1c7ce2d3b89fac8d781c
Iztok Jeras <iztok.jeras@gmail.com>
2016-07-01 20:45:51 +0200

    network_manager: moved a lua script to nginx.conf

--- apps-tools/network_manager/lua/eth0_status.lua
+++
@@ -1,9 +0,0 @@
-local shell = require("shell")
-
-local status, out, err = shell.execute("ip addr list eth0 > /tmp/eth0_status.txt")
-
-
-local f = io.open("/tmp/eth0_status.txt", "r")
-local content = f:read("*all")
-f:close()
-ngx.say(content)
