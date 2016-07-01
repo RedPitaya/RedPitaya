8efad8ebb23f057c8329b83337ba311b4fa4d4c8
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-07-01 19:02:46 +0200

    network_manager cleanup, moved LUA code into nginx.conf, and not the code shoud do the correct things

--- apps-tools/network_manager/generate_supplicant.sh
+++
@@ -1,7 +0,0 @@
-rw
-systemctl stop hostapd@wlan0wext.service
-rm -f /opt/redpitaya/hostapd.conf
-rm -f /opt/redpitaya/wpa_supplicant.conf
-wpa_passphrase "$1" "$2" > /opt/redpitaya/wpa_supplicant.conf
-systemctl restart wpa_supplicant_wext@wlan0wext.service
-# reboot
--- apps-tools/network_manager/lua/eth0_do_dhcp.lua
+++
@@ -1,4 +0,0 @@
-local shell = require("shell")
-
-local status, out, err = shell.execute("dhclient -r eth0")
-local status, out, err = shell.execute("dhclient eth0")
