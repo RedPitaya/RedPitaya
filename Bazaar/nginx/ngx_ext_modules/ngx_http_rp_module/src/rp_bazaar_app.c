a9e52631ed882dbd324c34be766e1e1b7a3684c4
A.Kaygorodov <a.kgorodov@gmail.com>
2016-07-03 11:06:01 +0700

    Attemt to fix licensing

--- Bazaar/nginx/ngx_ext_modules/ngx_http_rp_module/src/rp_bazaar_app.c
+++ Bazaar/nginx/ngx_ext_modules/ngx_http_rp_module/src/rp_bazaar_app.c
@@ -230,12 +230,6 @@ inline int is_registered(const char *dir,
     const mode_t perms = S_IRUSR | S_IXUSR;
 
     fprintf(stderr, "is_registered dir: %s\n app_id: %s\n fname: %s\n", dir, app_id, fname);
-    if(!strcmp(app_id, "scopegenpro"))
-    	return 1;
-
-	if(!strcmp(app_id, "spectrumpro"))
-    	return 1;
-
     sprintf(file, "%s/%s/%s", dir, app_id, fname);
 
     if(stat((const char *)file, &stat_buf) < 0) {
