0aa364f7ca0c5b07dceaed2d030c64298bb3e77a
Artem Kokos <a.kokos@integrasources.com>
2016-07-06 19:29:09 +0600

    Network Maanger: fixed space character issue

--- apps-tools/network_manager/js/manager.js
+++ apps-tools/network_manager/js/manager.js
@@ -127,7 +127,7 @@
                     return;
                 else {
                     WIZARD.connectedESSID = essid;
-                    $('.btn-wifi-item[key=' + WIZARD.connectedESSID + ']').css('color', 'red');
+                    $('.btn-wifi-item[key=\'' + WIZARD.connectedESSID + '\']').css('color', 'red');
                     $("#wlan0_essid_label").text(WIZARD.connectedESSID);
                 }
             });
