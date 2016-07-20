7a2292d1f5c50e1ce687a2acf651f5243805612a
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-07-20 15:20:02 +0200

    DFPGA2: added project paths to DTS and FSBL tools

--- fpga2/red_pitaya_hsi_dts.tcl
+++ fpga2/red_pitaya_hsi_dts.tcl
@@ -2,12 +2,12 @@
 # HSI tcl script for building RedPitaya DTS (device tree)
 #
 # Usage:
-# hsi -mode tcl -source red_pitaya_hsi_dts.tcl
+# hsi -mode tcl -source red_pitaya_hsi_dts.tcl -tclargs projectname
 ################################################################################
 
-set path_sdk sdk
+cd prj/$::argv
 
-#set boot_args {console=ttyPS0,115200n8 root=/dev/ram rw initrd=0x00800000,16M earlyprintk mtdparts=physmap-flash.0:512K(nor-fsbl),512K(nor-u-boot),5M(nor-linux),9M(nor-user),1M(nor-scratch),-(nor-rootfs)}
+set path_sdk sdk
 
 open_hw_design $path_sdk/red_pitaya.sysdef
 
@@ -16,7 +16,6 @@ set_repo_path ../tmp/device-tree-xlnx-xilinx-v2016.2/
 create_sw_design device-tree -os device_tree -proc ps7_cortexa9_0
 
 set_property CONFIG.kernel_version {2016.2} [get_os]
-#set_property CONFIG.bootargs $boot_args [get_os]
 
 generate_target -dir $path_sdk/dts
 
