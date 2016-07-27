2e65ffe2cad47c9c8e8c8c94e1afabf3c7fd8f0b
Mikhail Efimenko <m.efimenko@integrasources.com>
2016-07-27 16:01:46 +0700

    Added template application in Examples

---
+++ Examples/web-tutorial/1.template/template_app/src/main.h
@@ -0,0 +1,43 @@
+/**
+ * $Id: main.h 881 2013-12-16 05:37:34Z rp_jmenart $
+ *
+ * @brief Red Pitaya Oscilloscope main module.
+ *
+ * @Author Jure Menart <juremenart@gmail.com>
+ *         
+ * (c) Red Pitaya  http://www.redpitaya.com
+ *
+ * This part of code is written in C programming language.
+ * Please visit http://en.wikipedia.org/wiki/C_(programming_language)
+ * for more details on the language used herein.
+ */
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+
+/* Parameters description structure - must be the same for all RP controllers */
+typedef struct rp_app_params_s {
+    char  *name;
+    float  value;
+    int    fpga_update;
+    int    read_only;
+    float  min_val;
+    float  max_val;
+} rp_app_params_t;
+
+
+
+/* module entry points */
+int rp_app_init(void);
+int rp_app_exit(void);
+int rp_set_params(rp_app_params_t *p, int len);
+int rp_get_params(rp_app_params_t **p);
+int rp_get_signals(float ***s, int *sig_num, int *sig_len);
+
+
+/* Internal helper functions */
+int  rp_create_signals(float ***a_signals);
+void rp_cleanup_signals(float ***a_signals);
+
+#endif
