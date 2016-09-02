6f2302b562761c34509c4b68397c9250ce42543f
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-09-02 13:11:22 +0200

    FPGA-classic: removing GPIO/LED registers

--- fpga/prj/classic/tbn/red_pitaya_hk_tb.sv
+++ fpga/prj/classic/tbn/red_pitaya_hk_tb.sv
@@ -17,8 +17,7 @@
  *
  * Testbench for house keeping module.
  *
- * Simple testbench to test register read and write. Testing expansion connector
- * and DNA macro.
+ * Simple testbench to test register read and write. Testing DNA macro.
  * 
  */
 
@@ -28,8 +27,6 @@ module red_pitaya_hk_tb #(
   // time periods
   realtime  TP = 8.0ns,  // 125MHz
   // RTL config
-  parameter DWL = 8, // data width for LED
-  parameter DWE = 8, // data width for extension
   parameter [57-1:0] DNA = 57'h0823456789ABCDE
 );
 
@@ -53,17 +50,6 @@ initial begin
   rstn = 1'b1;
 end
 
-// tristate signals
-wire  [  8-1: 0] led    ;
-wire  [  8-1: 0] exp_p_dat;
-wire  [  8-1: 0] exp_n_dat;
-//               output       input        enable
-logic [  8-1: 0] exp_p_dat_o, exp_p_dat_i, exp_p_dat_e;
-logic [  8-1: 0] exp_n_dat_o, exp_n_dat_i, exp_n_dat_e;
-
-pullup   pullup_exp_dat   [8-1:0] (exp_p_dat);
-pulldown pulldown_exp_dat [8-1:0] (exp_n_dat);
-
 ////////////////////////////////////////////////////////////////////////////////
 // test sequence
 ////////////////////////////////////////////////////////////////////////////////
@@ -86,15 +72,6 @@ logic [57-1:0] dna;
 initial begin
   wait (rstn)
   repeat(600) @(posedge clk);
-    // LED
-    wdata = 32'h000000_03;
-    bus.write(32'h30, wdata);
-    if (led != (wdata[DWL-1:0] & 8'h01))  error++;
-
-    // EXPANSION P
-    bus.write(32'h10, 32'h000000_33); // direction
-    bus.write(32'h18, 32'h000000_0F); // value
-
     // read ID value
     bus.read(32'h00, rdata);
     $display ("ID = 0x%08x", rdata);
@@ -105,9 +82,6 @@ initial begin
     bus.read(32'h08, rdata); // higher part
     dna [57-1:32] = rdata [57-32-1:0];
     $display ("DNA = 0x%016x", dna);
-
-    bus.read(32'h24, rdata);
-    bus.read(32'h30, rdata);
   repeat(20000) @(posedge clk);
   $finish();
 end
@@ -134,15 +108,6 @@ sys_bus_model bus (
 red_pitaya_hk hk (
   .clk_i          (clk      ),
   .rstn_i         (rstn     ),
-   // LED
-  .led_o          (led      ),
-  // Expansion connector
-  .exp_p_dat_i    (exp_p_dat_i),
-  .exp_p_dat_o    (exp_p_dat_o),
-  .exp_p_dir_o    (exp_p_dat_e),
-  .exp_n_dat_i    (exp_n_dat_i),
-  .exp_n_dat_o    (exp_n_dat_o),
-  .exp_n_dir_o    (exp_n_dat_e),
    // System bus
   .sys_addr       (sys_addr ),
   .sys_wdata      (sys_wdata),
@@ -153,13 +118,6 @@ red_pitaya_hk hk (
   .sys_err        (sys_err  ),
   .sys_ack        (sys_ack  )
 );
-
-//                               tristate   output       enable
-bufif0 bufif0_exp_p_dat [8-1:0] (exp_p_dat, exp_p_dat_o, exp_p_dat_e);
-bufif0 bufif0_exp_n_dat [8-1:0] (exp_n_dat, exp_n_dat_o, exp_n_dat_e);
-//     input         tristate
-assign exp_p_dat_i = exp_p_dat;
-assign exp_n_dat_i = exp_n_dat;
 
 ////////////////////////////////////////////////////////////////////////////////
 // waveforms
