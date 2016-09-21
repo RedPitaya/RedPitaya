c19e221fe301c536a537189338166860068faaad
Uros Golob <uros.golob@redpitaya.com>
2016-09-21 12:11:41 +0200

    HAMLAB: correct path for overlay

--- patches/devicetree/hamlab_i2cmux.sh
+++ patches/devicetree/hamlab_i2cmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /sys/kernel/config/device-tree/overlays/i2cmux
-cat /root/hamlab_i2cmux.dtbo >/sys/kernel/config/device-tree/overlays/i2cmux/dtbo
+cat /boot/hamlab_i2cmux.dtbo >/sys/kernel/config/device-tree/overlays/i2cmux/dtbo
 
 i2cset -y 8 0x20 6 0
 i2cset -y 8 0x20 7 0
