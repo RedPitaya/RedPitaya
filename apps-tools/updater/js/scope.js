58e56133c186e292ed49a63acae9160b8617ccfd
Mikhail Efimenko <m.efimenko@integrasources.com>
2016-09-27 14:11:24 +0700

    Fixed updater

--- apps-tools/updater/js/scope.js
+++ apps-tools/updater/js/scope.js
@@ -9,13 +9,13 @@
 
 (function(UPD, $, undefined) {
     UPD.currentStep = 1;
-    var ecosystems = [];
-    var ecosystems_sizes = [];
-    var chosen_eco = -1;
-	var isApply = false;
+    UPD.ecosystems = [];
+    UPD.ecosystems_sizes = [];
+    UPD.chosen_eco = -1;
+	UPD.isApply = false;
 
     UPD.currentVer = undefined;
-	UPD.type = '0.96';
+	UPD.type = '0.97';
 
     UPD.startStep = function(step) {
         UPD.currentStep = step;
@@ -30,7 +30,7 @@
                 UPD.checkVersion();
                 break;
             case 3:
-                UPD.checkUpdates();
+                UPD.checkUpdates(UPD.type);
                 break;
             case 4:
                 UPD.downloadEcosystem();
@@ -83,7 +83,7 @@
         }, 500);
     }
 
-    UPD.checkUpdates = function(type = '0.96') {
+    UPD.checkUpdates = function(type) {
         $('#select_ver').hide();
         setTimeout(function() {
             $.ajax({
@@ -94,18 +94,27 @@
                     var resp = msg.responseText;
                     var arr = resp.split('\n');
                     if (arr.length == 0 || arr.length <= 2 || arr.length % 2 != 0) {
-                        $('#step_' + UPD.currentStep).find('.step_icon').find('img').attr('src', 'img/fail.png');
-                        $('#step_' + UPD.currentStep).find('.error_msg').show();
-                        return;
+                        if(UPD.type == "0.97")
+                        {
+                            $("#ecosystem_type").val("2");
+                            UPD.type = "0.96";
+                            UPD.checkUpdates('0.96');
+                            return;
+                        } else {
+                            $('#step_' + UPD.currentStep).find('.step_icon').find('img').attr('src', 'img/fail.png');
+                            $('#step_' + UPD.currentStep).find('.error_msg').show();
+                            return;
+                        }
                     }
                     var list = [];
-					ecosystems = [];
+					UPD.ecosystems = [];
+                    UPD.ecosystems_sizes = [];
                     for (var i = 0; i < arr.length; i += 2) {
                         if (arr[i] != "" && arr[i].startsWith("ecosystem")) {
                             var size = parseInt(arr[i + 1]) * 1;
                             var sizeM = size / (1024 * 1024);
-                            ecosystems_sizes.push(size);
-                            ecosystems.push(arr[i]);
+                            UPD.ecosystems_sizes.push(size);
+                            UPD.ecosystems.push(arr[i]);
                             list.push(arr[i] + "-" + sizeM.toFixed(2) + "M");
                         }
                     }
@@ -129,12 +138,13 @@
                     $('#ecosystem_ver').removeAttr('disabled');
                     $('.select_ver').show();
                     $('#apply').click(function(event) {
-						if (isApply)
+						if (UPD.isApply)
 							return; // FIXME
-						isApply = true;
+                        $('.select_ver').hide();
+						UPD.isApply = true;
                         var val = $('#ecosystem_ver').val();
-                        chosen_eco = ecosystems.indexOf(val);
-                        if (chosen_eco != -1) {
+                        UPD.chosen_eco = UPD.ecosystems.indexOf(val);
+                        if (UPD.chosen_eco != -1) {
                             UPD.nextStep();
                             $('#apply').hide();
                             $('#and_click').hide();
@@ -149,13 +159,13 @@
     }
 
     UPD.downloadEcosystem = function() {
-		if (!isApply) {
+		if (!UPD.isApply) {
         	--UPD.currentStep; // FIXME
 			return;
 		}
         setTimeout(function() {
             $.ajax({
-                url: '/update_download?ecosystem=' + UPD.type + '/' + ecosystems[chosen_eco],
+                url: '/update_download?ecosystem=' + UPD.type + '/' + UPD.ecosystems[UPD.chosen_eco],
                 type: 'GET',
             }).always(function() {
                 $('#step_' + UPD.currentStep).find('.step_icon').find('img').hide();
@@ -172,11 +182,11 @@
                             $('#step_' + UPD.currentStep).find('.error_msg').show();
                             clearInterval(check_progress);
                         } else {
-                            var percent = ((size / ecosystems_sizes[chosen_eco]) * 100).toFixed(2);
+                            var percent = ((size / UPD.ecosystems_sizes[UPD.chosen_eco]) * 100).toFixed(2);
                             $('#percent').text(percent + "%");
                             $('#percent').show();
 
-                            if (size >= ecosystems_sizes[chosen_eco]) {
+                            if (size >= UPD.ecosystems_sizes[UPD.chosen_eco]) {
                                 $('#percent').hide();
                                 UPD.nextStep();
                                 clearInterval(check_progress);
@@ -227,7 +237,7 @@
                                 })
                                 .done(function(msg) {
                                     if (msg != undefined && msg['version'] !== undefined) {
-                                        var eco = ecosystems[chosen_eco];
+                                        var eco = UPD.ecosystems[UPD.chosen_eco];
                                         var arr = eco.split('-');
                                         var ver = arr[1] + '-' + arr[2];
                                         if (msg['version'] == ver) {
@@ -312,10 +322,10 @@ $(document).ready(function() {
 	$('#ecosystem_type').change(function(){
 		if ($(this).val() == '1') {
 			$('#warn').hide();
-			UPD.type = '0.96';
+			UPD.type = '0.97';
 		} else if ($(this).val() == '2') {
 			$('#warn').hide();
-			UPD.type = '0.97';
+			UPD.type = '0.96';
 		} else if ($(this).val() == '3') {
 			$('#warn').show();
 			UPD.type = 'beta_0.97';
