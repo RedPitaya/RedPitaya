fddddc78f3aef38da0fcfc7c2d02c7834c0b6a8c
Uros Golob <uros.golob@redpitaya.com>
2016-10-10 15:55:49 +0200

    HAMLAB: change pwm frequency from 1.4kHz to 22.5kHz

--- patches/devicetree/hamlab_i2cmux.sh
+++ patches/devicetree/hamlab_i2cmux.sh
@@ -2,6 +2,7 @@
 mkdir /sys/kernel/config/device-tree/overlays/i2cmux
 cat /boot/hamlab_i2cmux.dtbo >/sys/kernel/config/device-tree/overlays/i2cmux/dtbo
 
+#just in case if pulldown resistor is not soldered ok
 i2cset -y 8 0x20 6 0
 i2cset -y 8 0x20 7 0
 i2cset -y 8 0x21 6 0
@@ -11,5 +12,10 @@ i2cset -y 8 0x20 3 0
 i2cset -y 8 0x21 2 0
 i2cset -y 8 0x21 3 0
 
+#set 22.5kHz pwm
+i2cset -y -r 2 0x2e 0x74 0x70
+#insert fan control kernel driver
 echo adt7470 0x2e > /sys/bus/i2c/devices/i2c-2/new_device
-echo 123 > /sys/class/hwmon/hwmon0/pwm3_auto_point2_pwm
+
+#systemctl stop fancontrol.service
+#systemctl start fancontrol.service
