e2e3a4dc088cc6e814f554159532baa5f5933fbe
Uros Golob <uros.golob@redpitaya.com>
2016-10-17 13:29:28 +0200

    hamlab: fix install script

--- OS/debian/hamlab.sh
+++ OS/debian/hamlab.sh
@@ -6,18 +6,17 @@
 # https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/COPYING
 ################################################################################
 
-# Overlay loading service
-install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/hamlab_i2cmux.service       $ROOT_DIR/etc/systemd/system/hamlab_i2cmux.service
-
 # Install lm-sensors, fancontrol
 apt-get install lm-sensors fancontrol
 
-# enable pwm control service
-
+# Overlay loading service
+install -v -m 664 -o root -D $OVERLAY/etc/systemd/system/hamlab.service $ROOT_DIR/etc/systemd/system/hamlab.service
 
 # add fancontrol config file, generated using pwmconfig
 install -v -m 664 -o root -D $OVERLAY/etc/fancontrol $ROOT_DIR/etc/fancontrol
 
+# enable services
 chroot $ROOT_DIR <<- EOF_CHROOT
 systemctl enable fancontrol.service
+systemctl enable hamlab.service
 EOF_CHROOT
