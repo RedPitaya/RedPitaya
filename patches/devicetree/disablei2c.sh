11865ad574f8a7feb573d96afeb2897f23b8b21d
Uros Golob <uros.golob@redpitaya.com>
2016-10-24 10:40:29 +0200

    HAMLAB: fix script

--- patches/devicetree/disablei2c.sh
+++ patches/devicetree/disablei2c.sh
@@ -3,11 +3,12 @@
 rmdir /sys/kernel/config/device-tree/overlays/i2cmux
 
 mkdir /sys/kernel/config/device-tree/overlays/disablei2c
-cat /opt/redpitaya/dts/disablei2c.dtbo >/sys/kernel/config/device-tree/overlays/disablei2c/dtbo
+cat /opt/redpitaya/disablei2c.dtbo >/sys/kernel/config/device-tree/overlays/disablei2c/dtbo
 rmdir /sys/kernel/config/device-tree/overlays/disablei2c
 
 mkdir /sys/kernel/config/device-tree/overlays/i2cmux
-cat /opt/redpitaya/dts/i2cmux.dtbo >/sys/kernel/config/device-tree/overlays/i2cmux/dtbo
+cat /opt/redpitaya/i2cmux.dtbo >/sys/kernel/config/device-tree/overlays/i2cmux/dtbo
 
 mkdir /sys/kernel/config/device-tree/overlays/gpioi2chamlab
-cat /opt/redpitaya/dts/gpioi2chamlab.dtbo >/sys/kernel/config/device-tree/overlays/gpioi2chamlab/dtbo
+cat /opt/redpitaya/gpioi2chamlab.dtbo >/sys/kernel/config/device-tree/overlays/gpioi2chamlab/dtbo
+
