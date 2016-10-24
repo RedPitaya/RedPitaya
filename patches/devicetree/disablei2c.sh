6bd5f88cbeb07d24b5c74f1b519cbf8e444774c7
Uros Golob <uros.golob@redpitaya.com>
2016-10-24 11:39:18 +0200

    HAMLAB: do not install gpio driver -  for testing only - remove shut down button

--- patches/devicetree/disablei2c.sh
+++ patches/devicetree/disablei2c.sh
@@ -9,6 +9,6 @@ rmdir /sys/kernel/config/device-tree/overlays/disablei2c
 mkdir /sys/kernel/config/device-tree/overlays/i2cmux
 cat /opt/redpitaya/i2cmux.dtbo >/sys/kernel/config/device-tree/overlays/i2cmux/dtbo
 
-mkdir /sys/kernel/config/device-tree/overlays/gpioi2chamlab
-cat /opt/redpitaya/gpioi2chamlab.dtbo >/sys/kernel/config/device-tree/overlays/gpioi2chamlab/dtbo
+#mkdir /sys/kernel/config/device-tree/overlays/gpioi2chamlab
+#cat /opt/redpitaya/gpioi2chamlab.dtbo >/sys/kernel/config/device-tree/overlays/gpioi2chamlab/dtbo
 
