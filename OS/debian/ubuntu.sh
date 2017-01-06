9e94799d2dd64dce8edee888b0c5ef4d1bc5a371
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-01-06 15:36:23 +0100

    Jupyter: add script to OS build procedure

--- OS/debian/ubuntu.sh
+++ OS/debian/ubuntu.sh
@@ -139,6 +139,7 @@ EOF_CHROOT
 . OS/debian/network.sh
 . OS/debian/redpitaya.sh
 #. OS/debian/wyliodrin.sh
+. OS/debian/jupyter.sh
 #. OS/debian/tft.sh
 
 ################################################################################
