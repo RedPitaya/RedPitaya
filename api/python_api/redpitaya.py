95e9741b708e57dda83833e640018f44d0e6e7a3
v.fomin <v.fomin@integrasources.com>
2017-01-16 05:19:02 +0000

    added base Python API and example

---
+++ api/python_api/redpitaya.py
@@ -0,0 +1,77 @@
+from ctypes import *
+import time
+
+rp_api = CDLL('/opt/redpitaya/lib/librp.so')
+
+CH_1 = 0
+CH_2 = 1
+
+TRIG_SRC_DISABLED = 0 # Trigger is disabled
+TRIG_SRC_NOW      = 1 # Trigger triggered now (immediately)
+TRIG_SRC_CHA_PE   = 2 # Trigger set to Channel A threshold positive edge
+TRIG_SRC_CHA_NE   = 3 # Trigger set to Channel A threshold negative edge
+TRIG_SRC_CHB_PE   = 4 # Trigger set to Channel B threshold positive edge
+TRIG_SRC_CHB_NE   = 5 # Trigger set to Channel B threshold negative edge
+TRIG_SRC_EXT_PE   = 6 # Trigger set to external trigger positive edge (DIO0_P pin)
+TRIG_SRC_EXT_NE   = 7 # Trigger set to external trigger negative edge (DIO0_P pin)
+TRIG_SRC_AWG_PE   = 8 # Trigger set to arbitrary wave generator application positive edge
+TRIG_SRC_AWG_NE   = 9 # Trigger set to arbitrary wave generator application negative edge
+
+WAVEFORM_SINE      = 0 # Wave form sine
+WAVEFORM_SQUARE    = 1 # Wave form square
+WAVEFORM_TRIANGLE  = 2 # Wave form triangle
+WAVEFORM_RAMP_UP   = 3 # Wave form sawtooth (/|)
+WAVEFORM_RAMP_DOWN = 4 # Wave form reversed sawtooth (|\)
+WAVEFORM_DC        = 5 # Wave form dc
+WAVEFORM_PWM       = 6 # Wave form pwm
+WAVEFORM_ARBITRARY = 7 # Use defined wave form
+
+TRIG_STATE_TRIGGERED = 0 # Trigger is triggered/disabled
+TRIG_STATE_WAITING   = 1 # Trigger is set up and waiting (to be triggered)
+
+
+class base:
+	def Init():
+		return rp_api.rp_Init()
+
+	def GenReset():
+		return rp_api.rp_GenReset()
+
+	def GenFreq(channel, freq):
+		return rp_api.rp_GenFreq(channel, c_float(freq))
+
+	def GenAmp(channel, freq):
+		return rp_api.rp_GenAmp(channel, c_float(freq))
+	
+	def GenWaveform(channel, form):
+		return rp_api.rp_GenWaveform(channel, form)
+
+	def GenOutEnable(channel):
+		return rp_api.rp_GenOutEnable(channel)
+
+	def AcqReset():
+		return rp_api.rp_AcqReset()
+
+	def AcqSetDecimation(dec_factor):
+		return rp_api.rp_AcqSetDecimation(dec_factor)
+
+	def AcqSetTriggerLevel(channel, level):
+		return rp_api.rp_AcqSetTriggerLevel(channel, c_float(level))
+
+	def AcqSetTriggerDelay(delay):
+		return rp_api.rp_AcqSetTriggerDelay(delay)
+
+	def AcqStart():
+		return rp_api.rp_AcqStart()
+
+	def AcqSetTriggerSrc(source):
+		return rp_api.rp_AcqSetTriggerSrc(source)
+
+	def AcqGetTriggerState(state):
+		return rp_api.rp_AcqGetTriggerState(byref(state))
+
+	def AcqGetOldestDataV(channel, buff_size, buff):
+		return rp_api.rp_AcqGetOldestDataV(0, byref(buff_size), byref(buff));
+
+	def CreateFloatBuffer(size):
+		return (c_float*size)()
---
+++ api/python_api/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from ctypes import *
+from redpitaya import *
+from time import sleep
+
+base.Init()
+base.GenReset()
+base.GenFreq(CH_1, 20000.0)
+base.GenAmp(CH_1, 1.0)
+base.GenWaveform(CH_1, WAVEFORM_SINE)
+base.GenOutEnable(CH_1)
+
+base.AcqReset()
+base.AcqSetDecimation(1)
+base.AcqSetTriggerLevel(CH_1, 0.1)
+base.AcqSetTriggerDelay(CH_1)
+
+base.AcqStart()
+
+sleep(1)
+base.AcqSetTriggerSrc(TRIG_SRC_CHA_PE)
+
+state = c_long(TRIG_STATE_TRIGGERED)
+while True:
+	ret = base.AcqGetTriggerState(state)
+	if (state.value == TRIG_STATE_TRIGGERED):
+		print("TRIGGERED")
+		break
+
+size = c_long(16384)
+buff = base.CreateFloatBuffer(size.value)
+base.AcqGetOldestDataV(CH_1, size, buff);
+for i in range(size.value):
+	print(buff[i])
