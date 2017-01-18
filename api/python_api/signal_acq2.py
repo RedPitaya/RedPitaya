d7eac07b2f2aea9813e0d0d0e45a523c4f9306c8
v.fomin <v.fomin@integrasources.com>
2017-01-18 09:12:04 +0000

    added Generate signal on external trigger example

---
+++ api/python_api/signal_acq2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from ctypes import *
+from redpitaya import *
+from time import sleep
+
+base.Init()
+
+base.AcqSetDecimation(8)
+base.AcqSetTriggerLevel(CH_1, 100)
+base.AcqStart()
+base.AcqSetTriggerSrc(TRIG_SRC_EXT_PE)
+
+while base.AcqGetTriggerState() != TRIG_STATE_TRIGGERED:
+	pass
+print('triggered')
+
+size = base.AcqGetBufSize();
+buff = base.AcqGetOldestDataV(CH_1, size)
+for v in buff:
+	print(v)
+
+base.Release()
