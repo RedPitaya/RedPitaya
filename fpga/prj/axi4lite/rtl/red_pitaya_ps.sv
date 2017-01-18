6ed83a89b250ea11fe66080beeb531b156193e50
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-01-18 12:38:17 +0100

    FPGA: axi4lite: changed clock source from PS internal to ADC reference clock

--- fpga/prj/axi4lite/rtl/red_pitaya_ps.sv
+++ fpga/prj/axi4lite/rtl/red_pitaya_ps.sv
@@ -1,60 +1,39 @@
 ////////////////////////////////////////////////////////////////////////////////
-// @brief Red Pitaya Processing System (PS) wrapper. Including simple AXI slave.
-// @Author Matej Oblak
+// Red Pitaya Processing System (PS) wrapper
+// Author: Iztok Jeras
 // (c) Red Pitaya  http://www.redpitaya.com
 ////////////////////////////////////////////////////////////////////////////////
 
-/**
- * GENERAL DESCRIPTION:
- *
- * Wrapper of block design.  
- *
- *                   /-------\
- *   PS CLK -------> |       | <---------------------> SPI master & slave
- *   PS RST -------> |  PS   |
- *                   |       | ------------+---------> FCLK & reset 
- *                   |       |             |
- *   PS DDR <------> |  ARM  |   AXI   /-------\
- *   PS MIO <------> |       | <-----> |  AXI  | <---> system bus
- *                   \-------/         | SLAVE |
- *                                     \-------/
- *
- * Module wrappes PS module (BD design from Vivado or EDK from PlanAhead).
- * There is also included simple AXI slave which serves as master for custom
- * system bus. With this simpler bus it is more easy for newbies to develop 
- * their own module communication with ARM.
- */
-
 module red_pitaya_ps (
   // PS peripherals
-  inout  logic [ 54-1:0] FIXED_IO_mio       ,
-  inout  logic           FIXED_IO_ps_clk    ,
-  inout  logic           FIXED_IO_ps_porb   ,
-  inout  logic           FIXED_IO_ps_srstb  ,
-  inout  logic           FIXED_IO_ddr_vrn   ,
-  inout  logic           FIXED_IO_ddr_vrp   ,
+  inout  logic [54-1:0] FIXED_IO_mio     ,
+  inout  logic          FIXED_IO_ps_clk  ,
+  inout  logic          FIXED_IO_ps_porb ,
+  inout  logic          FIXED_IO_ps_srstb,
+  inout  logic          FIXED_IO_ddr_vrn ,
+  inout  logic          FIXED_IO_ddr_vrp ,
   // DDR
-  inout  logic [ 15-1:0] DDR_addr           ,
-  inout  logic [  3-1:0] DDR_ba             ,
-  inout  logic           DDR_cas_n          ,
-  inout  logic           DDR_ck_n           ,
-  inout  logic           DDR_ck_p           ,
-  inout  logic           DDR_cke            ,
-  inout  logic           DDR_cs_n           ,
-  inout  logic [  4-1:0] DDR_dm             ,
-  inout  logic [ 32-1:0] DDR_dq             ,
-  inout  logic [  4-1:0] DDR_dqs_n          ,
-  inout  logic [  4-1:0] DDR_dqs_p          ,
-  inout  logic           DDR_odt            ,
-  inout  logic           DDR_ras_n          ,
-  inout  logic           DDR_reset_n        ,
-  inout  logic           DDR_we_n           ,
+  inout  logic [15-1:0] DDR_addr   ,
+  inout  logic [ 3-1:0] DDR_ba     ,
+  inout  logic          DDR_cas_n  ,
+  inout  logic          DDR_ck_n   ,
+  inout  logic          DDR_ck_p   ,
+  inout  logic          DDR_cke    ,
+  inout  logic          DDR_cs_n   ,
+  inout  logic [ 4-1:0] DDR_dm     ,
+  inout  logic [32-1:0] DDR_dq     ,
+  inout  logic [ 4-1:0] DDR_dqs_n  ,
+  inout  logic [ 4-1:0] DDR_dqs_p  ,
+  inout  logic          DDR_odt    ,
+  inout  logic          DDR_ras_n  ,
+  inout  logic          DDR_reset_n,
+  inout  logic          DDR_we_n   ,
   // system signals
-  output logic [  4-1:0] fclk_clk_o         ,
-  output logic [  4-1:0] fclk_rstn_o        ,
+  output logic  [4-1:0] fclk_clk_o ,
+  output logic  [4-1:0] fclk_rstn_o,
   // XADC
-  input  logic  [ 5-1:0] vinp_i             ,  // voltages p
-  input  logic  [ 5-1:0] vinn_i             ,  // voltages n
+  input  logic  [5-1:0] vinp_i,  // slow analog voltages p
+  input  logic  [5-1:0] vinn_i,  // slow analog voltages n
   // GPIO
   gpio_if.m              gpio,
   // system read/write channel
@@ -68,15 +47,15 @@ module red_pitaya_ps (
 logic [4-1:0] fclk_clk ;
 logic [4-1:0] fclk_rstn;
 
-////////////////////////////////////////////////////////////////////////////////
-// PS STUB
-////////////////////////////////////////////////////////////////////////////////
-
 assign fclk_rstn_o = fclk_rstn;
 
 BUFG fclk_buf [4-1:0] (.O(fclk_clk_o), .I(fclk_clk));
 
-system system_i (
+////////////////////////////////////////////////////////////////////////////////
+// PS STUB
+////////////////////////////////////////////////////////////////////////////////
+
+system system (
   // MIO
   .FIXED_IO_mio      (FIXED_IO_mio     ),
   .FIXED_IO_ps_clk   (FIXED_IO_ps_clk  ),
@@ -85,36 +64,39 @@ system system_i (
   .FIXED_IO_ddr_vrn  (FIXED_IO_ddr_vrn ),
   .FIXED_IO_ddr_vrp  (FIXED_IO_ddr_vrp ),
   // DDR
-  .DDR_addr          (DDR_addr         ),
-  .DDR_ba            (DDR_ba           ),
-  .DDR_cas_n         (DDR_cas_n        ),
-  .DDR_ck_n          (DDR_ck_n         ),
-  .DDR_ck_p          (DDR_ck_p         ),
-  .DDR_cke           (DDR_cke          ),
-  .DDR_cs_n          (DDR_cs_n         ),
-  .DDR_dm            (DDR_dm           ),
-  .DDR_dq            (DDR_dq           ),
-  .DDR_dqs_n         (DDR_dqs_n        ),
-  .DDR_dqs_p         (DDR_dqs_p        ),
-  .DDR_odt           (DDR_odt          ),
-  .DDR_ras_n         (DDR_ras_n        ),
-  .DDR_reset_n       (DDR_reset_n      ),
-  .DDR_we_n          (DDR_we_n         ),
+  .DDR_addr          (DDR_addr   ),
+  .DDR_ba            (DDR_ba     ),
+  .DDR_cas_n         (DDR_cas_n  ),
+  .DDR_ck_n          (DDR_ck_n   ),
+  .DDR_ck_p          (DDR_ck_p   ),
+  .DDR_cke           (DDR_cke    ),
+  .DDR_cs_n          (DDR_cs_n   ),
+  .DDR_dm            (DDR_dm     ),
+  .DDR_dq            (DDR_dq     ),
+  .DDR_dqs_n         (DDR_dqs_n  ),
+  .DDR_dqs_p         (DDR_dqs_p  ),
+  .DDR_odt           (DDR_odt    ),
+  .DDR_ras_n         (DDR_ras_n  ),
+  .DDR_reset_n       (DDR_reset_n),
+  .DDR_we_n          (DDR_we_n   ),
   // FCLKs
-  .FCLK_CLK0         (fclk_clk[0]      ),  // 125 MHz
-  .FCLK_CLK1         (fclk_clk[1]      ),  // 142 MHz
-  .FCLK_CLK2         (fclk_clk[2]      ),  // 166 MHz
-  .FCLK_CLK3         (fclk_clk[3]      ),  // 200 MHz
-  .FCLK_RESET0_N     (fclk_rstn[0]     ),
-  .FCLK_RESET1_N     (fclk_rstn[1]     ),
-  .FCLK_RESET2_N     (fclk_rstn[2]     ),
-  .FCLK_RESET3_N     (fclk_rstn[3]     ),
+  .FCLK_CLK0         (fclk_clk[0]),  // 125 MHz
+  .FCLK_CLK1         (fclk_clk[1]),  // 142 MHz
+  .FCLK_CLK2         (fclk_clk[2]),  // 166 MHz
+  .FCLK_CLK3         (fclk_clk[3]),  // 200 MHz
+  .FCLK_RESET0_N     (fclk_rstn[0]),
+  .FCLK_RESET1_N     (fclk_rstn[1]),
+  .FCLK_RESET2_N     (fclk_rstn[2]),
+  .FCLK_RESET3_N     (fclk_rstn[3]),
   // XADC
 //  .Vaux0_v_n (vinn_i[1]),  .Vaux0_v_p (vinp_i[1]),
 //  .Vaux1_v_n (vinn_i[2]),  .Vaux1_v_p (vinp_i[2]),
 //  .Vaux8_v_n (vinn_i[0]),  .Vaux8_v_p (vinp_i[0]),
 //  .Vaux9_v_n (vinn_i[3]),  .Vaux9_v_p (vinp_i[3]),
 //  .Vp_Vn_v_n (vinn_i[4]),  .Vp_Vn_v_p (vinp_i[4]),
+  // PL clock and reset
+  .PL_ACLK            (bus.ACLK   ),
+  .PL_ARESETn         (bus.ARESETn),
   // GP0
   .M_AXI_GP0_araddr   (bus.ARADDR ),
   .M_AXI_GP0_arprot   (bus.ARPROT ),
