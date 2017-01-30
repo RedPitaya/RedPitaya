3a64b249aef42297484a00051160d068ecdaad33
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-01-30 16:07:51 +0100

    Jupyter: Python API and files are beeing developed in the Jupyter directory

--- api/python_api/analog_input.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from redpitaya import *
-
-for i in range(4):
-    val = base.AIpinGetValue(i)
-    print("Measured voltage on AI[%d] = {%f} V" % (i, val))
--- api/python_api/analog_output.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from redpitaya import *
-
-values = [1.0, 0.5, 0.8, 0.3]
-    
-for i, v in enumerate(values):
-    print("pin[%d] = %f" % (i, v))
-    base.AOpinSetValue(i, v)
