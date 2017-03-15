e48da0a8b1dfc07f553823458e75cbf4610d1c62
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-03-15 13:13:20 +0100

    Jupyter: fixed issue, where overlay was deleted if already loaded

--- jupyter/experiments/overlay.py
+++ jupyter/experiments/overlay.py
@@ -18,7 +18,11 @@ class overlay (object):
             os.system("mkdir {}".format(self.syspath))
 
         if self.status():
-            raise ResourceWarning('Requested overlay is already loaded.')
+            print ('Requested overlay is already loaded.')
+            # TODO: issuing a working does not work as expected,
+            # since __init__ does not finish properly, __del__ is run
+            # and the overlay is removed, which is not the intention
+            #raise ResourceWarning('Requested overlay is already loaded.')
         else:
             os.system("dtc -I dts -O dtb -o {0}.dtbo -@ {0}.dts".format(self.overlay))
             # TODO: loading FPGA should be handled by device tree overlay
@@ -26,13 +30,17 @@ class overlay (object):
             os.system("cat {}.dtbo > {}/dtbo".format(self.overlay, self.syspath))
 
     def __del__ (self):
+        print ('Overlay __del__ was activated.')
         os.system("rmdir {}".format(self.syspath))
         del(self.syspath)
         del(self.overlay)
 
     def status (self):
-        with open('{}/status'.format(self.syspath), 'r') as status_file:
-            status_string = status_file.read()
-            if   (status_string ==   "applied\n"): return (True)
-            elif (status_string == "unapplied\n"): return (False)
-            else:                                  return (None)
+        if not os.path.exists(self.syspath):
+            return (False)
+        else:
+            with open('{}/status'.format(self.syspath), 'r') as status_file:
+                status_string = status_file.read()
+                if   (status_string ==   "applied\n"): return (True)
+                elif (status_string == "unapplied\n"): return (False)
+                else:                                  return (None)
