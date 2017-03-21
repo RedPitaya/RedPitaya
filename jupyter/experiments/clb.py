e1792767108b075f25329f32247cc82de2102763
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-03-21 15:32:07 +0100

    Jupyter: calibration, added some EEPROM reading code

--- jupyter/experiments/clb.py
+++ jupyter/experiments/clb.py
@@ -12,24 +12,39 @@ class clb (object):
     DWA1 = 2**(DWA-2)
     DWG1 = 2**(DWG-2)
     
+    MAGIC = 0xAABBCCDD
+    eeprom_device = "/sys/bus/i2c/devices/0-0050/eeprom"
+    eeprom_offset = 0x0008
+
+    # FPGA regset structure
     regset_channel_dtype = np.dtype([
         ('ctl_mul', 'int32'),  # multiplication
         ('ctl_sum', 'int32')   # summation
     ])
-
     regset_dtype = np.dtype([
         ('adc', regset_channel_dtype, 2),  # oscilloscope
         ('dac', regset_channel_dtype, 2),  # generator
     ])
 
+    # floating point structure
     clb_channel_dtype = np.dtype([
         ('gain'  , 'float32'),  # multiplication
         ('offset', 'float32')   # summation
     ])
-
     clb_dtype = np.dtype([
         ('adc', clb_channel_dtype, 2),  # oscilloscope
         ('dac', clb_channel_dtype, 2),  # generator
+    ])
+
+    # EEPROM structure
+    eeprom_dtype = np.dtype([
+        ('adc_hi_gain', 'uint32', 2),
+        ('adc_lo_gain', 'uint32', 2),
+        ('adc_lo_off' ,  'int32', 2),
+        ('dac_gain'   , 'uint32', 2),
+        ('dac_off'    ,  'int32', 2),
+        ('magic'      , 'uint32'),
+        ('adc_hi_off' ,  'int32', 2)
     ])
 
     def __init__ (self, uio:str = '/dev/uio/clb'):
@@ -92,4 +107,35 @@ class clb (object):
         self.regset.dac[ch].ctl_mul = int(offset * self.DWGr)
 
     def eeprom_read (self):
-        pass
+        # open EEPROM device
+        try:
+            eeprom_file = open(self.eeprom_device, 'rb')
+        except OSError as e:
+            raise IOError(e.errno, "Opening {}: {}".format(uio, e.strerror))
+
+        # seek to calibration data
+        try:
+            eeprom_file.seek(self.eeprom_offset)
+        except IOError as e:
+            raise IOError(e.errno, "Seek {}: {}".format(uio, e.strerror))
+
+        try:
+            buffer = eeprom_file.read (self.eeprom_dtype.itemsize)
+        except IOError as e:
+            raise IOError(e.errno, "Read {}: {}".format(uio, e.strerror))
+
+        try:
+            eeprom_file.close()
+        except IOError as e:
+            raise IOError(e.errno, "Close {}: {}".format(uio, e.strerror))
+
+        # map buffer onto structure
+        eeprom_array = np.recarray(1, self.eeprom_dtype, buf=buffer)
+        eeprom_struct = eeprom_array[0]
+    
+        if (eeprom_struct.magic != self.MAGIC):
+            channels = [0, 1]
+            for ch in channels:
+                eeprom_struct.adc_hi_off[ch] = eeprom_struct.adc_lo_off[ch];
+
+        self.eeprom_struct = eeprom_struct
