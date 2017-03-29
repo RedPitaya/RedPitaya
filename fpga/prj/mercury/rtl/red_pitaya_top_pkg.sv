a483682d2595bb1e6874fc76f78468c2931de5c2
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-03-29 17:57:12 +0200

    FPGA: added logic generator, but it is not properly connected to the output yet

--- fpga/prj/mercury/rtl/red_pitaya_top_pkg.sv
+++ fpga/prj/mercury/rtl/red_pitaya_top_pkg.sv
@@ -12,16 +12,18 @@ localparam int unsigned MNG = 2;  // number of generator    modules
 
 // all events
 typedef struct packed {
+  osc_pkg::evn_t           la;   // logic analyzer
+  gen_pkg::evn_t           lg;   // logic generator
   osc_pkg::evn_t [MNO-1:0] osc;  // oscilloscope
-  gen_pkg::evn_t [MNG-1:0] gen;  // generate
+  gen_pkg::evn_t [MNG-1:0] gen;  // generator
 } evn_t;
 
 // interrupts
 typedef struct packed {
-  logic           lg;   // logic generator
   logic           la;   // logic analyzer
+  logic           lg;   // logic generator
   logic [MNO-1:0] osc;  // oscilloscope
-  logic [MNG-1:0] gen;  // generate
+  logic [MNG-1:0] gen;  // generator
 } irq_t;
 
 endpackage: top_pkg
