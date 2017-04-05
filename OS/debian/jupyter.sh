d7ee1b2077f7d7eceb60bfe09f8cdaafa7a7669b
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-04-05 18:21:04 +0200

    OS: on this branch also use the corresponding Jupyter branch

--- OS/debian/jupyter.sh
+++ OS/debian/jupyter.sh
@@ -81,7 +81,7 @@ EOF_CHROOT
 ###############################################################################
 
 mkdir $ROOT_DIR/home/jupyter/RedPitaya
-git clone https://github.com/redpitaya/jupyter.git $ROOT_DIR/home/jupyter/RedPitaya
+git clone https://github.com/redpitaya/jupyter.git $ROOT_DIR/home/jupyter/RedPitaya -b mercury
 
 chroot $ROOT_DIR <<- EOF_CHROOT
 pip3 install -e /home/jupyter/RedPitaya/redpitaya/
