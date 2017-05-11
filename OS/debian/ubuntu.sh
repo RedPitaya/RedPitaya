3349cc4fccd0035bc22f170b895a116810c6b695
Iztok Jeras <iztok.jeras@gmail.com>
2017-05-11 15:10:10 +0200

    OS: disable building Jupyter code

--- OS/debian/ubuntu.sh
+++ OS/debian/ubuntu.sh
@@ -121,7 +121,7 @@ install -v -m 664 -o root -D $OVERLAY/etc/fstab  $ROOT_DIR/etc/fstab
 . OS/debian/network.sh
 . OS/debian/zynq.sh
 . OS/debian/redpitaya.sh
-. OS/debian/jupyter.sh
+#. OS/debian/jupyter.sh
 . OS/debian/tft.sh
 
 ################################################################################
