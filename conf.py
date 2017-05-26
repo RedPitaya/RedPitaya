95f1588a4559137f0df1bbe3539f56adda5fbdf8
Miha Cankar <miha.cankar@redpitaya.com>
2017-05-26 13:58:55 +0200

    DOC: added STEMlab logo and favicon

--- conf.py
+++ conf.py
@@ -136,12 +136,12 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "doc/img/logo_stemlab.svg"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "doc/img/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -237,7 +237,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = "doc/img/logo_stemlab.svg"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
