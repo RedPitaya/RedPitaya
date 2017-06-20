51fed8bba9b9c73c0ce071de2ab8b896d4dbba2f
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-20 14:11:29 +0200

    DOC: added custom role for GitHub links

--- conf.py
+++ conf.py
@@ -111,6 +111,37 @@ pygments_style = 'sphinx'
 todo_include_todos = True
 
 
+# -- Add custom role ------------------------------------------------------
+
+# based on http://protips.readthedocs.io/link-roles.html
+# and https://github.com/rtfd/readthedocs.org/issues/2926#issuecomment-309671915
+# and https://doughellmann.com/blog/2010/05/09/defining-custom-roles-in-sphinx/
+
+from docutils import nodes
+#from repo_util import run_cmd_get_output
+
+#def get_github_rev():
+#    path = run_cmd_get_output('git rev-parse --short HEAD')
+#    tag  = run_cmd_get_output('git describe --exact-match')
+#    print 'Git commit ID: ', path
+#    if len(tag):
+#        print 'Git tag: ', tag
+#        path = tag
+#    return path
+
+def setup(app):
+    baseurl = 'https://github.com/RedPitaya/RedPitaya'
+    #rev = get_github_rev()
+    rev = 'master'
+    app.add_role('source', autolink('{}/blob/{}/%s'.format(baseurl, rev)))
+
+def autolink(pattern):
+    def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+        url = pattern % (text,)
+        node = nodes.reference(rawtext, text, refuri=url, **options)
+        return [node], []
+    return role
+
 # -- Options for HTML output ----------------------------------------------
 
 import sphinx_rtd_theme
