f5349bf96ce1c2c5b6e09149f4b45859e89d85dd
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-22 18:21:07 +0200

    API1: added periodic generator functions

--- api1/src/asg_per.c
+++ api1/src/asg_per.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include "asg_per.h"
 
 void rp_asg_per_init (rp_asg_per_t *handle, volatile rp_asg_per_regset_t *regset, double FS, int unsigned buffer_size, int unsigned CWM, int unsigned CWF) {
@@ -23,30 +25,34 @@ int rp_asg_per_set_table_size(rp_asg_per_t *handle, uint32_t value) {
     }
 }
 
-//    @property
-//    def frequency (self) -> float:
-//        """Periodic signal frequency up to FS/2"""
-//        siz = self.regset.per.cfg_siz + 1
-//        stp = self.regset.per.cfg_ste + 1
-//        return (stp / siz * self.FS)
-//
-//    @frequency.setter
-//    def frequency (self, value: float):
-//        if (value < self.FS/2):
-//            siz = self.regset.per.cfg_siz + 1
-//            self.regset.per.cfg_ste = int(siz * (value / self.FS)) - 1
-//        else:
+double rp_asg_gen_get_frequency(rp_asg_per_t *handle) {
+    // uint32_t might not be enough to hold register value + 1
+    double siz = handle->regset->cfg_siz + 1;
+    double ste = handle->regset->cfg_ste + 1;
+    return (ste / siz * handle->FS);
+}
+
+int rp_asg_gen_set_frequency(rp_asg_per_t *handle, double value) {
+    if (value < handle->FS) {
+        double siz = handle->regset->cfg_siz + 1;
+        handle->regset->cfg_ste = (uint32_t) (siz * (value / handle->FS)) - 1;
+        return(0);
+    } else {
 //            raise ValueError("Frequency should be less then half the sample rate. f < FS/2 = {} Hz".format(self.FS/2))
-//
-//    @property
-//    def phase (self) -> float:
-//        """Periodic signal phase in angular degrees"""
-//        siz = self.regset.per.cfg_siz + 1
-//        off = self.regset.per.cfg_off
-//        return (off / siz * 360)
-//
-//    @phase.setter
-//    def phase (self, value: float):
-//        siz = self.regset.per.cfg_siz + 1
-//        self.regset.per.cfg_off = int(siz * (value % 360) / 360)
+      return(-1);
+    }
+}
+
+double rp_asg_gen_get_phase(rp_asg_per_t *handle) {
+    // uint32_t might not be enough to hold register value + 1
+    double siz = handle->regset->cfg_siz + 1;
+    double off = handle->regset->cfg_off;
+    return (off / siz * 360.0);
+}
+
+int rp_asg_gen_set_phase(rp_asg_per_t *handle, double value) {
+    double siz = handle->regset->cfg_siz + 1;
+    handle->regset->cfg_off = (uint32_t) (siz * fmod(value, 360.0) / 360.0);
+    return(0);
+}
 
