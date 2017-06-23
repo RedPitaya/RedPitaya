b6e0c62fb4046d0475b47ce1d585b9dce608e568
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-23 10:43:44 +0200

    API1: added functions to header file asg_per.h

--- api1/src/asg_per.h
+++ api1/src/asg_per.h
@@ -19,7 +19,14 @@ typedef struct {
     int unsigned CW ;
 } rp_asg_per_t;
 
-void rp_asg_per_init (rp_asg_per_t *handle, volatile rp_asg_per_regset_t *regset, double FS, int unsigned buffer_size, int unsigned CWM, int unsigned CWF);
+void     rp_asg_per_init (rp_asg_per_t *handle, volatile rp_asg_per_regset_t *regset, double FS, int unsigned buffer_size, int unsigned CWM, int unsigned CWF);
+
+uint32_t rp_asg_per_get_table_size(rp_asg_per_t *handle);
+int      rp_asg_per_set_table_size(rp_asg_per_t *handle, uint32_t value);
+double   rp_asg_gen_get_frequency (rp_asg_per_t *handle);
+int      rp_asg_gen_set_frequency (rp_asg_per_t *handle, double value);
+double   rp_asg_gen_get_phase     (rp_asg_per_t *handle);
+int      rp_asg_gen_set_phase     (rp_asg_per_t *handle, double value);
 
 #endif
 
