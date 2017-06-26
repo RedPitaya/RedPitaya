d0c5fd6415478355fa6d87d5dc8dcf0d346d1a53
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-26 15:45:55 +0200

    API1: added missing gen_out function implementations

--- api1/src/gen_out.c
+++ api1/src/gen_out.c
@@ -10,23 +10,39 @@ void rp_gen_out_init (rp_gen_out_t *handle, volatile rp_gen_out_regset_t *regset
     handle->sum_t = sum_t;
 }
 
-float    rp_gen_out_get_amplitude(rp_gen_out_t *handle) {
+float rp_gen_out_get_amplitude(rp_gen_out_t *handle) {
+    return((float) handle->regset->cfg_mul / (float) fixp_unit(handle->mul_t));
 }
 
-int      rp_gen_out_set_amplitude(rp_gen_out_t *handle, float value) {
-    return(0);
+int rp_gen_out_set_amplitude(rp_gen_out_t *handle, float value) {
+    if ((-1.0 <= value) || (value <= 1.0)) {
+        handle->regset->cfg_mul = (int32_t) (value * (float) fixp_unit(handle->mul_t));
+        return(0);
+    } else {
+//        raise ValueError("Output amplitude should be inside [-1,1] volts.")
+        return(-1);
+    }
 }
 
-float    rp_gen_out_get_offset   (rp_gen_out_t *handle) {
+float rp_gen_out_get_offset(rp_gen_out_t *handle) {
+    return((float) handle->regset->cfg_sum / (float) fixp_max(handle->sum_t));
 }
 
-int      rp_gen_out_set_offset   (rp_gen_out_t *handle, float value) {
-    return(0);
+int rp_gen_out_set_offset(rp_gen_out_t *handle, float value) {
+    if ((-1.0 <= value) || (value <= 1.0)) {
+        handle->regset->cfg_sum = (int32_t) (value * (float) fixp_max(handle->sum_t));
+        return(0);
+    } else {
+//        raise ValueError("Output offset should be inside [-1,1] volts.")
+        return(-1);
+    }
 }
 
-bool rp_gen_out_get_enable   (rp_gen_out_t *handle) {
+bool rp_gen_out_get_enable(rp_gen_out_t *handle) {
+    return((bool) handle->regset->cfg_ena);
 }
 
-void rp_gen_out_set_enable   (rp_gen_out_t *handle, bool value) {
+void rp_gen_out_set_enable(rp_gen_out_t *handle, bool value) {
+    handle->regset->cfg_ena = value;
 }
 
