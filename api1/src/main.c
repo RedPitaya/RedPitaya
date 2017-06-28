7dab34a03f2bc70865079c91806bd5638e2de713
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-28 15:07:53 +0200

    API2: added a simple periodic sinus to demo

--- api1/src/main.c
+++ api1/src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <math.h>
+#include <stdbool.h>
 
 #include "hwid.h"
 #include "gen.h"
+
+#define M_PI 3.14159265358979323846
 
 int main () {
     printf ("DEBUG: start\n");
@@ -17,7 +21,21 @@ int main () {
     rp_gen_init (&gen0, 0);
 
     rp_gen_default(&gen0);
-    
+    // generate and program sinusoidal waveform
+    float waveform [gen0.buffer_size];
+    const float step = 2 * M_PI / gen0.buffer_size;
+    for (int unsigned i=0; i<gen0.buffer_size; i++) {
+        waveform[i] = sinf(step * (float) i);
+    }
+    rp_gen_set_waveform(&gen0, waveform, gen0.buffer_size);
+    // set continuous/periodic mode
+    rp_gen_set_mode(&gen0, CONTINUOUS);
+    rp_asg_per_set_table_size(&gen0.per, gen0.buffer_size);
+    rp_asg_gen_set_frequency (&gen0.per, 1000);
+    rp_asg_gen_set_phase     (&gen0.per, 0);
+    // enable generator output
+    rp_gen_out_set_enable    (&gen0.out, true);
+
     rp_gen_release (&gen0);
 
     printf ("DEBUG: end\n");
