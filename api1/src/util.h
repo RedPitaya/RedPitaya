277a7932b2279a689905534e7205d1668b242696
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-29 13:42:44 +0200

    API1: updated comment

--- api1/src/util.h
+++ api1/src/util.h
@@ -7,7 +7,7 @@ typedef struct {uint8_t s; uint8_t m; uint8_t f;} fixp_t;
 
 // number of possible values
 #define fixp_num(v)          (1<<(v.s+v.m+v.f))
-// fixed point max/min value  (signed : unsigned)
+// fixed point max/min value
 #define fixp_max(v)        (+(1<<(    v.m+v.f))-1)
 #define fixp_min(v) (v.s ? (-(1<<(    v.m+v.f))  ) : 0)
 // fixed point unit value
