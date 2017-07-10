e9ac8fcfe600fafea022461931eadb35c1da48db
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-10 12:27:00 +0200

    OS: added debian package dependencies for numpy/scipy/mathplotlib

--- OS/debian/jupyter.sh
+++ OS/debian/jupyter.sh
@@ -24,8 +24,12 @@ pip3 install --upgrade pip
 # Python numerical processing and plotting
 #apt-get -y install python3-numpy python3-scipy python3-pandas
 #apt-get -y install python3-matplotlib
-pip3 install matplotlib
-pip3 install numpy scipy pandas
+apt-get -y install gfortran libopenblas-dev liblapack-dev python-dev
+pip3 install numpy
+pip3 install scipy
+pip3 install pandas
+apt-get -y install libpng16-16 libfreetype6
+pip3 install pycairo matplotlib
 
 # Jupyter and ipywidgets
 pip3 install jupyter
