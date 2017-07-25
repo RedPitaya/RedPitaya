3d24c1e0d33063494cad12970ee90ba926f2b8df
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-25 14:15:01 +0200

    API1: updated install code for handling missing symlink support on vfat

---
+++ api1/install_vfat.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so.0.1.0" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so.0"
+cp "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so.0.1.0" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so"
