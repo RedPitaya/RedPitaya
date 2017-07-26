293154eae17c7fe1558a905487c50ee6f3dbb1c4
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-26 19:24:11 +0200

    SCPI: examples: added option to run using NI-VISA

--- Examples/scpi/Python/gen_sine_signal.py
+++ Examples/scpi/Python/gen_sine_signal.py
@@ -5,6 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description='SCPI: generate sinusoidal signal.')
 parser.add_argument('adr',          type=str, default='127.0.0.1', help='provide IP address or URL')
 parser.add_argument('-p', '--port', type=int, default=5000,        help='specify SCPI port (default is 5000)')
+parser.add_argument('-b', '--bin',  action="store_true",           help='use binary data transfer instead of the default ASCII')
+parser.add_argument('--py',         action="store_true",           help='use PyVISA-py (by default the system visa library is used)')
 args = parser.parse_args()
 
 ###############################################################################
@@ -13,7 +15,7 @@ args = parser.parse_args()
 
 import visa
 
-rm = visa.ResourceManager('@py')
+rm = visa.ResourceManager('@py' if args.py else '')
 #rm.list_resources()
 rp = rm.open_resource('TCPIP::{}::{}::SOCKET'.format(args.adr, args.port), read_termination = '\r\n')
 
