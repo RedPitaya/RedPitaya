4fd26b9557c1d4776db655b59fb88ed7d4104e83
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-26 10:55:45 +0200

    SCPI: updated library install procedure

--- api1/install_vfat.sh
+++ api1/install_vfat.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-cp "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so.0.1.0" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so.0"
-cp "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so.0.1.0" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/librp1.so"
+LIBDIR=`dpkg-architecture -qDEB_HOST_MULTIARCH`
+
+cp --remove-destination "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/${LIBDIR}/librp1.so.0.1.0" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/${LIBDIR}/librp1.so.0"
+cp --remove-destination "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/${LIBDIR}/librp1.so.0.1.0" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/${LIBDIR}/librp1.so"
