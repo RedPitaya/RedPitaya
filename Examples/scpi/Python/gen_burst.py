854f3ca116f86c82081df1a4515d8beeee51ee46
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-27 11:50:46 +0200

    SCPI: example: burst period length fix

--- Examples/scpi/Python/gen_burst.py
+++ Examples/scpi/Python/gen_burst.py
@@ -31,14 +31,14 @@ rp.write(":SOURce1:VOLTage:IMMediate:OFFSet 0")
 
 # specify peridic mode, sinusoidal waveform
 rp.write(":SOURce1:MODE BURSt")
-#rp.write(":SOURce1:BURSt:MODE FINite")
-rp.write(":SOURce1:BURSt:MODE INFinite")
+rp.write(":SOURce1:BURSt:MODE FINite")
+#rp.write(":SOURce1:BURSt:MODE INFinite")
 rp.write(":SOURce1:FUNCtion:SHAPe SINusoid")
 
 # burst half the buffer with then idle for quarter buffer, repeat 4 times
 rp.write(":SOURce1:BURSt:DATA:REPetitions 1")
-rp.write(":SOURce1:BURSt:DATA:LENgth "   + str(buffer_size // 2))
-rp.write(":SOURce1:BURSt:PERiod:LENgth " + str(buffer_size // 4))
+rp.write(":SOURce1:BURSt:DATA:LENgth "   + str(1 * buffer_size // 2))
+rp.write(":SOURce1:BURSt:PERiod:LENgth " + str(3 * buffer_size // 4))
 rp.write(":SOURce1:BURSt:PERiod:NUMber 4")
 
 # reset and start state machine
