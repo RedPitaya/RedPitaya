e2b03d0ced3bd14caa2e662bed53522bafbdad9c
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-27 14:56:27 +0200

    SCPI: removing unused constatns

--- scpi/common.h
+++ scpi/common.h
@@ -12,17 +12,7 @@
 #include "scpi/parser.h"
 #include "redpitaya/rp1.h"
 
-#define SET_OK(cont) \
-    	SCPI_ResultString(cont, "OK"); \
-    	return SCPI_RES_OK;
-
 #define RPSCPI_CMD_NUM 	1
-
-#ifdef SCPI_DEBUG
-#define RP_LOG(...) syslog(__VA_ARGS__);
-#else
-#define RP_LOG(...)
-#endif
 
 // Red Pitaya specific contct inside SCPI context
 typedef struct {
