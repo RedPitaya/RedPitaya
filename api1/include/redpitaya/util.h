5cec27aa291f59039a8053e7f73684f2b8a1f4fb
Iztok Jeras <iztok.jeras@gmail.com>
2017-07-27 11:43:43 +0200

    API1: making sure the fixp_t macros can properly handle every number which fits into 32bits

--- api1/include/redpitaya/util.h
+++ api1/include/redpitaya/util.h
@@ -6,16 +6,16 @@
 typedef struct {uint8_t s; uint8_t m; uint8_t f;} fixp_t;
 
 // number of possible values
-#define fixp_num(v)          (1<<(v.s+v.m+v.f))
+#define fixp_num(v)          (INT64_C(1)<<(v.s+v.m+v.f))
 // fixed point max/min value
-#define fixp_max(v)        (+(1<<(    v.m+v.f))-1)
-#define fixp_min(v) (v.s ? (-(1<<(    v.m+v.f))  ) : 0)
+#define fixp_max(v)        (+(INT64_C(1)<<(    v.m+v.f))-1)
+#define fixp_min(v) (v.s ? (-(INT64_C(1)<<(    v.m+v.f))  ) : 0)
 // fixed point unit value
-#define fixp_unit(v)         (1<<(        v.f))
+#define fixp_unit(v)         (INT64_C(1)<<(        v.f))
 // number of bits
-#define fixp_bits(v)             (v.s+v.m+v.f)
+#define fixp_bits(v)                      (v.s+v.m+v.f)
 // bit mask
-#define fixp_mask(v)       ( (1<<(v.s+v.m+v.f))-1)
+#define fixp_mask(v)       ( (INT64_C(1)<<(v.s+v.m+v.f))-1)
 
 char * rp_util_fixp_print (fixp_t value);
 
