8504dd82edb393747b984ed31191e4bed2c910a8
Iztok Jeras <iztok.jeras@gmail.com>
2017-10-17 11:15:16 +0200

    FPGA: updated digital loop register width

--- fpga/rtl/mgmt.sv
+++ fpga/rtl/mgmt.sv
@@ -10,7 +10,7 @@ module mgmt #(
   // GPIO mode
   output logic  [GW-1:0] cfg_iom,
   // stream multiplexers
-  output logic   [3-1:0] cfg_loop,
+  output logic   [2-1:0] cfg_loop,
   // system bus
   sys_bus_if.s           bus
 );
