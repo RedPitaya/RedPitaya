1be2bc646797705bdadc19fea1430f7d52976d58
n.danilyuk <n.danilyuk@integrasources.com>
2019-02-22 18:37:57 +0700

    Modify calibration parameters before tests

--- Test/production/production_testing_script_z20.sh
+++ Test/production/production_testing_script_z20.sh
@@ -51,19 +51,20 @@ CALIB="$SD_CARD_PATH/bin/calib"
 
 
 # Default calibration parameters set during the process
-FE_CH1_FS_G_HI=45870551
-FE_CH2_FS_G_HI=45870551
-FE_CH1_FS_G_LO=1016267064
-FE_CH2_FS_G_LO=1016267064
-FE_CH1_DC_offs=78
-FE_CH2_DC_offs=25
-BE_CH1_FS=42755331
-BE_CH2_FS=42755331
-BE_CH1_DC_offs=-150
-BE_CH2_DC_offs=-150
-SOME_eeprom_value=-1430532899 #SOME_eeprom_value is some value in eeprom which is not used for anything but after Crt added hv offset calib values this value also appeard.
-FE_CH1_DC_offs_HI=100
-FE_CH2_DC_offs_HI=100          #FE_CHx_DC_offs_HI  are dc offset parameters for HV jumper settings
+FE_CH1_FS_G_HI=42949672
+FE_CH2_FS_G_HI=42949672
+FE_CH1_FS_G_LO=858993459
+FE_CH2_FS_G_LO=858993459
+FE_CH1_DC_offs=0
+FE_CH2_DC_offs=0
+BE_CH1_FS=42949672
+BE_CH2_FS=42949672
+BE_CH1_DC_offs=0
+BE_CH2_DC_offs=0
+SOME_eeprom_value=-143053289  #SOME_eeprom_value is some value in eeprom which is not used for anything but after Crt added hv offset calib values this value also appeard.
+FE_CH1_DC_offs_HI=0
+FE_CH2_DC_offs_HI=0
+
 #All calibration parameters in one string
 FACTORY_CAL="$FE_CH1_FS_G_HI $FE_CH2_FS_G_HI $FE_CH1_FS_G_LO $FE_CH2_FS_G_LO $FE_CH1_DC_offs $FE_CH2_DC_offs $BE_CH1_FS $BE_CH2_FS $BE_CH1_DC_offs $BE_CH2_DC_offs $SOME_eeprom_value $FE_CH1_DC_offs_HI $FE_CH2_DC_offs_HI"
 
