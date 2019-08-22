c6a864ec243a4e780648f96964a83b4f4676fb8b
n.danilyuk <n.danilyuk@integrasources.com>
2019-08-22 09:33:42 +0700

    Fix production script

--- Test/production/production_testing_script_z20.sh
+++ Test/production/production_testing_script_z20.sh
@@ -1500,12 +1500,12 @@ then
 
         exec 5>&1
         SFDR_VAL=$(/opt/redpitaya/bin/production_testing_script_z20_spectrum.sh|tee >(cat - >&5))
-        SFDR_RESULT=$(gawk 'match($0, /^SFDR_TEST_STATUS=\s(.+)/, a) {print a[1]}' <<< "${SFDR_VAL}")
+        SFDR_RESULT=$(gawk '{match($0, /SFDR_TEST_STATUS=[0-9]+/, a)}{split(a[0],b,"=")}{print b[2]}' <<< "${SFDR_VAL}")
         if [[ "$SFDR_RESULT" != "0" ]]
         then
         TEST_STATUS=0
         fi
-        SFDR_VAL_RES=$(gawk 'match($0, /^RES_SFDR=\s(.+)/, a) {print a[1]}' <<< "${SFDR_VAL}")
+        SFDR_VAL_RES=$(gawk '{match($0, /RES_SFDR=\s(.+);/, a)};{gsub("RES_SFDR=","",a[0])};{gsub(";","",a[0])};{print a[0]}' <<< "${SFDR_VAL}")
         LOG_VAR="$LOG_VAR $SFDR_VAL_RES"
 else
 echo "SFDR spectrum test skipped"
