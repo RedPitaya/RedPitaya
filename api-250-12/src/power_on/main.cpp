464eec666f35f2bd4a0004af7a9e05d8691d874c
n.danilyuk <n.danilyuk@integrasources.com>
2019-11-05 06:06:13 +0000

    Add initializaion max7133 on startup

--- api-250-12/src/power_on/main.cpp
+++ api-250-12/src/power_on/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "rp-spi.h"
 #include "rp-gpio-power.h"
+#include "rp-i2c-max7311.h"
 
 
 int main()
@@ -11,5 +12,6 @@ int main()
     rp_gpio_power::rp_set_power_mode(DAC_POWER,POWER_ON);
     rp_spi_fpga::rp_spi_load_via_fpga("/opt/redpitaya/lib/configs/AD9613BCPZ-250.xml");
     rp_spi_fpga::rp_spi_load_via_fpga("/opt/redpitaya/lib/configs/AD9746BCPZ-250.xml");
+    rp_max7311::rp_initController();
     return 0;
 }
