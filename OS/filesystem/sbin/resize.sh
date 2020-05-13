e5795a6b5957cf0fa9b908ac9a0e8aeeeb36edad
n.dnailyuk <n.dnailyuk@integrasources.com>
2020-05-13 17:25:34 +0700

    Update resize script

--- OS/filesystem/sbin/resize.sh
+++ OS/filesystem/sbin/resize.sh
@@ -31,32 +31,10 @@ p
 w
 EOF
 
-# now set up an init.d script
-cat <<\EOF > /etc/init.d/resize2fs_once &&
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          resize2fs_once
-# Required-Start:
-# Required-Stop:
-# Default-Start: 2 3 4 5 S
-# Default-Stop:
-# Short-Description: Resize the root filesystem to fill partition
-# Description:
-### END INIT INFO
-. /lib/lsb/init-functions
-case "$1" in
-  start)
-    log_daemon_msg "Starting resize2fs_once" &&
-    resize2fs /dev/mmcblk0p2 &&
-    update-rc.d -f resize2fs_once remove &&
-    log_end_msg $?
-    ;;
-  *)
-    echo "Usage: $0 start" >&2
-    exit 3
-    ;;
-esac
-EOF
-chmod +x /etc/init.d/resize2fs_once &&
-update-rc.d resize2fs_once defaults &&
-  echo "Root partition has been resized. The filesystem will be enlarged upon the next reboot"
+
+rw
+grep -v "resize2fs" /opt/redpitaya/sbin/startup.sh > /opt/redpitaya/sbin/startup.sh2; mv /opt/redpitaya/sbin/startup.sh2 /opt/redpitaya/sbin/startup.sh
+echo resize2fs /dev/mmcblk0p2 >> /opt/redpitaya/sbin/startup.sh
+echo 'rw;grep -v "resize2fs" /opt/redpitaya/sbin/startup.sh > /opt/redpitaya/sbin/startup.sh2; mv /opt/redpitaya/sbin/startup.sh2 /opt/redpitaya/sbin/startup.sh;ro' >> /opt/redpitaya/sbin/startup.sh
+ro
+echo "Root partition has been resized. The filesystem will be enlarged upon the next reboot"
