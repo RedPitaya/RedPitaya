9551130aa3af355ccd518f564f9ac0e0063409ac
Nikolaj P. Danilyuk <n.danilyuk@integrasources.com>
2020-05-18 12:24:41 +0700

    Fix resize.sh

--- OS/filesystem/sbin/resize.sh
+++ OS/filesystem/sbin/resize.sh
@@ -11,7 +11,7 @@ PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep "^2" | cut -f 2 -d: | grep 
 [ "$PART_START" ] || return 1
 # Return value will likely be error for fdisk as it fails to reload the
 # partition table because the root fs is mounted
-fdisk /dev/mmcblk0 <<EOF
+sudo fdisk /dev/mmcblk0 <<EOF
 p
 d
 2
