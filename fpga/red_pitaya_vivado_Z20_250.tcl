0e709f71110c20b59243a27699f47cf0669038e3
jure.trnovec <jure.trnovec@i-tech.si>
2020-08-06 10:06:25 +0200

    Changed paths to BD file for v094_250

--- fpga/red_pitaya_vivado_Z20_250.tcl
+++ fpga/red_pitaya_vivado_Z20_250.tcl
@@ -27,7 +27,7 @@ set path_brd brd
 set path_rtl rtl_250
 set path_ip  ip
 set path_sdc sdc_250
-set path_bd  .srcs/sources_1/bd/system/hdl
+set path_bd  $prj_dir/redpitaya.srcs/sources_1/bd/system/hdl
 
 set path_out out
 set path_sdk sdk
