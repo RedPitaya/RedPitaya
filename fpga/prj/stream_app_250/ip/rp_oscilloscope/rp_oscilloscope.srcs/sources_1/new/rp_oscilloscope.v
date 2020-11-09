259bc3947fe1587f9d1910b77a933451c4c5f6d0
n.danilyuk <n.danilyuk@integrasoruces.com>
2020-11-09 08:30:11 +0000

    Chnage adc bit to 12

--- fpga/prj/stream_app_250/ip/rp_oscilloscope/rp_oscilloscope.srcs/sources_1/new/rp_oscilloscope.v
+++ fpga/prj/stream_app_250/ip/rp_oscilloscope/rp_oscilloscope.srcs/sources_1/new/rp_oscilloscope.v
@@ -6,7 +6,7 @@ module rp_oscilloscope
     parameter M_AXI_OSC1_DATA_BITS  = 64,
     parameter M_AXI_OSC2_ADDR_BITS  = 32,
     parameter M_AXI_OSC2_DATA_BITS  = 64,
-    parameter ADC_DATA_BITS         = 14,
+    parameter ADC_DATA_BITS         = 12,
     parameter EVENT_SRC_NUM         = 7,
     parameter TRIG_SRC_NUM          = 7)(    
   input  wire                                   clk,
