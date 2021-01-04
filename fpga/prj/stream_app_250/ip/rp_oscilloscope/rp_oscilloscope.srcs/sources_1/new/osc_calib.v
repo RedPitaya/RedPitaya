f5aeaee37269803e403e1bdc36f4b247728144b9
jure.trnovec <jure.trnovec@i-tech.si>
2021-01-04 11:24:34 +0100

    Changed the order of calibration gain and offset calculation (offset first).

--- fpga/prj/stream_app_250/ip/rp_oscilloscope/rp_oscilloscope.srcs/sources_1/new/osc_calib.v
+++ fpga/prj/stream_app_250/ip/rp_oscilloscope/rp_oscilloscope.srcs/sources_1/new/osc_calib.v
@@ -25,9 +25,9 @@ localparam CALC_MIN   = -(2**(AXIS_DATA_BITS-1));
 localparam C_START    = 16;
 localparam C_END      = C_START+16;
 
-wire signed [AXIS_DATA_BITS-1:0]  adc_data;
-wire signed [15:0]                offset;
-wire signed [15:0]                gain;   
+reg signed [AXIS_DATA_BITS-1:0]  adc_data, adc_data_reg;
+reg signed [15:0]                offset, offset_reg;
+reg signed [15:0]                gain, gain_reg;   
 
 reg  signed [    CALC1_BITS-1:0]  offset_calc;
 wire                              offs_max, offs_min;
@@ -43,14 +43,25 @@ reg                               s_axis_tvalid_p2;
 
 
 ////////////////////////////////////////////////////////////
-// Input data
+// Input data registration
 // 
 ////////////////////////////////////////////////////////////
+always @(posedge clk)
+begin
+  adc_data_reg <= s_axis_tdata;
+  adc_data <= adc_data_reg;
 
+  offset_reg <= cfg_calib_offset;
+  offset <= offset_reg;
+
+  gain_reg <= cfg_calib_gain;
+  gain <= gain_reg;
+end
+/*
 assign adc_data = s_axis_tdata;
 assign offset   = cfg_calib_offset;
 assign gain     = cfg_calib_gain;
-
+*/
 ////////////////////////////////////////////////////////////
 // Name : Gain Calculation
 // 
@@ -74,7 +85,7 @@ assign gain_calc_limit = gain_max ? CALC_MAX : (gain_min ? CALC_MIN : gain_calc[
 
 always @(posedge clk)
 begin
-  offset_calc <= $signed(gain_calc_limit) + $signed(offset);  
+  offset_calc <= $signed(adc_data) + $signed(offset);  
 end
 
 assign offs_max = (offset_calc[16:15] == 2'b01);
@@ -89,7 +100,7 @@ assign offset_calc_limit = offs_max ? CALC_MAX : (offs_min ? CALC_MIN : offset_c
 
 always @(posedge clk)
 begin
-  m_axis_tdata <= offset_calc_limit[AXIS_DATA_BITS-1:0];  
+  m_axis_tdata <= gain_calc_limit[AXIS_DATA_BITS-1:0];  
 end
 
 ////////////////////////////////////////////////////////////
