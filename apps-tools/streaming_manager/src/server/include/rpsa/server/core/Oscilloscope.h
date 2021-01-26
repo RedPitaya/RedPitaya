c5c88f1e2727acc26d6f7d498ef20465a987f1eb
n.danilyuk <n.danilyuk@integrasources.com>
2021-01-26 04:13:30 +0000

    Add registers for filter

--- apps-tools/streaming_manager/src/server/include/rpsa/server/core/Oscilloscope.h
+++ apps-tools/streaming_manager/src/server/include/rpsa/server/core/Oscilloscope.h
@@ -33,10 +33,7 @@ struct OscilloscopeMapT
     uint32_t dec_rshift;            // 52  - offset 0x34
     uint32_t avg_en_addr;           // 56  - offset 0x38
     uint32_t filt_bypass;           // 60  - offset 0x3C
-    uint32_t filt_coeff_aa;         // 64  - offset 0x40
-    uint32_t filt_coeff_bb;         // 68  - offset 0x44
-    uint32_t filt_coeff_kk;         // 72  - offset 0x48
-    uint32_t filt_coeff_pp;         // 76  - offset 0x4C
+    uint32_t reserv[4];
     uint32_t dma_ctrl;              // 80  - offset 0x50
     uint32_t dma_sts_addr;          // 84  - offset 0x54
     uint32_t dma_buf_size;          // 88  - offset 0x58
@@ -49,7 +46,21 @@ struct OscilloscopeMapT
     uint32_t calib_offset_ch1;      // 116 - offset 0x74
     uint32_t calib_gain_ch1;        // 120 - offset 0x78  
     uint32_t calib_offset_ch2;      // 124 - offset 0x7C
-    uint32_t calib_gain_ch2;        // 128 - offset 0x80   
+    uint32_t calib_gain_ch2;        // 128 - offset 0x80
+    uint32_t reserv2[6];
+    uint32_t lost_samples_buf1_ch2;  // 156 - offset 0x9C
+    uint32_t lost_samples_buf2_ch2;  // 160 - offset 0xA0
+    uint32_t write_pointer_ch1;     // 164 - offset 0xA4
+    uint32_t write_pointer_ch2;     // 168 - offset 0xA8
+    uint32_t reserv3[5];
+    uint32_t filt_coeff_aa_ch1;       // 192  - offset 0xC0
+    uint32_t filt_coeff_bb_ch1;       // 196  - offset 0xC4
+    uint32_t filt_coeff_kk_ch1;       // 200  - offset 0xC8
+    uint32_t filt_coeff_pp_ch1;       // 204  - offset 0xCC
+    uint32_t filt_coeff_aa_ch2;       // 208  - offset 0xD0
+    uint32_t filt_coeff_bb_ch2;       // 212  - offset 0xD4
+    uint32_t filt_coeff_kk_ch2;       // 216  - offset 0xD8
+    uint32_t filt_coeff_pp_ch2;       // 220  - offset 0xDC
 };
 
 class COscilloscope
@@ -67,6 +78,8 @@ public:
     void prepare();
     bool next(uint8_t *&_buffer1,uint8_t *&_buffer2, size_t &_size,uint32_t &_overFlow);
     void setCalibration(int32_t ch1_offset,float ch1_gain, int32_t ch2_offset, float ch2_gain);
+    void setFilterCalibrationCh1(int32_t _aa,int32_t _bb, int32_t _pp, int32_t _kk);
+    void setFilterCalibrationCh2(int32_t _aa,int32_t _bb, int32_t _pp, int32_t _kk);
     bool clearBuffer();
     bool wait();
     bool clearInterrupt();
@@ -89,8 +102,16 @@ private:
     unsigned     m_OscBufferNumber;
     uint32_t     m_dec_factor;
     std::mutex   m_waitLock;
-    int32_t      m_calib_offset_ch1;   
-    uint32_t     m_calib_gain_ch1;        
-    int32_t      m_calib_offset_ch2;    
-    uint32_t     m_calib_gain_ch2;         
+    int32_t      m_calib_offset_ch1;
+    uint32_t     m_calib_gain_ch1;
+    int32_t      m_calib_offset_ch2;
+    uint32_t     m_calib_gain_ch2;
+    int32_t      m_AA_ch1;
+    int32_t      m_BB_ch1;
+    int32_t      m_PP_ch1;
+    int32_t      m_KK_ch1;
+    int32_t      m_AA_ch2;
+    int32_t      m_BB_ch2;
+    int32_t      m_PP_ch2;
+    int32_t      m_KK_ch2;
 };
