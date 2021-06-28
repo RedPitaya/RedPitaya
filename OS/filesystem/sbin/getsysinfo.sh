7d224c4d26b333921e3e347fd8553929cfba23b3
n.danilyuk <n.danilyuk@integrasources.com>
2021-06-28 07:03:42 +0300

    Modify getsysinfo.sh

--- OS/filesystem/sbin/getsysinfo.sh
+++ OS/filesystem/sbin/getsysinfo.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+READ_MAC=""
+READ_HWREV=""
+DNA_1=""
+DNA_2=""
+C=10
 
-READ_MAC=$( fw_printenv | grep ethaddr= | awk 'BEGIN {FS="="}{print $2}') > /dev/null 2>&1
-READ_HWREV=$( fw_printenv | grep hw_rev= | awk 'BEGIN {FS="="}{print $2}') > /dev/null 2>&1
-DNA_1=$( monitor 0x40000008 | awk 'BEGIN {FS="x"}{print $2}') > /dev/null 2>&1
-DNA_2=$( monitor 0x40000004 | awk 'BEGIN {FS="x"}{print $2}') > /dev/null 2>&1
+while [[ "$READ_MAC" == "" || "$READ_HWREV" == "" || "$DNA_1" == "" || "$DNA_2" == "" ]] && [[ $C -ge  0 ]]
+do
+    READ_MAC=$( fw_printenv | grep ethaddr= | awk 'BEGIN {FS="="}{print $2}') > /dev/null 2>&1
+    READ_HWREV=$( fw_printenv | grep hw_rev= | awk 'BEGIN {FS="="}{print $2}') > /dev/null 2>&1
+    DNA_1=$( monitor 0x40000008 | awk 'BEGIN {FS="x"}{print $2}') > /dev/null 2>&1
+    DNA_2=$( monitor 0x40000004 | awk 'BEGIN {FS="x"}{print $2}') > /dev/null 2>&1
+    C=$[$C - 1]
+    sleep 1
+done
 
 echo { > /tmp/sysinfo.json
 echo \"model\": \"$READ_HWREV\", >> /tmp/sysinfo.json
