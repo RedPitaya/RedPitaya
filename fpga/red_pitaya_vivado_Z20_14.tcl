464c6f486d34235fdec3a68f9b195ba599dc6c2b
jure.trnovec <jure.trnovec@i-tech.si>
2021-07-12 11:32:08 +0200

    Adding sync for RP125-Z20 variants

--- fpga/red_pitaya_vivado_Z20_14.tcl
+++ fpga/red_pitaya_vivado_Z20_14.tcl
@@ -6,7 +6,9 @@
 ################################################################################
 
 set prj_name [lindex $argv 0]
+set prj_defs [lindex $argv 1]
 puts "Project name: $prj_name"
+puts "Defines: $prj_defs"
 cd prj/$prj_name
 #cd prj/$::argv 0
 
@@ -24,6 +26,7 @@ set path_brd ../../brd
 set path_rtl rtl
 set path_ip  ip
 set path_bd  .srcs/sources_1/bd/system/hdl
+#set path_bd  .srcs/sources_1/bd/system
 set path_sdc ../../sdc
 set path_sdc_prj sdc
 
@@ -46,6 +49,7 @@ set_param board.repoPaths [list $path_brd]
 set part xc7z020clg400-1
 
 create_project -in_memory -part $part
+set_property verilog_define $prj_defs [current_fileset]
 
 ################################################################################
 # create PS BD (processing system block design)
