e64d6cc48b763e9629e8e5fdc4cf22871708bc91
n.dnailyuk <n.dnailyuk@integrasources.com>
2021-07-23 17:30:29 +0700

    Fix clean.sh

--- apps-tools/streaming_manager/src/server/clean-all.sh
+++ apps-tools/streaming_manager/src/server/clean-all.sh
@@ -8,18 +8,18 @@ rm -f Makefile
 rm -f cmake_install.cmake
 
 cd libs
-find . -not -name "CMakeLists.txt" -not -name "asio.zip"  -maxdepth 1 -type f -exec rm -r "{}" \;
+find . -not -name "CMakeLists.txt" -not -name "*.zip"  -maxdepth 1 -type f -exec rm -r "{}" \;
 cd ..
 
 
 cd client
-find . -not -name "CMakeLists.txt" -not -name "client.cpp" -maxdepth 1 -type f -exec rm -r "{}" \;
+find . -not -name "CMakeLists.txt" -not -name "*.cpp" -maxdepth 1 -type f -exec rm -r "{}" \;
 cd ..
 
 cd targets
-find . -not -name "CMakeLists.txt" -not -name "server.cpp" -maxdepth 2 -type f -exec rm -r "{}" \;
+find . -not -name "CMakeLists.txt" -not -name "*.cpp" -maxdepth 2 -type f -exec rm -r "{}" \;
 cd ..
 
 cd convert_tool
-find . -not -name "CMakeLists.txt" -not -name "main.cpp" -maxdepth 2 -type f -exec rm -r "{}" \;
+find . -not -name "CMakeLists.txt" -not -name "*.cpp" -maxdepth 2 -type f -exec rm -r "{}" \;
 cd ..
