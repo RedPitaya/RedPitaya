19368902ec65f71d1071bb85cd6dc26cc7ba3d61
n.danilyuk <n.danilyuk@inetgrasources.com>
2021-07-27 03:07:40 +0000

    Fix problem with build on RP

--- apps-tools/streaming_manager/src/server/clean-all.sh
+++ apps-tools/streaming_manager/src/server/clean-all.sh
@@ -4,8 +4,11 @@ find . -name "CMakeCache.txt" -type f -exec rm -r "{}" \;
 find . -name "CMakeFiles" -type d -exec rm -r "{}" \;
 
 rm -rf ./bin
+rm -rf ./lib
+rm -rf _deps
 rm -f Makefile
 rm -f cmake_install.cmake
+
 
 cd libs
 find . -not -name "CMakeLists.txt" -not -name "*.zip"  -maxdepth 1 -type f -exec rm -r "{}" \;
