8482172b5da91e384c6cd4394961ead3c76e4b5d
Nikolaj P. Danilyuk <n.danilyuk@integrasources.com>
2021-08-02 17:44:02 +0700

    Update build_iso script to version 1.05

--- build_scripts/ISO/build_iso.sh
+++ build_scripts/ISO/build_iso.sh
@@ -17,8 +17,8 @@ PREFIX=$2
 REV="$(echo $1 | cut -d'-' -f2)"
 NUM="$(echo $1 | cut -d'-' -f3)"
 
-wget -N https://downloads.redpitaya.com/downloads/LinuxOS/red_pitaya_OS-beta_1.04.img.zip
-unzip -n red_pitaya_OS-beta_1.04.img.zip
+wget -N https://downloads.redpitaya.com/downloads/LinuxOS/red_pitaya_OS-beta_1.05.img.zip
+unzip -n red_pitaya_OS-beta_1.05.img.zip
 rm -f redpitaya.img
 mv *.img redpitaya.img
 
