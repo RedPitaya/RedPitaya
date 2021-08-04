67535457519e27c82ca82f6485026bfb7c4f9d7d
n.danilyuk <n.danilyuk@integrasources.com>
2021-08-04 04:16:11 +0000

    Clean code

--- apps-tools/streaming_manager/src/console_server/main.cpp
+++ apps-tools/streaming_manager/src/console_server/main.cpp
@@ -43,10 +43,6 @@
 
 
 std::mutex g_print_mtx;
-// std::condition_variable cv;
-// COscilloscope::Ptr osc = nullptr;
-// CStreamingManager::Ptr s_manger = nullptr;
-// std::shared_ptr<CStreamingApplication> s_app = nullptr;
 std::shared_ptr<ServerNetConfigManager> con_server = nullptr;
 std::atomic_bool g_run(true);
 
@@ -106,26 +102,6 @@ int main(int argc, char *argv[])
 
     g_argv0 = argv[0];
     auto opt = ClientOpt::parse(argc,argv);
-
-// #ifndef Z20
-//     rp_CalibInit();
-//     auto osc_calib_params = rp_GetCalibrationSettings();
-// #endif
-
-    // int32_t  ch1_off = 0;
-    // int32_t  ch2_off = 0;
-    // float    ch1_gain = 1;
-    // float    ch2_gain = 1;
-    // bool     filterBypass = true;
-	// uint32_t aa_ch1 = 0;
-	// uint32_t bb_ch1 = 0;
-	// uint32_t kk_ch1 = 0xFFFFFF;
-	// uint32_t pp_ch1 = 0;
-	// uint32_t aa_ch2 = 0;
-	// uint32_t bb_ch2 = 0;
-	// uint32_t kk_ch2 = 0xFFFFFF;
-	// uint32_t pp_ch2 = 0;
-
 
     if (opt.background){
         FILE *fp= NULL;
@@ -232,96 +208,6 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-   
-//     int resolution = 1;
-//     int channel = 3;
-//     int protocol = 1;
-//     int sock_port = 8900;
-//     std::string ip_addr_host = "127.0.0.1";
-//     int format = 0;
-//     int rate = 1;
-//     bool use_file = false;
-//     int samples = -1;
-//     int save_mode = 1;
-//     bool use_calib = false;
-//     int attenuator = 0;
-//     int ac_dc = 0;
-
-//     try{
-//         ifstream file(filepath);
-//         std::string key;
-//         std::string value;
-
-// 	if (!file.good()) throw std::exception();
-
-//         while (file >> key >> value) {
-//             if ("host" == key) {
-//                 ip_addr_host = value;
-//                 continue;
-//             }
-//             if ("port" == key) {
-//                 sock_port = stoi(value);
-//                 continue;
-//             }
-//             if ("protocol" == key) {
-//                 protocol = stoi(value);
-//                 continue;
-//             }
-//             if ("rate" == key) {
-//                 rate = stoi(value);
-//                 continue;
-//             }
-//             if ("resolution" == key) {
-//                 resolution = stoi(value);
-//                 continue;
-//             }
-//             if ("use_file" == key) {
-//                 use_file = (bool)stoi(value);
-//                 continue;
-//             }
-//             if ("format" == key) {
-//                 format = stoi(value);
-//                 continue;
-//             }
-//             if ("samples" == key) {
-//                 samples = stoi(value);
-//                 continue;
-//             }
-//             if ("channels" == key) {
-//                 channel = stoi(value);
-//                 continue;
-//             }
-//             if ("save_mode" == key) {
-//                 save_mode = stoi(value);
-//                 continue;
-//             }
-// #ifndef Z20
-//             if ("attenuator" == key) {
-//                 attenuator = stoi(value);
-//                 continue;
-//             }
-//             if ("use_calib" == key) {
-//                 use_calib = (bool)(stoi(value) - 1);
-//                 continue;
-//             }
-// #endif
-
-// #ifdef Z20_250_12
-//             if ("coupling" == key) {
-//                 ac_dc = stoi(value);
-//                 continue;
-//             }
-// #endif
-//             throw std::exception();
-//         }
-//     }catch(std::exception& e)
-//     {
-//         fprintf(stderr,  "Error loading configuration file\n");
-//         RP_LOG (LOG_ERR, "Error loading configuration file");
-//         exit(1);
-//     }
-
-
     fprintf(stdout,"streaming-server started\n");
     RP_LOG (LOG_NOTICE, "streaming-server started");
 
@@ -342,115 +228,6 @@ int main(int argc, char *argv[])
     }
 
     try{
-
-//     if (use_calib) {
-// #ifdef Z20_250_12
-//         if (attenuator == 1) {
-//             if (ac_dc == 1) {
-//                 ch1_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch1_g_1_ac) / 20.0;  // 1:1
-//                 ch2_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch2_g_1_ac) / 20.0;  // 1:1
-//                 ch1_off  = osc_calib_params.osc_ch1_off_1_ac; 
-//                 ch2_off  = osc_calib_params.osc_ch2_off_1_ac;
-//             }
-//             else {
-//                 ch1_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch1_g_1_dc) / 20.0;  // 1:1
-//                 ch2_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch2_g_1_dc) / 20.0;  // 1:1
-//                 ch1_off  = osc_calib_params.osc_ch1_off_1_dc; 
-//                 ch2_off  = osc_calib_params.osc_ch2_off_1_dc; 
-//             }
-//         }else{
-//             if (ac_dc == 1) {
-//                 ch1_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch1_g_20_ac);  // 1:20
-//                 ch2_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch2_g_20_ac);  // 1:20
-//                 ch1_off  = osc_calib_params.osc_ch1_off_20_ac;
-//                 ch2_off  = osc_calib_params.osc_ch2_off_20_ac;
-//             } else {
-//                 ch1_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch1_g_20_dc);  // 1:20
-//                 ch2_gain = calibFullScaleToVoltage(osc_calib_params.osc_ch2_g_20_dc);  // 1:20
-//                 ch1_off  = osc_calib_params.osc_ch1_off_20_dc;
-//                 ch2_off  = osc_calib_params.osc_ch2_off_20_dc;
-//             }
-//         }
-// #endif
-
-// #if defined Z10 || defined Z20_125
-//         filterBypass = false;
-//         if (attenuator == 1) {
-//             ch1_gain = calibFullScaleToVoltage(osc_calib_params.fe_ch1_fs_g_lo) / 20.0;  
-//             ch2_gain = calibFullScaleToVoltage(osc_calib_params.fe_ch2_fs_g_lo) / 20.0;  
-//             ch1_off  = osc_calib_params.fe_ch1_lo_offs;
-//             ch2_off  = osc_calib_params.fe_ch2_lo_offs;
-//             aa_ch1 = osc_calib_params.low_filter_aa_ch1;
-//             bb_ch1 = osc_calib_params.low_filter_bb_ch1;
-//             pp_ch1 = osc_calib_params.low_filter_pp_ch1;
-//             kk_ch1 = osc_calib_params.low_filter_kk_ch1;
-//             aa_ch2 = osc_calib_params.low_filter_aa_ch2;
-//             bb_ch2 = osc_calib_params.low_filter_bb_ch2;
-//             pp_ch2 = osc_calib_params.low_filter_pp_ch2;
-//             kk_ch2 = osc_calib_params.low_filter_kk_ch2;
-//         }else{
-//             ch1_gain = calibFullScaleToVoltage(osc_calib_params.fe_ch1_fs_g_hi);  
-//             ch2_gain = calibFullScaleToVoltage(osc_calib_params.fe_ch2_fs_g_hi);  
-//             ch1_off  = osc_calib_params.fe_ch1_hi_offs;
-//             ch2_off  = osc_calib_params.fe_ch2_hi_offs;
-//             aa_ch1 = osc_calib_params.hi_filter_aa_ch1;
-//             bb_ch1 = osc_calib_params.hi_filter_bb_ch1;
-//             pp_ch1 = osc_calib_params.hi_filter_pp_ch1;
-//             kk_ch1 = osc_calib_params.hi_filter_kk_ch1;
-//             aa_ch2 = osc_calib_params.hi_filter_aa_ch2;
-//             bb_ch2 = osc_calib_params.hi_filter_bb_ch2;
-//             pp_ch2 = osc_calib_params.hi_filter_pp_ch2;
-//             kk_ch2 = osc_calib_params.hi_filter_kk_ch2;
-//         }
-// #endif 
-//     }
-
-// #ifdef Z20_250_12
-// //    rp_spi_fpga::rp_spi_load_via_fpga("/opt/redpitaya/lib/configs/AD9613BCPZ-250_streaming.xml");
-//     rp_max7311::rp_setAttenuator(RP_MAX7311_IN1, attenuator == 1  ? RP_ATTENUATOR_1_1 : RP_ATTENUATOR_1_20);
-//     rp_max7311::rp_setAttenuator(RP_MAX7311_IN2, attenuator == 1  ? RP_ATTENUATOR_1_1 : RP_ATTENUATOR_1_20);
-//     rp_max7311::rp_setAC_DC(RP_MAX7311_IN1, ac_dc == 1 ? RP_AC_MODE : RP_DC_MODE);
-//     rp_max7311::rp_setAC_DC(RP_MAX7311_IN2, ac_dc == 1 ? RP_AC_MODE : RP_DC_MODE);
-// #endif
-        
-//         std::vector<UioT> uioList = GetUioList();
-//         // Search oscilloscope
-//         auto file_type = Stream_FileType::WAV_TYPE;
-
-//         for (const UioT &uio : uioList)
-//         {
-//             if (uio.nodeName == "rp_oscilloscope")
-//             {
-//                 // TODO start server;
-//                 osc = COscilloscope::Create(uio, (channel ==1 || channel == 3) , (channel ==2 || channel == 3) , rate);
-//                 osc->setCalibration(ch1_off,ch1_gain,ch2_off,ch2_gain);
-//                 osc->setFilterCalibrationCh1(aa_ch1,bb_ch1,kk_ch1,pp_ch1);
-//                 osc->setFilterCalibrationCh2(aa_ch2,bb_ch2,kk_ch2,pp_ch2);
-//                 osc->setFilterBypass(filterBypass);
-//                 break;
-//             }
-//         }
-
-
-//         if (use_file == false) {
-//             s_manger = CStreamingManager::Create(
-//                     ip_addr_host,
-//                     std::to_string(sock_port).c_str(),
-//                     protocol == 1 ? asionet::Protocol::TCP : asionet::Protocol::UDP);
-//         }else{
-// 	    	if (format == 1) file_type = Stream_FileType::TDMS_TYPE;
-//     		if (format == 2) file_type = Stream_FileType::CSV_TYPE;
-//             s_manger = CStreamingManager::Create(file_type , FILE_PATH , samples, save_mode == 2);
-//             s_manger->notifyStop = [](int status)
-//                                 {
-//                                     StopNonBlocking(0);                                   
-//                                 };
-//         }
-//         int resolution_val = (resolution == 1 ? 8 : 16);
-//         s_app = new CStreamingApplication(s_manger, osc, resolution_val, rate, channel, attenuator , 16);
-//         s_app->run();
-//         delete s_app;
-//         if (file_type == Stream_FileType::CSV_TYPE) s_manger->convertToCSV();
         stopServer(0);
     }catch (std::exception& e)
     {
@@ -463,26 +240,3 @@ int main(int argc, char *argv[])
     closelog ();
     return (EXIT_SUCCESS);
 }
-
-// void StopServer(int x){
-// 	try{
-// 		if (s_app!= nullptr){
-// 			s_app->stop(false);
-// 		}
-// 	}catch (std::exception& e){
-// 		fprintf(stderr, "Error: StopServer() %s\n",e.what());
-//         RP_LOG(LOG_ERR, "Error: StopServer() %s\n",e.what());
-// 	}
-// }
-
-// void StopNonBlocking(int x){
-// 	try{
-// 		std::thread th(StopServer ,x);
-// 		th.detach();
-// 	}catch (std::exception& e){
-// 		fprintf(stderr, "Error: StopNonBlocking() %s\n",e.what());
-//         RP_LOG(LOG_ERR, "Error: StopNonBlocking() %s\n",e.what());
-// 	}
-// }
-
-
