77ef917f6be096b8df524d5c11144e91135b4989
Nikolaj P. Danilyuk <n.danilyuk@integrasources.com>
2021-08-05 13:45:43 +0700

    Added script for load overlay

--- apps-tools/streaming_manager/streaming-server.run.sh
+++ apps-tools/streaming_manager/streaming-server.run.sh
@@ -2,4 +2,5 @@
 LD_LIBRARY_PATH=/opt/redpitaya/lib
 export LD_LIBRARY_PATH
 cat /opt/redpitaya/fpga/fpga_streaming.bit  > /dev/xdevcfg
+/opt/redpitaya/sbin/mkoverlay.sh stream_app
 ./streaming-server
