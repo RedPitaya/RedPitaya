ccd1921d4608eb48e78a813aeb0802adedf20e40
jure.trnovec <jure.trnovec@i-tech.si>
2022-01-14 11:01:39 +0100

    reverting logic to 32 bit interface

--- fpga/prj/logic/ip/systemZ10.tcl
+++ fpga/prj/logic/ip/systemZ10.tcl
@@ -404,7 +404,7 @@ proc create_root_design { parentCell } {
   set_property -dict [ list \
    CONFIG.c_include_mm2s {1} \
    CONFIG.c_include_sg {1} \
-   CONFIG.c_m_axi_mm2s_data_width {64} \
+   CONFIG.c_m_axi_mm2s_data_width {32} \
    CONFIG.c_m_axis_mm2s_tdata_width {16} \
    CONFIG.c_mm2s_burst_size {64} \
    CONFIG.c_s2mm_burst_size {64} \
@@ -417,7 +417,7 @@ proc create_root_design { parentCell } {
   set_property -dict [ list \
    CONFIG.c_include_mm2s {1} \
    CONFIG.c_include_sg {1} \
-   CONFIG.c_m_axi_mm2s_data_width {64} \
+   CONFIG.c_m_axi_mm2s_data_width {32} \
    CONFIG.c_m_axis_mm2s_tdata_width {16} \
    CONFIG.c_mm2s_burst_size {16} \
    CONFIG.c_s2mm_burst_size {16} \
