b59249be282fd607a6e30e2de127597501ccd7db
Nikolaj P. Danilyuk <n.danilyuk@integrasources.com>
2022-03-14 14:48:47 +0700

    Modify build script

---
+++ download_fpga.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PRJ=$1
+MODE=$2
+COMMIT=$3
+TOKEN=$4
+
+if [[ "$MODE" == "GITLAB" ]]
+then
+    mkdir -p fpga/$PRJ/
+    echo "Download from gitlab $1"
+    curl -L "https://gitlab.redpitaya.com/redpitaya-3.0/redpitaya-fpga/-/archive/$COMMIT/redpitaya-public-$COMMIT.zip?private_token=$TOKEN" -o fpga/$PRJ/$COMMIT.zip
+    unzip fpga/$PRJ/$COMMIT.zip -d fpga/$PRJ
+    rm fpga/$PRJ/$COMMIT.zip
+    mv fpga/$PRJ/* fpga/$PRJ/fpga/
+    mv fpga/$PRJ/fpga/* fpga/$PRJ
+    rm -rf fpga/$PRJ/fpga/
+fi
