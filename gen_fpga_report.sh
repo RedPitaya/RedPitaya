b33cf968555e1e6736546b89f28d8cbfa2ed3919
n.danilyuk <n.danilyuk@inetegrasources.com>
2022-03-21 12:02:34 +0700

    Improve report script

--- gen_fpga_report.sh
+++ gen_fpga_report.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-FPGA_NAME=$1
-COMMIT=$2
+MODE=$1
+FPGA_NAME=$2
+COMMIT=$3
 REPORT_FILE=report.xml
 
+
+
+if [[ "$MODE" == "GEN" ]]
+then
 cd fpga/$FPGA_NAME
 BRANCH=$(git name-rev $COMMIT)
 LOG=$(git log -n 3)
 cd ../..
+echo "<field name=\"$FPGA_NAME\" titlecolor=\"blue\" value=\"$BRANCH\" detailcolor=\"black\" href=\"$BRANCH\"> <![CDATA[ $LOG ]]> </field>" >> $REPORT_FILE
+fi
 
-echo "<section name=\"$FPGA_NAME\" fontcolor=\"\">" >> $REPORT_FILE
+if [[ "$MODE" == "PACK" ]]
+then
+mv $REPORT_FILE $REPORT_FILE.fpga.sub.tmp
 
-echo "<field name=\"Branch\" titlecolor=\"\" value=\"$BRANCH\" detailcolor=\"\" href=\"\"> <![CDATA[ $LOG ]]> </field>" >> $REPORT_FILE
+echo "<section name=\"FPGA\" fontcolor=\"\">" > $REPORT_FILE
+
+cat $REPORT_FILE.fpga.sub.tmp >> $REPORT_FILE
  
 echo "</section>" >> $REPORT_FILE
+
+rm $REPORT_FILE.fpga.sub.tmp
+
+fi
