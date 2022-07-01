207cfa33f5ef1b48b809620a5ee5e34991740999
n.danilyuk <n.danilyuk@intgrasources.com>
2022-07-01 09:16:39 +0000

    Add deploy mode for streaming qt app

--- apps-tools/streaming_manager/src/toolchains/toolchain-i686-w64-mingw32.cmake
+++ apps-tools/streaming_manager/src/toolchains/toolchain-i686-w64-mingw32.cmake
@@ -13,9 +13,9 @@ set (CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 # search for programs in the build host directories
 set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set (CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set (CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 
 # set the resource compiler (RHBZ #652435)
 set (CMAKE_RC_COMPILER i686-w64-mingw32-windres)
