f175aa0334a2dd13b65b19110b830c75173b7d8b
n.danilyuk <n.danilyuk@inetegrasources.com>
2022-07-04 14:00:40 +0700

    Added scripts for build slave ecosystems. Fix CmakeLists.txt for sreaming

--- build_scripts/build_Z10_slave.sh
+++ build_scripts/build_Z10_slave.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MODEL=Z10
-export STREAMING_MODE=MASTER
+export STREAMING_MODE=SLAVE
 ./build_OS.sh $1
--- build_scripts/build_Z20.sh
+++ build_scripts/build_Z20.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export MODEL=Z20
+export STREAMING_MODE=MASTER
 ./build_OS.sh $1
