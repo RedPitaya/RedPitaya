dc5cc9de63c4d962e7df785bd6715c0e1be8b8cb
n.danilyuk <n.danilyuk@inetgrources.com>
2023-01-11 11:42:02 +0200

    Fix startup script

--- patches/startup/startup.sh
+++ patches/startup/startup.sh
@@ -4,5 +4,14 @@
 
 /opt/redpitaya/sbin/getsysinfo.sh
 
+# Turns on the power to the ADC and DAC
+
+MODEL=$(/opt/redpitaya/bin/monitor -f)
+
+if [ "$MODEL" = "z20_250" ]; then
+/opt/redpitaya/bin/rp_power_on -P
+/opt/redpitaya/bin/rp_power_on -C
+fi
+
 # Here you can specify commands for autorun at system startup
 
