858452cb9d05612ca642f5d017b916e52a11c7b9
n.danilyuk <n.danilyuk@integrasources.com>
2023-02-28 04:02:13 +0000

    Update report script

--- gen_fpga_report.sh
+++ gen_fpga_report.sh
@@ -2,6 +2,10 @@
 
 PROJECT_FPGA=$1
 REPORT_FILE=$2
+MODE=$3
+
+if [[ "$MODE" == "FPGA" ]]
+then
 
 echo "<section name=\"FPGA/$PROJECT_FPGA\" fontcolor=\"\">" > $REPORT_FILE
 
@@ -15,3 +19,26 @@ for f in build/fpga/$PROJECT_FPGA/*; do
 done
 
 echo "</section>" >> $REPORT_FILE
+fi
+
+if [[ "$MODE" == "KERNEL" ]]
+then
+    echo "<section name=\"Kernel\" fontcolor=\"\">" > $REPORT_FILE
+    BRANCH=$(git show -s --pretty=%D HEAD | awk '{gsub("origin/",""); print $2}')
+    LOG=$(git log -n 1)
+    COMMIT=$(git log -n -1 | awk 'NR==1 {print $2}')
+    echo "<field name=\"$BRANCH\" titlecolor=\"blue\" value=\"https://gitlab.redpitaya.com/redpitaya-3.0/redpitaya-public/-/commit/$COMMIT\" detailcolor=\"black\" href=\"https://gitlab.redpitaya.com/redpitaya-3.0/redpitaya-public/-/commit/$COMMIT\"> <![CDATA[ $LOG ]]> </field>" >> $REPORT_FILE
+    echo "</section>" >> $REPORT_FILE
+
+fi
+
+
+if [[ "$MODE" == "ECO" ]]
+then
+    echo "<section name=\"Ecosystem\" fontcolor=\"\">" > $REPORT_FILE
+    BRANCH=$(git branch --contains HEAD)
+    LOG=$(git log -n 1)
+    COMMIT=$(git log -n -1 | awk 'NR==1 {print $2}')
+    echo "<field name=\"$BRANCH\" titlecolor=\"blue\" value=\"https://gitlab.redpitaya.com/redpitaya-3.0/redpitaya-public/-/commit/$COMMIT\" detailcolor=\"black\" href=\"https://gitlab.redpitaya.com/redpitaya-3.0/redpitaya-public/-/commit/$COMMIT\"> <![CDATA[ $LOG ]]> </field>" >> $REPORT_FILE
+    echo "</section>" >> $REPORT_FILE
+fi
