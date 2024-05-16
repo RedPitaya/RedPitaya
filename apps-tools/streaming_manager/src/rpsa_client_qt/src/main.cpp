7ebee2354c77dd57f73ee04120cea7f78aea7f98
n.danilyuk <n.danilyuk@integrasources.com>
2024-05-16 02:55:30 +0000

    Return OpenGL in streaming qt client

--- apps-tools/streaming_manager/src/rpsa_client_qt/src/main.cpp
+++ apps-tools/streaming_manager/src/rpsa_client_qt/src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #else
-    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenVG);
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
 #endif
 
 
