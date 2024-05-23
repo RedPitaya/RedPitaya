c0598d7b28821866605ebc3dd8b5af0f25b94157
n.danilyuk <n.danilyuk@integrasources.com>
2024-05-23 08:52:12 +0000

    Added DHCP server in network manager

--- apps-tools/network_manager/js/help-nm.js
+++ apps-tools/network_manager/js/help-nm.js
@@ -8,3 +8,42 @@ var helpListNM =
     }
   ]
 };
+
+$.fn.fI = function(e) { //Flash Item
+  if (!e) { e = {} }
+  if (this) { e.e = this }
+  switch (e.f) {
+      case 0:
+          break;
+      default:
+          switch (e.css) {
+              case 0:
+                  e.d = 'background-color'
+                  break;
+              case undefined:
+                  e.d = 'border-color'
+                  break;
+              default:
+                  e.d = e.css
+                  break;
+          }
+          if (!e.c1) { e.c1 = '#FF0000' }
+          if (!e.c2) { e.c2 = '#A00000' }
+          if (!e.p) { e.p = 200 }
+          e.e.css(e.d, e.c1)
+          setTimeout(function() {
+              e.e.css(e.d, e.c2)
+              setTimeout(function() {
+                  e.e.css(e.d, e.c1)
+                  setTimeout(function() {
+                      e.e.css(e.d, e.c2)
+                      setTimeout(function() {
+                          e.e.css(e.d, '')
+                      }, e.p)
+                  }, e.p)
+              }, e.p)
+          }, e.p)
+          break;
+  }
+  return this
+}
