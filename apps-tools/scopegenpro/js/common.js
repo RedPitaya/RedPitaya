eb19512be13396173378ea8c0b5e29f7ce361c0e
n.danilyuk <n.danilyuk@integrasources.com>
2024-07-02 09:20:49 +0000

    Update html2canvas script files

--- apps-tools/scopegenpro/js/common.js
+++ apps-tools/scopegenpro/js/common.js
@@ -54,16 +54,13 @@
     }
 
     OSC.SaveGraphsPNG = function() {
-        html2canvas($('body'), {
-            background: '#343433', // Like background of BODY
-            onrendered: function(canvas) {
-                var a = document.createElement('a');
-                // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
-                a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-                a.download = 'graphs.jpg';
-                // a.click(); // Not working with firefox
-                fireEvent(a, 'click');
-            }
+        html2canvas(document.querySelector("body"), {backgroundColor: '#343433'}).then(canvas => {
+            var a = document.createElement('a');
+            // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.download = 'graphs.jpg';
+            // a.click(); // Not working with firefox
+            fireEvent(a, 'click');
         });
     }
 
--- apps-tools/spectrumpro/js/scope.js
+++ apps-tools/spectrumpro/js/scope.js
@@ -1392,14 +1392,13 @@
     };
 
     SPEC.SaveGraphsPNG = function() {
-        html2canvas($('body'), {
-            background: '#343433', // Like background of BODY
-            onrendered: function(canvas) {
-                var a = document.createElement('a');
-                a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-                a.download = 'graphs.jpg';
-                fireEvent(a, 'click');
-            }
+        html2canvas(document.querySelector("body"), {backgroundColor: '#343433'}).then(canvas => {
+            var a = document.createElement('a');
+            // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.download = 'graphs.jpg';
+            // a.click(); // Not working with firefox
+            fireEvent(a, 'click');
         });
     }
 
