85424b7922ff71f1fd10a44872e4b6fb83e69fa3
n.danilyuk <n.danilyuk@integrasources.com>
2024-10-17 08:30:40 +0000

    Remove examples

--- Examples/Tests/scpi/socket.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define SOCKET int
-
-int createClient(SOCKET* pSock, const char *serverIp, unsigned short localPort, unsigned short serverPort);
-int destroySock(SOCKET s);
-int sendTcp(SOCKET s, const char* pBuf, unsigned int len, int flags);
-int recvTcp(SOCKET sock, char* pBuf, unsigned int len, int timeOutMs);
