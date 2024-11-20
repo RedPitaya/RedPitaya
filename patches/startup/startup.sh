69d88e877ff45a6cadb1de34332ec0dec801f5a8
n.danilyuk <n.danilyuk@integrasources.com>
2024-11-20 11:58:34 +0200

    Fix startup script

--- patches/startup/startup.sh
+++ patches/startup/startup.sh
@@ -26,6 +26,15 @@ then
     /opt/redpitaya/bin/rp_power_on -C
     fi
 
+    PROD_MODE=$(cat /root/production_start_mode.conf 2> /dev/null)
+
+    if [ "$PROD_MODE" = "gen1" ]; then
+        /opt/redpitaya/bin/production_env_gen1_install.sh
+    fi
+
+    if [ "$PROD_MODE" = "gen2" ]; then
+        /opt/redpitaya/bin/production_env_gen2_install.sh
+    fi
 fi
 
 # Here you can specify commands for autorun at system startup
