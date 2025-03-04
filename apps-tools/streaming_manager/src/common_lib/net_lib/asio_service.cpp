42eadd8aa5734c370ec35e3c522f3fffdf9ee374
n.danilyuk <n.danilyuk@integrasources.com>
2025-03-04 05:09:46 +0200

    Update ASIO to 1.32 in streaming

--- apps-tools/streaming_manager/src/common_lib/net_lib/asio_service.cpp
+++ apps-tools/streaming_manager/src/common_lib/net_lib/asio_service.cpp
@@ -1,29 +1,27 @@
-#include <functional>
 #include "asio_service.h"
+#include <functional>
 
 using namespace net_lib;
 
 CAsioService::CAsioService()
-	: m_Ios()
-	, m_Work(m_Ios)
-	, m_asio_th(nullptr)
-{
-	auto func = std::bind(static_cast<size_t (asio::io_service::*)()>(&asio::io_service::run), &(m_Ios));
-	m_asio_th = new asio::thread(func);
+    : m_Ios()
+      // , m_Work(m_Ios)
+      ,
+      m_asio_th(nullptr) {
+    asio::executor_work_guard<asio::io_context::executor_type> work = asio::make_work_guard(m_Ios);
+    auto func = std::bind(static_cast<size_t (asio::io_context::*)()>(&asio::io_context::run), &(m_Ios));
+    m_asio_th = new std::thread(func);
 }
 
-CAsioService::~CAsioService()
-{
-	m_Ios.reset();
-	m_Ios.stop();
-	if (m_asio_th) {
-		m_asio_th->join();
-		delete m_asio_th;
-		m_asio_th = nullptr;
-	}
+CAsioService::~CAsioService() {
+    m_Ios.stop();
+    if (m_asio_th) {
+        m_asio_th->join();
+        delete m_asio_th;
+        m_asio_th = nullptr;
+    }
 }
 
-auto CAsioService::getIO() -> asio::io_service &
-{
-	return m_Ios;
+auto CAsioService::getIO() -> asio::io_context& {
+    return m_Ios;
 }
