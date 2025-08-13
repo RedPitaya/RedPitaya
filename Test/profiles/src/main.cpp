9eb8d006cb55abc5901ee91573b2741dedad8cf6
n.danilyuk <n.danilyuk@integrasources.com>
2025-08-13 05:25:27 +0000

    Added new keys in profiles tool

--- Test/profiles/src/main.cpp
+++ Test/profiles/src/main.cpp
@@ -37,6 +37,8 @@ void usage(char** argv) {
     fprintf(stdout,
             "\t\tKeys:\n"
             "\t\t\tosc_rate\t: OSC base rate\n"
+            "\t\t\tfast_adc_bits\t: HW ADC bits\n"
+            "\t\t\tfast_adc_fs\t: HW ADC full scale\n"
             "\t\t\tgpio_n\t: Number of GPIO channels N\n"
             "\t\t\tgpio_p\t: Number of GPIO channels P\n"
             "\t\t\te3\t: Availability of e3 connector\n");
@@ -112,7 +114,7 @@ int main(int argc, char** argv) {
                 uint32_t value;
                 auto ret = rp_HPGetBaseSpeedHz(&value);
                 if (ret == RP_HP_OK) {
-                    printf("%d", value);
+                    printf("%d\n", value);
                 } else {
                     printf("[Error]\n");
                     return EXIT_FAILURE;
@@ -123,7 +125,7 @@ int main(int argc, char** argv) {
                 uint8_t value;
                 auto ret = rp_HPGetGPIO_P_Count(&value);
                 if (ret == RP_HP_OK) {
-                    printf("%d", value);
+                    printf("%d\n", value);
                 } else {
                     printf("[Error]\n");
                     return EXIT_FAILURE;
@@ -134,7 +136,29 @@ int main(int argc, char** argv) {
                 uint8_t value;
                 auto ret = rp_HPGetGPIO_N_Count(&value);
                 if (ret == RP_HP_OK) {
-                    printf("%d", value);
+                    printf("%d\n", value);
+                } else {
+                    printf("[Error]\n");
+                    return EXIT_FAILURE;
+                }
+                return EXIT_SUCCESS;
+            }
+            if (key == "fast_adc_bits") {
+                uint8_t value;
+                auto ret = rp_HPGetFastADCBits(&value);
+                if (ret == RP_HP_OK) {
+                    printf("%d\n", value);
+                } else {
+                    printf("[Error]\n");
+                    return EXIT_FAILURE;
+                }
+                return EXIT_SUCCESS;
+            }
+            if (key == "fast_adc_fs") {
+                float value;
+                auto ret = rp_HPGetHWADCFullScale(&value);
+                if (ret == RP_HP_OK) {
+                    printf("%f\n", value);
                 } else {
                     printf("[Error]\n");
                     return EXIT_FAILURE;
@@ -145,7 +169,7 @@ int main(int argc, char** argv) {
                 bool value;
                 auto ret = rp_HPGetIsE3Present(&value);
                 if (ret == RP_HP_OK) {
-                    printf("%d", value);
+                    printf("%d\n", value);
                 } else {
                     printf("[Error]\n");
                     return EXIT_FAILURE;
