RedPitaya/RedPitaya

commit bd42e996e3c41e85d8c090c8fc2c7e07b79f8998

feat: any-edge trigger, timestamp API, modular Python tests
Added:
- Any-edge trigger sources (CHA_AE, CHB_AE, CHC_AE, CHD_AE, EXT_AE, AWG_AE)
- rp_AcqSetInitTimestamp / rp_AcqGetTimestamp API
- ACQ:TS and ACQ:TS:CH#? SCPI commands
- Timestamp registers in osc_control_t
- Modular Python test suite (7 files)
- --tr_ch 1A/2A/3A/4A options in acquire tools

Changed:
- rp_InitAdresses() -> rp_InitAdressess() (typo fix)
- scpi_RpTrigSrc extended with any-edge entries
- getTrigChByTrigSource supports any-edge
- Python tests reorganized by category

Fixed:
- CSweepController calls corrected API name
- osc_printRegset IRQ prints only for CH1
- Timestamp registers documented and dumped

rp-api/api-sweep/src/rp_sweep.cpp
@@ -68,7 +68,7 @@ CSweepController::CSweepController() {
     m_pimpl->m_Pause = false;
     setDefault();
     if (!rp_IsApiInit()) {
-        if (rp_InitAdresses() == RP_OK) {
+        if (rp_InitAdressess() == RP_OK) {
             m_pimpl->apiInit = true;
         }
     }

rp-api/api/include/rp.h
@@ -105,7 +105,7 @@
  * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
  */
 
-int rp_InitAdresses();
+int rp_InitAdressess();
 
 /**
  * Initializes the library. It must be called first, before any other library method.

rp-api/api/src/rp.cpp
@@ -39,7 +39,7 @@ pthread_mutex_t rp_init_mutex = PTHREAD_MUTEX_INITIALIZER;
  * Global methods
  */
 
-int rp_InitAdresses() {
+int rp_InitAdressess() {
     if (g_api_state)
         return RP_EOOR;
     pthread_mutex_lock(&rp_init_mutex);
@@ -99,7 +99,7 @@ int rp_InitAdresses() {
 
 int rp_Init() {
     if (!rp_IsApiInit()) {
-        ECHECK(rp_InitAdresses())
+        ECHECK(rp_InitAdressess())
     }
     int ret = rp_CalibInit();
     if (ret != RP_HP_OK) {
@@ -119,7 +119,7 @@ int rp_Init() {
 
 int rp_InitReset(bool reset) {
     if (!rp_IsApiInit()) {
-        ECHECK(rp_InitAdresses())
+        ECHECK(rp_InitAdressess())
     }
     if (reset) {
         int ret = rp_Reset();
@@ -1853,6 +1853,14 @@ int rp_AcqGetAC_DC(rp_channel_t channel, rp_acq_ac_dc_mode_t* status) {
     if (!rp_HPGetFastADCIsAC_DCOrDefault())
         return RP_NOTS;
     return acq_GetAC_DC(channel, status);
+}
+
+int rp_AcqSetInitTimestamp(uint64_t value) {
+    return acq_SetInitTimestamp(value);
+}
+
+int rp_AcqGetTimestamp(rp_channel_t channel, uint64_t* value) {
+    return acq_GetTimestamp(channel, value);
 }
 
 int rp_AcqSetBypassFilter(rp_channel_t channel, bool enable) {

rp-api/api/tests/rp_test_acq.py
@@ -1,0 +1,431 @@
+#!/usr/bin/python3
+
+import rp
+import time
+import numpy as np
+
+def init_rp():
+    print("rp.rp_InitAdressess()")
+    res = rp.rp_InitAdressess()
+    print(res)
+
+    print("rp.rp_Init()")
+    res = rp.rp_Init()
+    print(res)
+
+def test_acquisition_basic():
+    print("rp.rp_AcqResetFpga()")
+    res = rp.rp_AcqResetFpga()
+    print(res)
+
+    print("rp.rp_AcqSetArmKeep(False)")
+    res = rp.rp_AcqSetArmKeep(False)
+    print(res)
+
+    print("rp.rp_AcqGetArmKeep()")
+    res = rp.rp_AcqGetArmKeep()
+    print(res)
+
+    print("rp.rp_AcqGetBufferFillState()")
+    res = rp.rp_AcqGetBufferFillState()
+    print(res)
+
+    print("rp.rp_AcqSetDecimation(rp.RP_DEC_1)")
+    res = rp.rp_AcqSetDecimation(rp.RP_DEC_1)
+    print(res)
+
+    print("rp.rp_AcqGetDecimation()")
+    res = rp.rp_AcqGetDecimation()
+    print(res)
+
+def test_acquisition_decimation():
+    print("rp.rp_AcqConvertFactorToDecimation(1)")
+    res = rp.rp_AcqConvertFactorToDecimation(1)
+    print(res)
+
+    print("rp.rp_AcqSetDecimationFactor(2)")
+    res = rp.rp_AcqSetDecimationFactor(2)
+    print(res)
+
+    print("rp.rp_AcqGetDecimationFactor()")
+    res = rp.rp_AcqGetDecimationFactor()
+    print(res)
+
+    print("rp.rp_AcqGetSamplingRateHz()")
+    res = rp.rp_AcqGetSamplingRateHz()
+    print(res)
+
+    print("rp.rp_AcqSetAveraging(False)")
+    res = rp.rp_AcqSetAveraging(False)
+    print(res)
+
+    print("rp.rp_AcqGetAveraging()")
+    res = rp.rp_AcqGetAveraging()
+    print(res)
+
+    print("rp.rp_AcqSetAveragingCh(rp.RP_CH_1,True)")
+    res = rp.rp_AcqSetAveragingCh(rp.RP_CH_1,True)
+    print(res)
+
+    print("rp.rp_AcqGetAveragingCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetAveragingCh(rp.RP_CH_1)
+    print(res)
+
+def test_acquisition_filter():
+    print("rp.rp_AcqSetBypassFilter(rp.RP_CH_1,False)")
+    res = rp.rp_AcqSetBypassFilter(rp.RP_CH_1,False)
+    print(res)
+
+    print("rp.rp_AcqGetBypassFilter(rp.RP_CH_1)")
+    res = rp.rp_AcqGetBypassFilter(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetCalibInFPGA(rp.RP_CH_1)")
+    res = rp.rp_AcqSetCalibInFPGA(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetCalibInFPGA(rp.RP_CH_1)")
+    res = rp.rp_AcqGetCalibInFPGA(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqUpdateAcqFilter(rp.RP_CH_1)")
+    res = rp.rp_AcqUpdateAcqFilter(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetFilterCalibValue(rp.RP_CH_1)")
+    res = rp.rp_AcqGetFilterCalibValue(rp.RP_CH_1)
+    print(res)
+
+def test_acquisition_trigger():
+    print("rp.rp_AcqSetTriggerSrc(rp.RP_TRIG_SRC_DISABLED)")
+    res = rp.rp_AcqSetTriggerSrc(rp.RP_TRIG_SRC_DISABLED)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerSrc()")
+    res = rp.rp_AcqGetTriggerSrc()
+    print(res)
+
+    print("rp.rp_AcqGetTriggerState()")
+    res = rp.rp_AcqGetTriggerState()
+    print(res)
+
+    print("rp.rp_AcqSetTriggerDelay(100)")
+    res = rp.rp_AcqSetTriggerDelay(100)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelay()")
+    res = rp.rp_AcqGetTriggerDelay()
+    print(res)
+
+    print("rp.rp_AcqSetTriggerDelayNs(1000)")
+    res = rp.rp_AcqSetTriggerDelayNs(1000)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelayNs()")
+    res = rp.rp_AcqGetTriggerDelayNs()
+    print(res)
+
+    print("rp.rp_AcqSetTriggerDelayNsDirect(1000)")
+    res = rp.rp_AcqSetTriggerDelayNsDirect(1000)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelayNsDirect()")
+    res = rp.rp_AcqGetTriggerDelayNsDirect()
+    print(res)
+
+def test_acquisition_trigger_level():
+    print("rp.rp_AcqGetPreTriggerCounter()")
+    res = rp.rp_AcqGetPreTriggerCounter()
+    print(res)
+
+    print("rp.rp_AcqSetTriggerLevel(rp.RP_T_CH_1,0.1)")
+    res = rp.rp_AcqSetTriggerLevel(rp.RP_T_CH_1,0.1)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerLevel(rp.RP_T_CH_1)")
+    res = rp.rp_AcqGetTriggerLevel(rp.RP_T_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetTriggerHyst(0.01)")
+    res = rp.rp_AcqSetTriggerHyst(0.01)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerHyst()")
+    res = rp.rp_AcqGetTriggerHyst()
+    print(res)
+
+def test_acquisition_gain():
+    print("rp.rp_AcqSetGain(rp.RP_CH_1,rp.RP_HIGH)")
+    res = rp.rp_AcqSetGain(rp.RP_CH_1,rp.RP_HIGH)
+    print(res)
+
+    print("rp.rp_AcqGetGain(rp.RP_CH_1)")
+    res = rp.rp_AcqGetGain(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetGainV(rp.RP_CH_1)")
+    res = rp.rp_AcqGetGainV(rp.RP_CH_1)
+    print(res)
+
+def test_acquisition_offset():
+    print("rp.rp_AcqSetOffset(rp.RP_CH_1,0.5)")
+    res = rp.rp_AcqSetOffset(rp.RP_CH_1,0.5)
+    print(res)
+
+    print("rp.rp_AcqGetOffset(rp.RP_CH_1)")
+    res = rp.rp_AcqGetOffset(rp.RP_CH_1)
+    print(res)
+
+def test_acquisition_control():
+    print("rp.rp_AcqGetWritePointer()")
+    res = rp.rp_AcqGetWritePointer()
+    print(res)
+
+    print("rp.rp_AcqGetWritePointerAtTrig()")
+    res = rp.rp_AcqGetWritePointerAtTrig()
+    print(res)
+
+    print("rp.rp_AcqReset()")
+    res = rp.rp_AcqReset()
+    print(res)
+
+    print("rp.rp_AcqUnlockTrigger()")
+    res = rp.rp_AcqUnlockTrigger()
+    print(res)
+
+    print("rp.rp_AcqGetUnlockTrigger()")
+    res = rp.rp_AcqGetUnlockTrigger()
+    print(res)
+
+    print("rp.rp_AcqStart()")
+    res = rp.rp_AcqStart()
+    print(res)
+
+    time.sleep(1)
+
+    print("rp.rp_AcqSetTriggerSrc(rp.RP_TRIG_SRC_NOW)")
+    res = rp.rp_AcqSetTriggerSrc(rp.RP_TRIG_SRC_NOW)
+    print(res)
+
+    time.sleep(1)
+
+    print("rp.rp_AcqStop()")
+    res = rp.rp_AcqStop()
+    print(res)
+
+def test_acquisition_ext_trigger():
+    print("rp.rp_AcqSetExtTriggerDebouncerUs(123)")
+    res = rp.rp_AcqSetExtTriggerDebouncerUs(123)
+    print(res)
+
+    print("rp.rp_AcqGetExtTriggerDebouncerUs()")
+    res = rp.rp_AcqGetExtTriggerDebouncerUs()
+    print(res)
+
+def test_acquisition_ac_dc():
+    print("rp.rp_AcqSetAC_DC(rp.RP_CH_1,rp.RP_DC)")
+    res = rp.rp_AcqSetAC_DC(rp.RP_CH_1,rp.RP_DC)
+    print(res)
+
+    print("rp.rp_AcqGetAC_DC(rp.RP_CH_1)")
+    res = rp.rp_AcqGetAC_DC(rp.RP_CH_1)
+    print(res)
+
+def test_acquisition_data():
+    print("rp.rp_AcqGetNormalizedDataPos(3423432)")
+    res = rp.rp_AcqGetNormalizedDataPos(3423432)
+    print(res)
+
+    ibuff = rp.i16Buffer(1025)
+    print("rp.rp_AcqGetDataPosRaw(rp.RP_CH_1,0,1024,ibuff.cast(),1025)")
+    res = rp.rp_AcqGetDataPosRaw(rp.RP_CH_1,0,1024,ibuff.cast(),1025)
+    print(res,ibuff[0],ibuff[1],ibuff[2],ibuff[3],ibuff[4])
+
+    fbuff = rp.fBuffer(1025)
+    print("rp.rp_AcqGetDataPosV(rp.RP_CH_1,0,1024,fbuff,1025)")
+    res = rp.rp_AcqGetDataPosV(rp.RP_CH_1,0,1024,fbuff,1025)
+    print(res,fbuff[0],fbuff[1],fbuff[2],fbuff[3],fbuff[4])
+
+def test_acquisition_numpy():
+    print("Testing numpy")
+    N = 128
+    arr_i16 = np.zeros(N, dtype=np.int16)
+    arr_f = np.zeros(N, dtype=np.float32)
+
+    print("rp.rp_AcqGetDataPosRawNP(rp.RP_CH_1,0,128,arr_i16)")
+    res = rp.rp_AcqGetDataPosRawNP(rp.RP_CH_1,0,128,arr_i16)
+    print(arr_i16)
+
+    print("rp.rp_AcqGetDataPosVNP(rp.RP_CH_1,0,128,arr_f)")
+    res = rp.rp_AcqGetDataPosVNP(rp.RP_CH_1,0,128,arr_f)
+    print(arr_f)
+
+    print("rp.rp_AcqGetDataRawNP(rp.RP_CH_1,0,arr_i16)")
+    res = rp.rp_AcqGetDataRawNP(rp.RP_CH_1,0,arr_i16)
+    print(arr_i16)
+
+    print("rp.rp_AcqGetDataRawWithCalibNP(rp.RP_CH_1,0,arr_i16)")
+    res = rp.rp_AcqGetDataRawWithCalibNP(rp.RP_CH_1,0,arr_i16)
+    print(arr_i16)
+
+    print("rp.rp_AcqGetDataVNP(rp.RP_CH_1,0,arr_f)")
+    res = rp.rp_AcqGetDataVNP(rp.RP_CH_1,0,arr_f)
+    print(arr_f)
+
+    print("rp.rp_AcqGetOldestDataRawNP(rp.RP_CH_1,arr_i16)")
+    res = rp.rp_AcqGetOldestDataRawNP(rp.RP_CH_1,arr_i16)
+    print(arr_i16)
+
+    print("rp.rp_AcqGetLatestDataRawNP(rp.RP_CH_1,arr_i16)")
+    res = rp.rp_AcqGetLatestDataRawNP(rp.RP_CH_1,arr_i16)
+    print(arr_i16)
+
+    print("rp.rp_AcqGetOldestDataVNP(rp.RP_CH_1,arr_f)")
+    res = rp.rp_AcqGetOldestDataVNP(rp.RP_CH_1,arr_f)
+    print(arr_f)
+
+    print("rp.rp_AcqGetLatestDataVNP(rp.RP_CH_1,arr_f)")
+    res = rp.rp_AcqGetLatestDataVNP(rp.RP_CH_1,arr_f)
+    print(arr_f)
+
+    print("End testing numpy")
+
+def test_acquisition_data_advanced():
+    ibuff = rp.i16Buffer(1024)
+    print("rp.rp_AcqGetDataRaw(rp.RP_CH_1,0,1024,ibuff.cast())")
+    res = rp.rp_AcqGetDataRaw(rp.RP_CH_1,0,1024,ibuff.cast())
+    print(res,ibuff[0],ibuff[1],ibuff[2],ibuff[3],ibuff[4])
+
+    ibuff = rp.i16Buffer(1024)
+    print("rp.rp_AcqGetDataRawWithCalib(rp.RP_CH_1,0,1024,ibuff.cast())")
+    res = rp.rp_AcqGetDataRawWithCalib(rp.RP_CH_1,0,1024,ibuff.cast())
+    print(res,ibuff[0],ibuff[1],ibuff[2],ibuff[3],ibuff[4])
+
+    ibuff = rp.i16Buffer(1024)
+    print("rp.rp_AcqGetOldestDataRaw(rp.RP_CH_1,1024,ibuff.cast())")
+    res = rp.rp_AcqGetOldestDataRaw(rp.RP_CH_1,1024,ibuff.cast())
+    print(res,ibuff[0],ibuff[1],ibuff[2],ibuff[3],ibuff[4])
+
+    ibuff = rp.i16Buffer(1024)
+    print("rp.rp_AcqGetLatestDataRaw(rp.RP_CH_1,1024,ibuff.cast())")
+    res = rp.rp_AcqGetLatestDataRaw(rp.RP_CH_1,1024,ibuff.cast())
+    print(res,ibuff[0],ibuff[1],ibuff[2],ibuff[3],ibuff[4])
+
+    fbuff = rp.fBuffer(1025)
+    print("rp.rp_AcqGetDataV(rp.RP_CH_1,0,1024,fbuff)")
+    res = rp.rp_AcqGetDataV(rp.RP_CH_1,0,1024,fbuff)
+    print(res,fbuff[0],fbuff[1],fbuff[2],fbuff[3],fbuff[4])
+
+    fbuff = rp.fBuffer(1024)
+    print("rp.rp_AcqGetOldestDataV(rp.RP_CH_1,1024,fbuff.cast())")
+    res = rp.rp_AcqGetOldestDataV(rp.RP_CH_1,1024,fbuff.cast())
+    print(res,fbuff[0],fbuff[1],fbuff[2],fbuff[3],fbuff[4])
+
+    fbuff = rp.fBuffer(1024)
+    print("rp.rp_AcqGetLatestDataV(rp.RP_CH_1,1024,fbuff.cast())")
+    res = rp.rp_AcqGetLatestDataV(rp.RP_CH_1,1024,fbuff.cast())
+    print(res,fbuff[0],fbuff[1],fbuff[2],fbuff[3],fbuff[4])
+
+def test_acquisition_buffer():
+    print("rp.rp_AcqGetBufSize()")
+    res = rp.rp_AcqGetBufSize()
+    print(res)
+
+    print("rp.rp_createBuffer(2,1024,True,False,False)")
+    buff_t = rp.rp_createBuffer(2,1024,True,False,False)
+    print(buff_t)
+
+    print("rp.rp_AcqGetData(0,buff_t)")
+    res = rp.rp_AcqGetData(0,buff_t)
+    print(res)
+    print(buff_t.size)
+    print(buff_t.channels)
+
+    print("rp.rp_AcqGetDataWithCorrection(0,10,-5,buff_t)")
+    res = rp.rp_AcqGetDataWithCorrection(0,10,-5,buff_t)
+    print(res)
+
+    print("rp.rp_deleteBuffer(buff_t)")
+    res = rp.rp_deleteBuffer(buff_t)
+    print(res)
+
+    print("rp.rp_createBuffer(2,1024,False,False,True)")
+    buff_t = rp.rp_createBuffer(2,1024,False,False,True)
+    print(buff_t)
+
+    print("rp.rp_AcqGetData(0,buff_t)")
+    res = rp.rp_AcqGetData(0,buff_t)
+    print(res)
+
+    print("rp.rp_deleteBuffer(buff_t)")
+    res = rp.rp_deleteBuffer(buff_t)
+    print(res)
+
+def test_interrupts():
+    print("Testing interrupt functions")
+
+    print("rp.rp_AcqIntTriggerRead(1000)")
+    res = rp.rp_AcqIntTriggerRead(1000)
+    print(res)
+
+    print("rp.rp_AcqIntFillRead(1000)")
+    res = rp.rp_AcqIntFillRead(1000)
+    print(res)
+
+    print("rp.rp_AcqIntTriggerReadCh(rp.RP_CH_1, 1000)")
+    res = rp.rp_AcqIntTriggerReadCh(rp.RP_CH_1, 1000)
+    print(res)
+
+    print("rp.rp_AcqIntFillReadCh(rp.RP_CH_1, 1000)")
+    res = rp.rp_AcqIntFillReadCh(rp.RP_CH_1, 1000)
+    print(res)
+
+def test_timestamp():
+    print("Testing timestamp functions")
+
+    print("rp.rp_AcqSetInitTimestamp(1000)")
+    res = rp.rp_AcqSetInitTimestamp(1000)
+    print(res)
+
+    print("rp.rp_AcqGetTimestamp(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTimestamp(rp.RP_CH_1)
+    print(res)
+
+def test_16bit_mode():
+    print("Testing 16-bit mode functions")
+
+    print("rp.rp_AcqSet16BitMode(True)")
+    res = rp.rp_AcqSet16BitMode(True)
+    print(res)
+
+    print("rp.rp_AcqGet16BitMode()")
+    res = rp.rp_AcqGet16BitMode()
+    print(res)
+
+def test_waveform_data():
+    print("Testing waveform data functions")
+
+    print("rp.rp_GetWaveformDataV(rp.RP_CH_1)")
+    res = rp.rp_GetWaveformDataV(rp.RP_CH_1)
+    print(res)
+
+if __name__ == "__main__":
+    init_rp()
+    test_acquisition_basic()
+    test_acquisition_decimation()
+    test_acquisition_filter()
+    test_acquisition_trigger()
+    test_acquisition_trigger_level()
+    test_acquisition_gain()
+    test_acquisition_offset()
+    test_acquisition_control()
+    test_acquisition_ext_trigger()
+    test_acquisition_ac_dc()
+    test_acquisition_data()
+    test_acquisition_numpy()
+    test_acquisition_data_advanced()
+    test_acquisition_buffer()
+    test_interrupts()
+    test_timestamp()
+    test_16bit_mode()
+    test_waveform_data()

rp-api/api/tests/rp_test_acq_axi.py
@@ -1,0 +1,147 @@
+#!/usr/bin/python3
+
+import rp
+import time
+import numpy as np
+
+def init_rp():
+    print("rp.rp_InitAdressess()")
+    res = rp.rp_InitAdressess()
+    print(res)
+
+    print("rp.rp_Init()")
+    res = rp.rp_Init()
+    print(res)
+
+def test_acq_axi_basic():
+    print("rp.rp_AcqReset()")
+    res = rp.rp_AcqReset()
+    print(res)
+
+    print("rp.rp_AcqAxiGetBufferFillState(rp.RP_CH_1)")
+    res = rp.rp_AcqAxiGetBufferFillState(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqAxiSetDecimationFactor(1)")
+    res = rp.rp_AcqAxiSetDecimationFactor(1)
+    print(res)
+
+    print("rp.rp_AcqAxiGetDecimationFactor()")
+    res = rp.rp_AcqAxiGetDecimationFactor()
+    print(res)
+
+    print("rp.rp_AcqAxiSetDecimationFactorCh(rp.RP_CH_1, 1)")
+    res = rp.rp_AcqAxiSetDecimationFactorCh(rp.RP_CH_1, 1)
+    print(res)
+
+    print("rp.rp_AcqAxiGetDecimationFactorCh(rp.RP_CH_1)")
+    res = rp.rp_AcqAxiGetDecimationFactorCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqAxiSetTriggerDelay(rp.RP_CH_1,128)")
+    res = rp.rp_AcqAxiSetTriggerDelay(rp.RP_CH_1,128)
+    print(res)
+
+    print("rp.rp_AcqAxiGetTriggerDelay(rp.RP_CH_1)")
+    res = rp.rp_AcqAxiGetTriggerDelay(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqAxiGetMemoryRegion()")
+    res = rp.rp_AcqAxiGetMemoryRegion()
+    print(res)
+
+def test_acq_axi_buffer():
+    res = rp.rp_AcqAxiGetMemoryRegion()
+    start = res[1] if len(res) > 1 else 0
+    size = res[2] if len(res) > 2 else 0
+
+    print("rp.rp_AcqAxiSetBufferSamples(rp.RP_CH_1,start,1000)")
+    res = rp.rp_AcqAxiSetBufferSamples(rp.RP_CH_1,start,1000)
+    print(res)
+
+    print("rp.rp_AcqAxiSetBufferBytes(rp.RP_CH_1,start,size)")
+    res = rp.rp_AcqAxiSetBufferBytes(rp.RP_CH_1,start,size)
+    print(res)
+
+def test_acq_axi_enable():
+    print("rp.rp_AcqAxiEnable(rp.RP_CH_1,True)")
+    res = rp.rp_AcqAxiEnable(rp.RP_CH_1,True)
+    print(res)
+
+    print("rp.rp_AcqStart()")
+    res = rp.rp_AcqStart()
+    print(res)
+
+    time.sleep(1)
+
+    print("rp.rp_AcqSetTriggerSrc(rp.RP_TRIG_SRC_NOW)")
+    res = rp.rp_AcqSetTriggerSrc(rp.RP_TRIG_SRC_NOW)
+    print(res)
+
+    time.sleep(1)
+
+    print("rp.rp_AcqStop()")
+    res = rp.rp_AcqStop()
+    print(res)
+
+def test_acq_axi_pointers():
+    print("rp.rp_AcqAxiGetWritePointer(rp.RP_CH_1)")
+    res = rp.rp_AcqAxiGetWritePointer(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqAxiGetWritePointerAtTrig(rp.RP_CH_1)")
+    res = rp.rp_AcqAxiGetWritePointerAtTrig(rp.RP_CH_1)
+    print(res)
+    trig = res[1] if len(res) > 1 else 0
+
+    return trig
+
+def test_acq_axi_data(trig=0):
+    ibuff = rp.i16Buffer(128)
+    print("rp.rp_AcqAxiGetDataRaw(rp.RP_CH_1,trig,128,ibuff.cast())")
+    res = rp.rp_AcqAxiGetDataRaw(rp.RP_CH_1,trig, 128,ibuff.cast())
+    print(res,ibuff[0],ibuff[1],ibuff[2],ibuff[3],ibuff[4])
+
+    fbuff = rp.fBuffer(128)
+    print("rp.rp_AcqAxiGetDataV(rp.RP_CH_1,trig,128,fbuff.cast())")
+    res = rp.rp_AcqAxiGetDataV(rp.RP_CH_1,trig,128,fbuff.cast())
+    print(res,fbuff[0],fbuff[1],fbuff[2],fbuff[3],fbuff[4])
+
+def test_acq_axi_numpy():
+    print("Testing numpy")
+    N = 128
+    arr_i16 = np.zeros(N, dtype=np.int16)
+    arr_f = np.zeros(N, dtype=np.float32)
+
+    print("rp.rp_AcqAxiGetDataRawNP(rp.RP_CH_1,0,arr_i16)")
+    res = rp.rp_AcqAxiGetDataRawNP(rp.RP_CH_1,0,arr_i16)
+    print(arr_i16)
+
+    print("rp.rp_AcqAxiGetDataVNP(rp.RP_CH_1,0,arr_f)")
+    res = rp.rp_AcqAxiGetDataVNP(rp.RP_CH_1,0,arr_f)
+    print(arr_f)
+
+    print("rp.rp_AcqAxiGetDataRawDirect(rp.RP_CH_1,0,128)")
+    res = rp.rp_AcqAxiGetDataRawDirect(rp.RP_CH_1,0, 128)
+    print(res)
+
+    print("End testing numpy")
+
+def test_acq_axi_offset():
+    print("rp.rp_AcqAxiSetOffset(rp.RP_CH_1,0.5)")
+    res = rp.rp_AcqAxiSetOffset(rp.RP_CH_1,0.5)
+    print(res)
+
+    print("rp.rp_AcqAxiGetOffset(rp.RP_CH_1)")
+    res = rp.rp_AcqAxiGetOffset(rp.RP_CH_1)
+    print(res)
+
+if __name__ == "__main__":
+    init_rp()
+    test_acq_axi_basic()
+    test_acq_axi_buffer()
+    test_acq_axi_enable()
+    trig = test_acq_axi_pointers()
+    test_acq_axi_data(trig)
+    test_acq_axi_numpy()
+    test_acq_axi_offset()

rp-api/api/tests/rp_test_acq_split.py
@@ -1,0 +1,164 @@
+#!/usr/bin/python3
+
+import rp
+import time
+
+def init_rp():
+    print("rp.rp_InitAdressess()")
+    res = rp.rp_InitAdressess()
+    print(res)
+
+    print("rp.rp_Init()")
+    res = rp.rp_Init()
+    print(res)
+
+def test_split_trigger():
+    print("Testing split trigger functions")
+
+    print("rp.rp_AcqSetSplitTrigger(True)")
+    res = rp.rp_AcqSetSplitTrigger(True)
+    print(res)
+
+    print("rp.rp_AcqGetSplitTrigger()")
+    res = rp.rp_AcqGetSplitTrigger()
+    print(res)
+
+    print("rp.rp_AcqSetSplitTriggerPass(False)")
+    res = rp.rp_AcqSetSplitTriggerPass(False)
+    print(res)
+
+    print("rp.rp_AcqGetSplitTriggerPass()")
+    res = rp.rp_AcqGetSplitTriggerPass()
+    print(res)
+
+def test_split_trigger_channels():
+    print("rp.rp_AcqSetArmKeepCh(rp.RP_CH_1, False)")
+    res = rp.rp_AcqSetArmKeepCh(rp.RP_CH_1, False)
+    print(res)
+
+    print("rp.rp_AcqGetArmKeepCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetArmKeepCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetBufferFillStateCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetBufferFillStateCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetDecimationCh(rp.RP_CH_1, 1)")
+    res = rp.rp_AcqSetDecimationCh(rp.RP_CH_1, 1)
+    print(res)
+
+    print("rp.rp_AcqGetDecimationCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetDecimationCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetDecimationFactorCh(rp.RP_CH_1, 1)")
+    res = rp.rp_AcqSetDecimationFactorCh(rp.RP_CH_1, 1)
+    print(res)
+
+    print("rp.rp_AcqGetDecimationFactorCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetDecimationFactorCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetSamplingRateHzCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetSamplingRateHzCh(rp.RP_CH_1)
+    print(res)
+
+def test_split_trigger_sources():
+    print("rp.rp_AcqSetTriggerSrcCh(rp.RP_CH_1, rp.RP_TRIG_SRC_NOW)")
+    res = rp.rp_AcqSetTriggerSrcCh(rp.RP_CH_1, rp.RP_TRIG_SRC_NOW)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerSrcCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTriggerSrcCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerStateCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTriggerStateCh(rp.RP_CH_1)
+    print(res)
+
+def test_split_trigger_delay():
+    print("rp.rp_AcqSetTriggerDelayCh(rp.RP_CH_1, 1)")
+    res = rp.rp_AcqSetTriggerDelayCh(rp.RP_CH_1, 1)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelayCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTriggerDelayCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetTriggerDelayDirectCh(rp.RP_CH_1, 1)")
+    res = rp.rp_AcqSetTriggerDelayDirectCh(rp.RP_CH_1, 1)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelayDirectCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTriggerDelayDirectCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetTriggerDelayNsCh(rp.RP_CH_1, 10000)")
+    res = rp.rp_AcqSetTriggerDelayNsCh(rp.RP_CH_1, 10000)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelayNsCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTriggerDelayNsCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqSetTriggerDelayNsDirectCh(rp.RP_CH_1, 10000)")
+    res = rp.rp_AcqSetTriggerDelayNsDirectCh(rp.RP_CH_1, 10000)
+    print(res)
+
+    print("rp.rp_AcqGetTriggerDelayNsDirectCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetTriggerDelayNsDirectCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetPreTriggerCounterCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetPreTriggerCounterCh(rp.RP_CH_1)
+    print(res)
+
+def test_split_trigger_control():
+    print("rp.rp_AcqGetWritePointerCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetWritePointerCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetWritePointerAtTrigCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetWritePointerAtTrigCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqStartCh(rp.RP_CH_1)")
+    res = rp.rp_AcqStartCh(rp.RP_CH_1)
+    print(res)
+
+    time.sleep(1)
+
+    print("rp.rp_AcqStopCh(rp.RP_CH_1)")
+    res = rp.rp_AcqStopCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqResetCh(rp.RP_CH_1)")
+    res = rp.rp_AcqResetCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqUnlockTriggerCh(rp.RP_CH_1)")
+    res = rp.rp_AcqUnlockTriggerCh(rp.RP_CH_1)
+    print(res)
+
+    print("rp.rp_AcqGetUnlockTriggerCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetUnlockTriggerCh(rp.RP_CH_1)
+    print(res)
+
+def test_split_trigger_averaging():
+    print("rp.rp_AcqSetAveragingCh(rp.RP_CH_1,True)")
+    res = rp.rp_AcqSetAveragingCh(rp.RP_CH_1,True)
+    print(res)
+
+    print("rp.rp_AcqGetAveragingCh(rp.RP_CH_1)")
+    res = rp.rp_AcqGetAveragingCh(rp.RP_CH_1)
+    print(res)
+
+if __name__ == "__main__":
+    init_rp()
+    test_split_trigger()
+    test_split_trigger_channels()
+    test_split_trigger_sources()
+    test_split_trigger_delay()
+    test_split_trigger_control()
+    test_split_trigger_averaging()

rp-api/api/tests/rp_test_analog.py
@@ -1,0 +1,75 @@
+#!/usr/bin/python3
+
+import rp
+
+def init_rp():
+    print("rp.rp_InitAdressess()")
+    res = rp.rp_InitAdressess()
+    print(res)
+
+    print("rp.rp_Init()")
+    res = rp.rp_Init()
+    print(res)
+
+def test_analog_pins():
+    print("rp.rp_ApinReset()")
+    res = rp.rp_ApinReset()
+    print(res)
+
+    print("rp.rp_ApinGetValue(rp.RP_AIN0)")
+    res = rp.rp_ApinGetValue(rp.RP_AIN0)
+    print(res)
+
+    print("rp.rp_ApinGetValueRaw(rp.RP_AIN0)")
+    res = rp.rp_ApinGetValueRaw(rp.RP_AIN0)
+    print(res)
+
+    print("rp.rp_ApinSetValue(rp.RP_AOUT0,1)")
+    res = rp.rp_ApinSetValue(rp.RP_AOUT0,1)
+    print(res)
+
+    print("rp.rp_ApinSetValueRaw(rp.RP_AOUT0,0x50)")
+    res = rp.rp_ApinSetValueRaw(rp.RP_AOUT0,0x50)
+    print(res)
+
+    print("rp.rp_ApinGetRange(rp.RP_AIN0)")
+    res = rp.rp_ApinGetRange(rp.RP_AIN0)
+    print(res)
+
+    print("rp.rp_AIpinGetValue(0)")
+    res = rp.rp_AIpinGetValue(0)
+    print(res)
+
+    print("rp.rp_AIpinGetValueRaw(0)")
+    res = rp.rp_AIpinGetValueRaw(0)
+    print(res)
+
+def test_analog_outputs():
+    print("rp.rp_AOpinReset()")
+    res = rp.rp_ApinReset()
+    print(res)
+
+    print("rp.rp_AOpinGetValue(0)")
+    res = rp.rp_AOpinGetValue(0)
+    print(res)
+
+    print("rp.rp_AOpinGetValueRaw(0)")
+    res = rp.rp_AOpinGetValueRaw(0)
+    print(res)
+
+    print("rp.rp_AOpinSetValue(0,1)")
+    res = rp.rp_AOpinSetValue(0,1)
+    print(res)
+
+    print("rp.rp_AOpinSetValueRaw(0,0x50)")
+    res = rp.rp_AOpinSetValueRaw(0,0x50)
+    print(res)
+
+    print("rp.rp_AOpinGetRange(0)")
+    res = rp.rp_AOpinGetRange(0)
+    print(res)
+
+if __name__ == "__main__":
+    init_rp()
+    test_analog_pins()
+    test_analog_outputs()

commit 5f005eef8cac041629b8849fbc9b0223af041e15

Fix typo: rp_InitAdressess -> rp_InitAddresses
This change was scripted as follows:

    files=$(grep -lr rp_InitAdressess rp-api)
    sed -i -e 's/rp_InitAdressess/rp_InitAddresses/g' $files

Fixes: bd42e996e3c4 ("feat: any-edge trigger, timestamp API, modular Python tests")

rp-api/api-sweep/src/rp_sweep.cpp
@@ -68,7 +68,7 @@ CSweepController::CSweepController() {
     m_pimpl->m_Pause = false;
     setDefault();
     if (!rp_IsApiInit()) {
-        if (rp_InitAdressess() == RP_OK) {
+        if (rp_InitAddresses() == RP_OK) {
             m_pimpl->apiInit = true;
         }
     }

rp-api/api/include/rp.h
@@ -109,7 +109,7 @@
  * If the function is unsuccessful, the return value is any of RP_E* values that indicate an error.
  */
 
-int rp_InitAdressess();
+int rp_InitAddresses();
 
 /**
  * Initializes the library. It must be called first, before any other library method.

rp-api/api/src/rp.cpp
@@ -48,7 +48,7 @@ std::mutex g_adioMutex;
  * Global methods
  */
 
-int rp_InitAdressess() {
+int rp_InitAddresses() {
     std::unique_lock lock(g_initMutex);
     if (g_api_state)
         return RP_EOOR;
@@ -101,7 +101,7 @@ int rp_InitAdressess() {
 
 int rp_Init() {
     if (!rp_IsApiInit()) {
-        ECHECK(rp_InitAdressess())
+        ECHECK(rp_InitAddresses())
     }
     int ret = rp_CalibInit();
     if (ret != RP_HP_OK) {
@@ -121,7 +121,7 @@ int rp_Init() {
 
 int rp_InitReset(bool reset) {
     if (!rp_IsApiInit()) {
-        ECHECK(rp_InitAdressess())
+        ECHECK(rp_InitAddresses())
     }
     if (reset) {
         int ret = rp_Reset();

rp-api/api/tests/rp_test_acq.py
@@ -5,8 +5,8 @@ import time
 import numpy as np
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")

rp-api/api/tests/rp_test_acq_axi.py
@@ -5,8 +5,8 @@ import time
 import numpy as np
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")

rp-api/api/tests/rp_test_acq_split.py
@@ -4,8 +4,8 @@ import rp
 import time
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")

rp-api/api/tests/rp_test_analog.py
@@ -3,8 +3,8 @@
 import rp
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")

rp-api/api/tests/rp_test_gen.py
@@ -4,8 +4,8 @@ import rp
 import numpy as np
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")

rp-api/api/tests/rp_test_gen_axi.py
@@ -4,8 +4,8 @@ import rp
 import numpy as np
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")

rp-api/api/tests/rp_test_hk.py
@@ -5,8 +5,8 @@ import time
 import numpy as np
 
 def init_rp():
-    print("rp.rp_InitAdressess()")
-    res = rp.rp_InitAdressess()
+    print("rp.rp_InitAddresses()")
+    res = rp.rp_InitAddresses()
     print(res)
 
     print("rp.rp_Init()")